7813ce60117df9730d0c15a3448b27a96777e9b6
Steven Silvester <steven.silvester@ieee.org>
2016-01-11 13:55:40 -0600

    Fix docs push script

--- scripts/travis_after_success.sh
+++ scripts/travis_after_success.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-
-# submit coverage to coveralls
-cat ./test/coverage/Firefox*/lcov.info | ./node_modules/coveralls/bin/coveralls.js
-
-# push docs on master branch
 if [[ $TRAVIS_PULL_REQUEST == false && $TRAVIS_BRANCH == "master" ]]
 then
-    echo "-- will push docs --"
+    echo "-- pushing docs --"
 
-    git config --global user.email "travis@travis-ci.com"
-    git config --global user.name "Travis Bot"
+    ( cd docs 
+    git init
+    git config user.email "travis@travis-ci.com"
+    git config user.name "Travis Bot"
 
-    git clone https://${GHREPO}.git travis_docs_build
-    cd travis_docs_build
-    git checkout gh-pages
-
-    echo "https://${GHTOKEN}:@github.com" > .git/credentials
-    git config credential.helper "store --file=.git/credentials"
-
-    rm -rf ./*
-    cp -r ../docs/* ./.
-    git add -A
-    git commit -m "autocommit docs"
-    git push origin gh-pages
+    git add .
+    git commit -m "Deployed to GitHub Pages"
+    git push --force --quiet "https://${GHTOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+    )
 else
     echo "-- will only push docs from master --"
 fi
