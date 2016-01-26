a3fdaf823cc8bd96785ac57ddcfc4717c0df1f6c
A. Darian <git@darian.af>
2016-01-26 17:21:58 +0000

    unnecessary filter

--- scripts/copythemecss.js
+++ scripts/copythemecss.js
@@ -1,3 +1,2 @@
 var fs = require('fs-extra');
-var filter = { filter: /\.css$/ };
-fs.copySync('src/theme/index.css', 'example/build/theme.css', filter);
+fs.copySync('src/theme/index.css', 'example/build/theme.css');
