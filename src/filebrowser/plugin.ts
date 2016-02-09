ee49a445e3ca2a6188a479584d2ed80d42c56aff
Steven Silvester <steven.silvester@ieee.org>
2016-02-09 08:29:05 -0600

    Add context menu to filebrowser

--- src/filebrowser/plugin.ts
+++ src/filebrowser/plugin.ts
@@ -11,6 +11,10 @@ import {
 } from 'phosphor-di';
 
 import {
+  Menu, MenuBar, MenuItem
+} from 'phosphor-menus';
+
+import {
   IFileBrowserWidget
 } from './index';
 
@@ -35,7 +39,80 @@ function register(container: Container): void {
       let contents = provider.contentsManager;
       let sessions = provider.notebookSessionManager;
       let model = new FileBrowserModel(contents, sessions);
-      return new FileBrowserWidget(model);
+      let widget = new FileBrowserWidget(model);
+
+      let menu = createMenu(widget);
+
+      // Add a context menu to the dir listing.
+      let node = widget.node.getElementsByClassName('jp-DirListing-list')[0];
+      node.addEventListener('contextmenu', (event: MouseEvent) => {
+        event.preventDefault();
+        let x = event.clientX;
+        let y = event.clientY;
+        menu.popup(x, y);
+      });
+
+      return widget;
     }
   });
+}
+
+
+/**
+ * Create a context menu for the file browser listing.
+ */
+function createMenu(fbWidget: FileBrowserWidget):  Menu {
+  return new Menu([
+    new MenuItem({
+      text: '&Open',
+      icon: 'fa fa-folder-open-o',
+      shortcut: 'Ctrl+O',
+      handler: () => { fbWidget.open(); }
+    }),
+    new MenuItem({
+      text: '&Rename',
+      icon: 'fa fa-edit',
+      shortcut: 'Ctrl+R',
+      handler: () => { fbWidget.rename(); }
+    }),
+    new MenuItem({
+      text: '&Delete',
+      icon: 'fa fa-remove',
+      shortcut: 'Ctrl+D',
+      handler: () => { fbWidget.delete(); }
+    }),
+    new MenuItem({
+      text: 'Duplicate',
+      icon: 'fa fa-copy',
+      handler: () => { fbWidget.duplicate(); }
+    }),
+    new MenuItem({
+      text: 'Cut',
+      icon: 'fa fa-cut',
+      shortcut: 'Ctrl+X',
+      handler: () => { fbWidget.cut(); }
+    }),
+    new MenuItem({
+      text: '&Copy',
+      icon: 'fa fa-copy',
+      shortcut: 'Ctrl+C',
+      handler: () => { fbWidget.copy(); }
+    }),
+    new MenuItem({
+      text: '&Paste',
+      icon: 'fa fa-paste',
+      shortcut: 'Ctrl+V',
+      handler: () => { fbWidget.paste(); }
+    }),
+    new MenuItem({
+      text: 'Download',
+      icon: 'fa fa-download',
+      handler: () => { fbWidget.download(); }
+    }),
+    new MenuItem({
+      text: 'Shutdown Kernel',
+      icon: 'fa fa-stop-circle-o',
+      handler: () => { fbWidget.shutdownKernels(); }
+    }),
+  ]);
 }
