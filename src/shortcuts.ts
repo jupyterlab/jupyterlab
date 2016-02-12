158247874811a9e295e3343e63b082a7c5e8ca66
A. Darian <git@darian.af>
2016-02-12 12:58:48 +0000

    remove superfluous focus class, remove undefined args

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -10,43 +10,36 @@ export
 const SHORTCUTS: IShortcutItem[] = [
   {
     command: 'command-palette:activate',
-    args: void 0,
     selector: '*',
     sequence: ['Alt Space']
   },
   {
     command: 'command-palette:deactivate',
-    args: void 0,
     selector: '*',
     sequence: ['Escape']
   },
   {
     command: 'file-operations:new-text-file',
-    args: void 0,
     selector: '*',
     sequence: ['Ctrl O']
   },
   {
     command: 'file-operations:new-notebook',
-    args: void 0,
     selector: '*',
     sequence: ['Ctrl Shift N']
   },
   {
     command: 'file-operations:save',
-    args: void 0,
-    selector: '.jp-Document.jp-mod-focus',
+    selector: '.jp-Document',
     sequence: ['Accel S']
   },
   {
     command: 'file-operations:close',
-    args: void 0,
-    selector: '.jp-Document.jp-mod-focus',
+    selector: '.jp-Document',
     sequence: ['Ctrl Q']
   },
   {
     command: 'file-operations:close-all',
-    args: void 0,
     selector: '.jp-Document',
     sequence: ['Ctrl Shift Q']
   }
