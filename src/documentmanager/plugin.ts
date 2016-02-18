83fc21f562aa148c97fe79fe1828f7f16a49f6c3
Steven Silvester <steven.silvester@ieee.org>
2016-02-17 21:00:28 -0600

    wip update for new phosphide

--- src/documentmanager/plugin.ts
+++ src/documentmanager/plugin.ts
@@ -15,15 +15,11 @@ import {
 } from 'jupyter-js-services';
 
 import {
-  IAppShell, ICommandPalette, ICommandRegistry
-} from 'phosphide';
+  Application
+} from 'phosphide/lib/core/application';
 
 import * as arrays
  from 'phosphor-arrays';
-
-import {
-  Container, Token
-} from 'phosphor-di';
 
 import {
   Property
@@ -36,14 +32,6 @@ import {
 import {
   Widget
 } from 'phosphor-widget';
-
-import {
-  IFileBrowserWidget
-} from '../index';
-
-import {
-  IDocumentManager
-} from './index';
 
 
 /**
@@ -59,180 +47,184 @@ export
 const FOCUS_CLASS = 'jp-mod-focus';
 
 
+
 /**
- * Register the plugin contributions.
+ * The default document manager provider.
  */
 export
-function resolve(container: Container): Promise<void> {
-  return container.resolve({
-    requires: [IAppShell, IDocumentManager, IFileBrowserWidget, ICommandPalette, ICommandRegistry],
-    create: (appShell: IAppShell, manager: IDocumentManager, browser: IFileBrowserWidget, palette: ICommandPalette, registry: ICommandRegistry): void => {
+const documentManagerProvider = {
+  id: 'jupyter.services.documentManager',
+  provides: DocumentManager,
+  resolve: () => {
+    return new DocumentManager();
+  },
+};
 
-      // Create a command to add a new empty text file.
-      // This requires an id and an instance of a command object.
-      let newTextFileId = 'file-operations:new-text-file';
 
-      // Add the command to the command registry and command palette plugins.
-      registry.add([
-        {
-          id: newTextFileId,
-          handler: () => {
-            browser.newUntitled('file', '.txt').then(
-              contents => manager.open(contents)
-            );
-          }
-        }
-      ]);
-      palette.add([
-        {
-          id: newTextFileId,
-          args: void 0,
-          category: 'File Operations',
-          text: 'New Text File',
-          caption: 'Create a new text file'
-        }
-      ]);
+/**
+ * The default document manager extension. 
+ */
+export
+const documentManagerExtension = {
+  id: 'jupyter.extensions.documentManager',
+  requires: [DocumentManager, FileBrowserWidget],
+  activate: activateDocumentManager
+};
 
-      // Add the command for a new notebook.
-      let newNotebookId = 'file-operations:new-notebook';
 
-      registry.add([
-        {
-          id: newNotebookId,
-          handler: () => {
-            browser.newUntitled('notebook').then(
-              contents => manager.open(contents)
-            );
-          }
-        }
-      ]);
-      palette.add([
-        {
-          id: newNotebookId,
-          args: void 0,
-          category: 'File Operations',
-          text: 'New Notebook',
-          caption: 'Create a new Jupyter Notebook'
-        }
-      ]);
+/**
+ * Activate the document manager.
+ */
+function activateDocumentManager(app: Application, manager: DocumentManager, browser: FileBrowserWidget): Promise<void> {
 
-      // Add the command for saving a document.
-      let saveDocumentId = 'file-operations:save';
+  // Create a command to add a new empty text file.
+  // This requires an id and an instance of a command object.
+  let newTextFileId = 'file-operations:new-text-file';
 
-      registry.add([
-        {
-          id: saveDocumentId,
-          handler: () => {
-            manager.save();
-            return true;
-          }
-        }
-      ]);
-      palette.add([
-        {
-          id: saveDocumentId,
-          args: void 0,
-          category: 'File Operations',
-          text: 'Save Document',
-          caption: 'Save the current document'
-        }
-      ]);
-
-      // Add the command for reverting a document.
-      let revertDocumentId = 'file-operations:revert';
-
-      registry.add([
-        {
-          id: revertDocumentId,
-          handler: () => {
-            manager.revert();
-            return true;
-          }
-        }
-      ]);
-      palette.add([
-        {
-          id: revertDocumentId,
-          args: void 0,
-          category: 'File Operations',
-          text: 'Revert Document',
-          caption: 'Revert the current document'
-        }
-      ]);
-
-      // Add the command for closing a document.
-      let closeDocumentId = 'file-operations:close';
-
-      registry.add([
-        {
-          id: closeDocumentId,
-          handler: () => {
-            manager.close();
-            return true;
-          }
-        }
-      ]);
-      palette.add([
-        {
-          id: closeDocumentId,
-          args: void 0,
-          category: 'File Operations',
-          text: 'Close Document',
-          caption: 'Close the current document'
-        }
-      ]);
-
-      // Add the command for closing all documents.
-      let closeAllId = 'file-operations:close-all';
-
-      registry.add([
-        {
-          id: closeAllId,
-          handler: () => {
-            manager.closeAll();
-            return true;
-          }
-        }
-      ]);
-      palette.add([
-        {
-          id: closeAllId,
-          args: void 0,
-          category: 'File Operations',
-          text: 'Close All',
-          caption: 'Close all open documents'
-        }
-      ]);
-
-      browser.widgetFactory = model => {
-        return manager.open(model);
+  // Add the command to the command registry and command palette plugins.
+  app.commands.add([
+    {
+      id: newTextFileId,
+      handler: () => {
+        browser.newUntitled('file', '.txt').then(
+          contents => manager.open(contents)
+        );
       }
     }
-  });
-}
+  ]);
+  app.palette.add([
+    {
+      command: newTextFileId,
+      category: 'File Operations',
+      text: 'New Text File',
+      caption: 'Create a new text file'
+    }
+  ]);
 
+  // Add the command for a new notebook.
+  let newNotebookId = 'file-operations:new-notebook';
 
-export
-function register(container: Container): void {
-  container.register(IDocumentManager, {
-    requires: [IAppShell, IFileBrowserWidget],
-    create: (appShell: IAppShell, browser: IFileBrowserWidget): IDocumentManager => {
-      let manager = new DocumentManager();
-      let id = 0;
-      browser.openRequested.connect((browser, model) => {
-        let widget = manager.open(model);
-        if (!widget.id) widget.id = `document-manager-${++id}`;
-        if (!widget.isAttached) appShell.addToMainArea(widget);
-        let stack = widget.parent;
-        if (!stack) {
-          return;
-        }
-        let tabs = stack.parent;
-        if (tabs instanceof TabPanel) {
-          tabs.currentWidget = widget;
-        }
-      });
-      return manager;
+  app.commands.add([
+    {
+      id: newNotebookId,
+      handler: () => {
+        browser.newUntitled('notebook').then(
+          contents => manager.open(contents)
+        );
+      }
+    }
+  ]);
+  app.palette.add([
+    {
+      command: newNotebookId,
+      category: 'File Operations',
+      text: 'New Notebook',
+      caption: 'Create a new Jupyter Notebook'
+    }
+  ]);
+
+  // Add the command for saving a document.
+  let saveDocumentId = 'file-operations:save';
+
+  app.commands.add([
+    {
+      id: saveDocumentId,
+      handler: () => {
+        manager.save();
+        return true;
+      }
+    }
+  ]);
+  app.palette.add([
+    {
+      command: saveDocumentId,
+      category: 'File Operations',
+      text: 'Save Document',
+      caption: 'Save the current document'
+    }
+  ]);
+
+  // Add the command for reverting a document.
+  let revertDocumentId = 'file-operations:revert';
+
+  app.commands.add([
+    {
+      id: revertDocumentId,
+      handler: () => {
+        manager.revert();
+        return true;
+      }
+    }
+  ]);
+  app.palette.add([
+    {
+      command: revertDocumentId,
+      category: 'File Operations',
+      text: 'Revert Document',
+      caption: 'Revert the current document'
+    }
+  ]);
+
+  // Add the command for closing a document.
+  let closeDocumentId = 'file-operations:close';
+
+  app.commands.add([
+    {
+      id: closeDocumentId,
+      handler: () => {
+        manager.close();
+        return true;
+      }
+    }
+  ]);
+  app.palette.add([
+    {
+      command: closeDocumentId,
+      category: 'File Operations',
+      text: 'Close Document',
+      caption: 'Close the current document'
+    }
+  ]);
+
+  // Add the command for closing all documents.
+  let closeAllId = 'file-operations:close-all';
+
+  app.commands.add([
+    {
+      id: closeAllId,
+      handler: () => {
+        manager.closeAll();
+        return true;
+      }
+    }
+  ]);
+  app.palette.add([
+    {
+      command: closeAllId,
+      category: 'File Operations',
+      text: 'Close All',
+      caption: 'Close all open documents'
+    }
+  ]);
+
+  browser.widgetFactory = model => {
+    return manager.open(model);
+  }
+
+  let id = 0;
+  browser.openRequested.connect((browser, model) => {
+    let widget = manager.open(model);
+    if (!widget.id) widget.id = `document-manager-${++id}`;
+    if (!widget.isAttached) app.shell.addToMainArea(widget);
+    let stack = widget.parent;
+    if (!stack) {
+      return;
+    }
+    let tabs = stack.parent;
+    if (tabs instanceof TabPanel) {
+      tabs.currentWidget = widget;
     }
   });
+
+  return Promise.resolve(void 0);
 }
