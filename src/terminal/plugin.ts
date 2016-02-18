bba1008c9b49e3ccc79f67b9f83a2cf0d6a36d18
Steven Silvester <steven.silvester@ieee.org>
2016-02-17 21:25:31 -0600

    Update terminal plugin

--- src/terminal/plugin.ts
+++ src/terminal/plugin.ts
@@ -7,63 +7,54 @@ import {
 } from 'jupyter-js-terminal';
 
 import {
-  IAppShell, ICommandPalette, ICommandRegistry, IShortcutManager
-} from 'phosphide';
-
-import {
-  Container, Token
-} from 'phosphor-di';
+  Application
+} from 'phosphide/lib/core/application';
 
 import {
   TabPanel
 } from 'phosphor-tabs';
 
 
+/**
+ * The default terminal extension. 
+ */
 export
-function resolve(container: Container): Promise<void> {
-  return container.resolve({
-    requires: [IAppShell, ICommandPalette, ICommandRegistry, IShortcutManager],
-    create: (shell: IAppShell, palette: ICommandPalette, registry: ICommandRegistry, shortcuts: IShortcutManager) => {
+const terminalExtension = {
+  id: 'jupyter.extensions.terminal',
+  activate: activateTerminal
+};
 
-      let newTerminalId = 'terminal:new';
 
-      registry.add([
-        {
-          id: newTerminalId,
-          handler: () => {
-            let term = new TerminalWidget();
-            term.color = 'black';
-            term.background = 'white';
-            term.title.closable = true;
-            shell.addToMainArea(term);
-            let stack = term.parent;
-            if (!stack) {
-              return;
-            }
-            let tabs = stack.parent;
-            if (tabs instanceof TabPanel) {
-              tabs.currentWidget = term;
-            }
-          }
-        }
-      ]);
-      shortcuts.add([
-        {
-          sequence: ['Ctrl T'],
-          selector: '*',
-          command: newTerminalId,
-          args: void 0
-        }
-      ]);
-      palette.add([
-        {
-          id: newTerminalId,
-          args: void 0,
-          category: 'Terminal',
-          text: 'New Terminal',
-          caption: 'Start a new terminal session'
-        }
-      ]);
+function activateTerminal(app: Application): Promise<void> {
+
+  let newTerminalId = 'terminal:new';
+
+  app.commands.add([{
+    id: newTerminalId,
+    handler: () => {
+      let term = new TerminalWidget();
+      term.color = 'black';
+      term.background = 'white';
+      term.title.closable = true;
+      app.shell.addToMainArea(term);
+      let stack = term.parent;
+      if (!stack) {
+        return;
+      }
+      let tabs = stack.parent;
+      if (tabs instanceof TabPanel) {
+        tabs.currentWidget = term;
+      }
     }
-  });
+  }]);
+  app.palette.add([
+    {
+      command: newTerminalId,
+      category: 'Terminal',
+      text: 'New Terminal',
+      caption: 'Start a new terminal session'
+    }
+  ]);
+
+  return Promise.resolve(void 0);
 }
