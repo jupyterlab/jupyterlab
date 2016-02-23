09e601b56e397efb15a2135475ee309a281ac55d
Steven Silvester <steven.silvester@ieee.org>
2016-02-23 14:17:13 -0600

    Add a terminal example

---
+++ examples/terminal/src/index.ts
@@ -0,0 +1,35 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2015, Jupyter Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+'use strict';
+
+import {
+  DockPanel
+} from 'phosphor-dockpanel';
+
+
+import {
+  TerminalWidget
+} from 'jupyter-js-ui/lib/terminal';
+
+
+function main(): void {
+  let term1 = new TerminalWidget({ background: 'black',
+                                  color: 'white'});
+  let term2 = new TerminalWidget({ background: 'white',
+                                  color: 'black'});
+
+  let dock = new DockPanel();
+  dock.insertTabBefore(term1);
+  dock.insertTabBefore(term2);
+
+  dock.attach(document.body);
+  dock.id = 'main';
+
+  window.onresize = () => dock.fit();
+}
+
+
+window.onload = main;
