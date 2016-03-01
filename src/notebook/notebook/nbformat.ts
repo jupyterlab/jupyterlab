3257798f4640254b14ac8d186c93ff1ce1a5424b
Steven Silvester <steven.silvester@ieee.org>
2016-03-01 07:05:00 -0600

    Rectify nbformat with source and add comments

--- src/notebook/notebook/nbformat.ts
+++ src/notebook/notebook/nbformat.ts
@@ -16,28 +16,221 @@ const MINOR_VERSION = 0;
 
 
 /**
- * A type alias for a multiline string.
- * In the notebook format *disk* representation, this would be 
- * string | string[]
+ * The notebook metadata.
  */
-export 
-type multilineString = string;
+export
+interface INotebookMetadata {
+  kernelspec: {
+    name: string;
+    display_name: string;
+  };
+  language_info: {
+    name: string;
+    codemirror_mode?: any;
+    file_extension?: string;
+    mimetype?: string;
+    pygments_lexer?: string
+  };
+  orig_nbformat?: number;
+}
 
 
 /**
- * A Mime bundle of data.
+ * The notebook content.
+ */
+export
+interface INotebookContent {
+  metadata: INotebookMetadata
+  nbformat_minor: number;
+  nbformat: number;
+  cells: ICell[];
+}
+
+
+/**
+ * A type alias for a multiline string.
+ */
+export 
+type multilineString = string | string[];
+
+
+/**
+ * A mime-type keyed dictionary of data.
  */
 export interface MimeBundle {
-  [key: string]: string;
+  [key: string]: multilineString;
   'application/json'?: any;
 }
 
 
 /**
+ * A type which describes the type of cell.
+ */
+export
+type CellType = 'code' | 'markdown' | 'raw';
+
+
+/**
+ * The base cell interface.
+ */
+export
+interface IBaseCell {
+  /**
+   * String identifying the type of cell.
+   */
+  cell_type: CellType;
+
+  /**
+   * Contents of the cell, represented as an array of lines.
+   */
+  source: multilineString;
+
+  /**
+   * Cell-level metadata.
+   */
+  metadata: {
+    /**
+     * The cell's name. If present, must be a non-empty string.
+     */
+    name?: string;
+
+    /**
+     * The cell's tags. Tags must be unique, and must not contain commas.
+     */
+    tags?: string[];
+  }
+}
+
+
+/**
+ * A raw cell.
+ */
+export
+interface IRawCell extends IBaseCell {
+  /**
+   * String identifying the type of cell.
+   */
+  cell_type: 'raw';
+
+  /**
+   * Cell-level metadata.
+   */
+  metadata: {
+    /**
+     * The cell's name. If present, must be a non-empty string.
+     */
+    name?: string;
+
+    /**
+     * The cell's tags. Tags must be unique, and must not contain commas.
+     */
+    tags?: string[];
+
+    /**
+     * Raw cell metadata format for nbconvert.
+     */
+    format?: string;
+  }
+}
+
+
+/**
+ * A markdown cell.
+ */
+export
+interface IMarkdownCell extends IBaseCell {
+  /**
+   * String identifying the type of cell.
+   */
+  cell_type: 'markdown';
+}
+
+
+/**
+ * A code cell.
+ */
+export
+interface ICodeCell extends IBaseCell {
+  /**
+   * String identifying the type of cell.
+   */
+  cell_type: 'code';
+
+  /**
+   * Cell-level metadata.
+   */
+  metadata: {
+    /**
+     * The cell's name. If present, must be a non-empty string.
+     */
+    name?: string;
+
+    /**
+     * The cell's tags. Tags must be unique, and must not contain commas.
+     */
+    tags?: string[];
+
+    /**
+     * Whether the cell is collapsed/expanded.
+     */
+    collapsed?: boolean;
+
+    /**
+     * Whether the cell's output is scrolled, unscrolled, or autoscrolled.
+     */
+    scrolled?: boolean | 'auto';
+  }
+
+  /**
+   * Execution, display, or stream outputs.
+   */
+  outputs: IOutput[];
+
+  /**
+   * The code cell's prompt number. Will be null if the cell has not been run.
+   */
+  execution_count: number;
+}
+
+
+/**
+ * A cell union type.
+ */
+export
+type ICell = IBaseCell | IRawCell | IMarkdownCell | ICodeCell;
+
+
+/**
+ * Check if cell is of markdown type.
+ */
+export
+function isMarkdownCell(d: IBaseCell): d is IMarkdownCell {
+  return d.cell_type === 'markdown';
+}
+
+
+/**
+ * Check if cell is of code type.
+ */
+export
+function isCodeCell(d: IBaseCell): d is ICodeCell {
+  return d.cell_type === 'code';
+}
+
+
+/**
+ * Check if cell is of raw type.
+ */
+export
+function isRawCell(d: IBaseCell): d is IRawCell {
+  return d.cell_type === 'raw';
+}
+
+/**
  * The valid output types.
  */
 export 
-type IOutputType = 'execute_result' | 'display_data' | 'stream' | 'error';
+type OutputType = 'execute_result' | 'display_data' | 'stream' | 'error';
 
 
 /**
@@ -45,52 +238,107 @@ type IOutputType = 'execute_result' | 'display_data' | 'stream' | 'error';
  */
 export
 interface IBaseOutput {
-  output_type: IOutputType;
+  /**
+   * Type of cell output.
+   */
+  output_type: OutputType;
 }
 
 
 /**
- * The `display_data` output type.
- */
-export
-interface IDisplayData extends IBaseOutput {
-  output_type: 'display_data';
-  data: MimeBundle;
-  metadata: {};
-}
-
-
-/**
- * The `execute_result` output type.
+ * Result of executing a code cell.
  */
 export
 interface IExecuteResult extends IBaseOutput {
+  /**
+   * Type of cell output.
+   */
   output_type: 'execute_result';
+
+  /**
+   * A result's prompt number.
+   */
   execution_count: number;
+
+  /**
+   * A mime-type keyed dictionary of data.
+   */
   data: MimeBundle;
+
+  /**
+   * Cell output metadata.
+   */
   metadata: {};
 }
 
 
 /**
- * The `stream` output type.
+ * Data displayed as a result of code cell execution.
+ */
+export
+interface IDisplayData extends IBaseOutput {
+  /**
+   * Type of cell output.
+   */
+  output_type: 'display_data';
+
+  /**
+   * A mime-type keyed dictionary of data.
+   */
+  data: MimeBundle;
+
+  /**
+   * Cell output metadata.
+   */
+  metadata: {};
+}
+
+
+/**
+ * Stream output from a code cell.
  */
 export
 interface IStream extends IBaseOutput {
+  /**
+   * Type of cell output.
+   */
   output_type: 'stream';
+
+  /**
+   * The name of the stream.
+   */
   name: 'stdout' | 'stderr';
+
+  /**
+   * The stream's text output.
+   */
   text: multilineString;
 }
 
 
 /** 
- * The `error` output type.
+ * Output of an error that occurred during code cell execution.
  */
 export
 interface IError extends IBaseOutput {
+  /**
+   * Type of cell output.
+   */
   output_type: 'error';
+
+  /**
+   * The name of the error.
+   */
   ename: string;
+
+  /**
+   * The value, or message, of the error.
+   */
   evalue: string;
+
+  /**
+   * The error's traceback.
+   */
   traceback: string[];
 }
 
@@ -135,123 +383,4 @@ function isStream(d: IBaseOutput): d is IStream {
 export
 function isError(d: IBaseOutput): d is IError {
   return d.output_type === "error";
-}
-
-
-/**
- * The base cell interface.
- */
-export
-interface IBaseCell {
-  cell_type: 'raw' | 'markdown' | 'code';
-  source: multilineString;
-  metadata: {
-    name?: string;
-    tags?: string[];
-  }
-}
-
-
-/**
- * A raw cell.
- */
-export
-interface IRawCell extends IBaseCell {
-  cell_type: 'raw';
-  metadata: {
-    format?: string;
-  }
-}
-
-
-/**
- * A markdown cell.
- */
-export
-interface IMarkdownCell extends IBaseCell {
-  cell_type: 'markdown';
-}
-
-
-
-/**
- * A code cell.
- */
-export
-interface ICodeCell extends IBaseCell {
-  cell_type: 'code';
-  metadata: {
-    name?: string;
-    tags?: string[];
-    collapsed?: boolean;
-    scrolled?: boolean | string;
-  }
-  outputs: IOutput[];
-  execution_count: number;
-}
-
-
-/**
- * A cell union type.
- */
-export
-type ICell = IBaseCell | IRawCell | IMarkdownCell | ICodeCell;
-
-
-/**
- * Check if cell is of markdown type.
- */
-export
-function isMarkdownCell(d: IBaseCell): d is IMarkdownCell {
-  return d.cell_type === 'markdown';
-}
-
-
-/**
- * Check if cell is of code type.
- */
-export
-function isCodeCell(d: IBaseCell): d is ICodeCell {
-  return d.cell_type === 'code';
-}
-
-
-/**
- * Check if cell is of raw type.
- */
-export
-function isRawCell(d: IBaseCell): d is IRawCell {
-  return d.cell_type === 'raw';
-}
-
-
-/**
- * The notebook metadata.
- */
-export
-interface INotebookMetadata {
-  kernelspec: {
-    name: string;
-    display_name: string;
-  };
-  language_info: {
-    name: string;
-    codemirror_mode?: string | {};
-    file_extension?: string;
-    mimetype?: string;
-    pygments_lexer?: string
-  };
-  orig_nbformat?: number;
-}
-
-
-/**
- * The notebook content.
- */
-export
-interface INotebookContent {
-  metadata: INotebookMetadata
-  nbformat_minor: number;
-  nbformat: number;
-  cells: ICell[];
 }
