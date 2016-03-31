1826ebbfe00fa29b872ff51d510b954344482678
Jason Grout <jgrout6@bloomberg.net>
2016-03-31 15:48:01 -0700

    Fix typing for widget manager.

--- src/typings/jupyter-js-widgets/jupyter-js-widgets.d.ts
+++ src/typings/jupyter-js-widgets/jupyter-js-widgets.d.ts
@@ -3,24 +3,25 @@
 
 /// <reference path="../backbone/backbone-global.d.ts" />
 
-declare module Widgets {
-    export class ManagerBase<T> {
-        display_view(msg: any, view: Backbone.View<Backbone.Model>, options: any): T;
-        handle_comm_open(comm: shims.services.Comm, msg: any): Promise<Backbone.Model>;
-        // how do I say msg is an IKernelMessage from 'jupyter-js-services'?
-        display_model(msg: any, model: Backbone.Model, options: any): Promise<T>;
-        get_model(id: string): Promise<Backbone.Model>;
-    }
-    
-    export module shims {
-        export module services {
-            export class Comm {
-                constructor(comm: any);
-            }            
-        }
-    }
-}
 
 declare module "jupyter-js-widgets" {
-    export = Widgets;
+  import {
+    IKernelIOPubCommOpenMessage, IKernelMessage
+  } from 'jupyter-js-services';
+
+  export class ManagerBase<T> {
+    display_view(msg: IKernelMessage, view: Backbone.View<Backbone.Model>, options: any): T;
+    handle_comm_open(comm: shims.services.Comm, msg: IKernelIOPubCommOpenMessage): Promise<Backbone.Model>;
+    // how do I say msg is an IKernelMessage from 'jupyter-js-services'?
+    display_model(msg: IKernelMessage, model: Backbone.Model, options: any): Promise<T>;
+    get_model(id: string): Promise<Backbone.Model>;
+  }
+
+  export namespace shims {
+    export namespace services {
+      export class Comm {
+        constructor(comm: any);
+      }
+    }
+  }
 }
