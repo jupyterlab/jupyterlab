cd3d1461f2cd438e14e3939e5cfbedde552aeda0
Jason Grout <jgrout6@bloomberg.net>
2016-04-01 05:24:10 -0700

    Add clarifying comment about .d.ts imports

--- src/typings/jupyter-js-widgets/jupyter-js-widgets.d.ts
+++ src/typings/jupyter-js-widgets/jupyter-js-widgets.d.ts
@@ -5,6 +5,9 @@
 
 
 declare module "jupyter-js-widgets" {
+  // We have to use es5-style imports rather than es6-style imports
+  // since typedoc only supports TS 1.6, and es6-style imports were
+  // introduced in TS 1.7. See https://github.com/jasongrout/jupyter-js-ui/pull/1.
   import services = require('jupyter-js-services');
 
   export class ManagerBase<T> {
