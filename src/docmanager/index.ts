e97347d2b9766552c828f83bc0b15920649fb221
Steven Silvester <steven.silvester@ieee.org>
2016-05-12 14:44:40 -0500

    Fix property lookup

--- src/docmanager/index.ts
+++ src/docmanager/index.ts
@@ -625,7 +625,7 @@ class DocumentManager implements IDisposable {
   clone(widget: Widget): Widget {
     let parent = new Widget();
     let id = Private.contextProperty.get(widget);
-    let name = Private.contextProperty.get(id);
+    let name = Private.contextProperty.get(widget);
     this._createWidget(id, name, parent);
     return parent;
   }
