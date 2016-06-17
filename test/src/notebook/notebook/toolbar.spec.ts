76627604c6dce2b3a38142c34b3f81b08008fabc
Steven Silvester <steven.silvester@ieee.org>
2016-06-17 12:23:10 -0500

    wip toolbar tests

---
+++ test/src/notebook/notebook/toolbar.spec.ts
@@ -0,0 +1,49 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import expect = require('expect.js');
+
+import {
+  NotebookToolbar, ToolbarButton
+} from '../../../../lib/notebook/notebook/toolbar';
+
+
+describe('notebook/notebook/toolbar', () => {
+
+  describe('NotebookToolbar', () => {
+
+    describe('#constructor()', () => {
+
+    });
+
+    describe('#add()', () => {
+
+    });
+
+    describe('#list()', () => {
+
+    });
+
+  });
+
+  describe('ToolbarButton', () => {
+
+    describe('#createNode()', () => {
+
+    });
+
+    describe('#constructor()', () => {
+
+    });
+
+    describe('#dispose()', () => {
+
+    });
+
+    describe('#handleEvent()', () => {
+
+    });
+
+  });
+
+});
