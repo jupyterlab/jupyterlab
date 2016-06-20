a7a97322a11eff6d2bc007978ca24f526cea671b
Steven Silvester <steven.silvester@ieee.org>
2016-06-20 12:46:25 -0500

    Add test stub

---
+++ test/src/notebook/notebook/panel.spec.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import expect = require('expect.js');
+
+import {
+  INotebookModel, NotebookModel
+} from '../../../../lib/notebook/notebook/model';
+
+import {
+  Notebook, StaticNotebook
+} from '../../../../lib/notebook/notebook/widget';
+
+import {
+  nbformat
+} from '../../../../lib/notebook/notebook/nbformat';
+
+import {
+  defaultRenderMime
+} from '../../rendermime/rendermime.spec';
+
+
+
+describe('notebook/notebook/panel', () => {
+
+  describe('NotebookPanel', () => {
+
+    describe('#constructor()', () => {
+
+      it('should create a notebook panel', () => {
+
+      });
+
+
+      it('should accept an optional render', () => {
+
+      });
+
+    });
+
+  });
+
+});
