9be58ba2bf760c51651c9bf1ff8ef0f0c994ca12
Steven Silvester <steven.silvester@ieee.org>
2016-06-21 08:01:05 -0500

    update tests

--- test/src/notebook/notebook/panel.spec.ts
+++ test/src/notebook/notebook/panel.spec.ts
@@ -20,6 +20,46 @@ import {
 } from '../../rendermime/rendermime.spec';
 
 
+class LogNotebookPanel extends NotebookPanel {
+
+  methods: string[] = [];
+
+  protected onContextChanged(oldValue: IDocumentContext<INotebookModel>, newValue: IDocumentContext<INotebookModel>): void {
+    super.onContextChanged(oldValue, newValue);
+    this.methods.push('onContextChanged');
+  }
+
+  protected onModelStateChanged(sender: INotebookModel, args: IChangedArgs<any>): void {
+    super.onModelStateChanged(sender, args);
+    this.methods.push('onModelStateChanged');
+  }
+
+  protected onPathChanged(sender: IDocumentContext<INotebookModel>, path: string): void {
+    super.onPathChanged(sender, args);
+    this.methods.push('onPathChanged');
+  }
+
+  protected onContentStateChanged(sender: Notebook, args: IChangedArgs<any>): void {
+    super.onContentStateChanged(sender, args);
+    this.methods.push('onContentStateChanged');
+  }
+
+  protected onTextChanged(editor: CellEditorWidget, change: ITextChange): void {
+    super.onTextChanged(editor, change);
+    this.methods.push('onTextChanged');
+  }
+
+  protected onCompletionRequested(editor: CellEditorWidget, change: ICompletionRequest): void {
+    super.onCompletionRequested(editor, change);
+    this.methods.push('onCompletionRequested');
+  }
+
+  protected onCompletionSelected(widget: CompletionWidget, value: string): void {
+    super.onCompletionSelected(widget, value);
+    this.methods.push('onCompletionSelected');
+  }
+}
+
 
 describe('notebook/notebook/panel', () => {
 
@@ -35,6 +75,82 @@ describe('notebook/notebook/panel', () => {
       it('should accept an optional render', () => {
 
       });
+
+    });
+
+    describe('#contextChanged', () => {
+
+    });
+
+    describe('#kernelChanged', () => {
+
+    });
+
+    describe('#toolbar', () => {
+
+    });
+
+    describe('#content', () => {
+
+    });
+
+    describe('#kernel', () => {
+
+    });
+
+    describe('#rendermime', () => {
+
+    });
+
+    describe('#renderer', () => {
+
+    });
+
+    describe('#clipboard', () => {
+
+    });
+
+    describe('#model', () => {
+
+    });
+
+    describe('#context', () => {
+
+    });
+
+    describe('#dispose()', () => {
+
+    });
+
+    describe('#onContextChanged()', () => {
+
+    });
+
+    describe('#onKernelChanged()', () => {
+
+    });
+
+    describe('#onModelStateChanged()', () => {
+
+    });
+
+    describe('#onPathChanged()', () => {
+
+    });
+
+    describe('#onContentStateChanged()', () => {
+
+    });
+
+    describe('#onTextChanged()', () => {
+
+    });
+
+    describe('#onCompletionRequested()', () => {
+
+    });
+
+    describe('#onCompletionSelected()', () => {
 
     });
 
