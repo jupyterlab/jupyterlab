e529739266610f07ff663e0c47622f5558f68254
Steven Silvester <steven.silvester@ieee.org>
2016-06-27 12:16:11 -0500

    wip default toolbar tests

---
+++ test/src/notebook/notebook/default-toolbar.spec.ts
@@ -0,0 +1,65 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import expect = require('expect.js');
+
+import {
+ TooolbarItems
+} from '../../../../lib/notebook/notebook/model';
+
+
+describe('notebook/notebook/default-toolbar', () => {
+
+  describe('TooolbarItems', () => {
+
+    describe('#createSaveButton()', () => {
+
+    });
+
+    describe('#createInsertButton()', () => {
+
+    });
+
+    describe('#createCutButton()', () => {
+
+    });
+
+    describe('#createCopyButton()', () => {
+
+    });
+
+    describe('#createPasteButton()', () => {
+
+    });
+
+    describe('#createRunButton()', () => {
+
+    });
+
+    describe('#createInterruptButton()', () => {
+
+    });
+
+    describe('#createRestartButton()', () => {
+
+    });
+
+    describe('#createCellTypeItem()', () => {
+
+    });
+
+    describe('#createKernelNameItem()', () => {
+
+    });
+
+    describe('#createKernelStatusItem()', () => {
+
+    });
+
+    describe('#populateDefaults()', () => {
+
+    });
+
+  });
+
+});
