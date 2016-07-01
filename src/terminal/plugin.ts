2c51fb3877d49fa78021449e585fb27058a31b61
charnpreetsingh185 <csingh02@calpoly.edu>
2016-07-01 16:21:51 -0700

    Implemented some commands

--- src/terminal/plugin.ts
+++ src/terminal/plugin.ts
@@ -27,6 +27,9 @@ const terminalExtension = {
 function activateTerminal(app: Application): void {
 
   let newTerminalId = 'terminal:create-new';
+  let increaseTerminalFontSize = 'terminal:increase-font';
+  let decreaseTerminalFontSize = 'terminal:decrease-font';
+  let toggleTerminalTheme = 'terminal:toggle-theme';
 
   // Track the current active terminal.
   let tracker = new WidgetTracker<TerminalWidget>();
@@ -35,21 +38,86 @@ function activateTerminal(app: Application): void {
     activeTerm = widget;
   });
 
-  app.commands.add([{
-    id: newTerminalId,
-    handler: () => {
-      let term = new TerminalWidget();
-      term.title.closable = true;
-      app.shell.addToMainArea(term);
-      tracker.addWidget(term);
+  app.commands.add([
+    {
+      id: newTerminalId,
+      handler: () => {
+        let term = new TerminalWidget();
+        term.title.closable = true;
+        app.shell.addToMainArea(term);
+        tracker.addWidget(term);
+      }
+    },
+    {
+      id: increaseTerminalFontSize,
+      handler: increaseFont
+    },
+    {
+      id: decreaseTerminalFontSize,
+      handler: decreaseFont
+    },
+    {
+      id: toggleTerminalTheme,
+      handler: toggleTheme
     }
-  }]);
+]);
   app.palette.add([
     {
       command: newTerminalId,
       category: 'Terminal',
-      text: 'New Terminal',
+      text: 'Create a new Terminal',
       caption: 'Start a new terminal session'
+    },
+    {
+      command: increaseTerminalFontSize,
+      category: 'Terminal',
+      text: 'Increase Terminal Font Size',
+    },
+    {
+      command: decreaseTerminalFontSize,
+      category: 'Terminal',
+      text: 'Decrease Terminal Font Size',
+    },
+    {
+      command: toggleTerminalTheme,
+      category: 'Terminal',
+      text: 'Toggle Terminal Theme',
+      caption: 'Switch Terminal Background and Font Colors'
     }
   ]);
+
+  function increaseFont(): void {
+    if (!tracker.isDisposed) {
+      for (var i = 0; i < tracker.widgets.length; i++) {
+        if (tracker.widgets[i].fontSize < 72) {
+          tracker.widgets[i].fontSize = tracker.widgets[i].fontSize + 1;
+        }
+      }
+    }
+  }
+
+  function decreaseFont(): void {
+    if (!tracker.isDisposed) {
+      for (var i = 0; i < tracker.widgets.length; i++) {
+        if (tracker.widgets[i].fontSize > 9) {
+          tracker.widgets[i].fontSize = tracker.widgets[i].fontSize - 1;
+        }
+      }
+    }
+  }
+
+  function toggleTheme(): void {
+    if (!tracker.isDisposed) {
+      for (var i = 0; i < tracker.widgets.length; i++) {
+        if (tracker.widgets[i].background === 'black') {
+          tracker.widgets[i].background = 'white';
+          tracker.widgets[i].color = 'black';
+        }
+        else {
+          tracker.widgets[i].background = 'black';
+          tracker.widgets[i].color = 'white';
+        }
+      }
+    }
+  }
 }
