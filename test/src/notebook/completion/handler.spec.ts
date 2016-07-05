c1f0af7cf84dbfa9758684c985cd16deff1ca666
A. Darian <git@darian.af>
2016-07-05 15:56:53 +0100

    Add more handler tests.

--- test/src/notebook/completion/handler.spec.ts
+++ test/src/notebook/completion/handler.spec.ts
@@ -16,21 +16,44 @@ import {
 } from '../../../../lib/notebook/cells';
 
 import {
-  ICompletionRequest
+  ICompletionRequest, CellEditorWidget, ITextChange
 } from '../../../../lib/notebook/cells/editor';
 
 import {
-  CompletionWidget, CellCompletionHandler
+  CompletionWidget, CellCompletionHandler, CompletionModel
 } from '../../../../lib/notebook/completion';
 
 
+class TestCompletionModel extends CompletionModel {
+  methods: string[] = [];
+
+  handleTextChange(change: ITextChange): void {
+    this.methods.push('handleTextChange');
+    super.handleTextChange(change);
+  }
+}
+
+
 class TestCompletionHandler extends CellCompletionHandler {
+  methods: string[] = [];
+
   makeRequest(request: ICompletionRequest): Promise<void> {
+    this.methods.push('makeRequest');
     return super.makeRequest(request);
   }
 
   onReply(pending: number, request: ICompletionRequest, msg: KernelMessage.ICompleteReplyMsg): void {
     super.onReply(pending, request, msg);
+  }
+
+  onTextChanged(editor: CellEditorWidget, change: ITextChange): void {
+    this.methods.push('onTextChanged');
+    super.onTextChanged(editor, change);
+  }
+
+  onCompletionRequested(editor: CellEditorWidget, request: ICompletionRequest): void {
+    this.methods.push('onCompletionRequested');
+    super.onCompletionRequested(editor, request);
   }
 }
 
@@ -174,14 +197,183 @@ describe('notebook/completion/handler', () => {
     describe('#onReply()', () => {
 
       it('should do nothing if handler has been disposed', () => {
-        let handler = new TestCompletionHandler(new CompletionWidget());
+        let completion = new CompletionWidget();
+        let handler = new TestCompletionHandler(completion);
+        completion.model = new CompletionModel();
+        completion.model.options = ['foo', 'bar', 'baz'];
         handler.dispose();
         handler.onReply(0, null, null);
+        expect(completion.model).to.be.ok();
       });
 
       it('should do nothing if pending request ID does not match', () => {
+        let completion = new CompletionWidget();
+        let handler = new TestCompletionHandler(completion);
+        completion.model = new CompletionModel();
+        completion.model.options = ['foo', 'bar', 'baz'];
+        handler.onReply(2, null, null);
+        expect(completion.model).to.be.ok();
+      });
+
+      it('should reset model if status is not ok', () => {
+        let completion = new CompletionWidget();
+        let handler = new TestCompletionHandler(completion);
+        let options = ['a', 'b', 'c'];
+        let request: ICompletionRequest = {
+          ch: 0,
+          chHeight: 0,
+          chWidth: 0,
+          line: 0,
+          coords: null,
+          position: 0,
+          currentValue: 'f'
+        };
+        let reply: KernelMessage.ICompleteReplyMsg = {
+          header: null,
+          parent_header: {},
+          metadata: {},
+          buffers: null,
+          channel: 'shell',
+          content: {
+            status: 'error',
+            cursor_start: 0,
+            cursor_end: 0,
+            metadata: {},
+            matches: ['foo']
+          }
+        };
+        completion.model = new CompletionModel();
+        completion.model.options = options;
+        expect(completion.model.options).to.eql(options);
+        handler.onReply(0, request, reply);
+        expect(completion.model.options).to.be(null);
+      });
+
+      it('should update model if status is ok', () => {
+        let completion = new CompletionWidget();
+        let handler = new TestCompletionHandler(completion);
+        let options = ['a', 'b', 'c'];
+        let request: ICompletionRequest = {
+          ch: 0,
+          chHeight: 0,
+          chWidth: 0,
+          line: 0,
+          coords: null,
+          position: 0,
+          currentValue: 'f'
+        };
+        let reply: KernelMessage.ICompleteReplyMsg = {
+          header: null,
+          parent_header: {},
+          metadata: {},
+          buffers: null,
+          channel: 'shell',
+          content: {
+            status: 'ok',
+            cursor_start: 0,
+            cursor_end: 0,
+            metadata: {},
+            matches: ['foo']
+          }
+        };
+        completion.model = new CompletionModel();
+        completion.model.options = options;
+        expect(completion.model.options).to.eql(options);
+        handler.onReply(0, request, reply);
+        expect(completion.model.options).to.eql(reply.content.matches);
+      });
+
+    });
+
+    describe('#onTextChanged()', () => {
+
+      it('should fire when the active editor emits a text change', () => {
         let handler = new TestCompletionHandler(new CompletionWidget());
-        handler.onReply(1, null, null);
+        let change: ITextChange = {
+          ch: 0,
+          chHeight: 0,
+          chWidth: 0,
+          line: 0,
+          coords: null,
+          oldValue: 'fo',
+          newValue: 'foo'
+        };
+        let cell = new BaseCellWidget();
+
+        handler.activeCell = cell;
+        expect(handler.methods).to.not.contain('onTextChanged');
+        cell.editor.textChanged.emit(change);
+        expect(handler.methods).to.contain('onTextChanged');
+      });
+
+      it('should call model change handler if model exists', () => {
+        let completion = new CompletionWidget({
+          model: new TestCompletionModel()
+        });
+        let handler = new TestCompletionHandler(completion);
+        let change: ITextChange = {
+          ch: 0,
+          chHeight: 0,
+          chWidth: 0,
+          line: 0,
+          coords: null,
+          oldValue: 'fo',
+          newValue: 'foo'
+        };
+        let cell = new BaseCellWidget();
+        let model = completion.model as TestCompletionModel;
+
+        handler.activeCell = cell;
+        expect(model.methods).to.not.contain('handleTextChange');
+        cell.editor.textChanged.emit(change);
+        expect(model.methods).to.contain('handleTextChange');
+      });
+
+    });
+
+    describe('#onCompletionRequested()', () => {
+
+      it('should fire when the active editor emits a request', () => {
+        let handler = new TestCompletionHandler(new CompletionWidget());
+        let request: ICompletionRequest = {
+          ch: 0,
+          chHeight: 0,
+          chWidth: 0,
+          line: 0,
+          coords: null,
+          position: 0,
+          currentValue: 'foo'
+        };
+        let cell = new BaseCellWidget();
+
+        handler.activeCell = cell;
+        expect(handler.methods).to.not.contain('onCompletionRequested');
+        cell.editor.completionRequested.emit(request);
+        expect(handler.methods).to.contain('onCompletionRequested');
+      });
+
+      it('should make a kernel request if kernel and model exist', () => {
+        let completion = new CompletionWidget({
+          model: new TestCompletionModel()
+        });
+        let handler = new TestCompletionHandler(completion);
+        let request: ICompletionRequest = {
+          ch: 0,
+          chHeight: 0,
+          chWidth: 0,
+          line: 0,
+          coords: null,
+          position: 0,
+          currentValue: 'foo'
+        };
+        let cell = new BaseCellWidget();
+        let model = completion.model as TestCompletionModel;
+
+        handler.kernel = new MockKernel();
+        handler.activeCell = cell;
+        expect(handler.methods).to.not.contain('makeRequest');
+        cell.editor.completionRequested.emit(request);
+        expect(handler.methods).to.contain('makeRequest');
       });
 
     });
