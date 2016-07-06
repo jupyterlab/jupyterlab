15481b10dc1c4038bae33c69930f01526c8dfc44
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-07-05 17:31:11 -0700

    Test branch

--- src/about/plugin.ts
+++ src/about/plugin.ts
@@ -5,9 +5,8 @@ import 'jQuery';
 import 'fullpage.js'; 
 import 'fullpage.js/jquery.fullPage.css';
 
-const url = require('./about_page.html');
+// const url = require('./about_page.html');
 let options = { controlArrows: true };
-jQuery('about-jupyterlab').fullpage(options);
 
 import {
   Application
@@ -30,17 +29,30 @@ import {
 
  function activateAbout(app: Application): void {
    let widget = new Widget();
-   let iframe = document.createElement('iframe');
+   // let iframe = document.createElement('iframe');
    let commandId = 'about-jupyterlab:show';
    widget.id = 'about-jupyterlab';
    widget.title.text = 'About';
    widget.title.closable = true;
    widget.addClass('fullPage-about-class');
-   widget.node.appendChild(iframe);
-   iframe.src = url as string;
-   iframe.height = '100%';
-   iframe.width = '100%';
-   widget.node.style.overflowY = 'auto';
+   widget.node.innerHTML = `
+   <div id="fullpage-about-jupyterlab">
+   <div class="section active" id="section0"><h1>fullPage.js</h1></div>
+   <div class="section" id="section1">
+   <div class="slide "><h1>Simple Demo</h1></div>
+   <div class="slide active"><h1>Only text</h1></div>
+   <div class="slide"><h1>And text</h1></div>
+   <div class="slide"><h1>And more text</h1></div>
+   </div>
+   <div class="section" id="section2"><h1>No wraps, no extra markup</h1></div>
+   <div class="section" id="section3"><h1>Just the simplest demo ever</h1></div>
+   </div>
+   `;
+   // widget.node.appendChild(iframe);
+   // iframe.src = url as string;
+   // iframe.height = '100%';
+   // iframe.width = '100%';
+   // widget.node.style.overflowY = 'auto';
    app.commands.add([{
      id: commandId,
      handler: () => {
@@ -56,4 +68,6 @@ import {
    }]);
 
    app.shell.addToMainArea(widget);
+   console.log(jQuery('div.p-Widget').length);
+   jQuery('#fullpage-about-jupyterlab').fullpage(options);
  }
