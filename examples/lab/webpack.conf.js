2596e2882ca9dbb0642635c12db9caba8047d8ec
Spoorthy Vemula <sckortyman@gmail.com>
2016-07-08 12:51:36 -0700

    bug fixes

--- examples/lab/webpack.conf.js
+++ examples/lab/webpack.conf.js
@@ -16,9 +16,19 @@ module.exports = {
     loaders: [
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.html$/, loader: 'file' },
-      // Handle image
-      { test: /\.(jpg|png|gif|svg)$/, loader: "file" },
+      { test: /\.html$/, loader: 'file'},
+      // jquery-ui loads some images
+      { test: /\.(jpg|png|gif)$/, loader: "file" },
+      // required to load font-awesome
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=image/svg+xml" }
     ]
+  },
+  externals: {
+    jquery: '$',
+    'jquery-ui': '$'
   }
 }
