ccdd3104407d62dbcc63183d1904fd91d208efbb
A. Darian <git@darian.af>
2016-07-14 10:31:03 -0500

    Documentation and adding rel="nofollow".

--- src/sanitizer/index.ts
+++ src/sanitizer/index.ts
@@ -1,21 +1,40 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
-
 import * as sanitize from 'sanitize-html';
 
+
+/**
+ * A class to sanitize HTML strings.
+ *
+ * #### Notes
+ * This class should not ordinarily need to be instantiated by user code.
+ * Instead, the `defaultSanitizer` instance should be used.
+ */
 export
 class Sanitizer {
+  /**
+   * Sanitize an HTML string.
+   */
   sanitize(dirty: string): string {
     return sanitize(dirty, this._options);
   }
 
   private _options: sanitize.IOptions = {
     allowedTags: sanitize.defaults.allowedTags.concat('img'),
+    allowedAttributes: {
+      // Allow the "rel" attribute for <a> tags.
+      'a': sanitize.defaults.allowedAttributes['a'].concat('rel')
+    },
     transformTags: {
+      // Set the "rel" attribute for <a> tags to "nofollow".
       'a': sanitize.simpleTransform('a', { 'rel': 'nofollow' })
     }
   };
 }
 
+
+/**
+ * The default instance of the `Sanitizer` class meant for use by user code.
+ */
 export
 const defaultSanitizer = new Sanitizer();
