c88b620e036d1ae08b0878af171ddbbbd7c9dcd6
A. Darian <git@darian.af>
2016-07-14 16:35:16 -0500

    Remove superfluous imports.

--- src/markdownwidget/widget.ts
+++ src/markdownwidget/widget.ts
@@ -14,10 +14,6 @@ import {
 } from 'phosphor-widget';
 
 import {
-  Message
-} from 'phosphor-messaging';
-
-import {
   IDocumentModel, IDocumentContext, ABCWidgetFactory
 } from '../docregistry';
 
@@ -25,11 +21,6 @@ import {
   MarkdownRenderer
 } from '../renderers'
 
-
-/**
- * The class name added to a dirty widget.
- */
-const DIRTY_CLASS = 'jp-mod-dirty';
 
 /**
  * The class name added to a jupyter MarkdownWidget
@@ -45,29 +36,19 @@ class MarkdownWidget extends Widget {
   /**
    * Construct a new markdown widget.
    */
-
   constructor(context: IDocumentContext<IDocumentModel>) {
     super();
-    let model = context.model;
     this.addClass(MD_CLASS);
     this.layout = new PanelLayout();
     this.title.text = context.path.split('/').pop();
 
-    // model.stateChanged.connect((m, args) => {
-    //   if (args.name === 'dirty') {
-    //     if (args.newValue) {
-    //       this.title.className += ` ${DIRTY_CLASS}`;
-    //     } else {
-    //       this.title.className = this.title.className.replace(DIRTY_CLASS, '');
-    //     }
-    //   }
-    // });
+    let model = context.model;
+    let layout = this.layout as PanelLayout;
+    let renderer = new MarkdownRenderer();
+
     context.pathChanged.connect((c, path) => {
       this.title.text = path.split('/').pop();
     });
-
-    let layout = this.layout as PanelLayout;
-    let renderer = new MarkdownRenderer();
 
     model.contentChanged.connect(() => {
       let widget = renderer.render('text/markdown', model.toString());
@@ -87,8 +68,7 @@ class MarkdownWidget extends Widget {
 export
 class MarkdownWidgetFactory extends ABCWidgetFactory<MarkdownWidget, IDocumentModel> {
   /**
-   * Create a new widget given a context
-   * .
+   * Create a new widget given a context.
    */
   createNew(context: IDocumentContext<IDocumentModel>, kernel?: IKernel.IModel): MarkdownWidget {
     let widget = new MarkdownWidget(context);
