551c610a12ee55ee4d9e02d7d262e73b1c0af832
Afshin Darian <git@darian.af>
2016-08-09 20:04:19 +0100

    Create a JupyterLab#palette attribute.

--- src/application/index.ts
+++ src/application/index.ts
@@ -6,6 +6,10 @@ import {
 } from 'phosphor/lib/ui/application';
 
 import {
+  CommandPalette
+} from 'phosphor/lib/ui/commandpalette';
+
+import {
   ApplicationShell
 } from './shell';
 
@@ -22,10 +26,73 @@ type JupyterLabPlugin<T> = Application.IPlugin<JupyterLab, T>;
  */
 export
 class JupyterLab extends Application<ApplicationShell> {
+  constructor() {
+    super();
+    this._palette = Private.createCommandPalette(this);
+  }
+
+  /**
+   * The command palette instance of the JupyterLab application.
+   */
+  get palette(): CommandPalette {
+    return this._palette;
+  }
+
   /**
    * Create the application shell for the JupyterLab application.
    */
   protected createShell(): ApplicationShell {
     return new ApplicationShell();
+  }
+
+  private _palette: CommandPalette = null;
+}
+
+
+/**
+ * A namespace for `JupyterLab` private data.
+ */
+namespace Private {
+  /**
+   * Create a command palette and register its commands.
+   */
+  export
+  function createCommandPalette(app: JupyterLab): CommandPalette {
+    const { commands, keymap } = app;
+    const palette = new CommandPalette({ commands, keymap });
+
+    palette.id = 'command-palette';
+    palette.title.label = 'Commands';
+
+    app.commands.addCommand('command-palette:activate', {
+      execute: () => {
+        app.shell.activateLeft(palette.id);
+        app.palette.inputNode.focus();
+        app.palette.inputNode.select();
+      },
+      label: 'Activate Command Palette'
+    });
+    app.commands.addCommand('command-palette:hide', {
+      execute: () => {
+        if (!app.palette.isHidden) {
+          app.shell.collapseLeft();
+        }
+      },
+      label: 'Hide Command Palette'
+    });
+    app.commands.addCommand('command-palette:toggle', {
+      execute: () => {
+        if (app.palette.isHidden) {
+          app.commands.execute('command-palette:activate', null);
+        } else {
+          app.commands.execute('command-palette:hide', null);
+        }
+      },
+      label: 'Toggle Command Palette'
+    });
+
+    app.shell.addToLeftArea(palette);
+
+    return palette;
   }
 }
