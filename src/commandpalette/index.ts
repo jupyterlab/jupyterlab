ef45ab3448ccb6e25b84af5fae75103a3163dd32
Afshin Darian <git@darian.af>
2016-08-10 17:31:36 +0100

    Update the ICommandPalette interface to shrink the API surface for plugin authors.

--- src/commandpalette/index.ts
+++ src/commandpalette/index.ts
@@ -4,6 +4,10 @@
 |----------------------------------------------------------------------------*/
 
 import {
+  DisposableDelegate, IDisposable
+} from 'phosphor/lib/core/disposable';
+
+import {
   Token
 } from 'phosphor/lib/core/token';
 
@@ -21,4 +25,48 @@ const ICommandPalette = new Token<ICommandPalette>('jupyter.services.commandpale
 /* tslint:enable */
 
 export
-interface ICommandPalette extends CommandPalette {}
+interface ICommandPalette {
+  /**
+   * Get the command palette input node.
+   *
+   * #### Notes
+   * This is a read-only property.
+   */
+  inputNode: HTMLInputElement;
+
+  /**
+   * Add a command item to the command palette.
+   *
+   * @param options - The options for creating the command item.
+   *
+   * @returns A disposable that will remove the item from the palette.
+   */
+  add(options: CommandPalette.IItemOptions | CommandPalette.IItemOptions[]): IDisposable;
+}
+
+
+/**
+ * A thin wrapper around the `CommandPalette` class to conform with the
+ * JupyterLab interface for the application-wide command palette.
+ */
+export
+class Palette extends CommandPalette {
+  /**
+   * Add a command item to the command palette.
+   *
+   * @param options - The options for creating the command item.
+   *
+   * @returns A disposable that will remove the item from the palette.
+   */
+  add(options: CommandPalette.IItemOptions | CommandPalette.IItemOptions[]): IDisposable {
+    if (Array.isArray(options)) {
+      let items = options.map(item => super.addItem(item));
+      return new DisposableDelegate(() => {
+        items.forEach(item => super.removeItem(item));
+      });
+    }
+
+    let item = super.addItem(options as CommandPalette.IItemOptions);
+    return new DisposableDelegate(() => this.removeItem(item));
+  }
+}
