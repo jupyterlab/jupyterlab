25d79dcd8f7955941b67bc24cfd8d261d3eb20d7
Afshin Darian <git@darian.af>
2016-08-10 01:37:05 +0100

    Clean up main plugin's  event listener.

--- src/main/plugin.ts
+++ src/main/plugin.ts
@@ -13,11 +13,17 @@ export
 const mainExtension: JupyterLabPlugin<void> = {
   id: 'jupyter.extensions.main',
   activate: (app: JupyterLab) => {
-    window.onbeforeunload = event => {
-      let msg = 'Are you sure you want to exit JupyterLab?';
-      msg += '\nAny unsaved changes will be lost.';
-      return msg;
-    };
+    const message = 'Are you sure you want to exit JupyterLab?\n' +
+                    'Any unsaved changes will be lost.';
+
+    // The spec for the `beforeunload` event is implemented differently by
+    // the different browser vendors. Consequently, the `event.returnValue`
+    // attribute needs to set in addition to a return value being returned.
+    // For more information, see:
+    // https://developer.mozilla.org/en/docs/Web/Events/beforeunload
+    window.addEventListener('beforeunload', event => {
+      return (event as any).returnValue = message;
+    });
   },
   autoStart: true
 };
