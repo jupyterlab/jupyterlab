4a8dbbdbe707a8b7a5d9ee2d48eac8adad6bdccc
Afshin Darian <git@darian.af>
2016-08-11 14:11:41 +0100

    Add focus() method to ICommandPalette interface.

--- src/commandpalette/index.ts
+++ src/commandpalette/index.ts
@@ -44,6 +44,11 @@ interface ICommandPalette {
    * @returns A disposable that will remove the item from the palette.
    */
   addItem(options: IPaletteItem): IDisposable;
+
+  /**
+   * Focus the search input node of the command palette.
+   */
+  focus(): void;
 }
 
 
@@ -58,11 +63,7 @@ class Palette {
   }
 
   /**
-   * Add a command item to the command palette.
-   *
-   * @param options - The options for creating the command item(s).
-   *
-   * @returns A disposable that will remove the item from the palette.
+   * The placeholder text of the command palette's search input.
    */
   set placeholder(placeholder: string) {
     this._palette.inputNode.placeholder = placeholder;
@@ -81,6 +82,14 @@ class Palette {
   addItem(options: IPaletteItem): IDisposable {
     let item = this._palette.addItem(options as CommandPalette.IItemOptions);
     return new DisposableDelegate(() => this._palette.removeItem(item));
+  }
+
+  /**
+   * Focus the search input node of the command palette.
+   */
+  focus(): void {
+    this._palette.inputNode.focus();
+    this._palette.inputNode.select();
   }
 
   private _palette: CommandPalette = null;
