75f46089219f0aebda9bef07ac6574391e48220f
Steven Silvester <steven.silvester@ieee.org>
2016-08-17 12:51:32 -0500

    Run npm install locally first

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -3,6 +3,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+npm install
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
