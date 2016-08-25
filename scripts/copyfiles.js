fed2ebc43694fef6f0e09e86b56299afddf25fe3
Jason Grout <jgrout6@bloomberg.net>
2016-08-25 14:10:33 -0400

    Don’t copy css files since we are now generating them via less.

--- scripts/copyfiles.js
+++ scripts/copyfiles.js
@@ -2,6 +2,5 @@
 // Distributed under the terms of the Modified BSD License.
 
 var fs = require('fs-extra');
-fs.copySync('src/', 'lib/', { filter: /\.css$/ });
 fs.copySync('src/', 'lib/', { filter: /\.svg$/ });
 fs.copySync('src/', 'lib/', { filter: /\.gif$/ });
