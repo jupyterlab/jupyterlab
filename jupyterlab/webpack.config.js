4d55e932a29908f5e8c23a49a73ca136cae78ac1
Steven Silvester <steven.silvester@ieee.org>
2016-08-30 14:01:38 -0500

    Add initial JupyterLab webpack plugin

--- jupyterlab/webpack.config.js
+++ jupyterlab/webpack.config.js
@@ -7,20 +7,58 @@ require('es6-promise').polyfill();
 
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var findImports = require('find-imports');
-
-console.log('Generating config...');
-
-// Get the list of vendor files.
-var VENDOR_FILES = findImports('../lib/**/*.js', { flatten: true });
-
 
 console.log('Generating bundles...');
 
+
+function JupyterLabPlugin(options) {}
+
+JupyterLabPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('emit', function(compilation, callback) {
+    // Create a header string for the generated file:
+    var modlist = ['In this build:\n\n'];
+
+    // Explore each chunk (build output):
+    compilation.chunks.forEach(function(chunk) {
+      // Explore each module within the chunk (built inputs):
+      chunk.modules.forEach(function(module) {
+        if (!module.getAllModuleDependencies) {
+          return;
+        }
+        var source = module.source().source();
+        for (var dep of module.getAllModuleDependencies()) {
+          var id = dep.id;
+          var request = dep.request;
+          // TODO: Mangle the request using package.json
+          var path = 'require("' + request + '")';
+          source = source.replace('__webpack_require__(' + id + ')', path);
+        }
+        var name = module.request;
+        // TODO: Mangle the request using package.json
+        var header = 'define("' + name + '", function (require, exports, module) {\n'
+        source = header + source + '\n}),\n';
+        modlist.push(source);
+      });
+    });
+
+    // Insert this list into the Webpack build as a new file asset:
+    compilation.assets['modlist.md'] = {
+      source: function() {
+        return modlist.join('\n\n');
+      },
+      size: function() {
+        return modlist.join('\n\n').length;
+      }
+    };
+
+    callback();
+  });
+};
+
+
 module.exports = {
   entry: {
-    jupyterlab: './index.js',
-    vendor: VENDOR_FILES
+    jupyterlab: './index.js'
   },
   output: {
     path: __dirname + '/build',
@@ -54,6 +92,6 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin('[name].css'),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+    new JupyterLabPlugin()
   ]
 }
