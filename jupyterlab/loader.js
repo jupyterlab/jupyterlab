34b02f6436957726bbcffa9524f947ddfe40638c
Steven Silvester <steven.silvester@ieee.org>
2016-08-31 17:02:54 -0500

    Finish the loader implementation

--- jupyterlab/loader.js
+++ jupyterlab/loader.js
@@ -2,31 +2,34 @@
 var semver = require('semver');
 
 function loadWithSemver(name, req, onLoad, config) {
-    console.log(semver);
     var modules = Object.keys(requirejs.s.contexts['_'].registry);
     // Get the package name, semver string, and module name.
     var parts = name.match(/(^.*?):(.*?)(\/.*$)/);
     var matches = [];
+    var versions = [];
     for (var mod of modules) {
       var modParts = mod.match(/(^.*?):(.*?)(\/.*$)/);
       if (!modParts) {
-        console.log('skipping', mod);
         continue;
       }
       if (modParts[1] === parts[1] && modParts[3] === parts[3]) {
         matches.push(mod);
+        versions.push(modParts[2]);
       }
     }
+
     if (!matches.length) {
       onLoad.error('No module found matching: ' + name);
-    } else if (matches.length == 1) {
-      requirejs([matches[0]], function(mod) {
-        onLoad(mod);
-      });
-    } else {
-      // Find the best match using semver.
-      console.log('multiple matches for', name);
+      return;
     }
+    var index = 0;
+    if (matches.length > 1) {
+      var best = semver.maxSatisfying(versions, parts[2]);
+      index = versions.indexOf(best);
+    }
+    requirejs([matches[index]], function(mod) {
+      onLoad(mod);
+    });
 };
 
 
