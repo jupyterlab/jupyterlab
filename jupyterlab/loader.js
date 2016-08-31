e75fc9b7efd29c29997bb58e472d059faecc0637
Steven Silvester <steven.silvester@ieee.org>
2016-08-31 16:37:00 -0500

    Add the semver import

--- jupyterlab/loader.js
+++ jupyterlab/loader.js
@@ -1,6 +1,8 @@
 
+var semver = require('semver');
 
 function load(name, req, onLoad, config) {
+    console.log(semver);
     var modules = Object.keys(requirejs.s.contexts['_'].registry);
     console.log(modules);
     console.log(name, req, onLoad, config);
