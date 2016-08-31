21df7592c7ef2ee710cd235644be746a73d3b798
Steven Silvester <steven.silvester@ieee.org>
2016-08-31 16:56:36 -0500

    wip implement module search

--- jupyterlab/loader.js
+++ jupyterlab/loader.js
@@ -1,14 +1,35 @@
 
 var semver = require('semver');
 
-function load(name, req, onLoad, config) {
+function loadWithSemver(name, req, onLoad, config) {
     console.log(semver);
     var modules = Object.keys(requirejs.s.contexts['_'].registry);
-    console.log(modules);
-    console.log(name, req, onLoad, config);
+    // Get the package name, semver string, and module name.
+    var parts = name.match(/(^.*?):(.*?)(\/.*$)/);
+    var matches = [];
+    for (var mod of modules) {
+      var modParts = mod.match(/(^.*?):(.*?)(\/.*$)/);
+      if (!modParts) {
+        console.log('skipping', mod);
+        continue;
+      }
+      if (modParts[1] === parts[1] && modParts[3] === parts[3]) {
+        matches.push(mod);
+      }
+    }
+    if (!matches.length) {
+      onLoad.error('No module found matching: ' + name);
+    } else if (matches.length == 1) {
+      requirejs([matches[0]], function(mod) {
+        onLoad(mod);
+      });
+    } else {
+      // Find the best match using semver.
+      console.log('multiple matches for', name);
+    }
 };
 
 
 module.exports = {
-    load: load
+    load: loadWithSemver
 }
