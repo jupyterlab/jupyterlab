ff2d1c0cc12165c30152bf9565959951db44f3f8
Jason Grout <jgrout6@bloomberg.net>
2016-09-02 10:50:58 -0400

    Clean up examples scripts and add it to clean:slate.

--- scripts/buildexamples.js
+++ scripts/buildexamples.js
@@ -7,14 +7,7 @@ var fs = require('fs');
 // Build all of the example folders.
 dirs = fs.readdirSync('examples');
 
-var cmd;
 for (var i = 0; i < dirs.length; i++) {
-  if (dirs[i].indexOf('.') !== -1) {
-    continue;
-  }
-  if (dirs[i].indexOf('node_modules') !== -1) {
-    continue;
-  }
   console.log('Building: ' + dirs[i] + '...');
   process.chdir('examples/' + dirs[i]);
   childProcess.execSync('npm run update', { stdio: [0, 1, 2] });
--- scripts/cleanexamples.js
+++ scripts/cleanexamples.js
@@ -4,15 +4,13 @@
 var childProcess = require('child_process');
 var fs = require('fs');
 
-childProcess.execSync('rimraf examples/node_modules', { stdio: 'inherit' });
-
 // Clean all of the example folders.
 dirs = fs.readdirSync('examples');
 
 for (var i = 0; i < dirs.length; i++) {
-  if (dirs[i].indexOf('.') !== -1) {
-    continue;
-  }
-  var cmd = 'rimraf examples/' + dirs[i] + '/build';
-  childProcess.execSync(cmd, { stdio: [0, 1, 2] });
+  console.log('Cleaning: ' + dirs[i] + '...');
+  process.chdir('examples/' + dirs[i]);
+  childProcess.execSync('npm run clean', { stdio: [0, 1, 2] });
+  process.chdir('../..');
 }
+console.log('\n********\nDone!');
