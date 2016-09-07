8cc9d9fa4e4708426e4665a462e7eade32eb3a55
Steven Silvester <steven.silvester@ieee.org>
2016-09-07 10:37:58 -0500

    Add entry point validation on the client

--- jupyterlab/index.js
+++ jupyterlab/index.js
@@ -11,4 +11,37 @@ require('material-design-icons/iconfont/material-icons.css');
 
 require('jupyterlab/lib/default-theme/index.css');
 
-module.exports = new JupyterLab();
+
+/**
+ * Test whether a value is a function.
+ */
+function isFunction(obj) {
+    // http://stackoverflow.com/a/6000016.
+    return !!(obj && obj.constructor && obj.call && obj.apply);
+}
+
+/**
+ * Validate an entry point given by the user.
+ */
+function validateEntryPoint(entryPoint) {
+    var data = jupyter.require(entryPoint);
+    if (!Array.isArray(data)) {
+        data = [data];
+    }
+    var plugins = [];
+    for (let i = 0; i < data.length; i++) {
+        var plugin = data[i];
+        if (!plugin.hasOwnProperty('id') || !isFunction(plugin['activate'])) {
+            console.warn('Invalid plugin found in: ', entryPoint);
+            continue;
+        }
+        plugins.push(plugin);
+    }
+    return plugins;
+}
+
+
+jupyter.lab = new JupyterLab();
+jupyter.validateEntryPoint = validateEntryPoint;
+
+module.exports = jupyter.lab;
