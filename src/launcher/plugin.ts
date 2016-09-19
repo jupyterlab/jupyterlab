de5782254a7f592c4575a5f9071a9f709d753a25
Paul Ivanov <pivanov5@bloomberg.net>
2016-09-19 15:09:28 -0400

    modifying list works
    Now I just need to make it also store callbacks, not just names, so that
    re-rendering can apply cleanly...

--- src/launcher/plugin.ts
+++ src/launcher/plugin.ts
@@ -6,6 +6,14 @@ import {
 } from 'phosphor/lib/ui/widget';
 
 import {
+  ObservableList
+} from '../common/observablelist';
+
+import {
+  DisposableDelegate, IDisposable
+} from 'phosphor/lib/core/disposable';
+
+import {
   JupyterLab, JupyterLabPlugin
 } from '../application';
 
@@ -33,6 +41,25 @@ const launcherExtension: JupyterLabPlugin<void> = {
 };
 
 
+// want
+//
+// - LauncherItem
+//   -()
+//      - name
+//      - imageName (can be standardize from name)
+//      - className (can be standardize from name)
+//      - eventListener - 1 (or more?) - 'click' but maybe also 'hover'?
+//
+// - widget container
+//   -()
+//     - an observable list of LauncherItems
+//
+//   - renders its children (adds them to the DOM)
+//   - some API for adding (removing) children)
+// - on activate, each plugin registers itself with the launcher
+// - requires all extensions to depend on the Launcher if they want to register with it...
+//   - what if I given person doesn't want the launcher...
+
 function activateLauncher(app: JupyterLab, services: IServiceManager, pathTracker: IPathTracker, palette: ICommandPalette): void {
   let widget = new Widget();
   widget.id = 'landing-jupyterlab';
@@ -42,7 +69,7 @@ function activateLauncher(app: JupyterLab, services: IServiceManager, pathTracke
   let dialog = document.createElement('div');
   dialog.className = 'jp-Launcher-dialog';
   widget.node.appendChild(dialog);
-  
+
   let path = document.createElement('span');
   path.textContent = 'home';
   pathTracker.pathChanged.connect(() => {
@@ -59,7 +86,7 @@ function activateLauncher(app: JupyterLab, services: IServiceManager, pathTracke
 
   let cwd = document.createElement('div');
   cwd.className = 'jp-Launcher-cwd';
-  
+
   let folderImage = document.createElement('span');
   folderImage.className = 'jp-Launcher-folder';
 
@@ -72,45 +99,72 @@ function activateLauncher(app: JupyterLab, services: IServiceManager, pathTracke
   body.className = 'jp-Launcher-body';
   dialog.appendChild(body);
 
-  for (let name of ['Notebook', 'Notebook2', 'Terminal', 'Text Editor', 'Notebook', 'Console', 'Terminal', 'Text Editor']) {
-    let column = document.createElement('div');
-    body.appendChild(column);
-    column.className = 'jp-Launcher-column';
+  // new ObservableList<string>(
+  let launcher = new Launcher(body);
 
-    let img = document.createElement('span');
-    let imgName = name.replace(' ', '');
-    img.className = `jp-Image${imgName} jp-Launcher-image`;
+  let list : IDisposable[] = [];
+  let l = launcher.addItem('Notebook');
+  list.push(l)
+  l = launcher.addItem('Terminal');
+  list.push(l)
+  l = launcher.addItem('Terminal');
+  list.push(l)
+  l = launcher.addItem('Terminal');
+  list.push(l)
+  l = launcher.addItem('Terminal');
+  list.push(l)
+  l = launcher.addItem('Console');
+  list.push(l)
 
-    column.appendChild(img);
+  l = launcher.addItem('Add Terminal');
+  list.push(l)
 
-    let text = document.createElement('span');
-    text.textContent = name;
-    text.className = 'jp-Launcher-text';
-    column.appendChild(text);
-  }
 
-  let img = body.getElementsByClassName('jp-ImageNotebook')[0];
-  img.addEventListener('click', () => {
-    app.commands.execute('file-operations:new-notebook', void 0);
+  l = launcher.addItem('Remove Terminal');
+  list.push(l)
+
+  launcher.render()
+
+  applyClickCallbacks(launcher.body, 'jp-ImageNotebook jp-Launcher-image', () => { 
+      app.commands.execute('file-operations:new-notebook', void 0);
   });
 
-  img = body.getElementsByClassName('jp-ImageConsole')[0];
-  img.addEventListener('click', () => {
-    app.commands.execute(`console:create-${services.kernelspecs.default}`, void 0);
+  applyClickCallbacks(launcher.body, 'jp-ImageConsole jp-Launcher-image', () => {
+      app.commands.execute(`console:create-${services.kernelspecs.default}`, void 0);
   });
 
-  img = body.getElementsByClassName('jp-ImageTextEditor')[0];
-  img.addEventListener('click', () => {
-    app.commands.execute('file-operations:new-text-file', void 0);
+  applyClickCallbacks(launcher.body, 'jp-ImageTextEditor jp-Launcher-image', () => {
+      app.commands.execute('file-operations:new-text-file', void 0);
+  });
+  
+  applyClickCallbacks(launcher.body, 'jp-ImageTerminal jp-Launcher-image', () => {
+      app.commands.execute('terminal:create-new', void 0);
   });
 
-  img = body.getElementsByClassName('jp-ImageTerminal')[0];
-  img.addEventListener('click', () => {
-    app.commands.execute('terminal:create-new', void 0);
+  applyClickCallbacks(launcher.body, 'jp-ImageRemoveTerminal jp-Launcher-image', () => { 
+      console.log(list);
+      console.log(list.length);
+      list.pop().dispose();
+
+      //launcher.render()
+      // XXX: re-render here
+
+  });
+  applyClickCallbacks(launcher.body, 'jp-ImageAddTerminal jp-Launcher-image', () => { 
+      l = launcher.addItem('Remove Terminal');
+      list.push(l)
   });
 
 
 
+
+  app.commands.addCommand('jupyterlab-launcher:add item', {
+    label: 'Add Launcher Item',
+    execute: () => {
+        launcher.addItem("plusOne");
+    }
+
+  });
 
 
   app.commands.addCommand('jupyterlab-launcher:show', {
@@ -119,7 +173,8 @@ function activateLauncher(app: JupyterLab, services: IServiceManager, pathTracke
       if (!widget.isAttached) {
         app.shell.addToLeftArea(widget);
       } else {
-        app.shell.activateMain(widget.id);
+          app.shell.activateMain(widget.id);
+        console.log("bye" + launcher.strList)
       }
     }
   });
@@ -130,4 +185,68 @@ function activateLauncher(app: JupyterLab, services: IServiceManager, pathTracke
   });
 
   app.shell.addToLeftArea(widget);
+}
+
+function applyClickCallbacks(body: HTMLElement, className: string, callback: () => void) {
+  let nodes = body.getElementsByClassName(className);
+
+  for(let i = 0; i < nodes.length; i++) {
+      nodes[i].addEventListener('click', callback);
+  };
+}
+
+export interface ILauncherItem {
+
+}
+
+class Launcher {
+    constructor(public body: HTMLElement){
+    }
+ 
+    public strList: string[] =  [];
+    //public strList: string[] =  ['Notebook', 'Notebook2', 'Terminal', 'Text Editor', 'Notebook', 'Console', 'Terminal', 'Text Editor'];
+    
+    addItem(options: string): IDisposable {
+        console.log('hello, adding ' + options);
+        this.strList.push(options);
+        return new DisposableDelegate(() => {
+            console.log("removing " + options);
+            // XXX: remove corresponding DOM element
+            this.strList.splice(this.strList.indexOf(options), 1);
+            console.log(this.strList);
+
+            // XXX:re-render the component when this happens...
+            //this.render()
+        });
+    }
+
+    render() {
+
+        // ['Notebook', 'Notebook2', 'Terminal', 'Text Editor', 'Notebook', 'Console', 'Terminal', 'Text Editor']
+        // XXX: hack - remove all child nodes
+        while (this.body.firstChild) {
+            this.body.removeChild(this.body.firstChild);
+        }
+
+        console.log('render called')
+
+        for (let name of this.strList) {
+            let column = document.createElement('div');
+            this.body.appendChild(column);
+            column.className = 'jp-Launcher-column';
+
+            let img = document.createElement('span');
+            let imgName = name.replace(' ', '');
+            img.className = `jp-Image${imgName} jp-Launcher-image`;
+
+            column.appendChild(img);
+
+            let text = document.createElement('span');
+            text.textContent = name;
+            text.className = 'jp-Launcher-text';
+            column.appendChild(text);
+        }
+
+
+    }
 }
