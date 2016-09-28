2520398d576cf048a5a2c088e766fa362201cb6f
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-09-28 18:56:42 +0200

    Remove remnant files

--- test/src/notebook/typings.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../../typings/codemirror/codemirror.d.ts"/>
-/// <reference path="../../../typings/expect.js/expect.js.d.ts"/>
-/// <reference path="../../../typings/mocha/mocha.d.ts"/>
-/// <reference path="../../../typings/es6-promise/es6-promise.d.ts"/>
-/// <reference path="../../../typings/require/require.d.ts"/>
