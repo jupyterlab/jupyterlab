d9b375c31d827aced5298139c491e46fe7efb9a8
Vidar Tonaas Fauske <vidartf@gmail.com>
2016-09-28 20:22:09 +0200

    Workaround loader bug

--- test/src/typings.d.ts
+++ test/src/typings.d.ts
@@ -1,4 +1,8 @@
 /// <reference path="../../typings/codemirror/codemirror.d.ts"/>
+/// <reference types="expect.js" />
+/// <reference types="typescript" />
+/// <reference types="mocha" />
+/// <reference types="requirejs" />
 
 // For a Thenable reference in jupyter-js-services
 type Thenable<T> = Promise<T>
