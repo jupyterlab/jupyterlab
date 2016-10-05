dcfdb233aca26d1bd026342417224ee0e1b600c0
Steven Silvester <steven.silvester@ieee.org>
2016-10-04 19:45:09 -0500

    Finish test infrastructure

--- test/karma-cov.conf.js
+++ test/karma-cov.conf.js
@@ -2,24 +2,25 @@ var path = require('path');
 
 module.exports = function (config) {
   config.set({
-    basePath: '..',
+    basePath: '.',
     browsers: ['Firefox'],
     frameworks: ['mocha'],
     reporters: ['mocha', 'coverage', 'remap-coverage'],
     files: [
       'node_modules/es6-promise/dist/es6-promise.js',
-      'test/build/coverage.js'
+      'build/injector.js',
+      'build/coverage.js'
     ],
     preprocessors: {
-      'test/build/coverage.js': ['sourcemap']
+      'build/coverage.js': ['sourcemap']
     },
     coverageReporter: {
       type: 'in-memory'
     },
     remapCoverageReporter: {
       'text-summary': null, // to show summary in console
-      json: 'test/coverage/remapped.json',
-      html: 'test/coverage/html'
+      json: 'coverage/remapped.json',
+      html: 'coverage/html'
     },
     port: 9876,
     colors: true,
--- test/run-test.py
+++ test/run-test.py
@@ -20,16 +20,12 @@ with open(os.path.join(root_dir, 'src', 'temp.txt'), 'w') as fid:
 
 HERE = os.path.dirname(__file__)
 
-
-KARMA_PORT = 9876
-
-
 shell = (sys.platform == 'win32')
 
 
 def start_notebook():
     nb_command = [sys.executable, '-m', 'notebook', root_dir, '--no-browser',
-                  '--debug', '--NotebookApp.allow_origin="*"']
+                  '--NotebookApp.allow_origin="*"']
     nb_server = subprocess.Popen(nb_command, shell=shell,
                                  stderr=subprocess.STDOUT,
                                  stdout=subprocess.PIPE)
@@ -66,7 +62,7 @@ def start_notebook():
     return nb_server, base_url
 
 
-def run_karma(options, base_url):
+def run_karma(base_url):
     with open(os.path.join(HERE, 'build', 'injector.js'), 'w') as fid:
         fid.write("""
         var node = document.createElement('script');
@@ -76,26 +72,17 @@ def run_karma(options, base_url):
         document.body.appendChild(node);
         """ % base_url)
 
-    cmd = 'karma start --browsers=Firefox karma.conf.js'
-    return subprocess.check_call(cmd.split(), shell=shell,
-                                 stderr=subprocess.STDOUT)
+    cmd = ['karma', 'start'] + sys.argv[1:]
+    return subprocess.check_call(cmd, shell=shell, stderr=subprocess.STDOUT)
 
 
 if __name__ == '__main__':
-    argparser = argparse.ArgumentParser(
-        description='Run Jupyter JS Sevices integration tests'
-    )
-    argparser.add_argument('-b', '--browsers', default='Firefox',
-                           help="Browsers to use for Karma test")
-    argparser.add_argument('-d', '--debug', action='store_true',
-                           help="Whether to enter debug mode in Karma")
-    options = argparser.parse_args(sys.argv[1:])
 
     nb_server, base_url = start_notebook()
 
     try:
-        resp = run_karma(options, base_url)
-    except subprocess.CalledProcessError:
+        resp = run_karma(base_url)
+    except (subprocess.CalledProcessError, KeyboardInterrupt):
         resp = 1
     finally:
         nb_server.kill()
