afb6532d3d2b52ee47ac48f40a547a678e33f37b
Afshin Darian <git@darian.af>
2016-10-05 10:27:48 +0100

    Add more inspector tests.

--- test/src/inspector/inspector.spec.ts
+++ test/src/inspector/inspector.spec.ts
@@ -4,8 +4,25 @@
 import expect = require('expect.js');
 
 import {
+  ISignal, defineSignal
+} from 'phosphor/lib/core/signaling';
+
+import {
   Inspector
 } from '../../../lib/inspector';
+
+
+class TestInspectable implements Inspector.IInspectable {
+  disposed: ISignal<any, void>;
+
+  ephemeralCleared: ISignal<any, void>;
+
+  inspected: ISignal<any, Inspector.IInspectorUpdate>;
+}
+
+defineSignal(TestInspectable.prototype, 'disposed');
+defineSignal(TestInspectable.prototype, 'ephemeralCleared');
+defineSignal(TestInspectable.prototype, 'inspected');
 
 
 describe('inspector/index', () => {
@@ -32,6 +49,29 @@ describe('inspector/index', () => {
         };
         let widget = new Inspector(options);
         expect(widget).to.be.an(Inspector);
+      });
+
+    });
+
+    describe('#source', () => {
+
+      it('should default to `null`', () => {
+        let options: Inspector.IOptions = {};
+        let widget = new Inspector(options);
+        expect(widget.source).to.be(null);
+      });
+
+      it('should be settable multiple times', () => {
+        let options: Inspector.IOptions = {};
+        let widget = new Inspector(options);
+        let source = new TestInspectable();
+        expect(widget.source).to.be(null);
+        widget.source = source;
+        expect(widget.source).to.be(source);
+        widget.source = null;
+        expect(widget.source).to.be(null);
+        widget.source = new TestInspectable();
+        expect(widget.source).to.be.a(TestInspectable);
       });
 
     });
