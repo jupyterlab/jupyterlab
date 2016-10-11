84a81ae64f39ebf37bda675718149111a835e255
Afshin Darian <git@darian.af>
2016-10-11 10:55:26 +0100

    Fix examples build.

--- examples/console/src/typings.d.ts
+++ examples/console/src/typings.d.ts
@@ -1,4 +1,10 @@
 /// <reference path="../../../typings/codemirror/codemirror.d.ts"/>
+/// <reference path="../../../node_modules/@types/mathjax/index.d.ts"/>
+/// <reference path="../../../node_modules/@types/node/index.d.ts"/>
+/// <reference path="../../../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
+/// <reference path="../../../node_modules/typescript/lib/lib.dom.d.ts"/>
+/// <reference path="../../../node_modules/typescript/lib/lib.es5.d.ts"/>
+/// <reference path="../../../node_modules/typescript/lib/lib.es2015.collection.d.ts"/>
 
 // For a Thenable reference in jupyter-js-services
 type Thenable<T> = Promise<T>
