c986baf102e35b026b317ed472d4f9a18571c711
Steven Silvester <steven.silvester@ieee.org>
2016-10-14 09:09:05 -0500

    Update to new services

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -5,11 +5,8 @@
 /*
  * TODO: remove the below typings after typedoc understands the lib compiler option
  * and the @types typing resolution.
- * When this happens, use the typescript compiler option:
- * "lib": ["dom", "es5", "es2015.promise", "es2015.collection"],
  */
 /// <reference path="../node_modules/@types/mathjax/index.d.ts"/>
-/// <reference path="../node_modules/@types/node/index.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.dom.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.es5.d.ts"/>
