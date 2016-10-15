798d09f5b993062164eed0c824a3549af01650ed
Jason Grout <jgrout6@bloomberg.net>
2016-10-15 02:41:46 -0400

    Separate out the typedoc-specific typings.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,13 +1,6 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
 /// <reference path="../typings/ansi_up/ansi_up.d.ts"/>
 /// <reference path="../typings/codemirror/codemirror.d.ts"/>
 /// <reference path="../typings/xterm/xterm.d.ts"/>
-
-/*
- * TODO: remove the below typings after typedoc understands the lib compiler option
- * and the @types typing resolution.
- */
-/// <reference path="../node_modules/@types/mathjax/index.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.dom.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.es5.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.es2015.collection.d.ts"/>
