d75491d9462606ed2a1ea7d2e9d05bb935e90e98
Steven Silvester <steven.silvester@ieee.org>
2016-10-25 13:20:16 -0500

    Use setInterval

--- src/docmanager/savehandler.ts
+++ src/docmanager/savehandler.ts
@@ -106,7 +106,7 @@ class SaveHandler implements IDisposable {
     if (!this._isActive) {
       return;
     }
-    this._autosaveTimer = setTimeout(() => {
+    this._autosaveTimer = setInterval(() => {
       this._save();
     }, this._interval);
   }
