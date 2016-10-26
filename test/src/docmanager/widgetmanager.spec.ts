4e2c7d8b48050dde96cdcafba7ae520e4a3e9972
Steven Silvester <steven.silvester@ieee.org>
2016-10-26 08:56:06 -0500

    initial tests for document widget manager

---
+++ test/src/docmanager/widgetmanager.spec.ts
@@ -0,0 +1,71 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import expect = require('expect.js');
+
+import {
+  DocumentRegistry
+} from '../../../lib/docregistry';
+
+import {
+  DocumentWidgetManager
+} from '../../../lib/docmanager';
+
+
+describe('docmanager/widgetmanager', () => {
+
+  let manager: DocumentWidgetManager;
+
+  beforeEach(() => {
+    manager = new DocumentWidgetManager({ registry: new DocumentRegistry() });
+  });
+
+  describe('DocumentWidgetManager', () => {
+
+    describe('#constructor()', () => {
+
+      it('should create a new document widget manager', () => {
+        expect(manager).to.be.a(DocumentWidgetManager);
+      });
+
+    });
+
+    describe('#createWidget()', () => {
+
+    });
+
+    describe('#adoptWidget()', () => {
+
+    });
+
+    describe('#findWidget()', () => {
+
+    });
+
+    describe('#contextForWidget()', () => {
+
+    });
+
+    describe('#cloneWidget()', () => {
+
+    });
+
+    describe('#closeWidgets()', () => {
+
+    });
+
+    describe('#filterMessage()', () => {
+
+    });
+
+    describe('#setCaption()', () => {
+
+    });
+
+    describe('#onClose()', () => {
+
+    });
+
+  });
+
+});
