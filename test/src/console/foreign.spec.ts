3ef44484960870bf95c338dcffc8ecc25f22d444
Afshin Darian <git@darian.af>
2016-10-27 13:25:19 +0100

    More foreign handler tests.

--- test/src/console/foreign.spec.ts
+++ test/src/console/foreign.spec.ts
@@ -36,6 +36,8 @@ class TestHandler extends ForeignHandler {
 
   readonly injected: ISignal<this, void>;
 
+  readonly rejected: ISignal<this, void>;
+
   dispose(): void {
     if (this.isDisposed) {
       return;
@@ -48,6 +50,13 @@ class TestHandler extends ForeignHandler {
     let injected = super.onIOPubMessage(sender, msg);
     if (injected) {
       this.injected.emit(void 0);
+    } else {
+      // If the message was not injected but otherwise would have been, emit
+      // a rejected signal. This should only happen if `enabled` is `false`.
+      let session = (msg.parent_header as KernelMessage.IHeader).session;
+      if (session !== this.kernel.clientId) {
+        this.rejected.emit(void 0);
+      }
     }
     return injected;
   }
@@ -55,6 +64,7 @@ class TestHandler extends ForeignHandler {
 
 
 defineSignal(TestHandler.prototype, 'injected');
+defineSignal(TestHandler.prototype, 'rejected');
 
 
 const rendermime = defaultRenderMime();
@@ -100,15 +110,55 @@ describe('console/foreign', () => {
 
       it('should allow foreign cells to be injected if `true`', done => {
         let path = utils.uuid();
-        let code = 'print("#enabled")';
+        let code = 'print("#enabled:true")';
         let parent = new Panel();
         Promise.all([
           Session.startNew({ path }), Session.startNew({ path })
         ]).then(([a, b]) => {
           let handler = new TestHandler({ kernel: a.kernel, parent, renderer });
-          handler.injected.connect(() => { done(); });
+          handler.injected.connect(() => {
+            a.dispose();
+            b.dispose();
+            handler.dispose();
+            done();
+          });
           b.kernel.execute({ code, stop_on_error: true });
         }).catch(done);
+      });
+
+      it('should reject foreign cells if `false`', done => {
+        let path = utils.uuid();
+        let code = 'print("#enabled:false")';
+        let parent = new Panel();
+        Promise.all([
+          Session.startNew({ path }), Session.startNew({ path })
+        ]).then(([a, b]) => {
+          let handler = new TestHandler({ kernel: a.kernel, parent, renderer });
+          handler.enabled = false;
+          handler.rejected.connect(() => {
+            a.dispose();
+            b.dispose();
+            handler.dispose();
+            done();
+          });
+          b.kernel.execute({ code, stop_on_error: true });
+        }).catch(done);
+      });
+
+    });
+
+    describe('#isDisposed', () => {
+
+      it('should indicate whether the handler is disposed', () => {
+        let options: ForeignHandler.IOptions = {
+          kernel: null,
+          parent: null,
+          renderer
+        };
+        let handler = new ForeignHandler(options);
+        expect(handler.isDisposed).to.be(false);
+        handler.dispose();
+        expect(handler.isDisposed).to.be(true);
       });
 
     });
