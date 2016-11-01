0a817cb3ba494271c30aec623142f3fdb634fbf9
Afshin Darian <git@darian.af>
2016-11-01 18:57:17 +0000

    Create non-functional initial concrete implementation of command links interface.

--- src/commandlinks/plugin.ts
+++ src/commandlinks/plugin.ts
@@ -4,6 +4,14 @@
 |----------------------------------------------------------------------------*/
 
 import {
+  JSONObject
+} from 'phosphor/lib/algorithm/json';
+
+import {
+  IElementAttrs
+} from 'phosphor/lib/ui/vdom';
+
+import {
   JupyterLab, JupyterLabPlugin
 } from '../application';
 
@@ -28,7 +36,62 @@ const commandLinksProvider: JupyterLabPlugin<ICommandLinks> = {
  * A static class that provides helper methods to generate clickable nodes that
  * execute registered commands with pre-populated arguments.
  */
-class Links {}
+class Links implements ICommandLinks {
+  /**
+   * Connect a command/argument pair to a given node so that when it is clicked,
+   * the command will execute.
+   *
+   * @param node - The node being connected.
+   *
+   * @param command - The command ID to execute upon click.
+   *
+   * @param args - The arguments with which to invoke the command.
+   *
+   * #### Notes
+   * The node instance that is returned is identical to the node instance that
+   * was passed in.
+   */
+  connectNode(node: HTMLElement, command: string, args: JSONObject): HTMLElement {
+    return node;
+  }
+
+  /**
+   * Disconnect a node that has been connected to execute a command on click.
+   *
+   * @param node - The node being disconnected.
+   *
+   * #### Notes
+   * This method is safe to call multiple times and is safe to call on nodes
+   * that were never connected.
+   *
+   * This method can be called on rendered virtual DOM nodes that were populated
+   * using the `populateVNodeAttributes` method in order to disconnect them from
+   * executing their command/argument pair.
+   */
+  disconnectNode(node: HTMLElement): void {
+    /* no op for initial implementation */
+  }
+
+  /**
+   * Populate the attributes used to instantiate a virtual DOM node with the
+   * data set values necessary for its rendered DOM node to respond to clicks by
+   * executing a command/argument pair
+   *
+   * @param attrs - The attributes that will eventually be used to instantiate
+   * a virtual DOM node.
+   *
+   * @param command - The command ID to execute upon click.
+   *
+   * @param args - The arguments with which to invoke the command.
+   *
+   * #### Notes
+   * The attributes instance that is returned is identical to the attributes
+   * instance that was passed in, i.e., this method mutates the original.
+   */
+  populateVNodeAttrs(attrs: IElementAttrs, command: string, args: JSONObject): IElementAttrs {
+    return attrs;
+  }
+}
 
 
 /**
