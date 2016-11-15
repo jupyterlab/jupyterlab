ccac664ccc7bd0ba8e2bb1e5f3c9a867b5de08c6
Afshin Darian <git@darian.af>
2016-11-15 14:05:29 +0000

    work in progress

--- src/statedb/index.ts
+++ src/statedb/index.ts
@@ -2,6 +2,10 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
+  JSONValue
+} from 'phosphor/lib/algorithm/json';
+
+import {
   Token
 } from 'phosphor/lib/core/token';
 
@@ -16,10 +20,37 @@ const IStateDB = new Token<IStateDB>('jupyter.services.statedb');
 
 
 /**
+ * The value that is saved and retrieved from the database.
+ */
+export
+interface ISaveBundle {
+  /**
+   * The identifier used to save retrieve a data bundle.
+   */
+  id: string;
+
+  /**
+   * The actual value being stored or retrieved.
+   */
+  data: JSONValue;
+
+  /**
+   * An optional namespace to help categories saved bundles.
+   *
+   * #### Notes
+   * If a namespace is not provided, the default value will be `'statedb'`. An
+   * example of a namespace value would be a widget type, e.g., `'notebook'` or
+   * `'console'`.
+   */
+  namespace?: string;
+}
+
+
+/**
  * The description of a state database.
  */
 export
 interface IStateDB {
-  fetch(): Promise<void>;
+  fetch(): Promise<ISaveBundle>;
   save(): Promise<void>;
 }
