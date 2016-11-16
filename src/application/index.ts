9c9c716b81c9abfcc973310b382756994ae16d3b
Afshin Darian <git@darian.af>
2016-11-16 20:25:28 +0000

    Add `started` promise to JupyterLab class.

--- src/application/index.ts
+++ src/application/index.ts
@@ -23,9 +23,34 @@ type JupyterLabPlugin<T> = Application.IPlugin<JupyterLab, T>;
 export
 class JupyterLab extends Application<ApplicationShell> {
   /**
+   * Create a new JupyterLab instance.
+   */
+  constructor() {
+    super();
+    this._startedPromise = new Promise(fn => { this._startedResolve = fn; });
+  }
+
+  /**
+   * A promise that resolves when the JupyterLab application is started.
+   */
+  get started(): Promise<void> {
+    return this._startedPromise;
+  }
+
+  /**
+   * Start the JupyterLab application.
+   */
+  start(options: Application.IStartOptions = {}): Promise<void> {
+    return super.start(options).then(() => { this._startedResolve(); });
+  }
+
+  /**
    * Create the application shell for the JupyterLab application.
    */
   protected createShell(): ApplicationShell {
     return new ApplicationShell();
   }
+
+  private _startedResolve: () => void;
+  private _startedPromise: Promise<void> = null;
 }
