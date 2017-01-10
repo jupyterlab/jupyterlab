9d7549e24c07fde6be72c10d8d50caf1a4c04c07
Afshin Darian <git@darian.af>
2017-01-10 18:56:02 +0000

    More tests.

--- test/src/instancerestorer/instancerestorer.spec.ts
+++ test/src/instancerestorer/instancerestorer.spec.ts
@@ -4,8 +4,23 @@
 import expect = require('expect.js');
 
 import {
+  utils
+} from '@jupyterlab/services';
+
+import {
+  CommandRegistry
+} from 'phosphor/lib/ui/commandregistry';
+
+import {
   InstanceRestorer
 } from '../../../lib/instancerestorer/instancerestorer';
+
+import {
+  StateDB
+} from '../../../lib/statedb/statedb';
+
+
+const NAMESPACE = 'jupyterlab-instance-restorer-tests';
 
 
 describe('instancerestorer/instancerestorer', () => {
@@ -15,13 +30,36 @@ describe('instancerestorer/instancerestorer', () => {
     describe('#constructor()', () => {
 
       it('should construct a new instance restorer', () => {
-        let options: InstanceRestorer.IOptions = {
-          first: Promise.resolve(),
-          registry: null,
-          state: null
-        };
-        let restorer = new InstanceRestorer(options);
+        let restorer = new InstanceRestorer({
+          first: Promise.resolve<void>(void 0),
+          registry: new CommandRegistry(),
+          state: new StateDB({ namespace: NAMESPACE })
+        });
         expect(restorer).to.be.an(InstanceRestorer);
+      });
+
+    });
+
+    describe('#restored', () => {
+
+      it('should be a promise available right away', () => {
+        let restorer = new InstanceRestorer({
+          first: Promise.resolve<void>(void 0),
+          registry: new CommandRegistry(),
+          state: new StateDB({ namespace: NAMESPACE })
+        });
+        expect(restorer.restored).to.be.a(Promise);
+      });
+
+      it('should resolve when restorer is done', done => {
+        let ready = new utils.PromiseDelegate<void>();
+        let restorer = new InstanceRestorer({
+          first: ready.promise,
+          registry: new CommandRegistry(),
+          state: new StateDB({ namespace: NAMESPACE })
+        });
+        restorer.restored.then(() => { done(); }).catch(done);
+        ready.resolve(void 0);
       });
 
     });
