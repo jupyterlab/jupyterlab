4313b0d00b8133f235a80b41fb9cd17e2dec2ba1
Steven Silvester <steven.silvester@ieee.org>
2017-01-17 06:56:54 -0600

    Clean up install script

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -4,7 +4,7 @@
 # Distributed under the terms of the Modified BSD License.
 set -ex
 
-npm install
+npm update
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
@@ -12,8 +12,7 @@ hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
-conda install jupyter pytest
-conda install -c conda-forge notebook
+conda install -c conda-forge notebook pytest
 
 # create jupyter base dir (needed for config retreival)
 mkdir ~/.jupyter
