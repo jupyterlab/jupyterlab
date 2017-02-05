23777e88b9d24ae02ad18bd07a0d2087196a308a
Brian E. Granger <ellisonbg@gmail.com>
2017-02-05 13:38:20 -0700

    Open help links in iframes in mainarea.

--- src/help/plugin.ts
+++ src/help/plugin.ts
@@ -139,7 +139,6 @@ export default plugin;
  * returns A promise that resolves when the extension is activated.
  */
 function activate(app: JupyterLab, mainMenu: IMainMenu, palette: ICommandPalette, restorer: IInstanceRestorer): void {
-  let iframe: IFrame = null;
   const category = 'Help';
   const namespace = 'help-doc';
   const command = CommandIDs.open;
@@ -149,19 +148,21 @@ function activate(app: JupyterLab, mainMenu: IMainMenu, palette: ICommandPalette
   // Handle state restoration.
   restorer.restore(tracker, {
     command,
-    args: widget => ({ isHidden: widget.isHidden, url: widget.url }),
-    name: widget => namespace
+    args: widget => ({ url: widget.url }),
+    name: widget => widget.id
   });
 
   /**
    * Create a new IFrame widget.
    */
-  function newIFrame(url: string): IFrame {
+  function newIFrame(url: string, text: string): IFrame {
     let iframe = new IFrame();
     iframe.addClass(HELP_CLASS);
-    iframe.title.label = category;
-    iframe.id = `${namespace}`;
+    iframe.title.label = text;
+    iframe.title.closable = true;
+    iframe.id = `${namespace}-`+text;
     iframe.url = url;
+    tracker.add(iframe);
     return iframe;
   }
 
@@ -179,36 +180,16 @@ function activate(app: JupyterLab, mainMenu: IMainMenu, palette: ICommandPalette
     menu.addItem({ type: 'separator' });
     RESOURCES.forEach(args => { menu.addItem({ args, command }); });
     menu.addItem({ type: 'separator' });
-    menu.addItem({ command: CommandIDs.close });
     menu.addItem({ command: StateDBCommandIDs.clear });
 
     return menu;
-  }
-
-  /**
-   * Show the help widget.
-   */
-  function showHelp(): void {
-    if (!iframe) {
-      return;
-    }
-    app.shell.activateRight(iframe.id);
-  }
-
-  /**
-   * Hide the help widget.
-   */
-  function hideHelp(): void {
-    if (iframe && !iframe.isHidden) {
-      app.shell.collapseRight();
-    }
   }
 
   app.commands.addCommand(command, {
     label: args => args['text'] as string,
     execute: args => {
       const url = args['url'] as string;
-      const isHidden = args['isHidden'] as boolean || false;
+      const text = args['text'] as string;
 
       // If help resource will generate a mixed content error, load externally.
       if (LAB_IS_SECURE && utils.urlParse(url).protocol !== 'https:') {
@@ -216,60 +197,19 @@ function activate(app: JupyterLab, mainMenu: IMainMenu, palette: ICommandPalette
         return;
       }
 
-      if (iframe) {
-        iframe.url = url;
-      } else {
-        iframe = newIFrame(url);
-        tracker.add(iframe);
-      }
-
-      tracker.save(iframe);
-
-      if (!iframe.isAttached) {
-        app.shell.addToRightArea(iframe);
-      }
-
-      if (isHidden) {
-        hideHelp();
-      } else {
-        showHelp();
-      }
+      let iframe = newIFrame(url, text);
+      app.shell.addToMainArea(iframe);
+      app.shell.activateMain(iframe.id);
     }
   });
 
-  app.commands.addCommand(CommandIDs.close, {
-    label: 'Close Help',
-    execute: () => {
-      if (iframe) {
-        iframe.dispose();
-        iframe = null;
-      }
-    }
-  });
-
-  app.commands.addCommand(CommandIDs.toggle, {
-    execute: () => {
-      if (!iframe) {
-        return;
-      }
-      if (iframe.isHidden) {
-        showHelp();
-      } else {
-        hideHelp();
-      }
-    }
-  });
 
   app.commands.addCommand(CommandIDs.launchClassic, {
     label: 'Launch Classic Notebook',
     execute: () => { window.open(utils.getBaseUrl() + 'tree'); }
   });
 
-  app.commands.addCommand(CommandIDs.show, { execute: showHelp });
-  app.commands.addCommand(CommandIDs.hide, { execute: hideHelp });
-
   RESOURCES.forEach(args => { palette.addItem({ args, command, category }); });
-  palette.addItem({ command: CommandIDs.close, category });
   palette.addItem({ command: StateDBCommandIDs.clear, category });
   palette.addItem({ command: CommandIDs.launchClassic, category });
 
