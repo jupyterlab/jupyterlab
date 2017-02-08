a800b5fb735adb91398ec63653384a768c196478
Steven Silvester <steven.silvester@ieee.org>
2017-02-08 07:31:29 -0600

    Attempt to update node modules on travis

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -4,6 +4,7 @@
 # Distributed under the terms of the Modified BSD License.
 set -x
 
+npm install
 npm update
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
