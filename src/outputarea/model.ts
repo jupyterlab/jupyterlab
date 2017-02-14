ae126660df9d7bc8b2cfbe92ac850a0e4d9d5f6b
Steven Silvester <steven.silvester@ieee.org>
2017-02-14 04:50:47 -0600

    Finish output area model refactor

--- src/outputarea/model.ts
+++ src/outputarea/model.ts
@@ -6,6 +6,14 @@ import {
 } from '@jupyterlab/services';
 
 import {
+  each, toArray
+} from 'phosphor/lib/algorithm/iteration';
+
+import {
+  JSONObject
+} from 'phosphor/lib/algorithm/json';
+
+import {
   IDisposable
 } from 'phosphor/lib/core/disposable';
 
@@ -30,11 +38,8 @@ interface IOutputAreaModel extends IDisposable {
 
   /**
    * A signal emitted when a value in one of the outputs changes.
-   *
-   * #### Notes
-   * The payload is the index of the item that changed.
    */
-  readonly itemChanged: ISignal<IOutputAreaModel, number>;
+  readonly itemChanged: ISignal<IOutputAreaModel, void>;
 
   /**
    * A signal emitted when the model is disposed.
@@ -75,7 +80,7 @@ interface IOutputAreaModel extends IDisposable {
   /**
    * Serialize the model to JSON.
    */
-  toJSON(): nbformat.IOutput;
+  toJSON(): nbformat.IOutput[];
 }
 
 
@@ -99,11 +104,8 @@ class OutputAreaModel implements IOutputAreaModel {
 
   /**
    * A signal emitted when a value in one of the outputs changes.
-   *
-   * #### Notes
-   * The payload is the index of the item that changed.
    */
-  readonly itemChanged: ISignal<IOutputAreaModel, number>;
+  readonly itemChanged: ISignal<IOutputAreaModel, void>;
 
   /**
    * A signal emitted when the model is disposed.
@@ -115,6 +117,33 @@ class OutputAreaModel implements IOutputAreaModel {
    */
   get length(): number {
     return this.list ? this.list.length : 0;
+  }
+
+  /**
+   * Get whether the model is trusted.
+   */
+  get trusted(): boolean {
+    return this._trusted;
+  }
+
+  /**
+   * Set whether the model is trusted.
+   *
+   * #### Notes
+   * Changing the value will cause all of the models to re-set.
+   */
+  set trusted(value: boolean) {
+    if (value === this._trusted) {
+      return;
+    }
+    let trusted = this._trusted = value;
+    for (let i = 0; i < this.list.length, i++) {
+      let item = this.list.at(i);
+      let output = item.toJSON();
+      item.dispose();
+      item = this._createItem({ output, trusted });
+      this.set(i, item);
+    }
   }
 
   /**
@@ -141,7 +170,7 @@ class OutputAreaModel implements IOutputAreaModel {
   /**
    * Get an item at the specified index.
    */
-  get(index: number): OutputAreaModel.Output {
+  get(index: number): OutputAreaModel.IOutput {
     return this.list.at(index);
   }
 
@@ -152,51 +181,42 @@ class OutputAreaModel implements IOutputAreaModel {
    * The output bundle is copied.
    * Contiguous stream outputs of the same `name` are combined.
    */
-  add(output: OutputAreaModel.Output): number {
+  add(output: nbformat.IOutput): number {
     // If we received a delayed clear message, then clear now.
     if (this.clearNext) {
       this.clear();
       this.clearNext = false;
     }
-    if (output.output_type === 'input_request') {
-      this.list.pushBack(output);
-    }
 
     // Make a copy of the output bundle.
-    let value = JSON.parse(JSON.stringify(output)) as nbformat.IOutput;
-
-    // Join multiline text outputs.
-    if (value.output_type === 'stream') {
-      if (Array.isArray(value.text)) {
-        value.text = (value.text as string[]).join('\n');
-      }
-    }
+    output = JSON.parse(JSON.stringify(output)) as nbformat.IOutput;
+    let trusted = this._trusted;
 
     // Consolidate outputs if they are stream outputs of the same kind.
-    let index = this.length - 1;
-    let lastOutput = this.get(index);
-    if (value.output_type === 'stream'
-        && lastOutput && lastOutput.output_type === 'stream'
-        && value.name === lastOutput.name) {
+    if (output.output_type === 'stream' && this._lastStream &&
+        output.name === this._lastName) {
       // In order to get a list change event, we add the previous
       // text to the current item and replace the previous item.
       // This also replaces the metadata of the last item.
-      let text = value.text as string;
-      value.text = lastOutput.text as string + text;
-      this.list.set(index, value);
+      this._lastStream += output.text;
+      output.text = this._lastStream;
+      let item = this._createItem({ output, trusted });
+      let index = this.length - 1;
+      this.list.set(index, item);
       return index;
-    } else {
-      switch (value.output_type) {
-      case 'stream':
-      case 'execute_result':
-      case 'display_data':
-      case 'error':
-        return this.list.pushBack(value);
-      default:
-        break;
-      }
     }
-    return -1;
+
+    // Update the stream information.
+    if (item.output_type === 'stream') {
+      this._lastStream = output.text;
+      this._lastName = output.name;
+    } else {
+      this._lastStream = '';
+    }
+
+    // Create the new item and add it to our list.
+    let item = this._createItem({ output, trusted });
+    return this.list.pushBack(item);
   }
 
   /**
@@ -209,11 +229,29 @@ class OutputAreaModel implements IOutputAreaModel {
       this.clearNext = true;
       return;
     }
+    each(this.list => item => { item.dispose(); });
     this.list.clear();
+  }
+
+  /**
+   * Serialize the model to JSON.
+   */
+  toJSON(): nbformat.IOutput[] {
+    return toArray(each(this.list, output => output.toJSON() ));
   }
 
   protected clearNext = false;
   protected list: IObservableVector<OutputAreaModel.Output> = null;
+
+  /**
+   * Create an output item and hook up its signals.
+   */
+  private _createItem(options: OutputAreaModel.IOutputOptions): OutputAreaModel.IOutput {
+    let item = new OutputAreaModel.Output(options);
+    item.changed.connect(this._onItemChanged);
+    item.metadata.changed.connect(this._onItemChanged);
+    return item;
+  }
 
   /**
    * Handle a change to the list.
@@ -221,6 +259,10 @@ class OutputAreaModel implements IOutputAreaModel {
   private _onListChanged(sender: IObservableVector<OutputAreaModel.IOutput>, args: ObservableVector.IChangedArgs<OutputAreaModel.Output>) {
     this.changed.emit(args);
   }
+
+  private _lastStream: string;
+  private _lastName: 'stdout' | 'stderr';
+  private _trusted;
 }
 
 
@@ -229,15 +271,122 @@ class OutputAreaModel implements IOutputAreaModel {
  */
 export
 namespace OutputAreaModel {
-
   /**
-   * A valid output area item.
+   * The interface for an element of the output area.
    */
   export
-  type Output = nbformat.IOutput | IInputRequest;
+  interface IOutput extends RenderMime.IMimeBundle {
+    /**
+     * The output type.
+     */
+    readonly type: nbformat.OutputType;
+
+    /**
+     * Serialize the model to JSON.
+     */
+    toJSON(): nbformat.IOutput;
+  }
+
+  /**
+   * The default implementation of an output model.
+   */
+  export
+  class Output extends RenderMime.MimeBundle implements IOutput {
+    /**
+     * Construct a new IOutput.
+     */
+    constructor(options: IOutputOptions) {
+      super(Private.getBundleOptions(options));
+      let output = this._output = options.output;
+      this.type = output.output_type;
+      // Remove redundant data.
+      switch (output.output_type) {
+      case 'display_data':
+      case 'execute_result':
+        output.data = Object.create(null);
+        output.metadata = Object.create(null);
+        break;
+      case 'text':
+        output.text = '';
+        break;
+      default:
+        break;
+      }
+    }
+
+    /**
+     * The output type.
+     */
+    readonly type: nbformat.OutputType;
+
+    /**
+     * Serialize the model to JSON.
+     */
+    toJSON(): nbformat.IOutput {
+      let output = JSON.parse(JSON.stringify(this._output)) as nbformat.IOutput;
+      switch (output.output_type) {
+      case 'display_data':
+      case 'execute_result':
+        for (key in self.keys()) {
+          output.data[key] = self.get(key);
+        }
+        for (key in self.metadata.keys) {
+          output.metadata[key] = self.metadata.get(key);
+        }
+        break;
+      case 'stream':
+        output.text = self.get(RenderMime.CONSOLE_MIMETYPE);
+        break;
+      default:
+        break;
+      }
+      return output;
+    }
+
+    private _output: nbformat.IOutput;
+  }
+
+  /**
+   * The options used to create an IOutput.
+   */
+  export
+  interface IOutputOptions {
+    /**
+     * The original output.
+     */
+    output: nbformat.IOutput;
+
+    /**
+     * The parsed mime bundle.
+     */
+    data: JSONObject;
+
+    /**
+     * Whether the output is trusted.
+     */
+    trusted: boolean;
+  }
 }
 
 
 // Define the signals for the `OutputAreaModel` class.
 defineSignal(OutputAreaModel.prototype, 'changed');
+defineSignal(OutputAreaModel.prototype, 'itemChanged');
 defineSignal(OutputAreaModel.prototype, 'disposed');
+
+
+/**
+ * The namespace for module private data.
+ */
+namespace Private {
+  /**
+   * Get the bundle options given IOutputOptions.
+   */
+  export
+  function getBundleOptions(options: OutputAreaModel.IOptions): RenderMime.IMimeBundleOPtions {
+    let data = RenderMime.getData(options.output);
+    let metadata = RenderMime.getMetadata(options.output);
+    let trusted = data.trusted;
+    return { data, trusted, metadata };
+  }
+}
