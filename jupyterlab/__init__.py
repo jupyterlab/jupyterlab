853490b9d89b6e72d3b3f0e83fa2edb17d684547
Steven Silvester <steven.silvester@ieee.org>
2017-02-25 07:10:13 -0600

    Fix jupyterlab import error message

--- jupyterlab/__init__.py
+++ jupyterlab/__init__.py
@@ -7,7 +7,7 @@ try:
     from ._version import __version__
 except ImportError as e:
     # when we are python 3 only, add 'from e' at the end to chain the exception.
-    raise ImportError("No module named 'jupyter._version'. Build the jupyterlab package to generate this module, for example, with `pip install -e /path/to/jupyterlab/repo`.")
+    raise ImportError("No module named 'jupyterlab._version'. Build the jupyterlab package to generate this module, for example, with `pip install -e /path/to/jupyterlab/repo`.")
 
 from .extension import load_jupyter_server_extension
 
