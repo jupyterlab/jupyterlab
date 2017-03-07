fd188833c403f669d5a2d9946c4200ac35951aeb
Steven Silvester <steven.silvester@ieee.org>
2017-03-07 05:22:10 -0600

    Use selector in cell type and cleanups

--- src/common/styling.ts
+++ src/common/styling.ts
@@ -3,20 +3,30 @@
 
 /**
  * Style a node and its child elements with the default tag names.
+ *
+ * @param node - The base node.
+ *
+ * @param className - The optional CSS class to add to styled nodes.
  */
 export
-function styleNode(node: HTMLElement): void {
-  styleNodeByTag(node, 'select');
-  styleNodeByTag(node, 'input');
-  styleNodeByTag(node, 'button');
+function styleNode(node: HTMLElement, className=''): void {
+  styleNodeByTag(node, 'select', className);
+  styleNodeByTag(node, 'input', className);
+  styleNodeByTag(node, 'button', className);
 }
 
 
 /**
  * Style a node and its elements that have a given tag name.
+ *
+ * @param node - The base node.
+ *
+ * @param tagName - The html tag name to style.
+ *
+ * @param className - The optional CSS class to add to styled nodes.
  */
 export
-function styleNodeByTag(node: HTMLElement, tagName: string): void {
+function styleNodeByTag(node: HTMLElement, tagName: string, className=''): void {
   if (node.localName === tagName) {
     node.classList.add('jp-mod-styled');
   }
@@ -24,6 +34,9 @@ function styleNodeByTag(node: HTMLElement, tagName: string): void {
   for (let i = 0; i < nodes.length; i++) {
     let child = nodes[i];
     child.classList.add('jp-mod-styled');
+    if (className) {
+      child.classList.add(className);
+    }
     if (tagName === 'select') {
       let wrapper = document.createElement('div');
       wrapper.classList.add('jp-select-wrapper');
--- src/notebook/default-toolbar.ts
+++ src/notebook/default-toolbar.ts
@@ -18,6 +18,10 @@ import {
 } from './actions';
 
 import {
+  styleNode
+} from '../common/styling';
+
+import {
   NotebookPanel
 } from './panel';
 
@@ -220,6 +224,7 @@ class CellTypeSwitcher extends Widget {
     this.addClass(TOOLBAR_CELLTYPE_CLASS);
 
     this._select = this.node.firstChild as HTMLSelectElement;
+    styleNode(this.node);
     this._wildCard = document.createElement('option');
     this._wildCard.value = '-';
     this._wildCard.textContent = '-';
