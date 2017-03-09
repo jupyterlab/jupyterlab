cd793be697fdfa28f094b9cc2c1e654fc9bfb08a
Steven Silvester <steven.silvester@ieee.org>
2017-03-09 04:17:54 -0600

    Vendor a which function

--- setupbase.py
+++ setupbase.py
@@ -197,7 +197,7 @@ def install_npm(path=None, build_dir=None, source_dir=None, build_cmd='build'):
             node_package = path or here
             node_modules = pjoin(node_package, 'node_modules')
 
-            if not find_executable("npm"):
+            if not which("npm"):
                 log.error("`npm` unavailable.  If you're running this command "
                           "using sudo, make sure `npm` is availble to sudo")
                 return
@@ -263,52 +263,44 @@ class bdist_egg_disabled(bdist_egg):
                  " to install from source.")
 
 
-def find_executable(cmd, mode=os.F_OK | os.X_OK, path=None):
+# Everything below this point has been copied verbatim from the Python-3.3
+# sources.
+def which(cmd, mode=os.F_OK | os.X_OK, path=None):
     """Given a command, mode, and a PATH string, return the path which
     conforms to the given mode on the PATH, or None if there is no such
     file.
-
     `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
     of os.environ.get("PATH"), or can be overridden with a custom search
     path.
-
     """
+
     # Check that a given file can be accessed with the correct mode.
     # Additionally check that `file` is not a directory, as on Windows
     # directories pass the os.access check.
     def _access_check(fn, mode):
-        return (os.path.exists(fn) and os.access(fn, mode)
-                and not os.path.isdir(fn))
+        return (os.path.exists(fn) and os.access(fn, mode) and
+                not os.path.isdir(fn))
 
-    # If we're given a path with a directory part, look it up directly rather
-    # than referring to PATH directories. This includes checking relative to the
-    # current directory, e.g. ./script
-    if os.path.dirname(cmd):
-        if _access_check(cmd, mode):
-            return cmd
-        return None
+    # Short circuit. If we're given a full path which matches the mode
+    # and it exists, we're done here.
+    if _access_check(cmd, mode):
+        return cmd
 
-    if path is None:
-        path = os.environ.get("PATH", os.defpath)
-    if not path:
-        return None
-    path = path.split(os.pathsep)
+    path = (path or os.environ.get("PATH", os.defpath)).split(os.pathsep)
 
     if sys.platform == "win32":
         # The current directory takes precedence on Windows.
-        if not os.curdir in path:
+        if os.curdir not in path:
             path.insert(0, os.curdir)
 
         # PATHEXT is necessary to check on Windows.
         pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
         # See if the given file matches any of the expected path extensions.
         # This will allow us to short circuit when given "python.exe".
+        matches = [cmd for ext in pathext if cmd.lower().endswith(ext.lower())]
         # If it does match, only test that one, otherwise we have to try
         # others.
-        if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
-            files = [cmd]
-        else:
-            files = [cmd + ext for ext in pathext]
+        files = [cmd] if matches else [cmd + ext.lower() for ext in pathext]
     else:
         # On other platforms you don't have things like PATHEXT to tell you
         # what file suffixes are executable, so just pass on cmd as-is.
@@ -316,9 +308,9 @@ def find_executable(cmd, mode=os.F_OK | os.X_OK, path=None):
 
     seen = set()
     for dir in path:
-        normdir = os.path.normcase(dir)
-        if not normdir in seen:
-            seen.add(normdir)
+        dir = os.path.normcase(dir)
+        if dir not in seen:
+            seen.add(dir)
             for thefile in files:
                 name = os.path.join(dir, thefile)
                 if _access_check(name, mode):
