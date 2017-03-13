82cac42449e4618e1954a54aacd293639db2dd9f
Steven Silvester <steven.silvester@ieee.org>
2017-03-13 06:44:54 -0500

    Update the console plugin

--- src/console/index.ts
+++ src/console/index.ts
@@ -15,46 +15,6 @@ export * from './widget';
 
 
 /**
- * The command IDs used by the console plugin.
- */
-export
-namespace CommandIDs {
-  export
-  const create: string = 'console:create';
-
-  export
-  const clear: string = 'console:clear';
-
-  export
-  const run: string = 'console:run';
-
-  export
-  const runForced: string = 'console:run-forced';
-
-  export
-  const linebreak: string = 'console:linebreak';
-
-  export
-  const interrupt: string = 'console:interrupt-kernel';
-
-  export
-  const restart: string = 'console:restart-kernel';
-
-  export
-  const closeAndShutdown: string = 'console:close-and-shutdown';
-
-  export
-  const open: string = 'console:open';
-
-  export
-  const inject: string = 'console:inject';
-
-  export
-  const switchKernel: string = 'console:switch-kernel';
-};
-
-
-/**
  * The arguments used to create a console.
  */
 export
