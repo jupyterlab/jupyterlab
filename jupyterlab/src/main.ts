e59f97da89bc61d3a3194425e6c60ed9497683fe
Steven Silvester <steven.silvester@ieee.org>
2017-03-16 07:17:38 -0500

    Update main package

--- jupyterlab/src/main.ts
+++ jupyterlab/src/main.ts
@@ -7,10 +7,10 @@ import {
 
 import {
   JupyterLab, ModuleLoader
-} from '../../lib/application';
+} from '@jupyterlab/application';
 
 import 'font-awesome/css/font-awesome.min.css';
-import '../../lib/default-theme/index.css';
+import '@jupyterlab/default-theme/style/index.css';
 
 
 polyfill();
@@ -18,31 +18,31 @@ polyfill();
 
 /* tslint:disable */
 const mods: JupyterLab.IPluginModule[] = [
-  require('../../lib/about-extension'),
-  require('../../lib/application-extension'),
-  require('../../lib/apputils-extension'),
-  require('../../lib/codemirror-extension'),
-  require('../../lib/completer-extension'),
-  require('../../lib/console-extension'),
-  require('../../lib/csvwidget-extension'),
-  require('../../lib/docmanager-extension'),
-  require('../../lib/docregistry-extension'),
-  require('../../lib/editorwidget-extension'),
-  require('../../lib/faq-extension'),
-  require('../../lib/filebrowser-extension'),
-  require('../../lib/help-extension'),
-  require('../../lib/imagewidget-extension'),
-  require('../../lib/inspector-extension'),
-  require('../../lib/landing-extension'),
-  require('../../lib/launcher-extension'),
-  require('../../lib/markdownwidget-extension'),
-  require('../../lib/notebook-extension'),
-  require('../../lib/rendermime-extension'),
-  require('../../lib/running-extension'),
-  require('../../lib/services-extension'),
-  require('../../lib/shortcuts-extension'),
-  require('../../lib/terminal-extension'),
-  require('../../lib/tooltip-extension')
+  require('@jupyterlab/about-extension'),
+  require('@jupyterlab/application-extension'),
+  require('@jupyterlab/apputils-extension'),
+  require('@jupyterlab/codemirror-extension'),
+  require('@jupyterlab/completer-extension'),
+  require('@jupyterlab/console-extension'),
+  require('@jupyterlab/csvwidget-extension'),
+  require('@jupyterlab/docmanager-extension'),
+  require('@jupyterlab/docregistry-extension'),
+  require('@jupyterlab/editorwidget-extension'),
+  require('@jupyterlab/faq-extension'),
+  require('@jupyterlab/filebrowser-extension'),
+  require('@jupyterlab/help-extension'),
+  require('@jupyterlab/imagewidget-extension'),
+  require('@jupyterlab/inspector-extension'),
+  require('@jupyterlab/landing-extension'),
+  require('@jupyterlab/launcher-extension'),
+  require('@jupyterlab/markdownwidget-extension'),
+  require('@jupyterlab/notebook-extension'),
+  require('@jupyterlab/rendermime-extension'),
+  require('@jupyterlab/running-extension'),
+  require('@jupyterlab/services-extension'),
+  require('@jupyterlab/shortcuts-extension'),
+  require('@jupyterlab/terminal-extension'),
+  require('@jupyterlab/tooltip-extension')
 ];
 /* tslint:enable */
 
