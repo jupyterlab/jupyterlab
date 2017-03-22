6930423cb582b1d0c42b40b1637fb0a44da6a69b
Steven Silvester <steven.silvester@ieee.org>
2017-03-22 08:05:01 -0500

    wip parse deps script

--- jupyterlab/parse_deps.py
+++ jupyterlab/parse_deps.py
@@ -3,55 +3,70 @@ import json
 from distutils.version import LooseVersion
 from semver import max_satisfying, satisfies
 
-# Get the data.
-with open('depdata.json') as fid:
-    data = dict(json.load(fid))
+
+def handle_deps(data):
+    # Check for duplicates of packages.
+    seen = dict()
+    dupes = dict()
+
+    for (key, value) in data.items():
+        name = value['name']
+        if name in seen:
+            if name not in dupes:
+                dupes[name] = [seen[name]]
+            dupes[name].append(key)
+        else:
+            seen[name] = key
+
+    # For each dupe check for overlapping semver.
+    to_remove = []
+    for (key, value) in dupes.items():
+        to_remove.extend(handle_dupe(data, key, value))
+
+    print('We should remove %s' % to_remove)
 
 
-# Check for duplicates of packages.
-seen = dict()
-dupes = dict()
-
-for (key, value) in data.items():
-    name = value['name']
-    if name in seen:
-        if name not in dupes:
-            dupes[name] = [seen[name]]
-        dupes[name].append(key)
-    else:
-        seen[name] = key
-
-
-# For each dupe check for overlapping semver.
-replacements = dict()
-for dupe in dupes:
+def handle_dupe(data, name, dupes):
     deps = dict()
     best = dict()
     sats = defaultdict(list)
-    versions = [data[k]['version'] for k in dupes[dupe]]
+    versions = [data[k]['version'] for k in dupes]
     # sort the versions in decreasing order
     versions.sort(key=LooseVersion)
+    to_remove = []
 
     # Check the version compatibility of each dupe.
     for (key, value) in data.items():
-        if dupe in value['dependencies']:
-            deps[key] = value['dependencies'][dupe]
+        if name in value['dependencies']:
+            deps[key] = value['dependencies'][name]
             best[key] = max_satisfying(versions, deps[key], False)
             for version in versions:
                 sats[version].append(satisfies(version, deps[key], False))
 
     # Prefer all best being the same.
     if all(best.values()) == list(best.values())[0]:
-        break
+        return to_remove
+
     # Then find the best of the remaining overlap.
-    good = []
     for version in versions:
         if all(sats[version]):
             # handle the replacements
             for dep in deps:
-                replacements[dep] = dupe + '@' + version
-            break
+                if data[dep]['version'] == version:
+                    continue
+                to_remove.append(name + '@' + version)
+            return to_remove
 
     # If there is no overlap, make sure it is not marked as a singleton.
     # TODO: handle singleton data.
-    raise ValueError('"%s" must be a singleton' % dupe)
+    print('*** "%s" must be a singleton' % name)
+
+    return to_remove
+
+
+if __name__ == '__main__':
+    # Get the data.
+    with open('depdata.json') as fid:
+        data = dict(json.load(fid))
+
+    handle_deps(data)
