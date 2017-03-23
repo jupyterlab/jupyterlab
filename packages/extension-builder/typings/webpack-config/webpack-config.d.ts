f9c821578d543f25995d82c1ae6f1c624ea56a1d
Steven Silvester <steven.silvester@ieee.org>
2017-03-23 05:13:22 -0500

    absorb extension-builder source

---
+++ packages/extension-builder/typings/webpack-config/webpack-config.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for webpack-config
+// Project: https://github.com/mdreizin/webpack-config
+// Definitions by: Steven Silvester <https://github.com/blink1073>
+
+declare module 'webpack-config' {
+  export class Config {
+    merge(value: any): any;
+  }
+}
