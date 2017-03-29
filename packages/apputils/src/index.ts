7f0822f457e2c9f78dbccbfc83bdcd855d5bfbda
Steven Silvester <steven.silvester@ieee.org>
2017-03-29 06:40:34 -0500

    Remove the restart kernel dialog

--- packages/apputils/src/index.ts
+++ packages/apputils/src/index.ts
@@ -12,7 +12,6 @@ export * from './iframe';
 export * from './instancetracker';
 export * from './layoutrestorer';
 export * from './mainmenu';
-export * from './restartkernel';
 export * from './sanitizer';
 export * from './statedb';
 export * from './styling';
