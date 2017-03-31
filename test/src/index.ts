1c5cd98302046c0ef2c958c26c50cdaa876b97bd
Steven Silvester <steven.silvester@ieee.org>
2017-03-31 15:34:25 -0500

    Reinstate test index

--- test/src/index.ts
+++ test/src/index.ts
@@ -3,25 +3,94 @@
 
 import '@phosphor/widgets/style/index.css';
 
+import './application/loader.spec';
+import './application/shell.spec';
+
 import './apputils/clientsession.spec';
+import './apputils/commandlinker.spec';
+import './apputils/dialog.spec';
+import './apputils/iframe.spec';
+import './apputils/instancetracker.spec';
+import './apputils/jsoneditor.spec';
+import './apputils/layoutrestorer.spec';
+import './apputils/sanitizer.spec';
+import './apputils/statedb.spec';
+import './apputils/styling.spec';
 import './apputils/toolbar.spec';
+import './apputils/vdom.spec';
+
+import './cells/model.spec';
 import './cells/widget.spec';
+
+import './codeeditor/editor.spec';
+import './codeeditor/widget.spec';
+
+import './codemirror/editor.spec';
+import './codemirror/factory.spec';
+
 import './completer/handler.spec';
+import './completer/model.spec';
+import './completer/widget.spec';
+
 import './console/foreign.spec';
 import './console/history.spec';
 import './console/panel.spec';
 import './console/widget.spec';
+
+import './coreutils/activitymonitor.spec';
+import './coreutils/nbformat.spec';
+import './coreutils/observablejson.spec';
+import './coreutils/observablemap.spec';
+import './coreutils/observablestring.spec';
+import './coreutils/observablevector.spec';
+import './coreutils/path.spec';
+import './coreutils/time.spec';
 import './coreutils/url.spec';
+import './coreutils/uuid.spec';
+import './coreutils/undoablevector.spec';
+
+import './csvwidget/table.spec';
+import './csvwidget/toolbar.spec';
+import './csvwidget/widget.spec';
 
 import './docmanager/manager.spec';
 import './docmanager/savehandler.spec';
+import './docmanager/widgetmanager.spec';
 
 import './docregistry/context.spec';
+import './docregistry/default.spec';
 import './docregistry/registry.spec';
+
+import './editorwidget/widget.spec';
+
+import './filebrowser/crumbs.spec';
+import './filebrowser/model.spec';
+
+import './imagewidget/widget.spec';
+
+import './inspector/inspector.spec';
+
+import './markdownwidget/widget.spec';
+
+import './renderers/renderers.spec';
+import './renderers/latex.spec';
+
+import './rendermime/mimemodel.spec';
+import './rendermime/outputmodel.spec';
+import './rendermime/rendermime.spec';
+
 import './notebook/actions.spec';
 import './notebook/celltools.spec';
 import './notebook/default-toolbar.spec';
+import './notebook/model.spec';
+import './notebook/modelfactory.spec';
 import './notebook/panel.spec';
+import './notebook/trust.spec';
+import './notebook/widget.spec';
 import './notebook/widgetfactory.spec';
 import './notebook/tracker.spec';
+
+import './outputarea/model.spec';
 import './outputarea/widget.spec';
+
+import './terminal/terminal.spec';
