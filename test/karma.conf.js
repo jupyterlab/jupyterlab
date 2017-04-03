7677e2199303d6110d95ccaab921e48a9049bc49
Steven Silvester <steven.silvester@ieee.org>
2017-04-03 14:31:11 -0500

    Change the mocha timeout to 5sec

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
     reporters: ['mocha'],
     client: {
       mocha: {
-        timeout : 10000, // 10 seconds - upped from 2 seconds
+        timeout : 5000, // 5 seconds - upped from 2 seconds
         retries: 3 // Allow for slow server on CI.
       }
     },
