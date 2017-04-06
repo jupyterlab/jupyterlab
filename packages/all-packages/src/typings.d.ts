4264f8e43bffce058b8a06de55c334c876d3e4c7
Steven Silvester <steven.silvester@ieee.org>
2017-04-06 10:40:15 -0500

    make a meta-package that builds all

---
+++ packages/all-packages/src/typings.d.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+/// <reference path="../../codemirror/typings/codemirror/codemirror.d.ts"/>
+/// <reference path="../../coreutils/typings/path-posix/path-posix.d.ts"/>
+/// <reference path="../../coreutils/typings/url-parse/url-parse.d.ts"/>
+/// <reference path="../../rendermime/typings/ansi_up/ansi_up.d.ts"/>
+/// <reference path="../../terminal/typings/xterm/xterm.d.ts"/>
