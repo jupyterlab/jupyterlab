cf9a818918677cb2789ea24ec89cfe9d5eab9e32
alexandercbooth <alexander.c.booth@gmail.com>
2017-04-06 23:54:23 -0400

    fix(ipyMimeType): adding syntax highlighting for ipy extension

--- packages/codemirror/src/mimetype.ts
+++ packages/codemirror/src/mimetype.ts
@@ -51,6 +51,9 @@ class CodeMirrorMimeTypeService implements IEditorMimeTypeService {
    * If a mime type cannot be found returns the defaul mime type `text/plain`, never `null`.
    */
   getMimeTypeByFilePath(path: string): string {
+    if (path.split('.').slice(-1)[0] === 'ipy') {
+      return 'text/x-python';
+    }
     const mode = CodeMirror.findModeByFileName(path);
     return mode ? mode.mime : IEditorMimeTypeService.defaultMimeType;
   }
