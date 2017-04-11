a96ee5400bdcffed1c1a8f2edceedf634d0438db
Steven Silvester <steven.silvester@ieee.org>
2017-04-11 14:44:19 -0500

    wip filling in entry points

--- jupyterlab/entries.py
+++ jupyterlab/entries.py
@@ -14,6 +14,7 @@ from jupyter_core.paths import ENV_JUPYTER_PATH, ENV_CONFIG_PATH
 here = osp.dirname(osp.abspath(__file__))
 build_dir = osp.join(ENV_JUPYTER_PATH, 'lab')
 cache_dir = osp.join(build_dir, 'cache')
+pkg_path = osp.join(build_dir, 'package.json')
 config_dir = osp.join(ENV_CONFIG_PATH, 'labconfig')
 
 
@@ -32,13 +33,17 @@ def install(extension):
 def uninstall(extension):
     """Uninstall an extension by name.
     """
-    pass
+    pkg = _read_package()
+    del pkg['dependencies'][extension]
+    with open(pkg_path, 'w') as fid:
+        json.dump(pkg, fid)
 
 
 def list(extension):
     """List installed extensions.
     """
-    pass
+    pkg = _read_package()
+    print(list(pkg['dependencies'].keys()))
 
 
 def validate(extension):
@@ -46,6 +51,7 @@ def validate(extension):
     """
     if osp.exists(extension):
         extension = osp.abspath(extension)
+    _ensure_package()
     # npm pack the extension
     name = check_output(['npm', 'pack', extension], cwd=cache_dir)
     # read the package.json data from the file
@@ -72,7 +78,10 @@ def link(package):
 
 def build():
     """Build the JupyterLab application."""
-    pass
+    _ensure_package()
+    # Template and copy the index.js
+    # TODO
+    check_call(['npm', 'run', 'build'], cwd=build_dir)
 
 
 def set_config(name, value):
@@ -93,3 +102,21 @@ def list_config():
 def delete_config():
     """Deletes the key from the JupyterLab configuration."""
     pass
+
+
+def _ensure_package():
+    """Make sure there is a package.json file."""
+    # TODO copy src/package.json
+    # TODO copy src/webpack.config.js
+    pkg_data = dict(name='jupyterlab_app', private=True, dependencies=dict())
+    if not osp.exists(pkg_path):
+        with open(pkg_path, 'w') as fid:
+            json.dump(pkg_data, fid)
+
+
+def _read_package():
+    """Read the JupyterLab package.json data.
+    """
+    _ensure_package()
+    with open(pkg_path) as fid:
+        return json.load(fid)
