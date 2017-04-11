c683c14e88077da82598d1cd2d595eb2e96209ae
Afshin Darian <git@darian.af>
2017-04-11 20:06:56 +0100

    Populate tab bar.

--- packages/tabmanager-extension/src/index.ts
+++ packages/tabmanager-extension/src/index.ts
@@ -6,6 +6,10 @@ import {
 } from '@jupyterlab/application';
 
 import {
+  each
+} from '@phosphor/algorithm';
+
+import {
   TabBar
 } from '@phosphor/widgets';
 
@@ -17,9 +21,14 @@ const plugin: JupyterLabPlugin<void> = {
   id: 'jupyter.extensions.tab-manager',
   activate: (app: JupyterLab): void => {
     const tabs = new TabBar({ orientation: 'vertical' });
+    const populate = () => {
+      each(app.shell.widgets('main'), widget => { tabs.addTab(widget.title); });
+    };
 
     tabs.id = 'tab-manager';
     tabs.title.label = 'Tabs';
+    populate();
+    app.shell.currentChanged.connect(populate);
     app.shell.addToLeftArea(tabs, { rank: 600 });
   },
   autoStart: true
