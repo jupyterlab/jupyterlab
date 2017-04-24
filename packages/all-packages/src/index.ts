92dce1b23ca2e88ad334d179ffc87a1308713888
ian-r-rose <ian.r.rose@gmail.com>
2017-04-24 09:26:05 -0700

    Add to lerna build.

--- packages/all-packages/src/index.ts
+++ packages/all-packages/src/index.ts
@@ -6,6 +6,8 @@ import "@jupyterlab/application";
 import "@jupyterlab/application-extension";
 import "@jupyterlab/apputils";
 import "@jupyterlab/apputils-extension";
+import "@jupyterlab/chatbox";
+import "@jupyterlab/chatbox-extension";
 import "@jupyterlab/cells";
 import "@jupyterlab/codeeditor";
 import "@jupyterlab/codemirror";
