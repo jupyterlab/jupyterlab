a6fdc23f6b4e91868ab9f4f5f1f22bb2e2a7831a
Afshin Darian <git@darian.af>
2017-04-25 19:02:53 +0100

    Switch from `let` to `var` to support older versions of node.

--- scripts/watch-filter.js
+++ scripts/watch-filter.js
@@ -1,6 +1,6 @@
 var path = require('path');
 
 module.exports = function(f, stat) {
-    let parts = f.split(path.sep);
+    var parts = f.split(path.sep);
     return parts.indexOf('src') !== -1 || parts.indexOf('style') !== -1;
 }
