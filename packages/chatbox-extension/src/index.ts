2f2ac47005361b6818e791061f51a3b4da5fe608
ian-r-rose <ian.r.rose@gmail.com>
2017-04-25 10:18:08 -0700

    Remove instance tracker logic.

--- packages/chatbox-extension/src/index.ts
+++ packages/chatbox-extension/src/index.ts
@@ -2,20 +2,11 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
-  JSONObject
-} from '@phosphor/coreutils';
-
-import {
-  Menu
-} from '@phosphor/widgets';
-
-import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
 import {
-  ICommandPalette, InstanceTracker, ILayoutRestorer,
-  IMainMenu
+  ICommandPalette, ILayoutRestorer
 } from '@jupyterlab/apputils';
 
 import {
@@ -23,12 +14,8 @@ import {
 } from '@jupyterlab/codeeditor';
 
 import {
-  IChatboxTracker, ChatboxPanel
+  ChatboxPanel
 } from '@jupyterlab/chatbox';
-
-import {
-  ILauncher
-} from '@jupyterlab/launcher';
 
 import {
   IRenderMime
@@ -53,124 +40,53 @@ namespace CommandIDs {
 };
 
 /**
- * The chatbox widget tracker provider.
- */
-export
-const trackerPlugin: JupyterLabPlugin<IChatboxTracker> = {
-  id: 'jupyter.services.chatbox-tracker',
-  provides: IChatboxTracker,
-  requires: [
-    IRenderMime,
-    IMainMenu,
-    ICommandPalette,
-    ChatboxPanel.IContentFactory,
-    IEditorServices,
-    ILayoutRestorer
-  ],
-  optional: [ILauncher],
-  activate: activateChatbox,
-  autoStart: true
-};
-
-
-/**
  * The chatbox widget content factory.
  */
 export
-const contentFactoryPlugin: JupyterLabPlugin<ChatboxPanel.IContentFactory> = {
-  id: 'jupyter.services.chatbox-renderer',
-  provides: ChatboxPanel.IContentFactory,
-  requires: [IEditorServices],
+const chatboxPlugin: JupyterLabPlugin<void> = {
+  id: 'jupyter.extensions.chatbox',
+  requires: [IRenderMime, ICommandPalette, IEditorServices, ILayoutRestorer],
   autoStart: true,
-  activate: (app: JupyterLab, editorServices: IEditorServices) => {
-    let editorFactory = editorServices.factoryService.newInlineEditor.bind(
-      editorServices.factoryService);
-    return new ChatboxPanel.ContentFactory({ editorFactory });
-  }
-};
+  activate: activateChatbox
+}
 
 
 /**
- * Export the plugins as the default.
+ * Export the plugin as the default.
  */
-const plugins: JupyterLabPlugin<any>[] = [contentFactoryPlugin, trackerPlugin];
-export default plugins;
+export default chatboxPlugin;
 
 
 /**
  * Activate the chatbox extension.
  */
-function activateChatbox(app: JupyterLab, rendermime: IRenderMime, mainMenu: IMainMenu, palette: ICommandPalette, contentFactory: ChatboxPanel.IContentFactory,  editorServices: IEditorServices, restorer: ILayoutRestorer, launcher: ILauncher | null): IChatboxTracker {
+function activateChatbox(app: JupyterLab, rendermime: IRenderMime, palette: ICommandPalette, editorServices: IEditorServices, restorer: ILayoutRestorer): void {
   let { commands, shell } = app;
   let category = 'Chatbox';
   let command: string;
-  let menu = new Menu({ commands });
-
-  // Create an instance tracker for all chatbox panels.
-  const tracker = new InstanceTracker<ChatboxPanel>({
-    namespace: 'chatbox',
-    shell
-  });
-
-  // Add a launcher item if the launcher is available.
-  if (launcher) {
-    launcher.add({
-      name: 'Chatbox',
-      command: CommandIDs.create
-    });
-  }
-
-  // Set the main menu title.
-  menu.title.label = category;
 
   /**
    * Create a chatbox for a given path.
    */
-  function createChatbox(options: Partial<ChatboxPanel.IOptions>): void {
-    let panel = new ChatboxPanel({
-      rendermime: rendermime.clone(),
-      contentFactory,
-      mimeTypeService: editorServices.mimeTypeService,
-      ...options
-    });
-
-    // Add the chatbox panel to the tracker.
-    panel.title.label = 'Chatbox';
-    tracker.add(panel);
-    shell.addToLeftArea(panel);
-    tracker.activate(panel);
-  }
-
-  command = CommandIDs.create;
-  commands.addCommand(command, {
-    label: 'Start New Chatbox',
-    execute: (args: Partial<ChatboxPanel.IOptions>) => {
-      let basePath = args.basePath || '.';
-      createChatbox({ basePath, ...args });
-      return Promise.resolve(void 0);
-    }
+  let editorFactory = editorServices.factoryService.newInlineEditor.bind(
+    editorServices.factoryService);
+  let contentFactory = new ChatboxPanel.ContentFactory({ editorFactory });
+  let panel = new ChatboxPanel({
+    rendermime: rendermime.clone(),
+    contentFactory,
+    mimeTypeService: editorServices.mimeTypeService
   });
-  palette.addItem({ command, category });
 
-  // Get the current widget and activate unless the args specify otherwise.
-  function getCurrent(args: JSONObject): ChatboxPanel | null {
-    let widget = tracker.currentWidget;
-    let activate = args['activate'] !== false;
-    if (activate && widget) {
-      tracker.activate(widget);
-    }
-    return widget;
-  }
+  // Add the chatbox panel to the tracker.
+  panel.title.label = 'Chat';
+  shell.addToLeftArea(panel);
+
 
   command = CommandIDs.clear;
   commands.addCommand(command, {
     label: 'Clear Cells',
     execute: args => {
-      let current = getCurrent(args);
-      if (!current) {
-        return;
-      }
-      current.chatbox.clear();
+      panel.chatbox.clear();
     }
   });
   palette.addItem({ command, category });
@@ -179,11 +95,7 @@ function activateChatbox(app: JupyterLab, rendermime: IRenderMime, mainMenu: IMa
   commands.addCommand(command, {
     label: 'Run Cell',
     execute: args => {
-      let current = getCurrent(args);
-      if (!current) {
-        return;
-      }
-      return current.chatbox.execute();
+      return panel.chatbox.execute();
     }
   });
   palette.addItem({ command, category });
@@ -192,21 +104,8 @@ function activateChatbox(app: JupyterLab, rendermime: IRenderMime, mainMenu: IMa
   commands.addCommand(command, {
     label: 'Insert Line Break',
     execute: args => {
-      let current = getCurrent(args);
-      if (!current) {
-        return;
-      }
-      current.chatbox.insertLinebreak();
+      panel.chatbox.insertLinebreak();
     }
   });
   palette.addItem({ command, category });
-
-  menu.addItem({ command: CommandIDs.run });
-  menu.addItem({ command: CommandIDs.linebreak });
-  menu.addItem({ type: 'separator' });
-  menu.addItem({ command: CommandIDs.clear });
-  menu.addItem({ type: 'separator' });
-
-  mainMenu.addMenu(menu, {rank: 50});
-  return tracker;
 }
