54979fcb1e75336f236fdc0e9ddeb87f82c6ecb6
Steven Silvester <steven.silvester@ieee.org>
2017-05-01 07:49:25 -0500

    Test against python 2 and 3

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -5,7 +5,15 @@
 set -x
 
 npm update
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+
+if [[ $GROUP == tests ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+
+if [[ $GROUP == coverage ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
