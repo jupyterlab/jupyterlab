ed12930e3f7b3aa81083d1cbdc69821506f04e0d
Steven Silvester <steven.silvester@ieee.org>
2017-05-01 16:03:50 -0500

    Use canUndo and canRedo state

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -819,7 +819,10 @@ function addCommands(app: JupyterLab, services: IServiceManager, tracker: Notebo
       }
       return NotebookActions.undo(current.notebook);
     },
-    isEnabled: hasWidget
+    isEnabled: () => {
+      let current = tracker.currentWidget;
+      return current && current.notebook.model.cells.canUndo;
+    }
   });
   commands.addCommand(CommandIDs.redo, {
     label: 'Redo Cell Operation',
@@ -830,7 +833,10 @@ function addCommands(app: JupyterLab, services: IServiceManager, tracker: Notebo
       }
       return NotebookActions.redo(current.notebook);
     },
-    isEnabled: hasWidget
+    isEnabled: () => {
+      let current = tracker.currentWidget;
+      return current && current.notebook.model.cells.canRedo;
+    }
   });
   commands.addCommand(CommandIDs.switchKernel, {
     label: 'Switch Kernel',
