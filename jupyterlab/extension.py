3c11804c4c459923e537232f55f5578db8444341
Steven Silvester <steven.silvester@ieee.org>
2017-05-05 10:16:31 -0500

    Add handling of dev dir and core mode

--- jupyterlab/extension.py
+++ jupyterlab/extension.py
@@ -5,7 +5,6 @@
 # Distributed under the terms of the Modified BSD License.
 import os
 
-from jinja2 import FileSystemLoader
 from jupyterlab_launcher import add_handlers, LabConfig
 
 from .commands import _get_config_dir, _get_runtime_dir
@@ -25,16 +24,18 @@ system incrementally watch and build JupyterLab's TypeScript for you, as you
 make changes.
 """
 
-HERE = os.path.dirname(__file__)
-FILE_LOADER = FileSystemLoader(HERE)
-URL = '/lab'
+
+CORE_NOTE = """
+Running the core application with no additional extensions or settings
+"""
 
 
 def load_jupyter_server_extension(nbapp):
     """Load the JupyterLab server extension.
     """
     # Print messages.
-    nbapp.log.info('JupyterLab alpha preview extension loaded from %s' % HERE)
+    here = os.path.dirname(__file__)
+    nbapp.log.info('JupyterLab alpha preview extension loaded from %s' % here)
 
     web_app = nbapp.web_app
     config = LabConfig()
@@ -42,25 +43,40 @@ def load_jupyter_server_extension(nbapp):
     config.settings_dir = _get_config_dir()
     config.page_title = 'JupyterLab Alpha Preview'
     config.name = 'JupyterLab'
-    config.page_url = URL
+    config.page_url = '/lab'
     config.version = __version__
 
-    # Check for dev mode.
-    dev_mode = False
-    if hasattr(nbapp, 'dev_mode'):
-        dev_mode = nbapp.dev_mode
+    # Check for dev dir.
+    dev_dir = ''
+    if hasattr(nbapp, 'dev_dir'):
+        dev_dir = nbapp.dev_dir
 
-    if dev_mode:
+    if dev_dir:
         nbapp.log.info(DEV_NOTE_NPM)
-        config.assets_dir = os.path.join(HERE, 'build')
+        config.assets_dir = os.path.join(here, 'build')
         config.settings_dir = ''
         config.dev_mode = True
 
-    if not dev_mode and not os.path.exists(config.settings_dir):
-        config.assets_dir = os.path.join(HERE, 'static')
+        add_handlers(web_app, config)
+        return
+
+    # Check for explicit core mode.
+    core_mode = False
+    if hasattr(nbapp, 'core_mode'):
+        core_mode = nbapp.core_mode
+
+    # Run core mode if explicit or there is no settings directory.
+    if core_mode or not os.path.exists(config.settings_dir):
+        config.assets_dir = os.path.join(here, 'static', 'build')
         if not os.path.exists(config.assets_dir):
             msg = 'Static assets not built, please see CONTRIBUTING.md'
-            raise ValueError(msg)
-        config.settings_dir = ''
+            nbapp.log.error(msg)
+        else:
+            nbapp.log.info(CORE_NOTE.strip())
+
+    from PyQt5.QtCore import pyqtRemoveInputHook; pyqtRemoveInputHook()
+    import ipdb; ipdb.set_trace()
+    pass
+    
 
     add_handlers(web_app, config)
--- jupyterlab/labapp.py
+++ jupyterlab/labapp.py
@@ -4,7 +4,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-from notebook.notebookapp import NotebookApp, flags
+from notebook.notebookapp import NotebookApp, aliases, flags
 from jupyter_core.application import JupyterApp
 
 from traitlets import Bool, Unicode
@@ -30,19 +30,15 @@ class LabCleanApp(JupyterApp):
         clean()
 
 
-class LabDescribeApp(JupyterApp):
-    version = __version__
-    description = "Git description the JupyterLab application"
-
-    def start(self):
-        print(describe())
-
-
 lab_flags = dict(flags)
-lab_flags['dev-mode'] = (
-    {'LabApp': {'dev_mode': True}},
-    "Start the app in dev mode."
+lab_flags['core-mode'] = (
+    {'LabApp': {'core_mode': True}},
+    "Start the app in core mode."
 )
+
+
+lab_aliases = dict(aliases)
+lab_aliases['dev-dir'] = 'LabApp.dev_dir'
 
 
 class LabApp(NotebookApp):
@@ -60,19 +56,22 @@ class LabApp(NotebookApp):
         jupyter lab --certfile=mycert.pem # use SSL/TLS certificate
     """
 
+    aliases = lab_aliases
     flags = lab_flags
 
     subcommands = dict(
         build=(LabBuildApp, LabBuildApp.description.splitlines()[0]),
-        clean=(LabCleanApp, LabCleanApp.description.splitlines()[0]),
-        describe=(LabDescribeApp, LabBuildApp.description.splitlines()[0])
+        clean=(LabCleanApp, LabCleanApp.description.splitlines()[0])
     )
 
     default_url = Unicode('/lab', config=True,
         help="The default URL to redirect to from `/`")
 
-    dev_mode = Bool(False, config=True,
-        help="Whether to start the app in dev mode")
+    dev_dir = Unicode('', config=True,
+        help="The dev directory to launch")
+
+    core_mode = Bool(False, config=True,
+        help="Whether to start the app in core mode")
 
     def init_server_extensions(self):
         """Load any extensions specified by config.
@@ -90,9 +89,6 @@ class LabApp(NotebookApp):
         if not self.nbserver_extensions.get('jupyterlab', False):
             self.log.warn(msg)
             load_jupyter_server_extension(self)
-
-    def write_default_config(self):
-        print('hi hi')
 
 
 #-----------------------------------------------------------------------------
