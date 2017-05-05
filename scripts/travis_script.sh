a5fa9d1d2fcdd0c912c3d56a4f14b85632911727
Steven Silvester <steven.silvester@ieee.org>
2017-05-05 08:17:38 -0500

    fix docs script

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -42,10 +42,11 @@ if [[ $GROUP == coverage_and_docs ]]; then
     npm run build:test
     npm run coverage
 
-    npm run docs
-
     # Run the link check
     pip install -q pytest-check-links
     py.test --check-links -k .md .
+
+    # Build the api docs
+    npm run docs
 fi
 
