85f1f33e44bc5f2b2df03256ce61c2c3af52646f
Brian E. Granger <ellisonbg@gmail.com>
2017-05-11 18:17:41 -0400

    Theme refactor.

--- packages/notebook/src/index.ts
+++ packages/notebook/src/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+import '../style/index.css';
+
 export * from './actions';
 export * from './celltools';
 export * from './default-toolbar';
---
+++ packages/theme-light-extension/src/index.ts
@@ -0,0 +1,18 @@
+import {
+  JupyterLabPlugin
+} from '@jupyterlab/application';
+
+import '../style/index.css';
+
+/**
+ * Initialization data for the {{ cookiecutter.extension_name }} extension.
+ */
+const extension: JupyterLabPlugin<void> = {
+  id: 'jupyter.extensions.theme-light,
+  autoStart: true,
+  activate: (app) => {
+    console.log('Light theme is activated!');
+  }
+};
+
+export default extension;
