794958431e2a8212e2863352f6ea38a8ce512315
Vineet Gorhe <vineet.gorhe@twosigma.com>
2017-05-11 17:50:19 -0400

    #1357 Respect markdown code boundaries for code execution
    - moved into coreutils package
    - added tests

--- packages/fileeditor-extension/src/index.ts
+++ packages/fileeditor-extension/src/index.ts
@@ -29,6 +29,10 @@ import {
   ILauncher
 } from '@jupyterlab/launcher';
 
+import {
+   MarkdownCodeBlocks
+} from '@jupyterlab/coreutils'
+
 
 /**
  * The class name for the text editor icon from the default theme.
@@ -95,21 +99,6 @@ function activate(app: JupyterLab, registry: IDocumentRegistry, restorer: ILayou
     namespace: 'editor',
     shell
   });
-
-  const markdownMarkers: string[] = ["```", "~~~~", "`"]
-  const markdownExtensions: string[] = [
-    '.markdown',
-    '.mdown',
-    '.mkdn',
-    '.md',
-    '.mkd',
-    '.mdwn',
-    '.mdtxt',
-    '.mdtext',
-    '.text',
-    '.txt',
-    '.Rmd'
-  ];
 
   let lineNumbers = true;
   let wordWrap = true;
@@ -189,80 +178,6 @@ function activate(app: JupyterLab, registry: IDocumentRegistry, restorer: ILayou
     return tracker.currentWidget !== null;
   }
 
-
-  class CodeSnippet {
-    startLine: number;
-    endLine: number;
-    code: string;
-    constructor(startLine: number) {
-      this.startLine = startLine;
-      this.code = "";
-    }
-  }
-
-  function findNextMarker(text: string) {
-    for (let marker of markdownMarkers) {
-      const index = text.indexOf(marker);
-      if (index > -1) {
-        return marker;
-      }
-    }
-    return '';
-  }
-  /**
-   * Construct all code snippets from current text
-   * (this could be potentially optimized if we can cache and detect differences)
-   */
-  function findCodeSnippets(): CodeSnippet[] {
-    let widget = tracker.currentWidget;
-    if (!widget) {
-      return [];
-    }
-    const lines = widget.editor.model.value.text.split("\n");
-    const codeSnippets: CodeSnippet[] = [];
-    var currentCode = null;
-    for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
-      const line = lines[lineIndex];
-      const marker = findNextMarker(line);
-      const lineContainsMarker = marker != '';
-      const constructingSnippet = currentCode != null;
-      //skip this line if it is not part of any code snippet and doesn't contain a marker
-      if (!lineContainsMarker && !constructingSnippet) {
-        continue;
-      }
-
-      //check if we are already constructing a code snippet
-      if (!constructingSnippet) {
-        //start constructing
-        currentCode = new CodeSnippet(lineIndex);
-
-        //check whether this is a single line code snippet
-        const firstIndex = line.indexOf(marker);
-        const lastIndex = line.lastIndexOf(marker);
-        const isSingleLine = firstIndex != lastIndex
-        if (isSingleLine) {
-          currentCode.code = line.substring(firstIndex + marker.length, lastIndex);
-          currentCode.endLine = lineIndex;
-          codeSnippets.push(currentCode);
-          currentCode = null;
-        } else {
-          currentCode.code = line.substring(firstIndex + marker.length);
-        } 
-      } else {
-        //already constructing
-        if (lineContainsMarker) {
-          currentCode.code += "\n" + line.substring(0, line.indexOf(marker));
-          currentCode.endLine = lineIndex;
-          codeSnippets.push(currentCode);
-          currentCode = null;
-        } else {
-          currentCode.code += "\n" + line;
-        }
-      }
-    }
-    return codeSnippets;
-  }
-
   /** To detect if there is no current selection */
   function hasSelection(): boolean {
     let widget = tracker.currentWidget;
@@ -319,12 +234,12 @@ function activate(app: JupyterLab, registry: IDocumentRegistry, restorer: ILayou
       var code = ""
       const editor = widget.editor;
       const extension = widget.context.path.substring(widget.context.path.lastIndexOf("."));
-      if (!hasSelection() && markdownExtensions.indexOf(extension) > -1) {
-        var snippets = findCodeSnippets();
-        for (let codeSnippet of snippets) {
-          if (codeSnippet.startLine <= editor.getSelection().start.line &&
-            editor.getSelection().start.line <= codeSnippet.endLine) {
-            code = codeSnippet.code;
+      if (!hasSelection() && MarkdownCodeBlocks.isMarkdown(extension)) {
+        var codeBlocks = MarkdownCodeBlocks.findMarkdownCodeBlocks(editor.model.value.text);
+        for (let codeBlock of codeBlocks) {
+          if (codeBlock.startLine <= editor.getSelection().start.line &&
+            editor.getSelection().start.line <= codeBlock.endLine) {
+            code = codeBlock.code;
             break;
           }
         }
