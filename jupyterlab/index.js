e348d86893cd30e506e57a9b27a62bc5a30bf4ad
Brian E. Granger <ellisonbg@gmail.com>
2017-05-23 21:32:56 -0700

    Updating dep on theme.

--- jupyterlab/index.js
+++ jupyterlab/index.js
@@ -1,6 +1,6 @@
 require('es6-promise/auto');  // polyfill Promise on IE'
 require('font-awesome/css/font-awesome.min.css');
-require('@jupyterlab/default-theme/style/index.css');
+require('@jupyterlab/theme-assets/style/index.css');
 
 var app = require('@jupyterlab/application').JupyterLab;
 var PageConfig = require('@jupyterlab/coreutils').PageConfig;
