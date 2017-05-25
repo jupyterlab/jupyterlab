cb91f912699cf4c833ba39ae0b2f5bbb5a299fa0
Steven Silvester <steven.silvester@ieee.org>
2017-05-25 07:00:17 -0500

    Add extension metadata and cleanup

--- packages/theme-dark-extension/src/index.ts
+++ packages/theme-dark-extension/src/index.ts
@@ -3,18 +3,19 @@ import {
 } from '@jupyterlab/application';
 
 // All themes need to import the theme assets index.css.
-import '@jupyterlab/theme-assets/style/index.css'
+import '@jupyterlab/theme-assets/style/index.css';
 
 import '../style/index.css';
 
+
 /**
- * Initialization data for the {{ cookiecutter.extension_name }} extension.
+ * Initialization data for the dark theme extension.
  */
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyter.themes.dark',
   autoStart: true,
   activate: (app) => {
-    console.log('Dark theme is activated!');
+    // No-op
   }
 };
 
--- packages/theme-light-extension/src/index.ts
+++ packages/theme-light-extension/src/index.ts
@@ -3,18 +3,19 @@ import {
 } from '@jupyterlab/application';
 
 // All themes need to import the theme assets index.css.
-import '@jupyterlab/theme-assets/style/index.css'
+import '@jupyterlab/theme-assets/style/index.css';
 
 import '../style/index.css';
 
+
 /**
- * Initialization data for the {{ cookiecutter.extension_name }} extension.
+ * Initialization data for the light theme extension.
  */
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyter.themes.light',
   autoStart: true,
   activate: (app) => {
-    console.log('Light theme is activated!');
+    // No-op.
   }
 };
 
