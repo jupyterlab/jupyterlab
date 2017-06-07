50ce30c7bb8df25ddc7cd90b0910863dbbda8075
Brian E. Granger <ellisonbg@gmail.com>
2017-06-07 15:41:13 -0700

    Fixing tests.

--- test/src/markdownviewer/widget.spec.ts
+++ test/src/markdownviewer/widget.spec.ts
@@ -114,14 +114,14 @@ describe('markdownviewer/widget', () => {
         MessageLoop.sendMessage(widget, Widget.Msg.UpdateRequest);
 
         let layout = widget.layout as PanelLayout;
-        let oldChild = layout.widgets[0];
+        let oldChild = layout.widgets[1];
 
         MessageLoop.sendMessage(widget, Widget.Msg.UpdateRequest);
 
-        let newChild = layout.widgets[0];
+        let newChild = layout.widgets[1];
 
         expect(oldChild).to.not.be(newChild);
-        expect(layout.widgets.length).to.be(1);
+        expect(layout.widgets.length).to.be(2);
         widget.dispose();
       });
 
