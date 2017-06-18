a56d59ab100cbaf8a8874fd4f62c46ba91e791fb
Brian E. Granger <ellisonbg@gmail.com>
2017-06-18 16:50:47 -0700

    Document the collapser.

--- packages/cells/src/collapser.tsx
+++ packages/cells/src/collapser.tsx
@@ -16,37 +16,72 @@ import {
 } from './widget';
 
 
+/**
+ * The CSS class added to all collapsers.
+ */
 const COLLAPSER_CLASS = 'jp-Collapser';
 
+/**
+ * The CSS class added to the collapser child.
+ */
 const COLLAPSER_CHILD_CLASS = 'jp-Collapser-child';
 
+/**
+ * The CSS class added to the collapser icon.
+ */
 const COLLAPSER_ICON_CLASS = 'jp-Collapser-icon';
 
+/**
+ * The CSS class added to input collapsers.
+ */
 const INPUT_COLLAPSER = 'jp-InputCollapser';
 
+/**
+ * The CSS class added to output collapsers.
+ */
 const OUTPUT_COLLAPSER = 'jp-OutputCollapser';
 
+/**
+ * The CSS class added the collapser icon in the collapsed state.
+ */
 const COLLAPSED_ICON_CLASS = 'jp-ExpandMoreIcon';
 
+/**
+ * The CSS class added the collapser icon in the expanded state.
+ */
 const EXPANDED_ICON_CLASS = 'jp-ExpandLessIcon';
 
+/**
+ * The CSS class added the collapser child when collapsed.
+ */
 const MOD_COLLAPSED_CLASS = 'jp-mod-collapsed';
 
 
 /**
- * Default implementation of the collapser.
+ * Abstract collapser base class.
+ * 
+ * 
  */
 export
 abstract class Collapser extends VDomRenderer<null> {
+  /**
+   * Construct a new collapser.
+   */
   constructor() {
     super();
     this.addClass(COLLAPSER_CLASS);
   }
 
+  /**
+   * Is the input/output of the parent collapsed.
+   */
   get collapsed(): boolean {
     return false;
   }
 
+  /**
+   * Render the collapser with the virtual DOM.
+   */
   protected render(): VirtualNode | ReadonlyArray<VirtualNode> {
     let childClass = COLLAPSER_CHILD_CLASS;
     let iconClass = COLLAPSER_ICON_CLASS;
@@ -63,17 +98,29 @@ abstract class Collapser extends VDomRenderer<null> {
     );
   }
 
+  /**
+   * Handle the click event.
+   */
   protected abstract handleClick(e: Event): void;
 
 }
 
+/**
+ * A collapser subclass to collapse a cell's input area.
+ */
 export
 class InputCollapser extends Collapser {
+  /**
+   * Construct a new input collapser.
+   */
   constructor() {
       super();
       this.addClass(INPUT_COLLAPSER);
   }
 
+  /**
+   * Is the cell's input collapsed?
+   */
   get collapsed(): boolean {
     let cell = this.parent.parent as Cell;
     if (cell) {
@@ -83,6 +130,9 @@ class InputCollapser extends Collapser {
     }
   }
 
+  /**
+   * Handle a click event for the user to collapse the cell's input.
+   */
   protected handleClick(e: Event): void {
     let cell = this.parent.parent as Cell;
     if (cell) {
@@ -94,14 +144,22 @@ class InputCollapser extends Collapser {
 
 }
 
-
+/**
+ * A collapser subclass to collapse a cell's output area.
+ */
 export
 class OutputCollapser extends Collapser {
+    /**
+    * Construct a new output collapser.
+    */
     constructor() {
       super();
       this.addClass(OUTPUT_COLLAPSER);
     }
 
+  /**
+   * Is the cell's output collapsed?
+   */
   get collapsed(): boolean {
     let cell = this.parent.parent as CodeCell;
     if (cell) {
@@ -111,13 +169,15 @@ class OutputCollapser extends Collapser {
     }
   }
 
-    protected handleClick(e: Event): void {
-      let cell = this.parent.parent as CodeCell;
-      if (cell) {
-        cell.outputHidden = !cell.outputHidden;
-      }
-      /* We need this until we watch the cell state */
-      this.update();
+  /**
+   * Handle a click event for the user to collapse the cell's output.
+   */
+  protected handleClick(e: Event): void {
+    let cell = this.parent.parent as CodeCell;
+    if (cell) {
+      cell.outputHidden = !cell.outputHidden;
+    }
+    /* We need this until we watch the cell state */
+    this.update();
   }
-
 }
