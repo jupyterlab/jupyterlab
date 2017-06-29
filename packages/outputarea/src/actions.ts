1859cf0d900cb58e186bed4fa87fa5f445647d58
S. Chris Colbert <sccolbert@gmail.com>
2017-06-29 18:35:25 -0500

    use cross-cutting semantic actions instead of crud-like actions

--- packages/outputarea/src/actions.ts
+++ packages/outputarea/src/actions.ts
@@ -3,127 +3,17 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 import {
-  Action, Table
+  Action
 } from '@phosphor/datastore';
 
 
 /**
- * An action for creating a new mime model.
+ * An enum of the output action type names.
  */
 export
-class CreateMimeModel extends Action<'@jupyterlab/outputarea/CREATE_MIME_MODEL'> {
-  /**
-   * Construct a new action.
-   *
-   * @param modelId - The unique id to use for the mime model.
-   *
-   * @param model - The initial state for the mime model.
-   */
-  constructor(
-    public readonly modelId: string,
-    public readonly model: IMimeModel) {
-    super('@jupyterlab/outputarea/CREATE_MIME_MODEL');
-  }
-}
-
-
-/**
- * An action for creating a new output item.
- */
-export
-class CreateOutputItem extends Action<'@jupyterlab/outputarea/CREATE_OUTPUT_ITEM'> {
-  /**
-   * Construct a new action.
-   *
-   * @param itemId - The unique id to use for the output item.
-   *
-   * @param item - The initial state for the output item.
-   */
-  constructor(
-    public readonly itemId: string,
-    public readonly item: IOutputItem) {
-    super('@jupyterlab/outputarea/CREATE_OUTPUT_ITEM');
-  }
-}
-
-
-/**
- * An action for creating a new output list.
- */
-export
-class CreateOutputList extends Action<'@jupyterlab/outputarea/CREATE_OUTPUT_LIST'> {
-  /**
-   * Construct a new action.
-   *
-   * @param listId - The unique id to use for the output list.
-   *
-   * @param list - The initial state for the output list.
-   */
-  constructor(
-    public readonly listId: string,
-    public readonly list: Table.List<string>) {
-    super('@jupyterlab/outputarea/CREATE_OUTPUT_LIST');
-  }
-}
-
-
-/**
- * An action for creating a new output area.
- */
-export
-class CreateOutputArea extends Action<'@jupyterlab/outputarea/CREATE_OUTPUT_AREA'> {
-  /**
-   * Construct a new action.
-   *
-   * @param areaId - The unique id to use for the output area.
-   *
-   * @param area - The initial state for the output area.
-   */
-  constructor(
-    public readonly areaId: string,
-    public readonly area: IOutputArea) {
-    super('@jupyterlab/outputarea/CREATE_OUTPUT_AREA');
-  }
-}
-
-
-/**
- * An action for adding an output item to an output list.
- */
-export
-class AddOutput extends Action<'@jupyterlab/outputarea/ADD_OUTPUT'> {
-  /**
-   * Construct a new action.
-   *
-   * @param listId - The id of the ouptut list to modify.
-   *
-   * @param itemId - The id of the output item to add to the list.
-   */
-  constructor(
-    public readonly listId: string,
-    public readonly itemId: string) {
-    super('@jupyterlab/outputarea/ADD_OUTPUT');
-  }
-}
-
-
-/**
- * An action for setting the contents of an output list.
- */
-export
-class SetOutputs extends Action<'@jupyterlab/outputarea/SET_OUTPUTS'> {
-  /**
-   * Construct a new action.
-   *
-   * @param listId - The id of the ouptut list to modify.
-   *
-   * @param list - The new state for the output list.
-   */
-  constructor(
-    public readonly listId: string
-    public readonly list: Table.List<string>) {
-    super('@jupyterlab/outputarea/SET_OUTPUTS');
-  }
+enum OutputActionType {
+  APPEND_OUTPUT = '@jupyterlab/outputarea/APPEND_OUTPUT',
+  CLEAR_OUTPUTS = '@jupyterlab/outputarea/CLEAR_OUTPUTS'
 }
 
 
@@ -132,10 +22,41 @@ class SetOutputs extends Action<'@jupyterlab/outputarea/SET_OUTPUTS'> {
  */
 export
 type OutputAction = (
-  CreateMimeModel |
-  CreateOutputItem |
-  CreateOutputList |
-  CreateOutputArea |
-  AddOutput |
-  SetOutputs
+  AppendOutputAction |
+  ClearOutputsAction
 );
+
+
+/**
+ * An action for appending output to an output area.
+ */
+export
+class AppendOutputAction extends Action<OutputActionType.APPEND_OUTPUT> {
+  /**
+   * @param areaId - The id of the output area.
+   *
+   * @param output - The output object to add to the area.
+   */
+  constructor(
+    public readonly areaId: string,
+    public readonly ouptut: nbformat.IOutput) {
+    super(OutputActionType.APPEND_OUTPUT);
+  }
+}
+
+
+/**
+ * An action for clearing the outputs of an output area.
+ */
+export
+class ClearOutputsAction extends Action<OutputActionType.CLEAR_OUTPUTS> {
+  /**
+   * @param areaId - The id of the output area.
+   *
+   * @param output - The output object to add to the area.
+   */
+  constructor(
+    public readonly areaId: string) {
+    super(OutputActionType.CLEAR_OUTPUTS);
+  }
+}
--- packages/outputarea/src/reducers.ts
+++ packages/outputarea/src/reducers.ts
@@ -3,81 +3,260 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 import {
-  Table, combineReducers
+  JSONObject
+} from '@phosphor/coreutils';
+
+import {
+  Table, uuid4
 } from '@phosphor/datastore';
 
 import {
-  IMimeModel
-} from '@jupyterlab/rendermime';
+  AppendOutputAction, ClearOutputsAction, OutputAction, OutputActionType
+} from './actions';
 
 import {
-  IOutputArea, IOutputItem, IOutputStoreState
+  OutputState
 } from './models';
 
 
 /**
- * The root reducer for an output store.
+ * The mime type for stdout.
+ */
+const STDOUT = 'application/vnd.jupyter.stdout';
+
+/**
+ * The mime type for stderr.
+ */
+const STDERR = 'application/vnd.jupyter.stderr';
+
+
+/**
+ * The root reducer for the output area package.
  */
 export
-const reducer = combineReducers<IOutputStoreState>({
-  mimeModelTable,
-  outputItemTable,
-  outputAreaTable,
-  outputListTable
-});
-
-
-/**
- * A reducer which handles actions for the `mimeModelTable` branch.
- */
-function mimeModelTable(table: Table.RecordTable<IMimeModel>, action: OutputAction): Table.RecordTable<IMimeModel> {
+function outputReducer(state: OutputState, action: OutputAction): OutputState {
   switch (action.type) {
-  case '@jupyterlab/outputarea/CREATE_MIME_MODEL':
-    return Table.insert(table, action.modelId, action.model);
+  case OutputActionType.APPEND_OUTPUT:
+    return appendOutput(state, action);
+  case OutputActionType.CLEAR_OUTPUTS:
+    return clearOutputs(state, action);
   default:
-    return table;
+    return state;
   }
 }
 
 
 /**
- * A reducer which handles actions for the `outputItemTable` branch.
+ * Append an output item to an output area.
  */
-function outputItemTable(table: Table.RecordTable<IOutputItem>, action: OutputAction): Table.RecordTable<IOutputItem> {
-  switch (action.type) {
-  case '@jupyterlab/outputarea/CREATE_OUTPUT_ITEM':
-    return Table.insert(table, action.itemId, action.item);
-  default:
-    return table;
+function appendOutput(state: OutputState, action: AppendOutputAction): OutputState {
+  // Unpack the area id and output.
+  let { areaId, output } = action;
+
+  // Look up the output area object.
+  let area = state.outputAreaTable[areaId];
+
+  // Try to merge the output first, if possible.
+  let mergeState = mergeOutputs(state, area, output);
+
+  // Return the new state if the output was merged.
+  if (state !== mergeState) {
+    return mergeState;
   }
+
+  // Create the mime data for the output.
+  let mimeData = createMimeData(output);
+
+  // Create the mime metadata for the output.
+  let mimeMetadata = createMimeMetadata(output);
+
+  // Create the uuids needed for the tables.
+  let itemId = uuid4();
+  let mimeDataId = uuid4();
+  let mimeMetadataId = uuid4();
+
+  // Create the output item.
+  let item: OutputItem = {
+    type: output.output_type,
+    trusted: area.trusted,
+    mimeDataId,
+    mimeMetadataId,
+    streamType: getStreamType(output),
+    executionCount: getExecutionCount(output)
+  };
+
+  // Add the mime bundles to the table.
+  let mimeBundleTable = Table.apply(state.mimeBundleTable, [
+    Table.opInsert(mimeDataId, mimeData),
+    Table.opInsert(mimeMetadataId, mimeMetadata)
+  ]);
+
+  // Add the output item to the table.
+  let outputItemTable = Table.apply(state.outputItemTable,
+    Table.opInsert(itemId, item)
+  );
+
+  // Add the item id to the output list.
+  let outputListTable = Table.apply(state.outputListTable,
+    Table.opAppend(area.outputListId, [itemId])
+  );
+
+  // Return the updated state.
+  return { ...state, mimeBundleTable, outputItemTable, outputListTable };
 }
 
 
 /**
- * A reducer which handles actions for the `outputAreaTable` branch.
+ * Clear the outputs for an output area.
  */
-function outputAreaTable(table: Table.RecordTable<IOutputArea>, action: OutputAction): Table.RecordTable<IOutputArea> {
-  switch (action.type) {
-  case '@jupyterlab/outputarea/CREATE_OUTPUT_AREA':
-    return Table.insert(table, action.areaId, action.area);
-  default:
-    return table;
-  }
+function clearOutputs(state: OutputState, action: ClearOutputsAction): OutputState {
+  // Look up the output list id.
+  let outputListId = state.outputAreaTable[action.areaId].outputListId;
+
+  // Create the table operation to clear the list.
+  let op = Table.opReplace(outputListId, []);
+
+  // Apply the operation to the table.
+  let outputListTable = Table.apply(state.outputListTable, op);
+
+  // Return the updated state.
+  return { ...state, outputListTable };
 }
 
 
 /**
- * A reducer which handles actions for the `outputListTable` branch.
+ * Create the mime data for an output.
  */
-function outputListTable(table: Table.ListTable<string>, action: OutputAction): Table.ListTable<string> {
-  switch (action.type) {
-  case '@jupyterlab/outputarea/CREATE_OUTPUT_LIST':
-    return Table.insert(table, action.listId, action.list);
-  case '@jupyterlab/outputarea/ADD_OUTPUT':
-    return Table.push(table, action.listId, [action.itemId]);
-  case '@jupyterlab/outputarea/SET_OUTPUTS':
-    return Table.replace(table, action.listId, action.list);
+function createMimeData(output: nbformat.IOutput): JSONObject {
+  let data: JSONObject;
+  switch (true) {
+  case nbformat.isExecuteResult(output):
+  case nbformat.isDisplayUpdate(output):
+  case nbformat.isDisplayData(output):
+    data = normalizeMimeBundle(output.data);
+    break;
+  case nbformat.isStream(output):
+    data = { [getMimeType(output)]: output.text.join('\n') };
+    break;
+  case nbformat.isError(output):
+    data = { [stderr]: formatError(output) };
+    break;
   default:
-    return table;
+    data = {};
+    break;
   }
+  return data;
+}
+
+
+/**
+ * Create the mime metadata for an output.
+ */
+function createMimeMetadata(output: nbformat.IOutput): JSONObject {
+  let metadata: JSONObject;
+  switch (true) {
+  case nbformat.isExecuteResult(output):
+  case nbformat.isDisplayData(output):
+  case nbformat.isDisplayUpdate(output):
+    metadata = JSONExt.deepCopy(output.metadata);
+    break;
+  default:
+    metadata = {};
+    break;
+  }
+  return metadata;
+}
+
+
+/**
+ * Format the error text for an error output.
+ */
+function formatError(output: nbformat.IError): string {
+  return output.traceback.join('\n') || `${output.ename}: ${output.evalue}`;
+}
+
+
+/**
+ * Get the mimetype for an output stream.
+ */
+function getMimeType(output: nbformat.IStream): string {
+  return output.name === 'stdout' ? STDOUT : STDERR;
+}
+
+
+/**
+ * Get the stream type for an output.
+ */
+function getStreamType(output: nbformat.IOutput): string | null {
+  return nbformat.isStream(output) ? output.name : null;
+}
+
+
+/**
+ * Get the execution count for an output.
+ */
+function getExecutionCount(output: nbformat.IOutput): nbformat.ExecutionCount {
+  return nbformat.isExecuteResult(output) ? output.execution_count: null;
+}
+
+
+/**
+ * Create a normalized copy of a mime bundle.
+ *
+ * Top-level multiline strings (`string[]`) are joined with `\n`.
+ */
+function normalizeMimeBundle(bundle: IMimeBundle): JSONObject {
+  let result: JSONObject = {};
+  for (let key in bundle) {
+    let value = bundle[key];
+    if (Array.isArray(value)) {
+      result[key] = value.join('\n');
+    } else {
+      result[key] = JSONExt.deepCopy(value);
+    }
+  }
+  return result;
+}
+
+
+/**
+ * Merge an output with an existing output area, if possible.
+ *
+ * Returns the original state if no merge was performed.
+ */
+function mergeOutputs(state: OutputState, area: OutputArea, output: nbformat.IOutput): OutputState {
+  // Bail early if the output is not a stream.
+  if (!nbformat.isStream(output)) {
+    return state;
+  }
+
+  // Look up the output list for the area.
+  let outputList = state.outputListTable[area.outputListId];
+
+  // Bail early if the area has no outputs.
+  if (outputList.length === 0) {
+    return state;
+  }
+
+  // Look up the last output item for the area.
+  let lastItem = state.outputItemTable[outputList[outputList.length - 1]];
+
+  // Bail if the outputs cannot be merged.
+  if (lastItem.type !== 'stream' || lastItem.streamType !== output.name) {
+    return state;
+  }
+
+  // Shallow copy the mime data for the last stream item.
+  let mimeData = { ...state.mimeBundleTable[lastItem.mimeDataId] };
+
+  // Add the new output text to the existing text.
+  mimeData[getMimeType(output)] += output.text.join('\n');
+
+  // Replace the mime bundle in the table.
+  let mimeBundleTable = Table.apply(state.mimeBundleTable,
+    Table.opReplace(lastItem.mimeDataId, mimeData)
+  );
+
+  // Return the updated state.
+  return { ...state, mimeBundleTable };
 }
