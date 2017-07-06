8ab95dfc0706cf34de621eb3a97af5ab54a75f27
Steven Silvester <steven.silvester@ieee.org>
2017-07-06 16:48:13 -0500

    Simplify node version check

--- jupyterlab/node-version-check.js
+++ jupyterlab/node-version-check.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-var semver = require('semver');
-
-if (!semver.satisfies(semver(process.version), '>=5')) {
+var version = parseInt(process.version.replace('v', ''));
+if (version < 5) {
   process.exit(1);
 }
