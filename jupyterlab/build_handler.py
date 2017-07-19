9f24f78ab9e223daab84583e69f3969d0122c8c9
Steven Silvester <steven.silvester@ieee.org>
2017-07-19 15:49:30 -0500

    wip build handler

--- jupyterlab/build_handler.py
+++ jupyterlab/build_handler.py
@@ -2,10 +2,11 @@
 
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import json
 from tornado import gen, web
 
 from notebook.base.handlers import APIHandler
-from .commands import build
+from .commands import build, clean
 
 
 class BuildHandler(APIHandler):
@@ -16,14 +17,24 @@ class BuildHandler(APIHandler):
     @web.authenticated
     @gen.coroutine
     def get(self):
-        self.log.warn('Starting build')
+        self.log.debug('Starting build')
         try:
-            yield gen.maybe_future(build(self.app_dir, logger=self.log))
-            raise ValueError('whoops')
-        except ValueError:
-            self.log.warn('got value error')
-        self.log.warn('Finished build')
+            yield gen.maybe_future(self.run_build())
+        except Exception as e:
+            self.set_status(500)
+            msg = str(e)
+            self.finish(json.dumps(dict(message=msg)))
+            return
+        self.log.debug('Build succeeded')
         self.set_status(200)
+
+    def run_build(self):
+        try:
+            build(self.app_dir, logger=self.log)
+        except Exception:
+            self.log.warn('Build failed, running a clean and rebuild')
+            clean(self.app_dir)
+            build(self.app_dir, logger=self.log)
 
 
 # The path for lab build.
