2573cb49dcff03034b345c60a6bd1dbc40dc1eef
Steven Silvester <steven.silvester@ieee.org>
2017-07-27 10:06:32 -0500

    revert change to filebrowser model

--- packages/filebrowser/src/model.ts
+++ packages/filebrowser/src/model.ts
@@ -268,7 +268,7 @@ class FileBrowserModel implements IDisposable {
 
       const path = cwd['path'] as string;
       const localPath = path.split(':').pop();
-      return manager.services.contents.get(path, { content: false })
+      return manager.services.contents.get(path)
         .then(() => this.cd(localPath))
         .catch(() => state.remove(key));
     }).catch(() => state.remove(key))
