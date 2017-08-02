f3f97acd3f11a575a8a1275bfbf2d6774b6581a6
Steven Silvester <steven.silvester@ieee.org>
2017-08-02 12:02:50 -0500

    Update package integrity check and errant packages

--- scripts/package-integrity.js
+++ scripts/package-integrity.js
@@ -36,9 +36,6 @@ function getImports(sourceFile) {
 }
 
 
-var dname = '../packages/all-packages';
-
-
 /**
  * Validate the integrity of a package in a directory.
  */
@@ -83,6 +80,12 @@ function validate(dname) {
         }
     });
     Object.keys(deps).forEach(function(name) {
+        if (versions[name]) {
+            var desired = '^' + versions[name];
+            if (deps[name] !== desired) {
+                problems.push('Bad core version: ' + name + ' should be ' + desired);
+            }
+        }
         if (IGNORE.indexOf(name) !== -1) {
             return;
         }
@@ -96,6 +99,20 @@ function validate(dname) {
 // Find all of the packages.
 var basePath = path.resolve('.');
 var lernaConfig = require(path.join(basePath, 'lerna.json'));
+
+// Gather the versions of each package.
+var versions = {};
+lernaConfig.packages.forEach(function(spec) {
+    var dirs = glob.sync(path.join(basePath, spec));
+    dirs.forEach(function(dname) {
+        try {
+            var pkg = require(path.resolve(dname) + '/package.json');
+            versions[pkg['name']] = pkg['version'];
+        } catch (e) {
+            return;
+        }
+    });
+});
 
 var errors = {};
 
