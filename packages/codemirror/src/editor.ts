105365aec62d786d4e98514de1f8112dc732e24c
Steven Silvester <steven.silvester@ieee.org>
2017-08-03 17:24:55 -0500

    Add support for find and replace in the editor

--- packages/codemirror/src/editor.ts
+++ packages/codemirror/src/editor.ts
@@ -35,6 +35,8 @@ import {
 import 'codemirror/addon/edit/matchbrackets.js';
 import 'codemirror/addon/edit/closebrackets.js';
 import 'codemirror/addon/comment/comment.js';
+import 'codemirror/addon/search/searchcursor';
+import 'codemirror/addon/search/search';
 import 'codemirror/keymap/emacs.js';
 import 'codemirror/keymap/sublime.js';
 import 'codemirror/keymap/vim.js';
