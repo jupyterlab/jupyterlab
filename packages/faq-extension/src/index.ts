d0405f26b08bf4637ea4ed6526b75a01b006683a
Steven Silvester <steven.silvester@ieee.org>
2017-08-04 14:04:55 -0500

    Update the faq extension

--- packages/faq-extension/src/index.ts
+++ packages/faq-extension/src/index.ts
@@ -14,10 +14,11 @@ import {
 } from '@phosphor/coreutils';
 
 import {
-  FaqModel, FaqWidget
-} from './widget';
+  Widget
+} from '@phosphor/widgets';
 
 import '../style/index.css';
+
 
 /**
  * The command IDs used by the FAQ plugin.
@@ -46,14 +47,132 @@ export default plugin;
 
 
 /**
+ * The faq page source.
+ */
+const SOURCE = `
+## General
+* [What is JupyterLab?](#What-is-JupyterLab?)
+* [What new features does JupyterLab offer?](#What-new-features-does-JupyterLab-offer?)
+* [How stable is JupyterLab?](#How-stable-is-JupyterLab?)
+* [What will happen to the classic Jupyter Notebook?](#What-will-happen-to-the-classic-Jupyter-Notebook?)
+* [Where is the documentation for JupyterLab?](#Where-is-the-documentation-for-JupyterLab?)
+
+## Development
+
+* [How do I report a bug or provide feedback?](#How-do-I-report-a-bug-or-provide-feedback?)
+* [How can I contribute?](#How-can-I-contribute?)
+* [How can I extend or customize JupyterLab?](#How-can-I-extend-or-customize-JupyterLab?)
+
+## General
+
+### What is JupyterLab?
+
+JupyterLab is a new user interface for the Jupyter Notebook, allowing
+users to arrange multiple Jupyter notebooks, text editors, terminals,
+output areas, etc. on a single page with multiple panels and tabs in
+one integrated application. The codebase and user-interface of JupyterLab
+is based on a flexible extension system that makes it easy to extend with new
+functionality.
+
+### What new features does JupyterLab offer?
+
+JupyterLab offers a number of features beyond the classic Jupyter
+Notebook. Here are a few of them you may want to try out:
+
+* Arrange multiple notebooks, terminals, text files, etc. in the
+  application using drag and drop.
+* Run code interactively outside of a notebook in the Code Console,
+  and connect one  to a text file.
+* Right click on a markdown file and "Open with..." a live markdown
+  viewer.
+* Double click on CSV files to view them as a nicely formatted table.
+* Drag and drop notebook cells within a notebook or between notebooks.
+
+### How stable is JupyterLab?
+
+We are currently making a series of alpha releases of JupyterLab.
+These releases are mostly usable, but you may
+experience bugs and other rough edges as the code base is still
+changing rapidly.
+
+In June 2017, we will be releasing a beta version. This beta
+version will be characterized by:
+
+* Stable and featureful enough for daily usage by most Jupyter users.
+* Most of the commonly used features in the classic notebook are
+  implemented.
+* Developer APIs that are approaching stability but stil undergoing
+  significant changes.
+
+Later in 2017, we will release the 1.0 version of JupyterLab that will
+provide additional UI/UX improvements, features, and API stability. At
+that point, JupyterLab should be a full featured replacement for the
+classic notebook - and go far beyond its capabilities.
+
+### What will happen to the classic Jupyter Notebook?
+
+JupyterLab is intended to be a full replacement for the classic Jupyter
+Notebook. Because of this, our plan is to gradually retire the classic
+Jupyter Notebook. However, we will support the classic notebook for a
+signifciant period of time to help users and extension authors through
+this transition. It is important to note that the notebook server
+and the notebook document format is unchanged during this transition.
+
+### Where is the documentation for JupyterLab?
+
+The documentation for JupyterLab can be found on ReadTheDocs [here](http://jupyterlab.readthedocs.io/en/latest/).
+
+## Development
+
+### How do I report a bug or provide feedback?
+
+If you find a bug or want to provide feedback, please open an issue
+on our [GitHub Issues page](https://github.com/jupyterlab/jupyterlab/issues).
+
+### How can I contribute?
+
+We welcome other developers and designers to contribute to JupyterLab.
+Development of JupyterLab takes place on our [GitHub Repository](https://github.com/jupyterlab/jupyterlab).
+To get started with development, please have a look at our
+[Contributing Guide](https://github.com/jupyterlab/jupyterlab/blob/master/CONTRIBUTING.md)
+or chat with us on our [Gitter Channel](https://gitter.im/jupyterlab/jupyterlab).
+
+JupyterLab is a
+part of Project Jupyter and follows the [Jupyter Code of Conduct](https://github.com/jupyter/governance/blob/master/conduct/code_of_conduct.md).
+
+### How can I extend or customize JupyterLab?
+
+JupyterLab consists entirely of *JupyterLab Extensions*, which are simply
+[NPM](https://www.npmjs.com/) packages that utilize the public JupyterLab
+APIs. You can develop your own custom extensions that use these APIs to
+extend the functionality of JupyterLab. Examples of possible extensions include:
+
+* Custom renderers/viewers/editors for specific file types.
+* Renderers for custom output types in the notebook.
+* Entirely new user interfaces for working with data that utilize
+  JupyterLab's layout, command palette and integrate in various ways
+  with our core extensions (notebooks, code consoles, etc.).
+
+To start developing your own JupyterLab extension, please have a look
+at:
+
+* [JupyterLab Documentation](http://jupyterlab.readthedocs.io/en/latest/)
+* JupyterLab Extension Template for [TypeScript](Cookiecutters(https://github.com/jupyterlab/extension-cookiecutter-ts)
+* JupyterLab Extension Template for [JavaScript](https://github.com/jupyterlab/extension-cookiecutter-js)
+`;
+
+
+/**
  * Activate the FAQ plugin.
  */
 function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRestorer): void {
   const category = 'Help';
   const command = CommandIDs.open;
-  const model = new FaqModel();
-  const { commands, shell } = app;
-  const tracker = new InstanceTracker<FaqWidget>({ namespace: 'faq' });
+  const { commands, shell, rendermime } = app;
+  const tracker = new InstanceTracker<Widget>({
+    namespace: 'faq',
+    disposeOnDetach: true
+  });
 
   // Handle state restoration.
   restorer.restore(tracker, {
@@ -62,23 +181,32 @@ function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRe
     name: () => 'faq'
   });
 
-  let widget: FaqWidget;
+  let createWidget = () => {
+    let renderer = rendermime.createRenderer('text/markdown');
+    renderer.id = 'faq';
+    renderer.addClass('jp-FAQ');
+    renderer.title.label = 'FAQ';
+    renderer.title.closable = true;
+    renderer.node.tabIndex = -1;
 
-  function newWidget(): FaqWidget {
-    let widget = new FaqWidget({ linker: app.commandLinker });
-    widget.model = model;
-    widget.id = 'faq';
-    widget.title.label = 'FAQ';
-    widget.title.closable = true;
-    tracker.add(widget);
-    return widget;
-  }
+    const model = rendermime.createModel({
+      data: { 'text/markdown': SOURCE }
+    });
+    renderer.renderModel(model);
+
+    return renderer;
+  };
+
+  let widget = createWidget();
 
   commands.addCommand(command, {
     label: 'Open FAQ',
     execute: () => {
-      if (!widget || widget.isDisposed) {
-        widget = newWidget();
+      if (widget.isDisposed) {
+        widget = createWidget();
+      }
+      if (!tracker.has(widget)) {
+        tracker.add(widget);
         shell.addToMainArea(widget);
       }
       shell.activateById(widget.id);
