a0b50f474690cee1a836d099deee84b60f587b49
Steven Silvester <steven.silvester@ieee.org>
2017-08-11 12:48:52 -0500

    Clean up handling of read only files

--- packages/docmanager/src/widgetmanager.ts
+++ packages/docmanager/src/widgetmanager.ts
@@ -256,12 +256,16 @@ class DocumentWidgetManager implements IDisposable {
       }
       let last = checkpoints[checkpoints.length - 1];
       let checkpoint = last ? Time.format(last.last_modified) : 'None';
-      widget.title.caption = (
-        `Name: ${model.name}\n` +
-        `Path: ${model.path}\n` +
-        `Last Saved: ${Time.format(model.last_modified)}\n` +
-        `Last Checkpoint: ${checkpoint}`
-      );
+      let caption = `Name: ${model.name}\nPath: ${model.path}\n`;
+      if (context.model.readOnly) {
+        caption += 'Read-only';
+      } else {
+        caption += (
+          `Last Saved: ${Time.format(model.last_modified)}\n` +
+          `Last Checkpoint: ${checkpoint}`
+        );
+      }
+      widget.title.caption = caption;
     });
   }
 
