9b189610df1a421e516626dad25b485aacab4eb0
cameronoelsen <cameronoelsen@gmail.com>
2017-08-17 10:42:05 -0700

    Fixed bug related to drag image of tabs inside of the dockpanel

--- packages/settingeditor-extension/src/index.ts
+++ packages/settingeditor-extension/src/index.ts
@@ -69,7 +69,7 @@ const plugin: JupyterLabPlugin<void> = {
         tracker.add(editor);
         editor.id = namespace;
         editor.title.label = 'Settings';
-        editor.title.iconClass = 'jp-MaterialIcon jp-SettingsIcon';
+        editor.title.iconClass = 'jp-SettingsIcon';
         editor.title.closable = true;
         shell.addToMainArea(editor);
         shell.activateById(editor.id);
