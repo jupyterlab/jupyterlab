2f00b2720ef283498d917a8f6746007bb468dbf3
Steven Silvester <steven.silvester@ieee.org>
2017-10-02 11:56:06 -0500

    Update tests

--- jupyterlab/tests/test_settings_api.py
+++ jupyterlab/tests/test_settings_api.py
@@ -3,6 +3,7 @@ import json
 
 from jupyterlab.tests.utils import LabTestBase, APITester
 from notebook.tests.launchnotebook import assert_http_error
+from jupyterlab import __version__
 
 
 class SettingsAPI(APITester):
@@ -26,6 +27,9 @@ class SettingsAPITest(LabTestBase):
     def test_get(self):
         id = '@jupyterlab/apputils-extension:shortcuts'
         id = id.replace('@', '').replace('/', '').replace(':', '-')
+        if 'rc' in __version__:
+            raise ValueError('Remove this before final release')
+        id = 'jupyter.extensions.shortcuts'
         data = self.settings_api.get(id).json()
         assert data['id'] == id
         assert len(data['schema'])
@@ -38,6 +42,7 @@ class SettingsAPITest(LabTestBase):
     def test_patch(self):
         id = '@jupyterlab/apputils-extension:shortcuts'
         id = id.replace('@', '').replace('/', '').replace(':', '-')
+        id = 'jupyter.extensions.shortcuts'
         resp = self.settings_api.patch(id, dict())
         assert resp.status_code == 204
 
@@ -48,6 +53,7 @@ class SettingsAPITest(LabTestBase):
     def test_patch_bad_data(self):
         id = '@jupyterlab/codemirror-extension:commands'
         id = id.replace('@', '').replace('/', '').replace(':', '-')
+        id = 'jupyter.services.codemirror-commands'
         data = dict(keyMap=10)
         with assert_http_error(400):
             self.settings_api.patch(id, data)
