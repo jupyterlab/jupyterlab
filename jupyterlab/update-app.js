1b18a857836a80047330b386c2b52c9daad00158
Steven Silvester <steven.silvester@ieee.org>
2017-10-06 07:59:50 -0500

    Fix handling of non-dev installs

--- jupyterlab/update-app.js
+++ jupyterlab/update-app.js
@@ -9,3 +9,26 @@ Build.ensureAssets({
   packageNames: names,
   output: '..'
 });
+
+// Hacks to be removed in 0.28 final release.
+var path = require('path');
+var fs = require('fs-extra');
+
+var base = path.join('.', 'node_modules', '@jupyterlab');
+var schemas = path.join('..', 'schemas');
+
+var src = path.join(base, 'shortcuts-extension', 'schema', 'plugin.json')
+var dest = path.join(schemas, 'jupyter.extensions.shortcuts.json')
+fs.copySync(src, dest)
+
+src = path.join(base, 'apputils-extension', 'schema', 'themes.json')
+dest = path.join(schemas, 'jupyter.services.theme-manager.json')
+fs.copySync(src, dest)
+
+src = path.join(base, 'codemirror-extension', 'schema', 'commands.json')
+dest = path.join(schemas, 'jupyter.services.codemirror-commands.json')
+fs.copySync(src, dest)
+
+src = path.join(base, 'fileeditor-extension', 'schema', 'plugin.json')
+dest = path.join(schemas, 'jupyter.services.editor-tracker.json')
+fs.copySync(src, dest)
