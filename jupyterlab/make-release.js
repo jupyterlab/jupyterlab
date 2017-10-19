6a98011f5a726ada46e33198763896eec27e153a
Steven Silvester <steven.silvester@ieee.org>
2017-10-19 14:09:47 -0500

    Revert change to watch script invocation

--- jupyterlab/make-release.js
+++ jupyterlab/make-release.js
@@ -10,7 +10,7 @@ childProcess.execSync('npm run build:prod', {stdio:[0,1,2]});
 // Update the package.app.json file.
 var data = require('./package.json');
 data['scripts']['build'] = 'webpack'
-data['scripts']['watch'] = "concurrently \"webpack --watch\" \"node watch\""
+data['scripts']['watch'] = 'webpack --watch'
 data['jupyterlab']['outputDir'] = '..';
 data['jupyterlab']['linkedPackages'] = {};
 text = JSON.stringify(sortPackageJson(data), null, 2) + '\n';
