5e2da172d0609278aa1e841972c08aafa7365474
Steven Silvester <steven.silvester@ieee.org>
2017-10-20 07:20:53 -0500

    More logic simplification

--- jupyterlab/webpack.config.js
+++ jupyterlab/webpack.config.js
@@ -106,22 +106,19 @@ module.exports = {
       }
       // Limit the watched files to those in our local linked package dirs.
       var ignore = true;
-      var found = false;
-      Object.keys(localLinked).forEach(function (name) {
+      Object.keys(localLinked).some(function (name) {
         // Bail if already found.
-        if (found) {
-          return;
-        }
         var rootPath = localLinked[name];
         var contained = localPath.indexOf(rootPath + path.sep) !== -1;
-        if (localPath === rootPath || contained) {
-          found = true;
-          var rest = localPath.slice(rootPath.length);
-          if (rest.indexOf('node_modules') === -1) {
-            ignore = false;
-            maybeSync(localPath, name, rest);
-          }
+        if (localPath !== rootPath && !contained) {
+          return false;
         }
+        var rest = localPath.slice(rootPath.length);
+        if (rest.indexOf('node_modules') === -1) {
+          ignore = false;
+          maybeSync(localPath, name, rest);
+        }
+        return true;
       });
       ignoreCache.set(localPath, ignore);
       return ignore;
