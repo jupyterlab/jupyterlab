f089d2ba1083c57b91d45b4320e303204f76e09d
Grant Nestor <grantnestor@gmail.com>
2017-10-23 08:32:12 -0700

    Add json-extension

---
+++ packages/json-extension/src/component.tsx
@@ -0,0 +1,229 @@
+import * as React from 'react';
+
+import * as ReactDOM from 'react-dom';
+
+import JSONTree from 'react-json-tree';
+
+import * as Highlight from 'react-highlighter';
+
+import {
+  JSONValue,
+  JSONObject,
+  JSONArray
+} from '@phosphor/coreutils';
+
+export interface IProps {
+  data: JSONValue;
+  metadata?: JSONObject;
+  theme?: string;
+}
+
+export interface IState {
+  filter?: string;
+}
+
+export class Component extends React.Component<IProps, IState> {
+  state = { filter: '' };
+  input: Element = null;
+  timer: number = 0;
+
+  componentDidMount() {
+    /**
+     * Stop propagation of keyboard events to JupyterLab
+     */
+    ReactDOM.findDOMNode(this.input).addEventListener(
+      'keydown',
+      (event: Event) => {
+        event.stopPropagation();
+      },
+      false
+    );
+  }
+
+  componentWillUnmount() {
+    ReactDOM.findDOMNode(this.input).removeEventListener(
+      'keydown',
+      (event: Event) => {
+        event.stopPropagation();
+      },
+      false
+    );
+  }
+
+  render() {
+    const { data, metadata } = this.props;
+    const keyPaths = this.state.filter
+      ? filterPaths(data, this.state.filter)
+      : ['root'];
+    return (
+      <div style={{
+        position: 'relative',
+        width: '100%'
+      }}>
+        <input
+          ref={ref => this.input = ref}
+          onChange={event => {
+            if (this.timer) clearTimeout(this.timer);
+            const filter = event.target.value;
+            this.timer = window.setTimeout(
+              () => {
+                this.setState({ filter } as IState);
+                this.timer = 0;
+              },
+              300
+            );
+          }}
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            width: '33%',
+            maxWidth: 150,
+            zIndex: 10,
+            fontSize: 13,
+            padding: '4px 2px'
+          }}
+          type="text"
+          placeholder="Filter..."
+        />
+        <JSONTree
+          data={data}
+          collectionLimit={100}
+          theme={{
+            extend: theme,
+            // TODO: Use Jupyter Notebook's current CodeMirror theme vs. 'cm-s-ipython'
+            tree: `CodeMirror ${this.props.theme || 'cm-s-ipython'}`,
+            // valueLabel: 'cm-variable',
+            valueText: 'cm-string',
+            // nestedNodeLabel: 'cm-variable-2',
+            nestedNodeItemString: 'cm-comment',
+            // value: {},
+            // label: {},
+            // itemRange: {},
+            // nestedNode: {},
+            // nestedNodeItemType: {},
+            // nestedNodeChildren: {},
+            // rootNodeChildren: {},
+            arrowSign: { color: 'cm-variable' }
+          }}
+          invertTheme={false}
+          labelRenderer={([label, type]) => {
+            let className = 'cm-variable';
+            // if (type === 'root') className = 'cm-variable-2';
+            if (type === 'array') className = 'cm-variable-2';
+            if (type === 'object') className = 'cm-variable-3';
+            return (
+              <span className={className}>
+                <Highlight
+                  search={this.state.filter}
+                  matchStyle={{ backgroundColor: 'yellow' }}
+                >
+                  {`${label}: `}
+                </Highlight>
+              </span>
+            );
+          }}
+          valueRenderer={raw => {
+            let className = 'cm-string';
+            if (typeof raw === 'number') className = 'cm-number';
+            if (raw === 'true' || raw === 'false') className = 'cm-keyword';
+            return (
+              <span className={className}>
+                <Highlight
+                  search={this.state.filter}
+                  matchStyle={{ backgroundColor: 'yellow' }}
+                >
+                  {`${raw}`}
+                </Highlight>
+              </span>
+            );
+          }}
+          shouldExpandNode={(keyPath, data, level) =>
+            metadata && metadata.expanded
+              ? true
+              : keyPaths.join(',').includes(keyPath.join(','))}
+        />
+      </div>
+    );
+  }
+}
+
+const theme = {
+  scheme: 'jupyter',
+  base00: '#fff',
+  base01: '#fff',
+  base02: '#d7d4f0',
+  base03: '#408080',
+  base04: '#b4b7b4',
+  base05: '#c5c8c6',
+  base06: '#d7d4f0',
+  base07: '#fff',
+  base08: '#000',
+  base09: '#080',
+  base0A: '#fba922',
+  base0B: '#408080',
+  base0C: '#aa22ff',
+  base0D: '#00f',
+  base0E: '#008000',
+  base0F: '#00f'
+};
+
+function objectIncludes(data: JSONValue, query: string): boolean {
+  return JSON.stringify(data).includes(query);
+}
+
+function filterObject(data: JSONValue, query: string): JSONValue {
+  if (Array.isArray(data)) {
+    return data.reduce(
+      (result: JSONArray, item: JSONValue) => {
+        if (objectIncludes(item, query)) {
+          return [...result, filterObject(item, query)];
+        }
+        return result;
+      },
+      []
+    );
+  }
+  if (data && typeof data === 'object') {
+    return Object.keys(data).reduce((result: JSONObject, key: string) => {
+      let item = data[key];
+      if (key.includes(query) || objectIncludes(item, query))
+        result[key] = filterObject(item, query);
+      return result;
+    }, {});
+  }
+  return data;
+}
+
+function filterPaths(data: JSONValue, query: string, parent: JSONArray = ['root']): JSONArray {
+  if (Array.isArray(data)) {
+    return data.reduce(
+      (result: JSONArray, item: JSONValue, index: number) => {
+        if (item && typeof item === 'object' && objectIncludes(item, query))
+          return [
+            ...result,
+            [index, ...parent].join(','),
+            ...filterPaths(item, query, [index, ...parent])
+          ];
+        return result;
+      },
+      []
+    ) as JSONArray;
+  }
+  if (typeof data === 'object') {
+    return Object.keys(data).reduce((result: JSONArray, key: string) => {
+      let item = data[key];
+      if (
+        item &&
+        typeof item === 'object' &&
+        (key.includes(query) || objectIncludes(item, query))
+      )
+        return [
+          ...result,
+          [key, ...parent].join(','),
+          ...filterPaths(item, query, [key, ...parent])
+        ];
+      return result;
+    }, []);
+  }
+}
---
+++ packages/json-extension/src/index.tsx
@@ -0,0 +1,90 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
+  IRenderMime
+} from '@jupyterlab/rendermime-interfaces';
+
+import * as React from 'react';
+
+import * as ReactDOM from 'react-dom';
+
+import {
+  Component
+} from './component';
+
+import '../style/index.css';
+
+
+/**
+ * The CSS class to add to the JSON Widget.
+ */
+const CSS_CLASS = 'jp-RenderedJSON';
+
+/**
+ * The MIME type for JSON.
+ */
+export
+const MIME_TYPE = 'application/json';
+
+
+export
+class RenderedJSON extends Widget implements IRenderMime.IRenderer {
+  /**
+   * Create a new widget for rendering JSON.
+   */
+  constructor(options: IRenderMime.IRendererOptions) {
+    super();
+    this.addClass(CSS_CLASS);
+    this._mimeType = options.mimeType;
+  }
+
+  /**
+   * Render JSON into this widget's node.
+   */
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    const data = model.data[this._mimeType] as any;
+    const metadata = model.metadata[this._mimeType] as any || {};
+    const props = { data, metadata, theme: 'cm-s-jupyter' };
+    return new Promise<void>((resolve, reject) => {
+      ReactDOM.render(<Component {...props} />, this.node, () => {
+        resolve(undefined);
+      });
+    });
+  }
+
+  private _mimeType: string;
+}
+
+
+/**
+ * A mime renderer factory for JSON data.
+ */
+export
+const rendererFactory: IRenderMime.IRendererFactory = {
+  safe: true,
+  mimeTypes: [MIME_TYPE],
+  createRenderer: options => new RenderedJSON(options)
+};
+
+
+const extensions: IRenderMime.IExtension | IRenderMime.IExtension[] = [
+  {
+    id: '@jupyterlab/json-extension:factory',
+    rendererFactory,
+    rank: 0,
+    dataType: 'json',
+    documentWidgetFactoryOptions: {
+      name: 'JSON',
+      primaryFileType: 'json',
+      fileTypes: ['json', 'notebook'],
+      defaultFor: ['json']
+    }
+  }
+];
+
+export default extensions;
