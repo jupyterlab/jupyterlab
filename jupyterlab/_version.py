37178a1f6a2e98c5205a3235a17f1ea85d6f3e18
Steven Silvester <steven.silvester@ieee.org>
2017-10-25 08:36:11 -0500

    Release 0.27.1

--- jupyterlab/_version.py
+++ jupyterlab/_version.py
@@ -1,2 +1,2 @@
-version_info = (0, 27, 0)
+version_info = (0, 27, 1)
 __version__ = ".".join(map(str, version_info))
--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup_args['cmdclass'] = cmdclass
 setuptools_args = {}
 install_requires = setuptools_args['install_requires'] = [
     'notebook>=4.3.1',
-    'jupyterlab_launcher>=0.4.0'
+    'jupyterlab_launcher>=0.4.0,<0.6.0'
 ]
 
 extras_require = setuptools_args['extras_require'] = {
