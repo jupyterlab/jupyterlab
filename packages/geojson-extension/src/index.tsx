977c43ef5b8d76018251bfb2bab250c20378688d
Grant Nestor <grantnestor@gmail.com>
2017-10-26 11:49:34 -0700

    Fix geojson-extension document rendering

--- packages/geojson-extension/src/index.tsx
+++ packages/geojson-extension/src/index.tsx
@@ -67,6 +67,8 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
     super();
     this.addClass(CSS_CLASS);
     this._mimeType = options.mimeType;
+    // Create leaflet map object
+    this._map = leaflet.map(this.node);
   }
 
   /**
@@ -86,18 +88,6 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
     const data = model.data[this._mimeType] as any | GeoJSON.GeoJsonObject;
     const metadata = model.metadata[this._mimeType] as any || {};
     return new Promise<void>((resolve, reject) => {
-      // Create leaflet map object
-      this._map = leaflet.map(this.node);
-      // Disable scroll zoom by default to avoid conflicts with notebook scroll
-      this._map.scrollWheelZoom.disable();
-      // Enable scroll zoom on map focus
-      this._map.on('blur', (event) => {
-        this._map.scrollWheelZoom.disable();
-      });
-      // Disable scroll zoom on blur
-      this._map.on('focus', (event) => {
-        this._map.scrollWheelZoom.enable();
-      });
       // Add leaflet tile layer to map
       leaflet.tileLayer(
         metadata.url_template || URL_TEMPLATE,
@@ -105,17 +95,27 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
       ).addTo(this._map);
       // Create GeoJSON layer from data and add to map
       this._geoJSONLayer = leaflet.geoJSON(data).addTo(this._map);
+      // Update map size after panel/window is resized
+      this._map.fitBounds(this._geoJSONLayer.getBounds());
+      this._map.invalidateSize();
       resolve(undefined);
     });
   }
-  
+
   /**
    * A message handler invoked on a `'after-attach'` message.
    */
   protected onAfterAttach(msg: Message) {
-    // Set map size after widget is attached to DOM
-    this._map.fitBounds(this._geoJSONLayer.getBounds());
-    this._map.invalidateSize();
+    // Disable scroll zoom by default to avoid conflicts with notebook scroll
+    this._map.scrollWheelZoom.disable();
+    // Enable scroll zoom on map focus
+    this._map.on('blur', (event) => {
+      this._map.scrollWheelZoom.disable();
+    });
+    // Disable scroll zoom on blur
+    this._map.on('focus', (event) => {
+      this._map.scrollWheelZoom.enable();
+    });
   }
 
   /**
@@ -123,7 +123,9 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
    */
   protected onResize(msg: Widget.ResizeMessage) {
     // Update map size after panel/window is resized
-    this._map.fitBounds(this._geoJSONLayer.getBounds());
+    if (this._geoJSONLayer) {
+      this._map.fitBounds(this._geoJSONLayer.getBounds());
+    }
     this._map.invalidateSize();
   }
 
