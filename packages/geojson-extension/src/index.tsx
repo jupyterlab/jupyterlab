d6dfb7b2f53493d18fd186fa169bea1b87144c83
Grant Nestor <grantnestor@gmail.com>
2017-10-27 11:49:08 -0700

    Lazy load leaflet

--- packages/geojson-extension/src/index.tsx
+++ packages/geojson-extension/src/index.tsx
@@ -6,16 +6,11 @@ import {
 } from '@phosphor/widgets';
 
 import {
-  Message
-} from '@phosphor/messaging';
-
-import {
   IRenderMime
 } from '@jupyterlab/rendermime-interfaces';
 
+// Import only the typings for leaflet - do not use for values.
 import * as leaflet from 'leaflet';
-
-import 'leaflet/dist/leaflet.css';
 
 import '../style/index.css';
 
@@ -35,11 +30,6 @@ const CSS_ICON_CLASS = 'jp-MaterialIcon jp-GeoJSONIcon';
  */
 export
 const MIME_TYPE = 'application/geo+json';
-
-/**
- * Set base path for leaflet images.
- */
-leaflet.Icon.Default.imagePath = 'https://unpkg.com/leaflet/dist/images/';
 
 /**
  * The url template that leaflet tile layers.
@@ -67,8 +57,6 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
     super();
     this.addClass(CSS_CLASS);
     this._mimeType = options.mimeType;
-    // Create leaflet map object
-    this._map = leaflet.map(this.node);
   }
 
   /**
@@ -76,7 +64,9 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
    */
   dispose(): void {
     // Dispose of leaflet map
-    this._map.remove();
+    if (this._map) {
+      this._map.remove();
+    }
     this._map = null;
     super.dispose();
   }
@@ -85,36 +75,42 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
    * Render GeoJSON into this widget's node.
    */
   renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-    const data = model.data[this._mimeType] as any | GeoJSON.GeoJsonObject;
-    const metadata = model.metadata[this._mimeType] as any || {};
-    return new Promise<void>((resolve, reject) => {
+    const data = model.data[this._mimeType] as any;
+    const metadata = model.metadata[this._mimeType] as any | GeoJSON.GeoJsonObject || {};
+    return this._ensureMod().then(mod => {
+      // Create leaflet map object if needed.
+      if (!this._map) {
+        this._map = mod.map(this.node);
+      }
+
+      // Set base path for leaflet images.
+      mod.Icon.Default.imagePath = 'https://unpkg.com/leaflet/dist/images/';
+
       // Add leaflet tile layer to map
-      leaflet.tileLayer(
+      mod.tileLayer(
         metadata.url_template || URL_TEMPLATE,
         metadata.layer_options || LAYER_OPTIONS
       ).addTo(this._map);
+
       // Create GeoJSON layer from data and add to map
-      this._geoJSONLayer = leaflet.geoJSON(data).addTo(this._map);
+      this._geoJSONLayer = mod.geoJSON(data).addTo(this._map);
+
       // Update map size after panel/window is resized
       this._map.fitBounds(this._geoJSONLayer.getBounds());
       this._map.invalidateSize();
-      resolve(undefined);
-    });
-  }
 
-  /**
-   * A message handler invoked on a `'after-attach'` message.
-   */
-  protected onAfterAttach(msg: Message) {
-    // Disable scroll zoom by default to avoid conflicts with notebook scroll
-    this._map.scrollWheelZoom.disable();
-    // Enable scroll zoom on map focus
-    this._map.on('blur', (event) => {
+      // Disable scroll zoom by default to avoid conflicts with notebook scroll
       this._map.scrollWheelZoom.disable();
-    });
-    // Disable scroll zoom on blur
-    this._map.on('focus', (event) => {
-      this._map.scrollWheelZoom.enable();
+
+      // Enable scroll zoom on map focus
+      this._map.on('blur', (event: any) => {
+        this._map.scrollWheelZoom.disable();
+      });
+
+      // Disable scroll zoom on blur
+      this._map.on('focus', (event: any) => {
+        this._map.scrollWheelZoom.enable();
+      });
     });
   }
 
@@ -122,11 +118,30 @@ class RenderedGeoJSON extends Widget implements IRenderMime.IRenderer {
    * A message handler invoked on a `'resize'` message.
    */
   protected onResize(msg: Widget.ResizeMessage) {
+    if (!this._map) {
+      return;
+    }
     // Update map size after panel/window is resized
     if (this._geoJSONLayer) {
       this._map.fitBounds(this._geoJSONLayer.getBounds());
     }
     this._map.invalidateSize();
+  }
+
+  /**
+   * Initialize the leaflet module and our map.
+   */
+  private _ensureMod(): Promise<typeof leaflet> {
+    return new Promise((resolve, reject) => {
+      (require as any).ensure(['leaflet', 'leaflet/dist/leaflet.css'], (require: NodeRequire) => {
+        resolve(require('leaflet'));
+      },
+      (err: any) => {
+        reject(err);
+      },
+      'leaflet'
+      );
+    });
   }
 
   private _map: leaflet.Map;
