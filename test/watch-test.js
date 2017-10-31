bd786f556e78d96e41ccaa497030e4ad793ee1df
Steven Silvester <steven.silvester@ieee.org>
2017-10-31 07:07:51 -0500

    Run tests in test watch command

---
+++ test/watch-test.js
@@ -0,0 +1,9 @@
+var childProcess = require('child_process');
+var fs = require('fs-extra');
+
+
+fs.watch('build', { interval: 500 }, function (eventType, filename) {
+    if (filename === 'bundle.js') {
+        childProcess.execSync('npm run test', {stdio:[0,1,2]});
+    }
+})
