10ead2f3aba56926c63ea9b8683f45fbcb46c7f9
Steven Silvester <steven.silvester@ieee.org>
2017-11-03 03:14:55 -0500

    Move make release to scripts

---
+++ scripts/src/make-release.ts
@@ -0,0 +1,21 @@
+import fs = require('fs-extra');
+import utils = require('./utils');
+
+// Run a production build with Typescript source maps.
+utils.run('npm run build:prod');
+
+// Update the package.app.json file.
+let data= require('./package.json');
+data['scripts']['build'] = 'webpack';
+data['scripts']['watch'] = 'webpack --watch';
+data['jupyterlab']['outputDir'] = '..';
+data['jupyterlab']['linkedPackages'] = {};
+utils.ensurePackageData(data, './package.app.json');
+
+// Update our app index file.
+fs.copySync('./index.js', './index.app.js');
+
+// Add  the release metadata.
+let releaseData: any = { version: data.jupyterlab.version };
+let text = JSON.stringify(releaseData, null, 2) + '\n';
+fs.writeFileSync('./build/release_data.json', text);
