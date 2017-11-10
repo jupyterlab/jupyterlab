43bc6e2bc71d20f02fa2abf356b67675abf8c16d
Simon Biggs <mail@simonbiggs.net>
2017-11-10 22:30:08 +1100

    Fix code mirror sizing (#3219)
    * wrap the file editor in a panel
    
    added margin so that overflow bars are visible in code editor
    
    change test to set height larger than 11px
    
    swapped toolbar out for widget
    
    updated tests and renamed FileEditorInner --> FileEditorCodeWrapper
    
    integrity update
    
    * Add docstring
    
    * Revert changes to code editor
    
    * Revert changes to codeeditor test

--- packages/fileeditor/src/widget.ts
+++ packages/fileeditor/src/widget.ts
@@ -2,6 +2,10 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
+  BoxLayout, Widget
+} from '@phosphor/widgets';
+
+import {
   IChangedArgs, PathExt
 } from '@jupyterlab/coreutils';
 
@@ -30,10 +34,10 @@ const EDITOR_CLASS = 'jp-FileEditor';
 
 
 /**
- * A document widget for editors.
+ * A code editor wrapper for the file editor.
  */
 export
-class FileEditor extends CodeEditorWrapper implements DocumentRegistry.IReadyWidget {
+class FileEditorCodeWrapper extends CodeEditorWrapper {
   /**
    * Construct a new editor widget.
    */
@@ -47,11 +51,9 @@ class FileEditor extends CodeEditorWrapper implements DocumentRegistry.IReadyWid
     const editor = this.editor;
 
     this.addClass(EDITOR_CLASS);
-    this._mimeTypeService = options.mimeTypeService;
+
     editor.model.value.text = context.model.toString();
-    context.pathChanged.connect(this._onPathChanged, this);
     context.ready.then(() => { this._onContextReady(); });
-    this._onPathChanged();
 
     if (context.model.modelDB.isCollaborative) {
       let modelDB = context.model.modelDB;
@@ -151,17 +153,6 @@ class FileEditor extends CodeEditorWrapper implements DocumentRegistry.IReadyWid
   }
 
   /**
-   * Handle a change to the path.
-   */
-  private _onPathChanged(): void {
-    const editor = this.editor;
-    const path = this._context.path;
-
-    editor.model.mimeType = this._mimeTypeService.getMimeTypeByFilePath(path);
-    this.title.label = PathExt.basename(path.split(':').pop()!);
-  }
-
-  /**
    * Handle a change to the collaborators on the model
    * by updating UI elements associated with them.
    */
@@ -180,8 +171,72 @@ class FileEditor extends CodeEditorWrapper implements DocumentRegistry.IReadyWid
   }
 
   protected _context: DocumentRegistry.Context;
-  private _mimeTypeService: IEditorMimeTypeService;
   private _ready = new PromiseDelegate<void>();
+}
+
+
+/**
+ * A document widget for editors.
+ */
+export
+class FileEditor extends Widget implements DocumentRegistry.IReadyWidget {
+  /**
+   * Construct a new editor widget.
+   */
+  constructor(options: FileEditor.IOptions) {
+    super();
+
+    const context = this._context = options.context;
+    this._mimeTypeService = options.mimeTypeService;
+
+    let editorWidget = this.editorWidget = new FileEditorCodeWrapper(options);
+    this.editor = editorWidget.editor;
+    this.model = editorWidget.model;
+
+    context.pathChanged.connect(this._onPathChanged, this);
+    this._onPathChanged();
+
+
+    let layout = this.layout = new BoxLayout();
+    let toolbar = new Widget();
+    toolbar.addClass('jp-Toolbar');
+
+    layout.addWidget(toolbar);
+    BoxLayout.setStretch(toolbar, 0);
+    layout.addWidget(editorWidget);
+    BoxLayout.setStretch(editorWidget, 1);
+  }
+
+  /**
+   * Get the context for the editor widget.
+   */
+  get context(): DocumentRegistry.Context {
+    return this.editorWidget.context;
+  }
+
+  /**
+   * A promise that resolves when the file editor is ready.
+   */
+  get ready(): Promise<void> {
+    return this.editorWidget.ready;
+  }
+
+  /**
+   * Handle a change to the path.
+   */
+  private _onPathChanged(): void {
+    const editor = this.editor;
+    const path = this._context.path;
+
+    editor.model.mimeType = this._mimeTypeService.getMimeTypeByFilePath(path);
+    this.title.label = PathExt.basename(path.split(':').pop()!);
+  }
+
+  private editorWidget: FileEditorCodeWrapper;
+  public model: CodeEditor.IModel;
+  public editor: CodeEditor.IEditor;
+  protected _context: DocumentRegistry.Context;
+  private _mimeTypeService: IEditorMimeTypeService;
 }
 
 
