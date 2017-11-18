8cbe9af9feb9b5df92007c4e7a88ea62489a7aea
Jason Grout <jgrout6@bloomberg.net>
2017-11-17 23:21:09 -0800

    Make new mathjax data structures for every render.

--- src/index.ts
+++ src/index.ts
@@ -35,38 +35,23 @@ MathJax.handlers.register(new HTMLHandler());
 export
 class MathJax3Typesetter implements IRenderMime.ILatexTypesetter {
 
-  constructor() {
-    // initialize mathjax with with a DOM document (e.g., browser, jsdom); other documents are possible
-    const chtml = new CHTML();
-    chtml.nodes.document = window.document;
-    this._html = MathJax.document(window.document, {
-      InputJax: new TeX({inlineMath: [['$', '$'], ['\\(', '\\)'] ]}),
-      OutputJax: chtml
-    });
-    }
-
   /**
    * Typeset the math in a node.
    */
   typeset(node: HTMLElement): void {
-    // Note: these functions set fields
-    // that indicate they have already been run,
-    // which prevents them from being rerun.
-    // I am not sure if this is intended behavior,
-    // but setting those fields to false forces
-    // the rerender.
-    this._html.processed.findMath = false;
-    this._html.processed.compile = false;
-    this._html.processed.getMetrics = false;
-    this._html.processed.typeset = false;
-    this._html.processed.updateDocument = false;
-    this._html.findMath({ elements: [node] })
+    const chtml = new CHTML();
+    const tex = new TeX({inlineMath: [['$', '$'], ['\\(', '\\)'] ]});
+    chtml.nodes.document = window.document;
+    const html = MathJax.document(window.document, {
+      InputJax: tex,
+      OutputJax: chtml
+    });
+    html.findMath({elements: [node]})
     .compile()
     .getMetrics()
     .typeset()
     .updateDocument();
   }
-  private _html: any;
 }
 
 /**
