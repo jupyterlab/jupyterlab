cf9432074f056466100efb9f8b15549a527a5804
Steven Silvester <steven.silvester@ieee.org>
2017-12-02 05:28:05 -0600

    switch to separate cmd script

---
+++ scripts/appveyor.cmd
@@ -0,0 +1,14 @@
+
+jlpm
+
+IF "%PYTHON_VERSION%"=="3.6" ( 
+    py.test
+    python -m jupyterlab.selenium_check
+    python -m jupyterlab.selenium_check --dev-mode
+) ELSE (
+    jlpm run integrity
+    jlpm run build
+    jlpm run build:test
+    jlpm test || jlpm test || jlpm test
+    jupyter lab build
+)
