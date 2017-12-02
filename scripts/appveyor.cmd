484e287e8f7c88dbc6181246e1f3b9f32fcd258c
Steven Silvester <steven.silvester@ieee.org>
2017-12-02 05:49:39 -0600

    appveyor refactor

--- scripts/appveyor.cmd
+++ scripts/appveyor.cmd
@@ -1,12 +1,11 @@
 
-jlpm
 
 IF "%PYTHON_VERSION%"=="3.6" ( 
     py.test -v
-    python -m jupyterlab.selenium_check
-    python -m jupyterlab.selenium_check --dev-mode
 ) ELSE (
     jlpm run integrity
+    python -m jupyterlab.selenium_check
+    python -m jupyterlab.selenium_check --dev-mode
     jlpm run build
     jlpm run build:test
     jlpm test || jlpm test || jlpm test
