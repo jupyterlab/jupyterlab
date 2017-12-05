3ca9006cf807aa4b5f4cf3358c488da1283e3f90
Jason Grout <jgrout6@bloomberg.net>
2017-12-04 21:59:19 -0800

    Move callback definition out of the constructor.

--- packages/application/src/shell.ts
+++ packages/application/src/shell.ts
@@ -72,22 +72,7 @@ class ApplicationShell extends Widget {
     let topPanel = this._topPanel = new Panel();
     let hboxPanel = this._hboxPanel = new BoxPanel();
     let dockPanel = this._dockPanel = new DockPanel();
-    let dockChildHook = this._dockChildHook = (handler, msg) => {
-      switch (msg.type) {
-        case 'child-added':
-          (msg as Widget.ChildMessage).child.addClass(ACTIVITY_CLASS);
-          this._tracker.add((msg as Widget.ChildMessage).child);
-          break;
-        case 'child-removed':
-          (msg as Widget.ChildMessage).child.removeClass(ACTIVITY_CLASS);
-          this._tracker.remove((msg as Widget.ChildMessage).child);
-          break;
-        default:
-          break;
-      }
-      return true;
-    };
-    MessageLoop.installMessageHook(dockPanel, dockChildHook);
+    MessageLoop.installMessageHook(dockPanel, this._dockChildHook);
 
     let hsplitPanel = this._hsplitPanel = new SplitPanel();
     let leftHandler = this._leftHandler = new Private.SideBarHandler('left');
@@ -623,10 +608,28 @@ class ApplicationShell extends Widget {
     this._layoutModified.emit(void 0);
   }
 
+  /**
+   * A message hook for child add/remove messages on the main area dock panel.
+   */
+  private _dockChildHook = (handler: IMessageHandler, msg: Message): boolean => {
+    switch (msg.type) {
+      case 'child-added':
+        (msg as Widget.ChildMessage).child.addClass(ACTIVITY_CLASS);
+        this._tracker.add((msg as Widget.ChildMessage).child);
+        break;
+      case 'child-removed':
+        (msg as Widget.ChildMessage).child.removeClass(ACTIVITY_CLASS);
+        this._tracker.remove((msg as Widget.ChildMessage).child);
+        break;
+      default:
+        break;
+    }
+    return true;
+  };
+
   private _activeChanged = new Signal<this, ApplicationShell.IChangedArgs>(this);
   private _cachedLayout: DockLayout.ILayoutConfig | null = null;
   private _currentChanged = new Signal<this, ApplicationShell.IChangedArgs>(this);
-  private _dockChildHook: (handler: IMessageHandler, msg: Message) => boolean;
   private _dockPanel: DockPanel;
   private _hboxPanel: BoxPanel;
   private _hsplitPanel: SplitPanel;
