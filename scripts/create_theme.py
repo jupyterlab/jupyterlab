c38ef84db321b53cd3136f2c39b97df292657ea7
Steven Silvester <steven.silvester@ieee.org>
2017-12-13 04:33:47 -0600

    add file header

--- scripts/create_theme.py
+++ scripts/create_theme.py
@@ -1,4 +1,8 @@
+# coding: utf-8
+"""JupyterLab command handler"""
 
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
 
 import pexpect
 child = pexpect.spawn('node buildutils/lib/create-theme.js')
