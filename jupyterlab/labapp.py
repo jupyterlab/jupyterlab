701320889c94e6a2517a20de1f41a1f1a3d905c8
Brian E. Granger <ellisonbg@gmail.com>
2017-12-17 16:38:56 -0800

    Fixing indentation of help string (#3402)
    * Fixing indentation of help string.
    
    * Another indentation formatting fix.

--- jupyterlab/labapp.py
+++ jupyterlab/labapp.py
@@ -123,9 +123,9 @@ class LabApp(NotebookApp):
       assets contained in the installed `jupyterlab` Python package. In core mode, no
       extensions are enabled. This is the default in a stable JupyterLab release if you
       have no extensions installed.
-    * Dev mode (`--dev-mode`): uses the unpublished local JavaScript packages
-        in the `dev_mode` folder.  In this case JupyterLab will show a red stripe at the top of the page.  It can only be used if JupyterLab
-        is installed as `pip install -e .`.
+    * Dev mode (`--dev-mode`): uses the unpublished local JavaScript packages in the
+      `dev_mode` folder.  In this case JupyterLab will show a red stripe at the top of
+      the page.  It can only be used if JupyterLab is installed as `pip install -e .`.
     * App mode: JupyterLab allows multiple JupyterLab "applications" to be
       created by the user with different combinations of extensions. The `--app-dir` can
       be used to set a directory for different applications. The default application
@@ -165,8 +165,9 @@ class LabApp(NotebookApp):
         """)
 
     dev_mode = Bool(False, config=True,
-        help="""Whether to start the app in dev mode. Uses the unpublished local JavaScript packages
-        in the `dev_mode` folder.  In this case JupyterLab will show a red stripe at the top of the page.  It can only be used if JupyterLab
+        help="""Whether to start the app in dev mode. Uses the unpublished local
+        JavaScript packages in the `dev_mode` folder.  In this case JupyterLab will
+        show a red stripe at the top of the page.  It can only be used if JupyterLab
         is installed as `pip install -e .`.
         """)
 
