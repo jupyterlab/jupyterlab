e2b3e59f328ee9cb6b94ae713bb9ae2eb2a3781f
Steven Silvester <steven.silvester@ieee.org>
2017-12-19 04:25:55 -0600

    Fix handling of base url

--- dev_mode/webpack.config.js
+++ dev_mode/webpack.config.js
@@ -109,7 +109,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(buildDir),
-    publicPath: 'lab/static/',
+    publicPath: '{{base_url}}lab/static/',
     filename: '[name].[chunkhash].js'
   },
   module: {
