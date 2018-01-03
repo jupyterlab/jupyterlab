755bf38110cdf8aaaa083ce6aeae3252284921c1
Steven Silvester <steven.silvester@ieee.org>
2018-01-03 12:08:37 -0600

    Add ellipsis

--- packages/mainmenu-extension/src/index.ts
+++ packages/mainmenu-extension/src/index.ts
@@ -323,7 +323,7 @@ function createKernelMenu(app: JupyterLab, menu: KernelMenu): void {
   });
 
   commands.addCommand(CommandIDs.shutdownAllKernels, {
-    label: 'Shutdown All Kernels',
+    label: 'Shutdown All Kernels...',
     isEnabled: () => {
       return app.serviceManager.sessions.running().next() !== undefined;
     },
