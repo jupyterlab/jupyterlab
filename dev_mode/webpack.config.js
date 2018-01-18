f5006516eab397307ee6709bcf86612ddd1c1f8b
Steven Silvester <steven.silvester@ieee.org>
2018-01-18 12:37:06 -0600

    use full source map always

--- dev_mode/webpack.config.js
+++ dev_mode/webpack.config.js
@@ -160,7 +160,7 @@ module.exports = {
     fs: 'empty'
   },
   bail: true,
-  devtool: 'cheap-module-eval-sourcemap',
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join('templates', 'template.html'),
