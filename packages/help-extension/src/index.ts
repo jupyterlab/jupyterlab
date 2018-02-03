16076a4e44fd920fe14eccb3707548cc2139fa41
Steven Silvester <steven.silvester@ieee.org>
2018-02-03 16:04:06 -0600

    Update language from Beta 1 -> Beta Release Series

--- packages/help-extension/src/index.ts
+++ packages/help-extension/src/index.ts
@@ -311,7 +311,7 @@ function activate(app: JupyterLab, mainMenu: IMainMenu, palette: ICommandPalette
       // Create the header of the about dialog
       let headerLogo = h.div({className: 'jp-About-header-logo'});
       let headerWordmark = h.div({className: 'jp-About-header-wordmark'});
-      let release = 'Beta 1 Release';
+      let release = 'Beta Release Series';
       let versionNumber = `Version ${info.version}`;
       let versionInfo = h.span({className: 'jp-About-version-info'},
         h.span({className: 'jp-About-release'}, release),
