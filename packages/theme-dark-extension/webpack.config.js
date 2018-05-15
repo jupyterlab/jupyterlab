49b19737e95fc522fbdcd84ce3cccfe5d93182e2
Saul Shanabrook <s.shanabrook@gmail.com>
2018-05-15 16:11:44 -0400

    change to single quote strings

--- packages/theme-dark-extension/webpack.config.js
+++ packages/theme-dark-extension/webpack.config.js
@@ -1,34 +1,34 @@
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: {
-    index: "./style/index.css",
-    embed: "./style/embed.css"
+    index: './style/index.css',
+    embed: './style/embed.css'
   },
   output: {
-    path: path.resolve(__dirname, "static"),
+    path: path.resolve(__dirname, 'static'),
     // we won't use these JS files, only the extracted CSS
-    filename: "[name].js"
+    filename: '[name].js'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
+          fallback: 'style-loader',
+          use: 'css-loader'
         })
       },
       {
         test: /\.svg/,
         use: [
           {
-            loader: "svg-url-loader",
+            loader: 'svg-url-loader',
             options: {}
           },
           {
-            loader: "svgo-loader",
+            loader: 'svgo-loader',
             options: {
               plugins: []
             }
@@ -39,7 +39,7 @@ module.exports = {
         test: /\.(png|jpg|gif|ttf|woff|woff2|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 10000
             }
@@ -48,5 +48,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [new ExtractTextPlugin("[name].css")]
+  plugins: [new ExtractTextPlugin('[name].css')]
 };
