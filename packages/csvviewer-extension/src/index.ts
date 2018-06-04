490462fe1691d1d38d2db2f082d07993084315f6
Jiri Vrany <jiri.vrany@tul.cz>
2018-06-04 20:39:45 +0200

    refactored the solution to in module functions for cleaner code

--- packages/csvviewer-extension/src/index.ts
+++ packages/csvviewer-extension/src/index.ts
@@ -2,19 +2,19 @@
 // Distributed under the terms of the Modified BSD License.
 
 import {
-    ILayoutRestorer, JupyterLab, JupyterLabPlugin
+  ILayoutRestorer, JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
 import {
-    InstanceTracker
+  InstanceTracker
 } from '@jupyterlab/apputils';
 
 import {
-    CSVViewer, CSVViewerFactory, TSVViewerFactory
+  CSVViewer, CSVViewerFactory, TSVViewerFactory
 } from '@jupyterlab/csvviewer';
 
 import {
-    IDocumentWidget
+  IDocumentWidget
 } from '@jupyterlab/docregistry';
 
 /**
@@ -29,82 +29,95 @@ const FACTORY_TSV = 'TSVTable';
  */
 
 const csv: JupyterLabPlugin<void> = {
-    activate: (app: JupyterLab, restorer: ILayoutRestorer): void => {
-        const factory = new CSVViewerFactory({
-            name: FACTORY_CSV,
-            fileTypes: ['csv'],
-            defaultFor: ['csv'],
-            readOnly: true
-        });
-        const tracker = new InstanceTracker<IDocumentWidget<CSVViewer>>({ namespace: 'csvviewer' });
-
-        // Handle state restoration.
-        restorer.restore(tracker, {
-            command: 'docmanager:open',
-            args: widget => ({ path: widget.context.path, factory: FACTORY_CSV }),
-            name: widget => widget.context.path
-        });
-
-        app.docRegistry.addWidgetFactory(factory);
-        let ft = app.docRegistry.getFileType('csv');
-        factory.widgetCreated.connect((sender, widget) => {
-            // Track the widget.
-            tracker.add(widget);
-            // Notify the instance tracker if restore data needs to update.
-            widget.context.pathChanged.connect(() => { tracker.save(widget); });
-
-            if (ft) {
-                widget.title.iconClass = ft.iconClass;
-                widget.title.iconLabel = ft.iconLabel;
-            }
-        });
-    },
-    id: '@jupyterlab/csvviewer-extension:csv',
-    requires: [ILayoutRestorer],
-    autoStart: true
+  activate: activateCsv,
+  id: '@jupyterlab/csvviewer-extension:csv',
+  requires: [ILayoutRestorer],
+  autoStart: true
 };
-
 
 
 /**
  * The TSV file handler extension.
  */
 const tsv: JupyterLabPlugin<void> = {
-    activate: (app: JupyterLab, restorer: ILayoutRestorer): void => {
-        const factory = new TSVViewerFactory({
-            name: FACTORY_TSV,
-            fileTypes: ['tsv'],
-            defaultFor: ['tsv'],
-            readOnly: true
-        });
-        const tracker = new InstanceTracker<IDocumentWidget<CSVViewer>>({ namespace: 'tsvviewer' });
-
-        // Handle state restoration.
-        restorer.restore(tracker, {
-            command: 'docmanager:open',
-            args: widget => ({ path: widget.context.path, factory: FACTORY_TSV }),
-            name: widget => widget.context.path
-        });
-
-        app.docRegistry.addWidgetFactory(factory);
-        let ft = app.docRegistry.getFileType('tsv');
-        factory.widgetCreated.connect((sender, widget) => {
-            // Track the widget.
-            tracker.add(widget);
-            // Notify the instance tracker if restore data needs to update.
-            widget.context.pathChanged.connect(() => { tracker.save(widget); });
-
-            if (ft) {
-                widget.title.iconClass = ft.iconClass;
-                widget.title.iconLabel = ft.iconLabel;
-            }
-        });
-    },
-    id: '@jupyterlab/csvviewer-extension:tsv',
-    requires: [ILayoutRestorer],
-    autoStart: true
+  activate: activateTsv,
+  id: '@jupyterlab/csvviewer-extension:tsv',
+  requires: [ILayoutRestorer],
+  autoStart: true
 };
 
+
+/**
+ * Activate cssviewer extension for CSV files
+ */
+function activateCsv(app: JupyterLab, restorer: ILayoutRestorer): void {
+  const factory = new CSVViewerFactory({
+    name: FACTORY_CSV,
+    fileTypes: ['csv'],
+    defaultFor: ['csv'],
+    readOnly: true
+  });
+  const tracker = new InstanceTracker<IDocumentWidget<CSVViewer>>({namespace: 'csvviewer'});
+
+  // Handle state restoration.
+  restorer.restore(tracker, {
+    command: 'docmanager:open',
+    args: widget => ({path: widget.context.path, factory: FACTORY_CSV}),
+    name: widget => widget.context.path
+  });
+
+  app.docRegistry.addWidgetFactory(factory);
+  let ft = app.docRegistry.getFileType('csv');
+  factory.widgetCreated.connect((sender, widget) => {
+    // Track the widget.
+    tracker.add(widget);
+    // Notify the instance tracker if restore data needs to update.
+    widget.context.pathChanged.connect(() => {
+      tracker.save(widget);
+    });
+
+    if (ft) {
+      widget.title.iconClass = ft.iconClass;
+      widget.title.iconLabel = ft.iconLabel;
+    }
+  });
+};
+
+/**
+ * Activate cssviewer extension for TSV files
+ */
+function activateTsv(app: JupyterLab, restorer: ILayoutRestorer): void {
+  const factory = new TSVViewerFactory({
+    name: FACTORY_TSV,
+    fileTypes: ['tsv'],
+    defaultFor: ['tsv'],
+    readOnly: true
+  });
+  const tracker = new InstanceTracker<IDocumentWidget<CSVViewer>>({namespace: 'tsvviewer'});
+
+  // Handle state restoration.
+  restorer.restore(tracker, {
+    command: 'docmanager:open',
+    args: widget => ({path: widget.context.path, factory: FACTORY_TSV}),
+    name: widget => widget.context.path
+  });
+
+  app.docRegistry.addWidgetFactory(factory);
+  let ft = app.docRegistry.getFileType('tsv');
+  factory.widgetCreated.connect((sender, widget) => {
+    // Track the widget.
+    tracker.add(widget);
+    // Notify the instance tracker if restore data needs to update.
+    widget.context.pathChanged.connect(() => {
+      tracker.save(widget);
+    });
+
+    if (ft) {
+      widget.title.iconClass = ft.iconClass;
+      widget.title.iconLabel = ft.iconLabel;
+    }
+  });
+};
 
 /**
  * Export the plugins as default.
