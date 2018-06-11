cf8129237e4c2ba27551c3670a8ee2927c629c40
Jason Grout <jgrout6@bloomberg.net>
2018-06-11 14:05:34 -0700

    Restore old minification options.

--- dev_mode/webpack.prod.config.js
+++ dev_mode/webpack.prod.config.js
@@ -1,7 +1,24 @@
+var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 var merge = require('webpack-merge');
 var common = require('./webpack.config');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        uglifyOptions: {
+          beautify: false,
+          comments: false,
+          compress: false,
+          ecma: 6,
+          mangle: true
+        }
+      })
+    ]
+  }
 });
