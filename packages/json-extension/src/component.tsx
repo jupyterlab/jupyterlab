e125a474d971e4864bb5e5d4858aad12b64700eb
Jason Grout <jgrout6@bloomberg.net>
2018-06-12 08:11:39 -0700

    Switch json highlighter to use the esModuleInterop default export.

--- packages/json-extension/src/component.tsx
+++ packages/json-extension/src/component.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import * as ReactDOM from 'react-dom';
 
-import Highlight = require('react-highlighter');
+import Highlight from 'react-highlighter';
 
 import JSONTree from 'react-json-tree';
 
