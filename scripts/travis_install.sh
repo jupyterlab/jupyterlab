d384a8fb1b5d5ed9f444021181ff0c2b560cc1dd
Saul Shanabrook <s.shanabrook@gmail.com>
2018-06-15 16:27:55 -0400

    fail quicker

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -5,6 +5,10 @@
 set -ev
 set -o pipefail
 
+# Building should work without yarn installed globally
+rm /home/travis/.yarn/bin/*
+!yarn --version
+
 # The miniconda directory may exist if it has been restored from cache
 if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
     echo "Miniconda install already present from cache: $MINICONDA_DIR"
@@ -31,10 +35,6 @@ set -ev
 
 # create jupyter base dir (needed for config retrieval)
 mkdir ~/.jupyter
-
-# Building should work without yarn installed globally
-rm /home/travis/.yarn/bin/*
-!yarn --version
 
 # Install and enable the server extension
 pip install -q -e ".[test]"
