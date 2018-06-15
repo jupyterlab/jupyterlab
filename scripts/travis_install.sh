62a8a03ce9aa745b01512db63f7fd388b299fc81
Saul Shanabrook <s.shanabrook@gmail.com>
2018-06-15 11:57:18 -0400

    try again with wget

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -11,7 +11,7 @@ if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
 else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -b -O ~/miniconda.sh;
-    bash ~/miniconda.sh -b -p "$MINICONDA_DIR"
+    bash ~/miniconda.sh -nv -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
     hash -r
 fi
