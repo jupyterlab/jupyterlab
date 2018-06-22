16b636862c18a611aa482642722060447f53b3ce
Noah Stapp <noah@noahstapp.com>
2018-06-22 15:44:10 -0700

    Basic functionality finished

--- jupyterlab-shortcutui/src/index.tsx
+++ jupyterlab-shortcutui/src/index.tsx
@@ -7,20 +7,8 @@ import {
 } from '@jupyterlab/coreutils';
 
 import {
-  CommandRegistry
-} from '@phosphor/commands';
-
-import {
-  JSONObject, JSONValue
+  JSONValue
 } from '@phosphor/coreutils';
-
-import {
-  DisposableSet, IDisposable
-} from '@phosphor/disposable';
-
-// import {
-//   Widget
-// } from '@phosphor/widgets'; 
 
 import {
   ICommandPalette
@@ -34,85 +22,102 @@ import * as React from 'react';
 
 import '../style/index.css';
 
-/**
- * The default shortcuts extension.
- *
- * #### Notes
- * Shortcut values are stored in the setting system. The default values for each
- * shortcut are preset in the settings schema file of this extension.
- * Additionally, each shortcut can be individually set by the end user by
- * modifying its setting (either in the text editor or by modifying its
- * underlying JSON file).
- *
- * When setting shortcut selectors, there are two concepts to consider:
- * specificity and matchability. These two interact in sometimes
- * counterintuitive ways. Keyboard events are triggered from an element and
- * they propagate up the DOM until they reach the `documentElement` (`<body>`).
- *
- * When a registered shortcut sequence is fired, the shortcut manager checks
- * the node that fired the event and each of its ancestors until a node matches
- * one or more registered selectors. The *first* matching selector in the
- * chain of ancestors will invoke the shortcut handler and the traversal will
- * end at that point. If a node matches more than one selector, the handler for
- * whichever selector is more *specific* fires.
- * @see https://www.w3.org/TR/css3-selectors/#specificity
- *
- * The practical consequence of this is that a very broadly matching selector,
- * e.g. `'*'` or `'div'` may match and therefore invoke a handler *before* a
- * more specific selector. The most common pitfall is to use the universal
- * (`'*'`) selector. For almost any use case where a global keyboard shortcut is
- * required, using the `'body'` selector is more appropriate.
- */
-
-class ShortCutListWidget extends ReactElementWidget {
-  constructor(es: React.ReactElement<any>) {
-    super(es);
-  }
-}
-
 interface ShortcutListProps {
-  keyBindings: Array<CommandRegistry.IKeyBinding>;
+  commandList: string[];
+  settingRegistry: ISettingRegistry;
+  shortcutPlugin: string;
 }
-
- interface ShortcutListState {
-  keyBindings?: ReadonlyArray<CommandRegistry.IKeyBinding>;
- }
 
  interface ShortcutListItemProps {
    key: string;
-   command: CommandRegistry.IKeyBinding;
+   command: string;
+   settingRegistry: ISettingRegistry;
+   shortcutPlugin: string;
  }
 
-class ShortcutListItem extends React.Component<ShortcutListItemProps> {
+ interface ShortcutListItemState {
+   value: string;
+   keyBinding: JSONValue;
+   keyBindingFetched: boolean;
+ }
+
+class ShortcutListItem extends React.Component<ShortcutListItemProps, ShortcutListItemState> {
   constructor(props) {
     super(props);
+    this.state = {
+      value: "",
+      keyBindingFetched: false,
+      keyBinding: undefined
+    };
   }
+
+  componentDidMount() {
+    this.getCommandKeybinding(this.state.keyBinding);
+  }
+
+  handleUpdate = () => {
+    let removeKeybindingPromise = this.props.settingRegistry.remove(this.props.shortcutPlugin, this.props.command);
+    let setKeybindingPromise = this.props.settingRegistry.set(this.props.shortcutPlugin, this.props.command, {command: this.props.command, keys: [this.state.value], selector: this.props.command['selector']});
+    Promise.all([removeKeybindingPromise, setKeybindingPromise]);
+    this.setState({value:""});
+    this.getCommandKeybinding(this.state.keyBinding);
+  }
+
+  handleInput = (event) => {
+    if (event.key=="Backspace"){
+      this.setState({value: this.state.value.substr(0, this.state.value.lastIndexOf(' ')+1)});
+    }
+    else if (event.key == "Control"){
+      this.setState({value: this.state.value + " Ctrl"});
+    }
+    else if (event.key == "Meta"){
+      this.setState({value: this.state.value + " Accel"});
+    }
+    else if (event.key == "Alt" || event.key == "Shift" || event.key == "Enter" || event.ctrlKey || event.metaKey) {
+      this.setState({value: this.state.value + " " + event.key});
+    }
+    else {
+      this.setState({value: this.state.value + " "});
+    }
+  }
+
+  updateInputValue = (event) => {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  getCommandKeybinding = (keyBinding: JSONValue) => {
+    this.props.settingRegistry.get(this.props.shortcutPlugin, this.props.command).then(result => {
+      if(result != undefined) {
+        this.setState({keyBinding :result.composite});
+      }
+    }).then(result => this.setState({keyBindingFetched: true}));
+  }
+
   render() {
+    if(!this.state.keyBindingFetched) { 
+      return null
+    };
     return (
       <div className="jp-cmditem">
-        <div className="jp-cmdlabel">{this.props.command['command']}-{this.props.command['keys']}</div>
+        <div className="jp-cmdlabel">{this.props.command} {(this.state.keyBinding === undefined ? '' : this.state.keyBinding['keys'])}</div>
+        <input className="jp-input" value={this.state.value} onChange={this.updateInputValue} onKeyDown={this.handleInput}></input>
+        <button className="jp-button" onClick={this.handleUpdate}>Submit</button>
       </div>
     );
   }
 }
 
-class ShortcutList extends React.Component<ShortcutListProps, ShortcutListState> {
+class ShortcutList extends React.Component<ShortcutListProps, {}> {
   constructor(props) {
     super(props);
-    this.state = {keyBindings: this.props.keyBindings};
-    console.log('state:');
-    console.log(this.state);
-  }
-
-  updateCommands = () => {
-    this.setState({keyBindings: this.props.keyBindings});
-    console.log('state:');
-    console.log(this.state);
   }
 
   render() {
-    let commandItems = this.state.keyBindings.map(item => 
-      <ShortcutListItem key={item.command + item.keys[0]} command={item}/>
+    let commandItems: Array<JSX.Element> = new Array<JSX.Element>();
+    this.props.commandList.forEach(command => 
+      commandItems.push(<ShortcutListItem shortcutPlugin = {this.props.shortcutPlugin} key={command} command={command} settingRegistry={this.props.settingRegistry}/>)
       );
     return (
       <div className="jp-shortcutlist">
@@ -122,19 +127,12 @@ class ShortcutList extends React.Component<ShortcutListProps, ShortcutListState>
   }
 }
 
-//  function displayShortcuts(id: string, settingReqistry: ISettingRegistry, widget: ShortCutListWidget) {
-//   widget.es.updateCommands();
-//  }
-
 const plugin: JupyterLabPlugin<void> = {
   id: '@jupyterlab/jupyterlab-shortcutui:plugin',
   requires: [ISettingRegistry, ICommandPalette],
-  activate: (app: JupyterLab, settingReqistry: ISettingRegistry, palette: ICommandPalette): void => {
-    console.log('jupyterlab-shortcutui activated!');
-    const { commands } = app;
-
-    let shortcutList = React.createElement(ShortcutList, {keyBindings: app.commands.keyBindings});
-    let widget: ShortCutListWidget = new ShortCutListWidget(shortcutList);
+  activate: (app: JupyterLab, settingRegistry: ISettingRegistry, palette: ICommandPalette): void => {
+    let shortcutList = React.createElement(ShortcutList, {commandList: app.commands.listCommands(), settingRegistry: settingRegistry, shortcutPlugin: '@jupyterlab/shortcuts-extension:plugin'});
+    let widget: ReactElementWidget = new ReactElementWidget(shortcutList);
     widget.id = 'jupyterlab-shortcutui';
     widget.title.label = 'Shortcut UI';
     widget.title.closable = true;
@@ -155,18 +153,8 @@ const plugin: JupyterLabPlugin<void> = {
     }); 
 
     palette.addItem({command, category: 'AAA'});
-
-    // Load command settings
-    settingReqistry.load(plugin.id).then(settings => {
-      Private.loadShortcuts(commands, settings.composite, plugin.id, settingReqistry);
-      settings.changed.connect(() => {
-        Private.loadShortcuts(commands, settings.composite, plugin.id, settingReqistry);
-      });
-    }).catch((reason: Error) => {
-      console.error('Loading shortcut settings failed.', reason.message);
-    });
-},
-autoStart: true
+    },
+    autoStart: true
 };
 
 
@@ -174,51 +162,3 @@ autoStart: true
  * Export the plugin as default.
  */
 export default plugin;
-
-
-/**
- * A namespace for private module data.
- */
-namespace Private {
-  /**
-   * The internal collection of currently loaded shortcuts.
-   */
-  let disposables: IDisposable;
-
-  /**
-   * Load the keyboard shortcuts from settings.
-   */
-  export
-  function loadShortcuts(commands: CommandRegistry, composite: JSONObject, id: string, settingReqistry: ISettingRegistry): void {
-    console.log('Changed shortcut!');
-    if (disposables) {
-      disposables.dispose();
-    }
-    disposables = Object.keys(composite).reduce((acc, val): DisposableSet => {
-      const options = normalizeOptions(composite[val]);
-
-      if (options) {
-        acc.add(commands.addKeyBinding(options));
-      }
-
-      return acc;
-    }, new DisposableSet());
-  }
-
-  /**
-   * Normalize potential keyboard shortcut options.
-   */
-  function normalizeOptions(value: JSONValue | Partial<CommandRegistry.IKeyBindingOptions>): CommandRegistry.IKeyBindingOptions | undefined {
-    if (!value || typeof value !== 'object') {
-      return undefined;
-    }
-
-    const { isArray } = Array;
-    const valid = 'command' in value &&
-      'keys' in value &&
-      'selector' in value &&
-      isArray((value as Partial<CommandRegistry.IKeyBindingOptions>).keys);
-
-    return valid ? value as CommandRegistry.IKeyBindingOptions : undefined;
-  }
-}
