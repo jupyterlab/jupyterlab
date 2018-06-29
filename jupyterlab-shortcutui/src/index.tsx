ec61ea81818d1c845e0c81c31f42b72ab615358e
Jenna Landy <jennalandy0@gmail.com>
2018-06-29 10:50:18 -0700

    Updating for style guide

--- jupyterlab-shortcutui/src/index.tsx
+++ jupyterlab-shortcutui/src/index.tsx
@@ -6,10 +6,6 @@ import {
   ISettingRegistry
 } from '@jupyterlab/coreutils';
 
-// import {
-//   JSONValue //, JSONObject
-// } from '@phosphor/coreutils';
-
 import {
   ICommandPalette
 } from '@jupyterlab/apputils';
@@ -22,76 +18,73 @@ import * as React from 'react';
 
 import '../style/index.css';
 
-//import { CommandPalette } from '@phosphor/widgets';
-
-
-interface UserInterfaceProps {
+interface IUserInterfaceProps {
   commandList: string[];
   settingRegistry: ISettingRegistry;
   shortcutPlugin: string;
   categories: Array<string>;
  }
 
- interface UserInterfaceState {
+ interface IUserInterfaceState {
    shortcutList: Object[];
    shortcutsFetched: boolean;
  }
 
-interface ShortcutListProps {
+interface IShortcutListProps {
   shortcuts: Object[]
   handleUpdate: Function;
   resetShortcut: Function;
 }
 
-interface TopNavProps {
+interface ITopNavProps {
   resetShortcuts: Function;
 }
 
-interface ShortcutCategoryProps {
+interface IShortcutCategoryProps {
   category: string;
   shortcuts: Object[];
   handleUpdate: Function;
   resetShortcut: Function;
 }
 
-interface ShortcutItemProps {
+interface IShortcutItemProps {
   command: Object;
   category: string;
   handleUpdate: Function;
   resetShortcut: Function;
 }
 
-interface ShortcutItemState {
+interface IShortcutItemState {
   value: string;
   displayInput: boolean;
 }
 
 /* React component for each command shortcut item */
-class ShortcutItem extends React.Component<ShortcutItemProps, ShortcutItemState> {
+class ShortcutItem extends React.Component<IShortcutItemProps, IShortcutItemState> {
   constructor(props) {
     super(props);
     this.state = {
-      value: "",
+      value: '',
       displayInput: false
     }
   }
 
   /* Parse and normalize user input */
   handleInput = (event: any) => {
-    if (event.key == "Backspace"){
+    if (event.key == 'Backspace'){
       this.setState({value: this.state.value.substr(0, this.state.value.lastIndexOf(' ') + 1)});
     }
-    else if (event.key == "Control"){
-      this.setState({value: this.state.value + " Ctrl"});
+    else if (event.key == 'Control'){
+      this.setState({value: this.state.value + ' Ctrl'});
     }
-    else if (event.key == "Meta"){
-      this.setState({value: this.state.value + " Accel"});
+    else if (event.key == 'Meta'){
+      this.setState({value: this.state.value + ' Accel'});
     }
-    else if (event.key == "Alt" || event.key == "Shift" || event.key == "Enter" || event.ctrlKey || event.metaKey) {
-      this.setState({value: this.state.value + " " + event.key});
+    else if (event.key == 'Alt' || event.key == 'Shift' || event.key == 'Enter' || event.ctrlKey || event.metaKey) {
+      this.setState({value: this.state.value + ' ' + event.key});
     }
     else {
-      this.setState({value: this.state.value + " "});
+      this.setState({value: this.state.value + ' '});
     }
   }
 
@@ -106,7 +99,7 @@ class ShortcutItem extends React.Component<ShortcutItemProps, ShortcutItemState>
   toggleInput = () => {
     this.setState(prevState => ({
       displayInput: !prevState.displayInput,
-      value: ""
+      value: ''
     }));
   }
 
@@ -114,37 +107,37 @@ class ShortcutItem extends React.Component<ShortcutItemProps, ShortcutItemState>
   createLabel = (): string => {
     let commandLabel: string;
     let commandLabelArray: string[]
-    commandLabel = this.props.command['composite']['command'].split(":")[1].replace(/-/g, " ");
-    commandLabelArray = commandLabel.split(" ");
+    commandLabel = this.props.command['composite']['command'].split(':')[1].replace(/-/g, ' ');
+    commandLabelArray = commandLabel.split(' ');
     commandLabelArray = commandLabelArray.map(function(item) {
       return item.charAt(0).toUpperCase() + item.substring(1);
     })
-    commandLabel = commandLabelArray.toString().replace(/,/g, " ");
+    commandLabel = commandLabelArray.toString().replace(/,/g, ' ');
     return commandLabel;
   }
 
   render() {
     return (
-      <div className="jp-cmditem">
-        <div className="jp-label-container">
-          <div className="jp-label">{this.createLabel()}</div>
+      <div className='jp-cmditem'>
+        <div className='jp-label-container'>
+          <div className='jp-label'>{this.createLabel()}</div>
         </div>
-        <div className="jp-shortcut-container">
-          <button className="jp-shortcut" onClick={() => this.props.handleUpdate(this.props.command, "")}>{this.props.command['composite']['keys']}</button>
-          <span className="jp-input-plus" onClick={this.toggleInput}>+</span>
+        <div className='jp-shortcut-container'>
+          <button className='jp-shortcut' onClick={() => this.props.handleUpdate(this.props.command, '')}>{this.props.command['composite']['keys']}</button>
+          <span className='jp-input-plus' onClick={this.toggleInput}>+</span>
           {(this.state.displayInput ? (
-            <div className="jp-input-container">
-              <input className="jp-input" value={this.state.value} onChange={this.updateInputValue} onKeyDown={this.handleInput}></input>
-              <button className="jp-submit" onClick={() => {this.props.handleUpdate(this.props.command, this.state.value); this.toggleInput();}}>Submit</button>
+            <div className='jp-input-container'>
+              <input className='jp-input' value={this.state.value} onChange={this.updateInputValue} onKeyDown={this.handleInput}></input>
+              <button className='jp-submit' onClick={() => {this.props.handleUpdate(this.props.command, this.state.value); this.toggleInput();}}>Submit</button>
             </div>
           ) : (
             null
             )
           )}
         </div>
-        <div className="jp-source-container">
-          <div className="jp-source">{"Default"}</div>
-          <a className="jp-reset" onClick={() =>this.props.resetShortcut(this.props.command['composite']['command'])}>reset</a>
+        <div className='jp-source-container'>
+          <div className='jp-source'>{'Default'}</div>
+          <a className='jp-reset' onClick={() =>this.props.resetShortcut(this.props.command['composite']['command'])}>reset</a>
         </div>
       </div>
     );
@@ -152,7 +145,7 @@ class ShortcutItem extends React.Component<ShortcutItemProps, ShortcutItemState>
 }
 
 /* React component for listing each command category */
-class ShortcutCategory extends React.Component<ShortcutCategoryProps, {}> {
+class ShortcutCategory extends React.Component<IShortcutCategoryProps, {}> {
   constructor(props) {
     super(props);
   }
@@ -160,7 +153,7 @@ class ShortcutCategory extends React.Component<ShortcutCategoryProps, {}> {
   render() {
     console.log(this.props.shortcuts);
     return (
-      <div className="jp-shortcut-category">
+      <div className='jp-shortcut-category'>
         <p>{this.props.category}</p>
         {this.props.shortcuts.map(obj => <ShortcutItem key={obj['composite']['command']} resetShortcut={this.props.resetShortcut} command={obj} category={this.props.category} handleUpdate={this.props.handleUpdate}/>)}
       </div>
@@ -169,30 +162,30 @@ class ShortcutCategory extends React.Component<ShortcutCategoryProps, {}> {
 }
 
 /*React component for top navigation*/
-class TopNav extends React.Component<TopNavProps,{}> {
+class TopNav extends React.Component<ITopNavProps,{}> {
   render() {return (
-    <div className="jp-shortcuttopnav">
-      <a className="jp-link" onClick={() => this.props.resetShortcuts()}>Reset All</a>
-      <div className = "jp-searchcontainer">
-        <input className="jp-search"></input>
+    <div className='jp-shortcuttopnav'>
+      <a className='jp-link' onClick={() => this.props.resetShortcuts()}>Reset All</a>
+      <div className = 'jp-searchcontainer'>
+        <input className='jp-search'></input>
       </div>
-      <a className="jp-link">Advanced Editor</a>
+      <a className='jp-link'>Advanced Editor</a>
     </div>
   )}
 }
 
 /* React component for list of all categories and shortcuts */
-class ShortcutList extends React.Component<ShortcutListProps, {}> {
+class ShortcutList extends React.Component<IShortcutListProps, {}> {
   render() {
     return (
-      <div className="jp-shortcutlist">
-        <div className="jp-shortcutlistheader">
-          <div className="jp-col1">Command</div>
-          <div className="jp-col2">Shortcut</div>
-          <div className="jp-col3">Source</div>
+      <div className='jp-shortcutlist'>
+        <div className='jp-shortcutlistheader'>
+          <div className='jp-col1'>Command</div>
+          <div className='jp-col2'>Shortcut</div>
+          <div className='jp-col3'>Source</div>
         </div>
-        <div className="jp-shortcutlistmain">
-          <div className="jp-shortcutlistcontainer">
+        <div className='jp-shortcutlistmain'>
+          <div className='jp-shortcutlistcontainer'>
             {this.props.shortcuts.map((obj, index) => <ShortcutCategory handleUpdate={this.props.handleUpdate} resetShortcut={this.props.resetShortcut} key={obj['category']} category={obj['category']} shortcuts={obj['shortcuts']}/>)}
           </div>
         </div>
@@ -201,7 +194,7 @@ class ShortcutList extends React.Component<ShortcutListProps, {}> {
   }
 }
 
-class ShortcutUI extends React.Component<UserInterfaceProps, UserInterfaceState> {
+class ShortcutUI extends React.Component<IUserInterfaceProps, IUserInterfaceState> {
   constructor(props) {
     super(props);
   }
@@ -224,7 +217,7 @@ class ShortcutUI extends React.Component<UserInterfaceProps, UserInterfaceState>
 
     Promise.all(shortcutList).then(shortcuts => {
       let commandShortcuts = this.props.categories.map(category => {
-        return {category: category, shortcuts: shortcuts.filter(shortcut => shortcut.composite['command'].split(":")[0] === category)};
+        return {category: category, shortcuts: shortcuts.filter(shortcut => shortcut.composite['command'].split(':')[0] === category)};
       })
       this.setState({shortcutList: commandShortcuts, shortcutsFetched: true})
     })
@@ -262,7 +255,7 @@ class ShortcutUI extends React.Component<UserInterfaceProps, UserInterfaceState>
       return null
     }
     return (
-      <div className = "jp-shortcutui">
+      <div className = 'jp-shortcutui'>
         <div className = 'jp-topwhitespace'></div>
         <TopNav resetShortcuts={this.resetShortcuts}/>
         <ShortcutList shortcuts={this.state.shortcutList} resetShortcut={this.resetShortcut} handleUpdate={this.handleUpdate}/>
@@ -281,9 +274,8 @@ const plugin: JupyterLabPlugin<void> = {
       commandlist.push(key); })).then(settings => {
       let categories: Array<string> = new Array<string>();
       commandlist.forEach(command => { 
-        if(categori
-          es.indexOf(command.split(":")[0]) === -1) {
-          categories.push(command.split(":")[0]);
+        if(categories.indexOf(command.split(':')[0]) === -1) {
+          categories.push(command.split(':')[0]);
         }
       });
 
