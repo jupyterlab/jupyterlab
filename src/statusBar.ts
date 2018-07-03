340ab411001a32a194f4b4e94afc3cd6b1a307f9
Declan Kelly <dkelly.home@gmail.com>
2018-07-03 10:00:47 -0700

    Implement priority-based insertion for items
    Also formatting changes

--- src/statusBar.ts
+++ src/statusBar.ts
@@ -1,42 +1,33 @@
-import {
-  Widget, Panel, PanelLayout
-} from '@phosphor/widgets';
+import { Widget, Panel, PanelLayout } from '@phosphor/widgets';
 
-import {
-  Token
-} from '@phosphor/coreutils';
+import { Token } from '@phosphor/coreutils';
 
-import {
-  ApplicationShell
-} from '@jupyterlab/application';
+import { ApplicationShell } from '@jupyterlab/application';
+import { ArrayExt } from '@phosphor/algorithm';
 
-// import {
-//   IIterable, IIterator, iter
-// } from '@phosphor/algorithm';
-
-export
 // tslint:disable-next-line:variable-name
-const IStatusBar = new Token<IStatusBar>('jupyterlab-statusbar/IStatusBar');
+export const IStatusBar = new Token<IStatusBar>(
+    'jupyterlab-statusbar/IStatusBar'
+);
 
-export
-interface IStatusBar {
-  listItems(): string[];
-  hasItem(id: string): boolean;
+export interface IStatusBar {
+    listItems(): string[];
+    hasItem(id: string): boolean;
 
-  registerStatusItem(id: string, widget: Widget, opts: IStatusBar.IStatusItemOptions): void;
+    registerStatusItem(
+        id: string,
+        widget: Widget,
+        opts: IStatusBar.IStatusItemOptions
+    ): void;
 }
 
-export
-namespace IStatusBar {
+export namespace IStatusBar {
+    export type Alignment = 'right' | 'left';
 
-  export
-  type Alignment = 'right' | 'left';
-
-  export
-  interface IStatusItemOptions {
-    align?: IStatusBar.Alignment;
-    priority?: number;
-  }
+    export interface IStatusItemOptions {
+        align?: IStatusBar.Alignment;
+        priority?: number;
+    }
 }
 
 const STATUS_BAR_ID = 'jp-main-status-bar';
@@ -47,109 +38,144 @@ const STATUS_BAR_LEFT_SIDE_CLASS = 'jp-status-bar-left';
 const STATUS_BAR_RIGHT_SIDE_CLASS = 'jp-status-bar-right';
 const STATUS_BAR_ITEM_CLASS = 'jp-status-bar-item';
 
-export
-class StatusBar extends Widget implements IStatusBar {
-  constructor(options: StatusBar.IOptions) {
-    super();
+export class StatusBar extends Widget implements IStatusBar {
+    constructor(options: StatusBar.IOptions) {
+        super();
 
-    this._host = options.host;
+        this._host = options.host;
 
-    this.id = STATUS_BAR_ID;
-    this.addClass(STATUS_BAR_CLASS);
+        this.id = STATUS_BAR_ID;
+        this.addClass(STATUS_BAR_CLASS);
 
-    let rootLayout = this.layout = new PanelLayout();
+        let rootLayout = (this.layout = new PanelLayout());
 
-    let leftPanel = this._leftSide = new Panel();
-    let rightPanel = this._rightSide = new Panel();
+        let leftPanel = (this._leftSide = new Panel());
+        let rightPanel = (this._rightSide = new Panel());
 
-    leftPanel.addClass(STATUS_BAR_SIDE_CLASS);
-    leftPanel.addClass(STATUS_BAR_LEFT_SIDE_CLASS);
+        leftPanel.addClass(STATUS_BAR_SIDE_CLASS);
+        leftPanel.addClass(STATUS_BAR_LEFT_SIDE_CLASS);
 
-    rightPanel.addClass(STATUS_BAR_SIDE_CLASS);
-    rightPanel.addClass(STATUS_BAR_RIGHT_SIDE_CLASS);
+        rightPanel.addClass(STATUS_BAR_SIDE_CLASS);
+        rightPanel.addClass(STATUS_BAR_RIGHT_SIDE_CLASS);
 
-    rootLayout.addWidget(leftPanel);
-    rootLayout.addWidget(rightPanel);
+        rootLayout.addWidget(leftPanel);
+        rootLayout.addWidget(rightPanel);
 
-    this._host.addToBottomArea(this);
-  }
-
-  registerStatusItem(id: string, widget: Widget, opts: IStatusBar.IStatusItemOptions = {}) {
-    if (id in this._statusItems) {
-      throw new Error(`Status item ${id} already registered.`);
+        this._host.addToBottomArea(this);
     }
 
-    let align = opts.align ? opts.align : 'left';
-    let priority = (opts.priority !== undefined) ? opts.priority : 0;
+    registerStatusItem(
+        id: string,
+        widget: Widget,
+        opts: IStatusBar.IStatusItemOptions = {}
+    ) {
+        if (id in this._statusItems) {
+            throw new Error(`Status item ${id} already registered.`);
+        }
 
-    let wrapper = {
-      widget,
-      align,
-      priority
-    };
+        let align = opts.align ? opts.align : 'left';
+        let priority = opts.priority !== undefined ? opts.priority : 0;
 
-    widget.addClass(STATUS_BAR_ITEM_CLASS);
+        let wrapper = {
+            widget,
+            align,
+            priority
+        };
 
-    this._statusItems[id] = wrapper;
+        let rankItem = {
+            id,
+            priority
+        };
 
-    if (align === 'left') {
-      this._leftSide.addWidget(widget);
-    } else {
-      this._rightSide.addWidget(widget);
+        widget.addClass(STATUS_BAR_ITEM_CLASS);
+
+        this._statusItems[id] = wrapper;
+
+        if (align === 'left') {
+            let insertIndex = this._findInsertIndex(
+                this._leftRankItems,
+                rankItem
+            );
+
+            ArrayExt.insert(this._leftRankItems, insertIndex, rankItem);
+            this._leftSide.insertWidget(insertIndex, widget);
+        } else {
+            let insertIndex = this._findInsertIndex(
+                this._rightRankItems,
+                rankItem
+            );
+
+            ArrayExt.insert(this._rightRankItems, insertIndex, rankItem);
+            this._rightSide.insertWidget(insertIndex, widget);
+        }
+
+        widget.show();
     }
 
-    widget.show();
-  }
+    /**
+     * List the ids of the status bar items;
+     *
+     * @returns A new array of the status bar item ids.
+     */
+    listItems(): string[] {
+        return Object.keys(this._statusItems);
+    }
 
-  /**
-   * List the ids of the status bar items;
-   *
-   * @returns A new array of the status bar item ids.
-   */
-  listItems(): string[] {
-    return Object.keys(this._statusItems);
-  }
+    /**
+     * Test whether a specific status item is present in the status bar.
+     *
+     * @param id The id of the status item of interest
+     *
+     * @returns `true` if the status item is in the status bar, `false` otherwise.
+     */
+    hasItem(id: string): boolean {
+        return id in this._statusItems;
+    }
 
-  /**
-   * Test whether a specific status item is present in the status bar.
-   *
-   * @param id The id of the status item of interest
-   *
-   * @returns `true` if the status item is in the status bar, `false` otherwise.
-   */
-  hasItem(id: string): boolean {
-    return id in this._statusItems;
-  }
+    /**
+     * Get the parent ApplicationShell
+     */
+    get host(): ApplicationShell {
+        return this._host;
+    }
 
-  /**
-   * Get the parent ApplicationShell
-   */
-  get host(): ApplicationShell {
-    return this._host;
-  }
+    private _findInsertIndex(
+        side: StatusBar.IRankItem[],
+        newItem: StatusBar.IRankItem
+    ): number {
+        return ArrayExt.findFirstIndex(
+            side,
+            item => item.priority > newItem.priority
+        );
+    }
 
-  private _statusItems: { [id: string]: StatusBar.IItem } = Object.create(null);
-  private _host: ApplicationShell = null;
+    private _leftRankItems: StatusBar.IRankItem[] = [];
+    private _rightRankItems: StatusBar.IRankItem[] = [];
+    private _statusItems: { [id: string]: StatusBar.IItem } = Object.create(
+        null
+    );
+    private _host: ApplicationShell = null;
 
-  private _leftSide: Panel;
-  private _rightSide: Panel;
+    private _leftSide: Panel;
+    private _rightSide: Panel;
 }
 
-export
-namespace StatusBar {
+export namespace StatusBar {
+    export interface IRankItem {
+        id: string;
+        priority: number;
+    }
 
-  /**
-   * Options for creating a new StatusBar instance
-   */
-  export
-  interface IOptions {
-    host: ApplicationShell;
-  }
+    /**
+     * Options for creating a new StatusBar instance
+     */
+    export interface IOptions {
+        host: ApplicationShell;
+    }
 
-  export
-  interface IItem {
-    align: IStatusBar.Alignment;
-    priority: number;
-    widget: Widget;
-  }
+    export interface IItem {
+        align: IStatusBar.Alignment;
+        priority: number;
+        widget: Widget;
+    }
 }
