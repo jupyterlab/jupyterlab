76d1612bc53de04a6b9fbd8821eb832de318d67e
Declan Kelly <dkelly.home@gmail.com>
2018-07-03 17:30:16 -0700

    Add jupyterlab/signaling dependency and remove unneeded set utility functions

--- src/util/observableset.ts
+++ src/util/observableset.ts
@@ -133,19 +133,11 @@ export namespace ObservableSet {
 }
 
 namespace Private {
-    export function union<T>(a: Set<T>, b: Set<T>): Set<T> {
-        return new Set([...a, ...b]);
-    }
-
     export function intersection<T>(a: Set<T>, b: Set<T>): Set<T> {
         return new Set([...a].filter(x => b.has(x)));
     }
 
     export function difference<T>(a: Set<T>, b: Set<T>): Set<T> {
         return new Set([...a].filter(x => !b.has(x)));
-    }
-
-    export function symmetricDifference<T>(a: Set<T>, b: Set<T>): Set<T> {
-        return difference(union(a, b), intersection(a, b));
     }
 }
