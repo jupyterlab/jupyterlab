2151cf43bbc7ac101fd8a012426dda2efca1fb2e
RichaGadgil <gadgilricha@gmail.com>
2018-07-10 09:36:25 -0700

    fixed commas

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import {
     runningKernelsItem,
     notebookTrustItem,
     IDefaultStatusesManager,
-    lineColItem
+    lineColItem,
     fileUploadItem,
     kernelStatusItem
 } from './defaults';
@@ -37,7 +37,7 @@ const plugins: JupyterLabPlugin<any>[] = [
     defaultsManager,
     runningKernelsItem,
     notebookTrustItem,
-    lineColItem
+    lineColItem,
     fileUploadItem,
     kernelStatusItem
 ];
