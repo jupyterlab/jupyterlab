1398b55e414cf79495cadebb0d9960df5949ce7f
RichaGadgil <gadgilricha@gmail.com>
2018-07-11 10:05:12 -0700

    updated default KernelManager

--- src/defaults/runningKernels.tsx
+++ src/defaults/runningKernels.tsx
@@ -2,103 +2,109 @@ import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 
 import {
-    JupyterLabPlugin, JupyterLab, ApplicationShell,
+    JupyterLabPlugin,
+    JupyterLab,
+    ApplicationShell
 } from '@jupyterlab/application';
 
-import {
-   Kernel, KernelManager
-} from '@jupyterlab/services';
+import { Kernel, SessionManager } from '@jupyterlab/services';
 
-import {
-    Widget,
-} from '@phosphor/widgets';
+import { Widget } from '@phosphor/widgets';
 
-import {
-    IStatusBar
-} from './../statusBar';
+import { IStatusBar } from './../statusBar';
 
-
-
-
-export
-namespace RunningComponent {
-    export
-    interface IState {
+export namespace RunningKernelsComponent {
+    export interface IState {
         kernelSession: number;
     }
-    export
-    interface IProps {
-        kernelManager: KernelManager;
+    export interface IProps {
+        sessionManager: SessionManager;
         shellHost: ApplicationShell;
     }
 }
 
-export
-class RunningComponent extends React.Component<RunningComponent.IProps, RunningComponent.IState> {
+export class RunningKernelsComponent extends React.Component<
+    RunningKernelsComponent.IProps,
+    RunningKernelsComponent.IState
+> {
     state = {
-        kernelSession: 0,
+        kernelSession: 0
     };
 
-    constructor(props: RunningComponent.IProps) {
+    constructor(props: RunningKernelsComponent.IProps) {
         super(props);
-        this.props.kernelManager.runningChanged.connect(this.updateKernel);
+        this.props.sessionManager.runningChanged.connect(this.updateKernel);
     }
 
     componentDidMount() {
-        Kernel.listRunning().then(value => this.setState({kernelSession: value.length}));
+        Kernel.listRunning().then(value =>
+            this.setState({ kernelSession: value.length })
+        );
     }
 
-    updateKernel = (kernelManage: KernelManager, kernels: Kernel.IModel[]) =>  {
-        this.setState({kernelSession: kernels.length});
-    }
+    updateKernel = (kernelManage: SessionManager, kernels: Kernel.IModel[]) => {
+        this.setState({ kernelSession: kernels.length });
+    };
 
     handleClick = () => {
         this.props.shellHost.expandLeft();
         this.props.shellHost.activateById('jp-running-sessions');
-    }
+    };
     render() {
-        return(<div onClick = {this.handleClick}>{this.state.kernelSession} Kernels Active</div>);
+        return (
+            <div onClick={this.handleClick}>
+                {this.state.kernelSession} Kernels Active
+            </div>
+        );
     }
-
 }
 
-export
-class RunningKernels extends Widget {
+export class RunningKernels extends Widget {
     constructor(opts: RunningKernels.IOptions) {
         super();
-        this._manager = new KernelManager();
+        this._manager = opts.sessionManager;
         this._host = opts.host;
     }
-      onBeforeAttach() {
-            ReactDOM.render(<RunningComponent kernelManager = {this._manager} shellHost = {this._host}/>, this.node);
-      }
+    onBeforeAttach() {
+        ReactDOM.render(
+            <RunningKernelsComponent
+                sessionManager={this._manager}
+                shellHost={this._host}
+            />,
+            this.node
+        );
+    }
 
-      private _host: ApplicationShell = null;
-      private _manager: KernelManager;
+    private _host: ApplicationShell = null;
+    private _manager: SessionManager;
 }
 
 /*
  * Initialization data for the statusbar extension.
  */
 
-export
-const runningKernelsItem: JupyterLabPlugin<void> = {
+export const runningKernelsItem: JupyterLabPlugin<void> = {
     id: 'jupyterlab-statusbar/default-items:running-kernels',
     autoStart: true,
     requires: [IStatusBar],
-    activate: (app: JupyterLab, statusBar: IStatusBar, manager: KernelManager) => {
-        statusBar.registerStatusItem('running-kernels-item', new RunningKernels( {host: app.shell} ), {align: 'left'});
+    activate: (app: JupyterLab, statusBar: IStatusBar) => {
+        statusBar.registerStatusItem(
+            'running-kernels-item',
+            new RunningKernels({
+                host: app.shell,
+                sessionManager: app.serviceManager.sessions
+            }),
+            { align: 'left' }
+        );
     }
 };
 
-export
-namespace RunningKernels {
-
-  /**
-   * Options for creating a new StatusBar instance
-   */
-  export
-  interface IOptions {
-    host: ApplicationShell;
-  }
+export namespace RunningKernels {
+    /**
+     * Options for creating a new StatusBar instance
+     */
+    export interface IOptions {
+        host: ApplicationShell;
+        sessionManager: SessionManager;
+    }
 }
