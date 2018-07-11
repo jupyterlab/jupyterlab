5183b72dc9d3a945a4f83f16c23314f0d011ad2c
RichaGadgil <gadgilricha@gmail.com>
2018-07-11 14:08:50 -0700

    fixed priority

--- src/statusBar.ts
+++ src/statusBar.ts
@@ -112,6 +112,7 @@ export class StatusBar extends Widget implements IStatusBar {
             );
             if (insertIndex === -1) {
                 this._leftSide.addWidget(widget);
+                this._leftRankItems.push(rankItem);
             } else {
                 ArrayExt.insert(this._leftRankItems, insertIndex, rankItem);
                 this._leftSide.insertWidget(insertIndex, widget);
@@ -123,6 +124,7 @@ export class StatusBar extends Widget implements IStatusBar {
             );
             if (insertIndex === -1) {
                 this._rightSide.addWidget(widget);
+                this._rightRankItems.push(rankItem);
             } else {
                 ArrayExt.insert(this._rightRankItems, insertIndex, rankItem);
                 this._rightSide.insertWidget(insertIndex, widget);
