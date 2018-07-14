1a295b17a6dc3ce7d964007d3069236f9788d101
Steven Silvester <steven.silvester@ieee.org>
2018-07-14 07:08:46 -0500

    clean up linting
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    Clean up lint handling
    
    Remove changes

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -66,16 +66,13 @@ if [[ $GROUP == integrity ]]; then
     jlpm run integrity
 
     # Lint our JavaScript files.
-    ./node_modules/.bin/eslint .
+    jlpm run lint:check
 
     # Build the packages individually.
     jlpm run build:src
 
     # Make sure the examples build
     jlpm run build:examples
-
-    # Run a tslint check
-    ./node_modules/.bin/tslint -c tslint.json -e '**/*.d.ts' -e 'node_modules/**/*.ts' -e 'jupyterlab/**/*.ts' '**/*.ts'
 
     # Make sure we have CSS that can be converted with postcss
     jlpm global add postcss-cli
