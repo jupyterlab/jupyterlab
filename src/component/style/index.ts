7a022ae1e50e80204e364c1259ad05480a47620f
Declan Kelly <dkelly.home@gmail.com>
2018-07-23 16:18:10 -0700

    Move and cleanup component styles
    Extracte style variables to single global definition. Also, rename files from ts to tsx and moved status bar styles to typestyle.

--- src/component/style/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './icon';
-export * from './text';
-export * from './progressBar';
---
+++ src/style/text.ts
@@ -0,0 +1,12 @@
+import vars from './variables';
+import { NestedCSSProperties } from 'typestyle/lib/types';
+
+const STYLE: NestedCSSProperties = {
+    lineHeight: vars.height,
+    color: vars.textColor,
+    fontSize: vars.fontSize,
+    fontFamily:
+        '"HelveticaNeue-Medium", "Helvetica Neue Medium", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif'
+};
+
+export default STYLE;
