0f7302d91e68d9f951ae6ce041e6b2473174ed7d
Steven Silvester <steven.silvester@ieee.org>
2018-08-16 06:22:10 -0500

    Reinstate update-local

--- buildutils/src/prepublish.ts
+++ buildutils/src/prepublish.ts
@@ -7,4 +7,14 @@ import * as utils from './utils';
 utils.run('jlpm run clean:slate');
 utils.run('jlpm run build:packages');
 utils.run('jlpm run build:themes');
+
+// If we are on the main branch, make sure all of the local packages
+// are up to date with npm.
+let branch = utils.run('git rev-parse --abbrev-ref HEAD', {
+  stdio: ['ignore', 'pipe', 'ignore']
+});
+if (branch === 'master') {
+  utils.run('jlpm run update:local');
+}
+
 utils.run('jlpm integrity');
--- buildutils/src/update-local.ts
+++ buildutils/src/update-local.ts
@@ -8,6 +8,13 @@ import * as utils from './utils';
 import packageJson = require('package-json');
 
 console.log('Looking for outdated local package versions...');
+const cmd = 'python setup.py --version';
+const pyversion = utils.run(cmd, { stdio: 'pipe' }, true);
+let version = 'latest';
+if (pyversion.indexOf('rc') !== -1 || pyversion.indexOf('a') !== -1) {
+  version = 'next';
+}
+
 utils.getLernaPaths().map(async pkgPath => {
   const packagePath = path.join(pkgPath, 'package.json');
   let data: any;
@@ -20,13 +27,13 @@ utils.getLernaPaths().map(async pkgPath => {
   if (data.private === true) {
     return;
   }
-  const releaseData = await packageJson(data.name, { version: 'next' });
-  const version = releaseData.version;
-  if (data.version === version) {
+  const releaseData = await packageJson(data.name, { version });
+  const npmVersion = releaseData.version;
+  if (data.version === npmVersion) {
     return;
   }
-  data.version = version;
-  console.log('Updating', data.name, 'to', version);
+  data.version = npmVersion;
+  console.log('Updating', data.name, 'to', npmVersion);
   // Write the file back to disk.
   utils.writePackageData(packagePath, data);
 });
