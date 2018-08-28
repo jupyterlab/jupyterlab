aacc36eca8d763fb0e8d5494912c454229c10223
Steven Silvester <steven.silvester@ieee.org>
2018-08-28 04:37:15 -0500

    Release 0.34.4

--- jupyterlab/_version.py
+++ jupyterlab/_version.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 
 VersionInfo = namedtuple('VersionInfo', ['major', 'minor', 'micro', 'releaselevel', 'serial'])
 
-version_info = VersionInfo(0, 34, 3, 'final', 0)
+version_info = VersionInfo(0, 34, 4, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
