68a000b96176fc3851cff026fe299269995428a7
Ian Rose <ian.r.rose@gmail.com>
2018-09-13 03:17:51 -0600

    Dark csv viewer (#5304)
    * Restyle data grid according to application theme.
    
    * More style tweaks.
    
    * Right-align text.

--- packages/csvviewer-extension/src/index.ts
+++ packages/csvviewer-extension/src/index.ts
@@ -7,7 +7,7 @@ import {
   JupyterLabPlugin
 } from '@jupyterlab/application';
 
-import { InstanceTracker } from '@jupyterlab/apputils';
+import { InstanceTracker, IThemeManager } from '@jupyterlab/apputils';
 
 import {
   CSVViewer,
@@ -16,6 +16,8 @@ import {
 } from '@jupyterlab/csvviewer';
 
 import { IDocumentWidget } from '@jupyterlab/docregistry';
+
+import { DataGrid, TextRenderer } from '@phosphor/datagrid';
 
 /**
  * The name of the factories that creates widgets.
@@ -30,7 +32,7 @@ const FACTORY_TSV = 'TSVTable';
 const csv: JupyterLabPlugin<void> = {
   activate: activateCsv,
   id: '@jupyterlab/csvviewer-extension:csv',
-  requires: [ILayoutRestorer],
+  requires: [ILayoutRestorer, IThemeManager],
   autoStart: true
 };
 
@@ -40,14 +42,18 @@ const csv: JupyterLabPlugin<void> = {
 const tsv: JupyterLabPlugin<void> = {
   activate: activateTsv,
   id: '@jupyterlab/csvviewer-extension:tsv',
-  requires: [ILayoutRestorer],
+  requires: [ILayoutRestorer, IThemeManager],
   autoStart: true
 };
 
 /**
  * Activate cssviewer extension for CSV files
  */
-function activateCsv(app: JupyterLab, restorer: ILayoutRestorer): void {
+function activateCsv(
+  app: JupyterLab,
+  restorer: ILayoutRestorer,
+  themeManager: IThemeManager
+): void {
   const factory = new CSVViewerFactory({
     name: FACTORY_CSV,
     fileTypes: ['csv'],
@@ -57,6 +63,10 @@ function activateCsv(app: JupyterLab, restorer: ILayoutRestorer): void {
   const tracker = new InstanceTracker<IDocumentWidget<CSVViewer>>({
     namespace: 'csvviewer'
   });
+
+  // The current styles for the data grids.
+  let style: DataGrid.IStyle = Private.LIGHT_STYLE;
+  let renderer: TextRenderer = Private.LIGHT_RENDERER;
 
   // Handle state restoration.
   restorer.restore(tracker, {
@@ -79,13 +89,32 @@ function activateCsv(app: JupyterLab, restorer: ILayoutRestorer): void {
       widget.title.iconClass = ft.iconClass;
       widget.title.iconLabel = ft.iconLabel;
     }
+    // Set the theme for the new widget.
+    widget.content.style = style;
+    widget.content.renderer = renderer;
   });
+
+  // Keep the themes up-to-date.
+  const updateThemes = () => {
+    const isLight = themeManager.isLight(themeManager.theme);
+    style = isLight ? Private.LIGHT_STYLE : Private.DARK_STYLE;
+    renderer = isLight ? Private.LIGHT_RENDERER : Private.DARK_RENDERER;
+    tracker.forEach(grid => {
+      grid.content.style = style;
+      grid.content.renderer = renderer;
+    });
+  };
+  themeManager.themeChanged.connect(updateThemes);
 }
 
 /**
  * Activate cssviewer extension for TSV files
  */
-function activateTsv(app: JupyterLab, restorer: ILayoutRestorer): void {
+function activateTsv(
+  app: JupyterLab,
+  restorer: ILayoutRestorer,
+  themeManager: IThemeManager
+): void {
   const factory = new TSVViewerFactory({
     name: FACTORY_TSV,
     fileTypes: ['tsv'],
@@ -95,6 +124,10 @@ function activateTsv(app: JupyterLab, restorer: ILayoutRestorer): void {
   const tracker = new InstanceTracker<IDocumentWidget<CSVViewer>>({
     namespace: 'tsvviewer'
   });
+
+  // The current styles for the data grids.
+  let style: DataGrid.IStyle = Private.LIGHT_STYLE;
+  let renderer: TextRenderer = Private.LIGHT_RENDERER;
 
   // Handle state restoration.
   restorer.restore(tracker, {
@@ -117,7 +150,22 @@ function activateTsv(app: JupyterLab, restorer: ILayoutRestorer): void {
       widget.title.iconClass = ft.iconClass;
       widget.title.iconLabel = ft.iconLabel;
     }
+    // Set the theme for the new widget.
+    widget.content.style = style;
+    widget.content.renderer = renderer;
   });
+
+  // Keep the themes up-to-date.
+  const updateThemes = () => {
+    const isLight = themeManager.isLight(themeManager.theme);
+    style = isLight ? Private.LIGHT_STYLE : Private.DARK_STYLE;
+    renderer = isLight ? Private.LIGHT_RENDERER : Private.DARK_RENDERER;
+    tracker.forEach(grid => {
+      grid.content.style = style;
+      grid.content.renderer = renderer;
+    });
+  };
+  themeManager.themeChanged.connect(updateThemes);
 }
 
 /**
@@ -125,3 +173,49 @@ function activateTsv(app: JupyterLab, restorer: ILayoutRestorer): void {
  */
 const plugins: JupyterLabPlugin<any>[] = [csv, tsv];
 export default plugins;
+
+/**
+ * A namespace for private data.
+ */
+namespace Private {
+  /**
+   * The light theme for the data grid.
+   */
+  export const LIGHT_STYLE: DataGrid.IStyle = {
+    ...DataGrid.defaultStyle,
+    voidColor: '#F3F3F3',
+    backgroundColor: 'white',
+    headerBackgroundColor: '#EEEEEE',
+    gridLineColor: 'rgba(20, 20, 20, 0.15)',
+    headerGridLineColor: 'rgba(20, 20, 20, 0.25)',
+    rowBackgroundColor: i => (i % 2 === 0 ? '#F5F5F5' : 'white')
+  };
+
+  /**
+   * The dark theme for the data grid.
+   */
+  export const DARK_STYLE: DataGrid.IStyle = {
+    voidColor: 'black',
+    backgroundColor: '#111111',
+    headerBackgroundColor: '#424242',
+    gridLineColor: 'rgba(235, 235, 235, 0.15)',
+    headerGridLineColor: 'rgba(235, 235, 235, 0.25)',
+    rowBackgroundColor: i => (i % 2 === 0 ? '#212121' : '#111111')
+  };
+
+  /**
+   * The light renderer for the data grid.
+   */
+  export const LIGHT_RENDERER = new TextRenderer({
+    textColor: '#111111',
+    horizontalAlignment: 'right'
+  });
+
+  /**
+   * The dark renderer for the data grid.
+   */
+  export const DARK_RENDERER = new TextRenderer({
+    textColor: '#F5F5F5',
+    horizontalAlignment: 'right'
+  });
+}
