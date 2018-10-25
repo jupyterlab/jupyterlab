466e39affc01c7ca7e3556d8b7924e9104d0fd98
Ian Rose <ian.r.rose@gmail.com>
2018-10-25 11:36:08 -0700

    Cleanup and document notebook status items.

--- packages/notebook-extension/src/modestatus.tsx
+++ packages/notebook-extension/src/modestatus.tsx
@@ -15,19 +15,30 @@ import {
 
 import { IStatusBar, TextItem } from '@jupyterlab/statusbar';
 
-import { ISignal } from '@phosphor/signaling';
-
-import { IDisposable } from '@phosphor/disposable';
-
-// tslint:disable-next-line:variable-name
-const CommandEditComponent = (
+/**
+ * A pure function for rendering a Command/Edit mode component.
+ *
+ * @param props: the props for rendering the component.
+ *
+ * @returns a tsx component for command/edit mode.
+ */
+function CommandEditComponent(
   props: CommandEditComponent.IProps
-): React.ReactElement<CommandEditComponent.IProps> => {
+): React.ReactElement<CommandEditComponent.IProps> {
   return <TextItem source={`Mode: ${Text.titleCase(props.notebookMode)}`} />;
-};
+}
 
+/**
+ * A namespace for CommandEditComponent statics.
+ */
 namespace CommandEditComponent {
+  /**
+   * The props for the CommandEditComponent.
+   */
   export interface IProps {
+    /**
+     * The current mode of the current notebook.
+     */
     notebookMode: NotebookMode;
   }
 }
@@ -35,8 +46,10 @@ namespace CommandEditComponent {
 /**
  * StatusBar item to display which notebook mode user is in.
  */
-class CommandEdit extends VDomRenderer<CommandEdit.Model>
-  implements ICommandEdit {
+class CommandEdit extends VDomRenderer<CommandEdit.Model> {
+  /**
+   * Construct a new CommandEdit status item.
+   */
   constructor(opts: CommandEdit.IOptions) {
     super();
     this._tracker = opts.tracker;
@@ -46,54 +59,68 @@ class CommandEdit extends VDomRenderer<CommandEdit.Model>
     this.model = new CommandEdit.Model(
       this._tracker.currentWidget && this._tracker.currentWidget.content
     );
-    this.node.title = `Notebook is in ${this.model.notebookMode} mode`;
+    this.title.caption = `Notebook is in ${this.model.notebookMode} mode`;
   }
 
+  /**
+   * Render the CommandEdit status item.
+   */
   render() {
-    if (this.model === null) {
-      return null;
-    } else {
-      this.node.title = `Notebook is in ${this.model.notebookMode} mode`;
-      return <CommandEditComponent notebookMode={this.model.notebookMode} />;
-    }
+    this.node.title = `Notebook is in ${this.model.notebookMode} mode`;
+    return <CommandEditComponent notebookMode={this.model.notebookMode} />;
   }
 
+  /**
+   * Dispose of the status item.
+   */
   dispose() {
     super.dispose();
-
     this._tracker.currentChanged.disconnect(this._onNotebookChange);
   }
 
+  /**
+   * Update the model when the tracker current widget changes.
+   */
   private _onNotebookChange = (
     tracker: INotebookTracker,
     notebook: NotebookPanel | null
   ) => {
     if (notebook === null) {
-      this.model!.notebook = null;
+      this.model.notebook = null;
     } else {
-      this.model!.notebook = notebook.content;
+      this.model.notebook = notebook.content;
     }
   };
 
   private _tracker: INotebookTracker;
 }
 
+/**
+ * A namespace for CommandEdit statics.
+ */
 namespace CommandEdit {
-  export class Model extends VDomModel implements ICommandEdit.IModel {
+  /**
+   * A VDomModle for the CommandEdit renderer.
+   */
+  export class Model extends VDomModel {
+    /**
+     * Construct a new CommandEdit model.
+     */
     constructor(notebook: Notebook | null) {
       super();
-
       this.notebook = notebook;
     }
 
+    /**
+     * The current mode of the current notebook.
+     */
     get notebookMode() {
       return this._notebookMode;
     }
 
-    set notebookMode(notebookMode: NotebookMode) {
-      this._notebookMode = notebookMode;
-    }
-
+    /**
+     * Set the current notebook for the model.
+     */
     set notebook(notebook: Notebook | null) {
       const oldNotebook = this._notebook;
       if (oldNotebook !== null) {
@@ -102,7 +129,7 @@ namespace CommandEdit {
         oldNotebook.modelContentChanged.disconnect(this._onChanged);
       }
 
-      const oldState = this._getAllState();
+      const oldMode = this._notebookMode;
       this._notebook = notebook;
       if (this._notebook === null) {
         this._notebookMode = 'command';
@@ -113,23 +140,25 @@ namespace CommandEdit {
         this._notebook.modelContentChanged.connect(this._onChanged);
       }
 
-      this._triggerChange(oldState, this._getAllState());
+      this._triggerChange(oldMode, this._notebookMode);
     }
 
+    /**
+     * On a change to the notebook, update the mode.
+     */
     private _onChanged = (_notebook: Notebook) => {
-      const oldState = this._getAllState();
-      if (_notebook !== null && _notebook !== undefined) {
+      const oldMode = this._notebookMode;
+      if (this._notebook) {
         this._notebookMode = _notebook.mode;
       } else {
         this._notebookMode = 'command';
       }
-      this._triggerChange(oldState, this._getAllState());
+      this._triggerChange(oldMode, this._notebookMode);
     };
 
-    private _getAllState(): NotebookMode {
-      return this._notebookMode;
-    }
-
+    /**
+     * Trigger a state change for the renderer.
+     */
     private _triggerChange(oldState: NotebookMode, newState: NotebookMode) {
       if (oldState !== newState) {
         this.stateChanged.emit(void 0);
@@ -140,23 +169,17 @@ namespace CommandEdit {
     private _notebook: Notebook | null = null;
   }
 
+  /**
+   * Options for creating a CommandEdit status item.
+   */
   export interface IOptions {
     tracker: INotebookTracker;
   }
 }
 
-export interface ICommandEdit extends IDisposable {
-  readonly model: ICommandEdit.IModel | null;
-  readonly modelChanged: ISignal<this, void>;
-}
-
-export namespace ICommandEdit {
-  export interface IModel {
-    readonly notebookMode: NotebookMode;
-    readonly notebook: Notebook | null;
-  }
-}
-
+/**
+ * A plugin providing a CommandEdit status item.
+ */
 export const commandEditItem: JupyterLabPlugin<void> = {
   id: '@jupyterlab/notebook-extension:mode-status',
   autoStart: true,
--- packages/notebook-extension/src/truststatus.tsx
+++ packages/notebook-extension/src/truststatus.tsx
@@ -1,9 +1,3 @@
-/**
- * Default item to display if a cell is trusted in the notebook.
- */
-/**
- * Part of Jupyterlab status bar defaults.
- */
 import React from 'react';
 
 import { JupyterLabPlugin, JupyterLab } from '@jupyterlab/application';
@@ -23,132 +17,183 @@ import { IconItem, IStatusBar } from '@jupyterlab/statusbar';
 
 import { toArray } from '@phosphor/algorithm';
 
-import { IDisposable } from '@phosphor/disposable';
-
-import { ISignal } from '@phosphor/signaling';
-
-export const cellStatus = (
-  prop: NotebookTrustComponent.IProps | NotebookTrust.Model
-) => {
-  if (prop.trustedCells === prop.totalCells) {
+/**
+ * Determine the notebook trust status message.
+ */
+function cellTrust(
+  props: NotebookTrustComponent.IProps | NotebookTrust.Model
+): string[] {
+  if (props.trustedCells === props.totalCells) {
     return [
-      `Notebook trusted: ${prop.trustedCells} of ${
-        prop.totalCells
+      `Notebook trusted: ${props.trustedCells} of ${
+        props.totalCells
       } cells trusted.`,
       'trusted-item'
     ];
-  } else if (prop.activeCellTrusted) {
+  } else if (props.activeCellTrusted) {
     return [
-      `Active cell trusted: ${prop.trustedCells} of ${
-        prop.totalCells
+      `Active cell trusted: ${props.trustedCells} of ${
+        props.totalCells
       } cells trusted. `,
       'trusted-item'
     ];
   } else {
     return [
-      `Notebook not trusted: ${prop.trustedCells} of ${
-        prop.totalCells
+      `Notebook not trusted: ${props.trustedCells} of ${
+        props.totalCells
       } cells trusted.`,
       'not-trusted-item'
     ];
   }
-};
-// tslint:disable-next-line:variable-name
-const NotebookTrustComponent = (
+}
+
+/**
+ * A pure function for a notebook trust status component.
+ *
+ * @param props: the props for the component.
+ *
+ * @returns a tsx component for notebook trust.
+ */
+function NotebookTrustComponent(
   props: NotebookTrustComponent.IProps
-): React.ReactElement<NotebookTrustComponent.IProps> => {
-  let source: string;
-
-  source = cellStatus(props)[1];
-
+): React.ReactElement<NotebookTrustComponent.IProps> {
+  const source = cellTrust(props)[1];
   return <IconItem source={source} offset={{ x: 0, y: 2 }} />;
-};
+}
 
+/**
+ * A namespace for NotebookTrustComponent statics.
+ */
 namespace NotebookTrustComponent {
+  /**
+   * Props for the NotebookTrustComponent.
+   */
   export interface IProps {
+    /**
+     * Whether all the cells are trusted.
+     */
     allCellsTrusted: boolean;
+
+    /**
+     * Whether the currently active cell is trusted.
+     */
     activeCellTrusted: boolean;
+
+    /**
+     * The total number of cells for the current notebook.
+     */
     totalCells: number;
+
+    /**
+     * The number of trusted cells for the current notebook.
+     */
     trustedCells: number;
   }
 }
 
-class NotebookTrust extends VDomRenderer<NotebookTrust.Model>
-  implements INotebookTrust {
+/**
+ * The NotebookTrust status item.
+ */
+class NotebookTrust extends VDomRenderer<NotebookTrust.Model> {
+  /**
+   * Construct a new status item.
+   */
   constructor(opts: NotebookTrust.IOptions) {
     super();
     this._tracker = opts.tracker;
-
     this._tracker.currentChanged.connect(this._onNotebookChange);
     this.model = new NotebookTrust.Model(
       this._tracker.currentWidget && this._tracker.currentWidget.content
     );
 
-    this.node.title = cellStatus(this.model)[0];
+    this.title.caption = cellTrust(this.model)[0];
   }
 
-  private _onNotebookChange = (
-    tracker: INotebookTracker,
-    notebook: NotebookPanel | null
-  ) => {
-    if (notebook === null) {
-      this.model!.notebook = null;
-    } else {
-      this.model!.notebook = notebook.content;
-    }
-  };
-
+  /**
+   * Render the NotebookTrust status item.
+   */
   render() {
-    if (this.model === null) {
-      return null;
-    } else {
-      this.node.title = cellStatus(this.model)[0];
-      return (
-        <div>
-          <NotebookTrustComponent
-            allCellsTrusted={this.model.trustedCells === this.model.totalCells}
-            activeCellTrusted={this.model.activeCellTrusted}
-            totalCells={this.model.totalCells}
-            trustedCells={this.model.trustedCells}
-          />
-        </div>
-      );
-    }
+    this.node.title = cellTrust(this.model)[0];
+    return (
+      <div>
+        <NotebookTrustComponent
+          allCellsTrusted={this.model.trustedCells === this.model.totalCells}
+          activeCellTrusted={this.model.activeCellTrusted}
+          totalCells={this.model.totalCells}
+          trustedCells={this.model.trustedCells}
+        />
+      </div>
+    );
   }
 
+  /**
+   * Dispose of the notebook trust item.
+   */
   dispose() {
     super.dispose();
-
     this._tracker.currentChanged.disconnect(this._onNotebookChange);
+  }
+
+  /**
+   * When update the trust model when the notebook changes.
+   */
+  private _onNotebookChange(
+    tracker: INotebookTracker,
+    notebook: NotebookPanel | null
+  ): void {
+    if (notebook === null) {
+      this.model.notebook = null;
+    } else {
+      this.model.notebook = notebook.content;
+    }
   }
 
   private _tracker: INotebookTracker;
 }
 
+/**
+ * A namespace for NotebookTrust statics.
+ */
 namespace NotebookTrust {
-  export class Model extends VDomModel implements INotebookTrust.IModel {
+  /**
+   * A VDomModel for the NotebookTrust status item.
+   */
+  export class Model extends VDomModel {
+    /**
+     * Construct a new NotebookTrust model.
+     */
     constructor(notebook: Notebook | null) {
       super();
-
       this.notebook = notebook;
     }
 
-    get trustedCells() {
+    /**
+     * The number of trusted cells in the current notebook.
+     */
+    get trustedCells(): number {
       return this._trustedCells;
     }
 
+    /**
+     * The total number of cells in the current notebook.
+     */
     get totalCells() {
       return this._totalCells;
     }
 
+    /**
+     * Whether the active cell is trusted.
+     */
     get activeCellTrusted() {
       return this._activeCellTrusted;
     }
 
+    /**
+     * The current notebook for the model.
+     */
     get notebook() {
       return this._notebook;
     }
-
     set notebook(model: Notebook | null) {
       const oldNotebook = this._notebook;
       if (oldNotebook !== null) {
@@ -166,11 +211,10 @@ namespace NotebookTrust {
       } else {
         // Add listeners
         this._notebook.activeCellChanged.connect(this._onActiveCellChanged);
-
         this._notebook.modelContentChanged.connect(this._onModelChanged);
 
         // Derive values
-        if (this._notebook!.activeCell !== undefined) {
+        if (this._notebook.activeCell !== undefined) {
           this._activeCellTrusted = this._notebook!.activeCell!.model.trusted;
         } else {
           this._activeCellTrusted = false;
@@ -187,26 +231,34 @@ namespace NotebookTrust {
       this._triggerChange(oldState, this._getAllState());
     }
 
-    private _onModelChanged = (notebook: Notebook) => {
+    /**
+     * When the notebook model changes, update the trust state.
+     */
+    private _onModelChanged(notebook: Notebook): void {
       const oldState = this._getAllState();
       const { total, trusted } = this._deriveCellTrustState(notebook.model);
 
       this._totalCells = total;
       this._trustedCells = trusted;
       this._triggerChange(oldState, this._getAllState());
-    };
+    }
 
+    /**
+     * When the active cell changes, update the trust state.
+     */
     private _onActiveCellChanged = (model: Notebook, cell: Cell | null) => {
       const oldState = this._getAllState();
-      if (cell !== null && cell !== undefined) {
+      if (cell) {
         this._activeCellTrusted = cell.model.trusted;
       } else {
         this._activeCellTrusted = false;
       }
-
       this._triggerChange(oldState, this._getAllState());
     };
 
+    /**
+     * Given a notebook model, figure out how many of the cells are trusted.
+     */
     private _deriveCellTrustState(
       model: INotebookModel
     ): { total: number; trusted: number } {
@@ -228,10 +280,16 @@ namespace NotebookTrust {
       };
     }
 
+    /**
+     * Get the current state of the model.
+     */
     private _getAllState(): [number, number, boolean] {
       return [this._trustedCells, this._totalCells, this.activeCellTrusted];
     }
 
+    /**
+     * Trigger a change in the renderer.
+     */
     private _triggerChange(
       oldState: [number, number, boolean],
       newState: [number, number, boolean]
@@ -251,26 +309,17 @@ namespace NotebookTrust {
     private _notebook: Notebook | null = null;
   }
 
+  /**
+   * Options for creating a NotebookTrust status item.
+   */
   export interface IOptions {
     tracker: INotebookTracker;
   }
 }
 
-export interface INotebookTrust extends IDisposable {
-  readonly model: INotebookTrust.IModel | null;
-  readonly modelChanged: ISignal<this, void>;
-}
-
-export namespace INotebookTrust {
-  export interface IModel {
-    readonly trustedCells: number;
-    readonly totalCells: number;
-    readonly activeCellTrusted: boolean;
-
-    readonly notebook: Notebook | null;
-  }
-}
-
+/**
+ * A plugin that adds a notebook trust status item to the status bar.
+ */
 export const notebookTrustItem: JupyterLabPlugin<void> = {
   id: '@jupyterlab/notebook-extension:trust-status',
   autoStart: true,
