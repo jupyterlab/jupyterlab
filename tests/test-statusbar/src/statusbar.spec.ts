2f58260f906b5580164ab893c59054479528a4bd
Ian Rose <ian.r.rose@gmail.com>
2018-11-02 09:52:14 -0700

    Continue setting up test package.

--- tests/test-statusbar/src/statusbar.spec.ts
+++ tests/test-statusbar/src/statusbar.spec.ts
@@ -1,2 +1,27 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
+
+import { expect } from 'chai';
+
+import { StatusBar } from '@jupyterlab/statusbar/src';
+
+describe('@jupyterlab/statusbar', () => {
+  describe('StatusBar', () => {
+    let statusBar: StatusBar;
+
+    beforeEach(() => {
+      statusBar = new StatusBar();
+    });
+
+    afterEach(() => {
+      statusBar.dispose();
+    });
+
+    describe('#constructor()', () => {
+      it('should construct a new status bar', () => {
+        const statusBar = new StatusBar();
+        expect(statusBar).to.be.an.instanceof(StatusBar);
+      });
+    });
+  });
+});
