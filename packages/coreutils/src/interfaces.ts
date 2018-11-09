dd56571df19e9e4a562d7d97d61fd24884744f9a
Afshin Darian <git@darian.af>
2018-11-09 17:28:36 +0000

    Update the IDataConnector interface.

--- packages/coreutils/src/interfaces.ts
+++ packages/coreutils/src/interfaces.ts
@@ -23,6 +23,19 @@ export interface IChangedArgs<T> {
 
 /**
  * The description of a general purpose data connector.
+ *
+ * #### Notes *
+ * The generic type arguments <T, U = T, V = string> semantics are:
+ *
+ * T - is the basic entity response type a particular service's connector.
+ *
+ * U = T - is the basic entity request type, which is conventionally the same as
+ * the response type but may be different if a service's implementation requires
+ * input data to be different from output responses.
+ *
+ * V = string - is the basic token applied to a request, conventionally a string
+ * ID or filter, but may be set to a different type when an implementation
+ * requires it.
  */
 export interface IDataConnector<T, U = T, V = string> {
   /**
@@ -37,6 +50,19 @@ export interface IDataConnector<T, U = T, V = string> {
    * retrieving the data. Nonexistence of an `id` will succeed with `undefined`.
    */
   fetch(id: V): Promise<T | undefined>;
+
+  /**
+   * Retrieve the list of items available from the data connector.
+   *
+   * @param filter - The optional filter to apply to the connector request.
+   *
+   * @returns A promise that bears a list of data results. List may be empty.
+   *
+   * #### Notes
+   * The promise returned by this method may be rejected if an error occurs in
+   * retrieving the data. Nonexistence of data will succeed with an empty list.
+   */
+  list(filter?: V): Promise<T[]>;
 
   /**
    * Remove a value using the data connector.
