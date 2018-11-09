a36702335d7554ac2c5d3d14e7e90e9f15813565
Afshin Darian <git@darian.af>
2018-11-09 15:07:43 +0000

    Add list() to DataConnector and add documentation for generic type arguments.

--- packages/coreutils/src/dataconnector.ts
+++ packages/coreutils/src/dataconnector.ts
@@ -11,6 +11,18 @@ import { IDataConnector } from './interfaces';
  * reimplemented by all subclasses. The `remove` and `save` methods have a
  * default implementation that returns a promise that will always reject. This
  * class is a convenience superclass for connectors that only need to `fetch`.
+ *
+ * The generic type arguments <T, U = T, V = string> semantics are:
+ *
+ * T - is the basic entity response type a particular service's connector.
+ *
+ * U = T - is the basic entity request type, which is conventionally the same as
+ * the response type but may be different if a service's implementation requires
+ * input data to be different from output responses.
+ *
+ * V = string - is the basic token applied to a request, conventionally a string
+ * ID or filter, but may be set to a different type when an implementation
+ * requires it.
  */
 export abstract class DataConnector<T, U = T, V = string>
   implements IDataConnector<T, U, V> {
@@ -28,6 +40,21 @@ export abstract class DataConnector<T, U = T, V = string>
   abstract fetch(id: V): Promise<T | undefined>;
 
   /**
+   * Retrieve the list of items available from the data connector.
+   *
+   * @param filter - The optional filter to apply to the connector request.
+   *
+   * @returns A promise that bears a list of data results. List may be empty.
+   *
+   * #### Notes
+   * The promise returned by this method may be rejected if an error occurs in
+   * retrieving the data. Nonexistence of data will succeed with an empty list.
+   */
+  list(filter?: V): Promise<T[]> {
+    return Promise.reject(new Error('list method has not been implemented.'));
+  }
+
+  /**
    * Remove a value using the data connector.
    *
    * @param id - The identifier for the data being removed.
@@ -39,7 +66,7 @@ export abstract class DataConnector<T, U = T, V = string>
    * support a back-end that can remove resources.
    */
   remove(id: V): Promise<void> {
-    return Promise.reject(new Error('Removing has not been implemented.'));
+    return Promise.reject(new Error('remove method has not been implemented.'));
   }
 
   /**
@@ -56,6 +83,6 @@ export abstract class DataConnector<T, U = T, V = string>
    * support a back-end that can save resources.
    */
   save(id: V, value: U): Promise<void> {
-    return Promise.reject(new Error('Saving has not been implemented.'));
+    return Promise.reject(new Error('save method has not been implemented.'));
   }
 }
