344973be4d311447c3dc0d9c8e496de28a03adb6
Brian E. Granger <ellisonbg@gmail.com>
2018-12-18 19:57:27 -0800

    Refactor databus and add converters.

---
+++ packages/databus/src/converters.ts
@@ -0,0 +1,181 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+
+import { DisposableDelegate, IDisposable } from '@phosphor/disposable';
+
+import { ArrayExt } from '@phosphor/algorithm';
+
+import { ISignal, Signal } from '@phosphor/signaling';
+
+import { IDataset } from './dataregistry';
+
+/**
+ * An interface for a converter between data of two mime types.
+ */
+export interface IConverter<A, B> {
+  /**
+   * The input mime type of the data of type `A` to be converted.
+   */
+  sourceMimeType: string;
+
+  /**
+   * The output mime type of the converted data of type `B`.
+   */
+  targetMimeType: string;
+
+  /**
+   * The conversion function.
+   *
+   * #### Notes
+   * This takes an input `IDataset<A>` and converts it to a promose to
+   * an `IDataset<B>`.
+   */
+  converter: (input: IDataset<A>) => Promise<IDataset<B>>;
+}
+
+/**
+ * A registry for data converters.
+ *
+ * Questions:
+ *
+ * - Should we allow multiple converters between two mime types? If so, how do
+ *   we decide which to use?
+ * - Do we walk to graph to find chains of converters betweeen the source and target?
+ */
+export class ConverterRegistry {
+  /**
+   * Construct a new converter registry.
+   */
+  constructor() {}
+
+  /**
+   * Register a dataset converter
+   *
+   * @param converter - the `IConverter` to register.
+   *
+   * @returns A disposable which will remove the converter from the registry.
+   *
+   * @throws An error if the converter is already registered.
+   * */
+  register(converter: IConverter<any, any>): IDisposable {
+    let source = converter.sourceMimeType;
+
+    if (this._converters[source] === undefined) {
+      this._converters[source] = [];
+    }
+    this._converters[source].push(converter);
+
+    this._convertersChanged.emit({ converter, type: 'added' });
+
+    return new DisposableDelegate(() => {
+      ArrayExt.removeFirstOf(this._converters[source], converter);
+      this._convertersChanged.emit({ converter, type: 'removed' });
+    });
+  }
+
+  /**
+   * A signal that will fire when converters are registered or removed.
+   */
+  get convertersChanged(): ISignal<
+    this,
+    ConverterRegistry.IConvertersChangedArgs
+  > {
+    return this._convertersChanged;
+  }
+
+  /**
+   * Convert an `IDataset<A>` to an `IDataset<B>`.
+   *
+   * @param sourceDataset - the input dataset.
+   *
+   * @param targetMimeType - the desired mimeType of the output dataset.
+   *
+   * @returns A promise that resolves to the converted dataset with the target mime
+   *          type and type `IDataset<B>`.
+   */
+  convert<A, B>(
+    sourceDataset: IDataset<A>,
+    targetMimeType: string
+  ): Promise<IDataset<B>> {
+    let converter: IConverter<A, B> = this._resolveConverter(
+      sourceDataset.mimeType,
+      targetMimeType
+    );
+    return converter.converter(sourceDataset);
+  }
+
+  /**
+   * List the available target mime types for an input mime type.
+   *
+   * @param sourceMimeType - the input mime type.
+   *
+   * @returns An `Array<string>` of the available target mime types.
+   */
+  listTargetMimeTypes(sourceMimeType: string): Array<string> {
+    let converters: Array<IConverter<any, any>> = this._converters[
+      sourceMimeType
+    ];
+    if (converters === undefined) {
+      return [];
+    }
+    return converters.map(value => {
+      return (value as IConverter<any, any>).targetMimeType;
+    });
+  }
+
+  /**
+   * Lookup a converter between source and target mime types.
+   *
+   * @param sourceMimeType - the mime type of the input dataset.
+   *
+   * @param targetMimeType - the mime type of the converted output dataset.
+   *
+   * @returns The `IConverter<A.B>` capable of doing the conversion or undefined.
+   *
+   * #### Notes
+   * This does not currently traverse the directed graph of converters to identify chains of
+   * converters that match.
+   */
+  private _resolveConverter<A, B>(
+    sourceMimeType: string,
+    targetMimeType: string
+  ): IConverter<A, B> {
+    let converters: Array<IConverter<any, any>> = this._converters[
+      sourceMimeType
+    ];
+    if (converters === undefined) {
+      return undefined;
+    }
+    ArrayExt.findFirstValue(converters, value => {
+      return value.targetMimeType === targetMimeType;
+    });
+  }
+
+  private _converters: { [key: string]: Array<IConverter<any, any>> } = {};
+  private _convertersChanged = new Signal<
+    this,
+    ConverterRegistry.IConvertersChangedArgs
+  >(this);
+}
+
+/**
+ * A public namespace for the converter registry.
+ */
+export namespace ConverterRegistry {
+  /**
+   * An interface for the changed args of the converters changed signal.
+   */
+  export interface IConvertersChangedArgs {
+    /**
+     * The converter begin added or removed.
+     */
+    readonly converter: IConverter<any, any>;
+
+    /**
+     * The type of change.
+     */
+    readonly type: 'added' | 'removed';
+  }
+}
---
+++ packages/databus/src/dataregistry.ts
@@ -0,0 +1,136 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+
+import { DisposableDelegate, IDisposable } from '@phosphor/disposable';
+
+import { ISignal, Signal } from '@phosphor/signaling';
+
+/**
+ * An interface for an abstract dataset, with a mimetype, URI, and data.
+ *
+ * This is templated over the type of data `T`.
+ *
+ * #### Notes
+ * This interface is similar conceptually to that of nbformat.IMimeBundle,
+ * but with only a single mimetype and different structure. We expect there
+ * to be utilities to convert between this formats.
+ */
+export interface IDataset<T> {
+  /**
+   * The string mimetype for the dataset.
+   */
+  mimeType: string;
+
+  /**
+   * A persistent URI that points to the dataset.
+   *
+   * #### Notes
+   * This can be used by other extensions and services to maintain
+   * persistent pointers to datasets across sessions. This is needed
+   * as the datasets themselves are not assumed to be serializable.
+   */
+  uri?: string;
+
+  /***
+   * The data of the dataset.
+   *
+   * #### Notes
+   * The notion of data in the databus is completely abstract, and templated
+   * over. Publishers and consumers of a given mimetype will need to agree
+   * what the actual data is and cast it appropriately before use.
+   */
+  data: T;
+}
+
+/**
+ * A data registry object, for managing datasets in JupyterLab.
+ */
+export class DataRegistry {
+  /**
+   * Construct a new data registry.
+   */
+  constructor() {}
+
+  /**
+   * Publish a dataset to the data registry.
+   *
+   * @param dataset - the `IDataset` to publish to the data registry.
+   *
+   * @returns A disposable which will remove the dataset.
+   *
+   * @throws An error if the given dataset is already published.
+   */
+  publish(dataset: IDataset<any>): IDisposable {
+    if (this._datasets.has(dataset)) {
+      throw new Error(`Dataset already published`);
+    }
+
+    this._datasets.add(dataset);
+
+    this._datasetsChanged.emit({ dataset, type: 'added' });
+
+    return new DisposableDelegate(() => {
+      this._datasets.delete(dataset);
+      this._datasetsChanged.emit({ dataset, type: 'removed' });
+    });
+  }
+
+  /**
+   * Filter the published datasets using a filtering function.
+   *
+   * @param func - A function to use for filtering.
+   *
+   * @returns An set of matching `IDataset` objects.
+   */
+  filter<T>(func: (value: IDataset<T>) => boolean): Set<IDataset<T>> {
+    let result: Set<IDataset<T>> = new Set();
+    this._datasets.forEach((value: IDataset<T>) => {
+      if (func(value as IDataset<T>)) {
+        result.add(value);
+      }
+    });
+    return result;
+  }
+
+  /**
+   * Return a set of all published datasets.
+   */
+  get datasets(): Set<IDataset<any>> {
+    return this._datasets;
+  }
+
+  /**
+   * A signal that will fire when datasets are published or removed.
+   */
+  get datasetsChanged(): ISignal<this, DataRegistry.IDatasetsChangedArgs> {
+    return this._datasetsChanged;
+  }
+
+  private _datasets: Set<IDataset<any>> = new Set();
+  private _datasetsChanged = new Signal<
+    this,
+    DataRegistry.IDatasetsChangedArgs
+  >(this);
+}
+
+/**
+ * A public namespace for the databus.
+ */
+export namespace DataRegistry {
+  /**
+   * An interface for the changed args of the dataset changed signal.
+   */
+  export interface IDatasetsChangedArgs {
+    /**
+     * The dataset begin added or removed.
+     */
+    readonly dataset: IDataset<any>;
+
+    /**
+     * The type of change.
+     */
+    readonly type: 'added' | 'removed';
+  }
+}
