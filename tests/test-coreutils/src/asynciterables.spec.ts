d58f98e9c66fe31383eadb049273c88b3dce2909
Saul Shanabrook <s.shanabrook@gmail.com>
2019-01-02 13:20:24 -0500

    Add another test

--- tests/test-coreutils/src/asynciterables.spec.ts
+++ tests/test-coreutils/src/asynciterables.spec.ts
@@ -1,7 +1,10 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { AsyncIteratorSignal } from '@jupyterlab/coreutils/src';
+import {
+  AsyncIteratorSignal,
+  AsyncIterableSignal
+} from '@jupyterlab/coreutils/src';
 import { Signal } from '@phosphor/signaling';
 
 describe('@jupyterlab/coreutils', () => {
@@ -79,5 +82,45 @@ describe('@jupyterlab/coreutils', () => {
         expect(await second).toEqual({ done: true });
       });
     });
+  });
+  describe('AsyncIterableSignal', () => {
+    describe('async iteration', () => {
+      it('should get first value', async () => {
+        expect.assertions(1);
+        const sender = {};
+        const signal = new Signal<{}, number>(sender);
+        const asyncIterable = new AsyncIterableSignal(signal);
+
+        // NOTE: we have to have the signal emission happen
+        // after the iterator is generated from the iterable
+        // I am not sure how to do this deterministically,
+        // but this seems to work for now.
+        (async () => {
+          for await (const [sender, args] of asyncIterable) {
+            expect(args).toEqual(1);
+          }
+        })();
+        signal.emit(1);
+      });
+    });
+    //   it('should stop on break', async () => {
+    //     expect.assertions(1);
+    //     const sender = {};
+    //     const signal = new Signal<{}, number>(sender);
+    //     const asyncIterable = new AsyncIterableSignal(signal);
+
+    //     // NOTE: we have to have the signal emission happen
+    //     // after the iterator is generated from the iterable
+    //     // I am not sure how to do this deterministically,
+    //     // but this seems to work for now.
+    //     const gen = (async *() => {
+    //       for await (const [sender, args] of asyncIterable) {
+    //         expect(args).toEqual(1);
+    //         yield;
+    //       }
+    //     })();
+    //     signal.emit(1);
+    //   });
+    // });
   });
 });
