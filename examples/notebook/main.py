6c239899f204963d2528e7d08d4aac821ab1f252
yuvipanda <yuvipanda@gmail.com>
2019-01-04 02:36:09 -0800

    [examples/notebook] Support base_url properly

--- examples/notebook/main.py
+++ examples/notebook/main.py
@@ -15,6 +15,7 @@ import os
 from jinja2 import FileSystemLoader
 from notebook.base.handlers import IPythonHandler, FileFindHandler
 from notebook.notebookapp import NotebookApp
+from notebook.utils import url_path_join as ujoin
 from traitlets import Unicode
 
 HERE = os.path.dirname(__file__)
@@ -55,8 +56,8 @@ class ExampleApp(NotebookApp):
         """
         super(ExampleApp, self).init_webapp()
         default_handlers = [
-            (r'/notebook/(.*)?', NotebookHandler),
-            (r"/build/(.*)", FileFindHandler,
+            (ujoin(self.base_url, r'notebook/(.*)?'), NotebookHandler),
+            (ujoin(self.base_url, r"build/(.*)"), FileFindHandler,
                 {'path': os.path.join(HERE, 'build')})
         ]
         self.web_app.add_handlers('.*$', default_handlers)
