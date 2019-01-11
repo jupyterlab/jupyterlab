d7a0b9df773190d5ace82f69535a8e6e6685dde1
Ian Rose <ian.r.rose@gmail.com>
2019-01-10 18:20:49 -0800

    Restructure into a standard extension rather than a mimerenderer.

--- packages/htmlviewer-extension/src/index.tsx
+++ packages/htmlviewer-extension/src/index.tsx
@@ -1,23 +1,19 @@
-// Distributed under the terms of the Modified BSD License.
+/*-----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
 
 import {
-  PanelLayout, Widget
-} from '@phosphor/widgets';
+  ILayoutRestorer,
+  JupyterLab,
+  JupyterLabPlugin
+} from '@jupyterlab/application';
 
-import {
-  IFrame
-} from '@jupyterlab/apputils';
+import { InstanceTracker } from '@jupyterlab/apputils';
 
-import {
-  IRenderMime
-} from '@jupyterlab/rendermime-interfaces';
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 
-import '../style/index.css';
-
-/**
- * The CSS class to add to the Plotly Widget.
- */
-const CSS_CLASS = 'jp-RenderedIFrame';
+import { HTMLViewer, HTMLViewerFactory } from '@jupyterlab/htmlviewer';
 
 /**
  * The CSS class for an HTML5 icon.
@@ -25,107 +21,65 @@ const CSS_CLASS = 'jp-RenderedIFrame';
 const CSS_ICON_CLASS = 'jp-MaterialIcon jp-HTMLIcon';
 
 /**
- * The MIME type for HTML. We don't use `text/html` because that
- * will conflict with the already existing `text/html` mimetype
- * used for inline HTML in JupyterLab.
+ * The HTML file handler extension.
  */
-export
-const MIME_TYPE = 'application/vnd.jupyter.iframe.text';
-
-export
-const HTML_CLASS = 'jp-HTMLViewer';
-
-export
-const HTML_CONTAINER_CLASS = 'jp-HTMLContainer';
-
-export
-class RenderedIFrame extends Widget implements IRenderMime.IRenderer {
-  /**
-   * Create a new widget for rendering HTML.
-   */
-  constructor(options: IRenderMime.IRendererOptions) {
-    super();
-    const layout = this.layout = new PanelLayout();
-    this.addClass(CSS_CLASS);
-    this._iframe = new IFrame();
-    layout.addWidget(this._iframe);
-    this._mimeType = options.mimeType;
-    this._resolver = options.resolver;
-  }
-
-  /**
-   * Render HTML in IFrame into this widget's node.
-   */
-  async renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-    let data = model.data[this._mimeType] as string;
-    data = await this._setBase(data);
-
-    const blob = new Blob([data], {type: "text/html"});
-    this._iframe.url = URL.createObjectURL(blob);
-    return Promise.resolve(void 0);
-  }
-
-  /**
-   * Set a <base> element in the HTML string so that the iframe
-   * can correctly dereference relative links/
-   */
-  private async _setBase(data: string): Promise<string> {
-    const doc = this._parser.parseFromString(data, 'text/html');
-    let base: HTMLBaseElement;
-    base = doc.querySelector('base');
-    if (!base) {
-      base = doc.createElement('base');
-      doc.head.insertBefore(base, doc.head.firstChild);
-    }
-    const path = await this._resolver.resolveUrl('');
-    const baseUrl = await this._resolver.getDownloadUrl(path);
-
-    // Set the base href, plus a fake name for the url of this
-    // document. The fake name doesn't really matter, as long
-    // as the document can dereference relative links to resources
-    // (e.g. CSS and scripts).
-    base.href = `${baseUrl}/__fake__.html`;
-    base.target = '_blank';
-    return doc.documentElement.innerHTML;
-  }
-
-  private _parser = new DOMParser();
-  private _resolver: IRenderMime.IResolver;
-  private _iframe: IFrame;
-  private _mimeType: string;
-}
-
-
-/**
- * A mime renderer factory for HTML data.
- */
-export
-const rendererFactory: IRenderMime.IRendererFactory = {
-  safe: true,
-  mimeTypes: [MIME_TYPE],
-  createRenderer: options => new RenderedIFrame(options)
+const htmlPlugin: JupyterLabPlugin<void> = {
+  activate: activateHTMLViewer,
+  id: '@jupyterlab/htmlviewer-extension:plugin',
+  requires: [ILayoutRestorer],
+  autoStart: true
 };
 
+/**
+ * Activate the HTMLViewer extension.
+ */
+function activateHTMLViewer(app: JupyterLab, restorer: ILayoutRestorer): void {
+  // Add an HTML file type to the docregistry.
+  const ft: DocumentRegistry.IFileType = {
+    name: 'html',
+    contentType: 'file',
+    fileFormat: 'text',
+    displayName: 'HTML File',
+    extensions: ['.html'],
+    mimeTypes: ['text/html'],
+    iconClass: CSS_ICON_CLASS
+  };
+  app.docRegistry.addFileType(ft);
 
-const extensions: IRenderMime.IExtension | IRenderMime.IExtension[] = [
-  {
-    id: '@jupyterlab_html:factory',
-    rendererFactory,
-    rank: 0,
-    dataType: "string",
-    fileTypes: [{
-      name: 'html',
-      mimeTypes: [MIME_TYPE],
-      extensions: ['.html'],
-      iconClass: CSS_ICON_CLASS
-    }],
-    documentWidgetFactoryOptions: {
-      name: 'View HTML',
-      primaryFileType: 'html',
-      fileTypes: ['html'],
-      defaultFor: ['html']
-    }
-  }
-];
+  // Create a new viewer factory.
+  const factory = new HTMLViewerFactory({
+    name: 'HTML Viewer',
+    fileTypes: ['html'],
+    defaultFor: ['html'],
+    readOnly: true
+  });
 
-export default extensions;
+  // Create an instance tracker for HTML documents.
+  const tracker = new InstanceTracker<HTMLViewer>({
+    namespace: 'htmlviewer'
+  });
+
+  // Handle state restoration.
+  restorer.restore(tracker, {
+    command: 'docmanager:open',
+    args: widget => ({ path: widget.context.path, factory: 'HTML Viewer' }),
+    name: widget => widget.context.path
+  });
+
+  app.docRegistry.addWidgetFactory(factory);
+  factory.widgetCreated.connect((sender, widget) => {
+    // Track the widget.
+    tracker.add(widget);
+    // Notify the instance tracker if restore data needs to update.
+    widget.context.pathChanged.connect(() => {
+      tracker.save(widget);
+    });
+
+    widget.title.iconClass = ft.iconClass;
+    widget.title.iconLabel = ft.iconLabel;
+  });
+}
+/**
+ * Export the plugins as default.
+ */
+export default htmlPlugin;
---
+++ packages/htmlviewer/src/index.ts
@@ -0,0 +1,130 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+
+import { IFrame } from '@jupyterlab/apputils';
+
+import { ActivityMonitor } from '@jupyterlab/coreutils';
+
+import {
+  ABCWidgetFactory,
+  DocumentRegistry,
+  DocumentWidget,
+  IDocumentWidget
+} from '@jupyterlab/docregistry';
+
+import '../style/index.css';
+
+/**
+ * The timeout to wait for change activity to have ceased before rendering.
+ */
+const RENDER_TIMEOUT = 1000;
+
+/**
+ * The CSS class to add to the HTMLViewer Widget.
+ */
+const CSS_CLASS = 'jp-HTMLViewer';
+
+/**
+ * A viewer widget for HTML documents.
+ */
+export class HTMLViewer extends DocumentWidget<IFrame>
+  implements IDocumentWidget<IFrame> {
+  /**
+   * Create a new widget for rendering HTML.
+   */
+  constructor(options: DocumentWidget.IOptionsOptionalContent) {
+    super({ ...options, content: new IFrame() });
+    this.content.addClass(CSS_CLASS);
+
+    this.context.ready.then(() => {
+      this.update();
+      // Throttle the rendering rate of the widget.
+      this._monitor = new ActivityMonitor({
+        signal: this.context.model.contentChanged,
+        timeout: RENDER_TIMEOUT
+      });
+      this._monitor.activityStopped.connect(
+        this.update,
+        this
+      );
+    });
+  }
+
+  /**
+   * Handle and update request.
+   */
+  protected onUpdateRequest(): void {
+    if (this._renderPending) {
+      return;
+    }
+    this._renderPending = true;
+    this._renderModel().then(() => (this._renderPending = false));
+  }
+
+  /**
+   * Render HTML in IFrame into this widget's node.
+   */
+  private async _renderModel(): Promise<void> {
+    let data = this.context.model.toString();
+    data = await this._setBase(data);
+
+    // Set the new iframe url.
+    const blob = new Blob([data], { type: 'text/html' });
+    const oldUrl = this._objectUrl;
+    this._objectUrl = URL.createObjectURL(blob);
+    this.content.url = this._objectUrl;
+
+    // Release reference to any previous object url.
+    if (oldUrl) {
+      try {
+        URL.revokeObjectURL(oldUrl);
+      } catch (error) {
+        /* no-op */
+      }
+    }
+    return;
+  }
+
+  /**
+   * Set a <base> element in the HTML string so that the iframe
+   * can correctly dereference relative links.
+   */
+  private async _setBase(data: string): Promise<string> {
+    const doc = this._parser.parseFromString(data, 'text/html');
+    let base: HTMLBaseElement;
+    base = doc.querySelector('base');
+    if (!base) {
+      base = doc.createElement('base');
+      doc.head.insertBefore(base, doc.head.firstChild);
+    }
+    const path = this.context.path;
+    const baseUrl = await this.context.urlResolver.getDownloadUrl(path);
+
+    // Set the base href, plus a fake name for the url of this
+    // document. The fake name doesn't really matter, as long
+    // as the document can dereference relative links to resources
+    // (e.g. CSS and scripts).
+    base.href = baseUrl;
+    base.target = '_blank';
+    return doc.documentElement.innerHTML;
+  }
+
+  private _renderPending = false;
+  private _parser = new DOMParser();
+  private _monitor: ActivityMonitor<any, any> | null = null;
+  private _objectUrl: string = '';
+}
+
+/**
+ * A widget factory for HTMLViewers.
+ */
+export class HTMLViewerFactory extends ABCWidgetFactory<HTMLViewer> {
+  /**
+   * Create a new widget given a context.
+   */
+  protected createNewWidget(context: DocumentRegistry.Context): HTMLViewer {
+    return new HTMLViewer({ context });
+  }
+}
