bfd8021eb315289b73c01775acf9aaba088a0a2e
Saul Shanabrook <s.shanabrook@gmail.com>
2019-02-22 19:14:28 -0500

    Support auto registering datasets

--- packages/databus/src/rendermime.tsx
+++ packages/databus/src/rendermime.tsx
@@ -11,11 +11,15 @@ import { Dataset } from './dataregistry';
 // https://github.com/jupyter/nbformat/blob/9dd1b8719c9095eaebce1846416bb3a5e939758f/nbformat/v4/nbformat.v4.schema.json#L407
 const datasetMimeType = 'application/x.jupyter.dataset+json';
 
-type DatasetMimeData = Array<{
-  mimeType: string;
-  url: string;
-  data: JSONValue;
-}>;
+type DatasetMimeData = {
+  autoRegister: boolean;
+  datasets: Array<{
+    mimeType: string;
+    url: string;
+    data: JSONValue;
+  }>;
+};
+
 type Register = (datasets: Array<Dataset<any>>) => Promise<void>;
 class RendererDataset extends ReactWidget implements IRenderMime.IRenderer {
   constructor(private _register: Register) {
@@ -23,21 +27,26 @@ class RendererDataset extends ReactWidget implements IRenderMime.IRenderer {
   }
 
   render() {
-    return (
-      <button onClick={() => this._register(this._datasets)}>
-        Register datasets
-      </button>
-    );
+    if (this._data!.autoRegister) {
+      return <div />;
+    }
+    return <button onClick={() => this.register()}>Register datasets</button>;
   }
   async renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-    this._datasets = [];
-    for (const { mimeType, url, data } of model.data[
-      datasetMimeType
-    ] as DatasetMimeData) {
-      this._datasets.push(new Dataset(mimeType, new URL(url), data));
+    this._data = model.data[datasetMimeType] as DatasetMimeData;
+    if (this._data.autoRegister) {
+      this.register();
     }
   }
-  private _datasets: Array<Dataset<any>> = [];
+
+  register() {
+    this._register(
+      this._data!.datasets.map(
+        ({ mimeType, url, data }) => new Dataset(mimeType, new URL(url), data)
+      )
+    );
+  }
+  private _data: DatasetMimeData | null = null;
 }
 
 export function createRenderMimeFactory(
