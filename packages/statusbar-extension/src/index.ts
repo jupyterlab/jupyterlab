585c20d3be2939ff783fa537b5189f83e4028664
Ian Rose <ian.r.rose@gmail.com>
2019-02-27 09:46:03 -0800

    Restore statusbar icon.

--- packages/statusbar-extension/src/index.ts
+++ packages/statusbar-extension/src/index.ts
@@ -42,6 +42,8 @@ import { IMainMenu } from '@jupyterlab/mainmenu';
 
 import { Title, Widget } from '@phosphor/widgets';
 
+import '../style/index.css';
+
 export const STATUSBAR_PLUGIN_ID = '@jupyterlab/statusbar-extension:plugin';
 
 /**
