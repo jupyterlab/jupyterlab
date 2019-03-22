426adbdd7c792c6633b1d3d9859797aaad30bcef
Ian Rose <ian.r.rose@gmail.com>
2019-03-22 09:20:40 -0700

    Revert "Revert 98cee8c92587a8e037b5fac6cb6, which, while not necessarily a bug,"
    This reverts commit a33bdcb67fafede6ad95619388b0e986c03bed5d.

--- packages/services/src/kernel/default.ts
+++ packages/services/src/kernel/default.ts
@@ -344,8 +344,8 @@ export class DefaultKernel implements Kernel.IKernel {
    * Handle a restart on the kernel.  This is not part of the `IKernel`
    * interface.
    */
-  handleRestart(): void {
-    this._clearState();
+  async handleRestart(): Promise<void> {
+    await this._clearState();
     this._updateStatus('restarting');
     this._clearSocket();
   }
@@ -380,11 +380,11 @@ export class DefaultKernel implements Kernel.IKernel {
   async shutdown(): Promise<void> {
     if (this.status === 'dead') {
       this._clearSocket();
-      this._clearState();
+      await this._clearState();
       return;
     }
     await Private.shutdownKernel(this.id, this.serverSettings);
-    this._clearState();
+    await this._clearState();
     this._clearSocket();
   }
 
@@ -850,11 +850,13 @@ export class DefaultKernel implements Kernel.IKernel {
   /**
    * Clear the internal state.
    */
-  private _clearState(): void {
+  private async _clearState(): Promise<void> {
     this._isReady = false;
     this._pendingMessages = [];
+    const futuresResolved: Promise<KernelMessage.IShellMessage>[] = [];
     this._futures.forEach(future => {
       future.dispose();
+      futuresResolved.push(future.done);
     });
     this._comms.forEach(comm => {
       comm.dispose();
@@ -865,6 +867,10 @@ export class DefaultKernel implements Kernel.IKernel {
     this._comms = new Map<string, Kernel.IComm>();
     this._displayIdToParentIds.clear();
     this._msgIdToDisplayIds.clear();
+
+    await Promise.all(futuresResolved).catch(() => {
+      /* no-op */
+    });
   }
 
   /**
@@ -1526,7 +1532,9 @@ namespace Private {
     // We might want to move the handleRestart to after we get the response back
 
     // Handle the restart on all of the kernels with the same id.
-    runningKernels.forEach(k => k.handleRestart());
+    await Promise.all(
+      runningKernels.filter(k => k.id === kernel.id).map(k => k.handleRestart())
+    );
     let response = await ServerConnection.makeRequest(url, init, settings);
     if (response.status !== 200) {
       throw new ServerConnection.ResponseError(response);
