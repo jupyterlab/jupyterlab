17f06dc477829c8e61e890f4e9c961f46276eb09
Steven Silvester <steven.silvester@ieee.org>
2019-05-08 08:09:24 -0500

    fix _version.py

--- jupyterlab/_version.py
+++ jupyterlab/_version.py
@@ -1,6 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import re
 from collections import namedtuple
 
 VersionInfo = namedtuple('VersionInfo', [
@@ -14,24 +15,21 @@ VersionInfo = namedtuple('VersionInfo', [
 # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
 __version__ = '1.0.0a3'
 
-parts = __version__.split('.')
 
-major = int(parts[0])
-minor = int(parts[1])
-micro = int(parts[2])
-rel = 'final'
-ser = 0
+# Extract the version info from the version.
+patt = r'(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)((?P<release>\S+)(?P<build>\d+))?'
+match = re.match(patt, __version__)
 
-# handle the release level
-if len(parts) >= 4:
-    if parts[3] == 'a':
-        rel = 'alpha'
-    elif parts[3] == 'rc':
-        rel == 'candidate'
+major = match['major']
+minor = match['minor']
+micro = match['patch']
+release = match['release'] or 'final'
+build = match['build'] or 0
 
-# Handle the serial
-if len(parts) < 5:
-   ser = int(parts[4])
+if release == 'a':
+    release = 'alpha'
+elif release == 'rc':
+    release = 'candidate'
 
 # Create the version info
-version_info = VersionInfo(major, minor, micro, rel, ser)
+version_info = VersionInfo(major, minor, micro, release, build)
