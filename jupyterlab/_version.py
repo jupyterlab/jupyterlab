71fe80b6c13646316ccb5080ef37d13ccef923a7
Steven Silvester <steven.silvester@ieee.org>
2019-05-09 02:30:43 -0500

    return to original version.py with a comment

--- jupyterlab/_version.py
+++ jupyterlab/_version.py
@@ -1,7 +1,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-import re
 from collections import namedtuple
 
 VersionInfo = namedtuple('VersionInfo', [
@@ -13,23 +12,14 @@ VersionInfo = namedtuple('VersionInfo', [
 ])
 
 # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
-__version__ = '1.0.0a3'
+version_info = VersionInfo(1, 0, 0, 'alpha', 3)
 
+_specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
-# Extract the version info from the version.
-patt = r'(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)((?P<release>\S+)(?P<build>\d+))?'
-match = re.match(patt, __version__)
-
-major = int(match.group('major'))
-minor = int(match.group('minor'))
-patch = int(match.group('patch'))
-release = match.group('release') or 'final'
-build = int(match.group('build') or 0)
-
-if release == 'a':
-    release = 'alpha'
-elif release == 'rc':
-    release = 'candidate'
-
-# Create the version info
-version_info = VersionInfo(major, minor, patch, release, build)
+__version__ = '{}.{}.{}{}'.format(
+    version_info.major,
+    version_info.minor,
+    version_info.micro,
+    (''
+     if version_info.releaselevel == 'final'
+else _specifier_[version_info.releaselevel] + str(version_info.serial)))
