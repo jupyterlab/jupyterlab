a9cc9d2ee5037a23c85c7731a78227bc54b73124
Steven Silvester <steven.silvester@ieee.org>
2019-06-01 04:30:28 -0500

    wip hub integration

--- jupyterlab/labhubapp.py
+++ jupyterlab/labhubapp.py
@@ -18,22 +18,10 @@ else:
             return "/lab"
 
         def init_webapp(self, *args, **kwargs):
+             warnings.warn(
+                "SingleUserLabApp is deprecated, use SingleUserNotebookApp and set " + "c.Spawner.default_url = '/lab' in jupyterhub_config.py", DeprecationWarning
+            )
             super().init_webapp(*args, **kwargs)
-            settings = self.web_app.settings
-            if 'page_config_data' not in settings:
-                settings['page_config_data'] = {}
-            settings['page_config_data']['hub_prefix'] = self.hub_prefix
-            settings['page_config_data']['hub_host'] = self.hub_host
-            settings['page_config_data']['hub_user'] = self.user
-            api_token = os.getenv('JUPYTERHUB_API_TOKEN')
-            if not api_token:
-                api_token = ''
-            if not self.token:
-                try:
-                    self.token = api_token
-                except AttributeError:
-                    self.log.error("Can't set self.token")
-            settings['page_config_data']['token'] = api_token
 
 
 def main(argv=None):
