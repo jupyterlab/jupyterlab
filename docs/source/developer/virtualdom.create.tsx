40bcad2cbf9e5ecb27fce6b186cc45329bf5e462
Saul Shanabrook <s.shanabrook@gmail.com>
2019-06-18 10:32:11 -0700

    Add full react component to docs

--- docs/source/developer/virtualdom.create.tsx
+++ docs/source/developer/virtualdom.create.tsx
@@ -3,4 +3,8 @@ import * as React from 'react';
 import { Widget } from '@phosphor/widgets';
 import { ReactWidget } from '@jupyterlab/apputils';
 
-const myWidget: Widget = ReactWidget.create(<div>My Widget</div>);
+function MyComponent() {
+  return <div>My Widget</div>;
+}
+
+const myWidget: Widget = ReactWidget.create(<MyComponent />);
--- docs/source/developer/virtualdom.reactwidget.tsx
+++ docs/source/developer/virtualdom.reactwidget.tsx
@@ -3,9 +3,12 @@ import * as React from 'react';
 import { Widget } from '@phosphor/widgets';
 import { ReactWidget } from '@jupyterlab/apputils';
 
+function MyComponent() {
+  return <div>My Widget</div>;
+}
 class MyWidget extends ReactWidget {
   render() {
-    return <div>My Widget</div>;
+    return <MyComponent />;
   }
 }
 const myWidget: Widget = new MyWidget();
