f5358b19d23459fb0f9e8aad5c4126474535c1b4
Afshin T. Darian <git@darian.af>
2019-07-24 15:04:06 +0100

    Remove launcher item for now as well. It may be unnecessary.

--- src/index.ts
+++ src/index.ts
@@ -7,45 +7,30 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ILauncher } from '@jupyterlab/launcher';
-
 import { Debugger } from './debugger';
 
 import { IDebugger } from './tokens';
-
-/**
- * The command IDs used by the debugger plugin.
- */
-namespace CommandIDs {
-  export const open = 'debugger:open';
-}
 
 /**
  * A plugin providing a UI for code debugging and environment inspection.
  */
 const plugin: JupyterFrontEndPlugin<IDebugger> = {
   id: '@jupyterlab/debugger:plugin',
-  optional: [ILauncher, ILayoutRestorer],
+  optional: [ILayoutRestorer],
   provides: IDebugger,
   autoStart: true,
   activate: (
     app: JupyterFrontEnd,
-    launcher: ILauncher | null,
     restorer: ILayoutRestorer | null
   ): IDebugger => {
     console.log(plugin.id, 'Hello, world.');
     const { shell } = app;
-    const command = CommandIDs.open;
     const label = 'Environment';
     const widget = new Debugger();
 
     widget.id = 'jp-debugger';
     widget.title.label = label;
     shell.add(widget, 'right', { activate: false });
-
-    if (launcher) {
-      launcher.add({ command, args: { isLauncher: true } });
-    }
 
     if (restorer) {
       restorer.add(widget, widget.id);
