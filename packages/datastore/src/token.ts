ec1d6a5751ea2ab4b25e9f699b8ca104a375a525
Saul Shanabrook <s.shanabrook@gmail.com>
2019-07-27 17:27:04 -0400

    Fix todo example.
    Now this simple todo example works, showing an example
    of how we could create react components using the data store
    
    ![](https://media.giphy.com/media/dWrUxOWwtge4fA171l/giphy.gif)

--- packages/datastore/src/token.ts
+++ packages/datastore/src/token.ts
@@ -1,16 +1,26 @@
 import { Token } from '@phosphor/coreutils';
-import { Schema, Table } from '@phosphor/datastore';
+import { Schema, Table, Datastore } from '@phosphor/datastore';
 import { DatastoreManager } from './manager';
-import { ISignal } from '@phosphor/signaling';
+import { ISignal, Signal } from '@phosphor/signaling';
 
+/**
+ * The table manager is what is exposed to the user to access a table.
+ */
 export class TableManager<T extends Schema> {
-  constructor(private manager: DatastoreManager, private schema: T) {}
+  constructor(private manager: DatastoreManager, private schema: T) {
+    this._changed = new Signal(this);
+    this._currentDatastore = this.manager.datastore;
+    this._currentDatastore.changed.connect(this._datastoreSlot, this);
+    this.manager.datastoreChanged.connect(this._managerSlot, this);
+  }
 
   get table(): Table<T> {
     return this.manager.datastore.get(this.schema);
   }
 
   dispose(): void {
+    this.manager.datastoreChanged.disconnect(this._managerSlot, this);
+    this._currentDatastore.changed.disconnect(this._datastoreSlot, this);
     this.manager.dispose();
   }
 
@@ -23,8 +33,21 @@ export class TableManager<T extends Schema> {
   }
 
   get changed(): ISignal<unknown, unknown> {
-    return this.manager.datastore.changed;
+    return this._changed;
   }
+
+  private _datastoreSlot(): void {
+    this._changed.emit(null);
+  }
+
+  private _managerSlot(): void {
+    this._currentDatastore.changed.disconnect(this._datastoreSlot, this);
+    this._currentDatastore = this.manager.datastore;
+    this._currentDatastore.changed.connect(this._datastoreSlot, this);
+  }
+
+  private _currentDatastore: Datastore;
+  private readonly _changed: Signal<unknown, unknown>;
 }
 export class DatastoreCreator {
   async createTable<S extends Schema>(
