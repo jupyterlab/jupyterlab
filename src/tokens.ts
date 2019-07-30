c42ad23b6d047030e747cbb13587b1ce2ae9eb3d
Afshin T. Darian <git@darian.af>
2019-07-30 12:35:00 +0100

    Add initial interfaces.

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,8 +1,42 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+import { IClientSession } from '@jupyterlab/apputils';
+
+import { CodeEditor } from '@jupyterlab/codeeditor';
+
 import { Token } from '@phosphor/coreutils';
 
-export interface IDebugger {}
+import { IDisposable } from '@phosphor/disposable';
+
+/**
+ * A visual debugger.
+ */
+export interface IDebugger {
+  /**
+   * The active debugger session.
+   */
+  session: IDebugger.ISession | null;
+}
+
+/**
+ * A visual debugger.
+ */
+export namespace IDebugger {
+  /**
+   * A visual debugger session.
+   */
+  export interface ISession extends IDisposable {
+    /**
+     * The API client session to connect to a debugger.
+     */
+    client: IClientSession;
+
+    /**
+     * The code editor to connect to a debugger.
+     */
+    editor: CodeEditor.IEditor;
+  }
+}
 
 export const IDebugger = new Token<IDebugger>('@jupyterlab/debugger');
