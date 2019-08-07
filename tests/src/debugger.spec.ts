b2616d49ac7ff2f627190cc1a3f7f868e8705ae0
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-08-07 18:38:47 +0200

    Add simple test for Debugger

--- tests/src/debugger.spec.ts
+++ tests/src/debugger.spec.ts
@@ -1,9 +1,23 @@
-import { expect } from "chai";
+import { expect } from 'chai';
 
-describe("Debugger", () => {
-  describe("#constructor()", () => {
-    it("should create a new debugger panel", () => {
-      expect(true).to.be.true;
+import { Debugger } from '../../lib/debugger';
+
+class TestPanel extends Debugger {}
+
+describe('Debugger', () => {
+  let panel: TestPanel;
+
+  beforeEach(() => {
+    panel = new TestPanel({});
+  });
+
+  afterEach(() => {
+    panel.dispose();
+  });
+
+  describe('#constructor()', () => {
+    it('should create a new debugger panel', () => {
+      expect(panel).to.be.an.instanceOf(Debugger);
     });
   });
 });
