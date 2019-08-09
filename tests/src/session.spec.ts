6e54de6d33a943b907ae75204bb683257b16504b
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-08-09 20:46:24 +0200

    Add missing license header

--- tests/src/session.spec.ts
+++ tests/src/session.spec.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
 import { expect } from 'chai';
 
 import { ClientSession, IClientSession } from '@jupyterlab/apputils';
