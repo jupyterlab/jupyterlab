024c446a30f9328bf19e93c87bb77405f8c685ab
Vidar Tonaas Fauske <vidartf@gmail.com>
2019-08-14 10:53:43 +0100

    Fix missing remove of IStatusBar

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -344,7 +344,7 @@ export const commandEditItem: JupyterFrontEndPlugin<void> = {
 export const notebookTrustItem: JupyterFrontEndPlugin<void> = {
   id: '@jupyterlab/notebook-extension:trust-status',
   autoStart: true,
-  requires: [IStatusBar, INotebookTracker, ILabShell],
+  requires: [INotebookTracker, ILabShell],
   optional: [IStatusBar],
   activate: (
     app: JupyterFrontEnd,
