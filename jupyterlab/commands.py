ea8811b444dc191fe9666562547c0cb654b3757a
Steven Silvester <steven.silvester@ieee.org>
2019-08-16 13:57:37 -0500

    Backport PR #7013: Do not fail on deduplicate and ignore optional

--- jupyterlab/commands.py
+++ jupyterlab/commands.py
@@ -163,7 +163,7 @@ def dedupe_yarn(path, logger=None):
         pins above, for example, known-bad versions
     """
     had_dupes = ProgressProcess(
-        ['node', YARN_PATH, 'yarn-deduplicate', '-s', 'fewer', '--fail'],
+        ['node', YARN_PATH, 'yarn-deduplicate', '-s', 'fewer'],
         cwd=path, logger=logger
     ).wait() != 0
 
