7152e5a7e94e041f059136b1e2840415a294a8da
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-08-20 12:10:45 +0200

    Remove ci_after_success.sh

--- scripts/ci_after_success.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-set -ex
-set -o pipefail
-
-echo "-- will only push docs from master branch \"coverage_and_docs\" build --"
