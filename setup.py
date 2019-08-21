d440713eb4649745550a1199d7c263dddac3d022
Steven Silvester <steven.silvester@ieee.org>
2019-08-21 04:21:56 -0500

    fix package name

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ setup_args['install_requires'] = [
     'notebook>=4.3.1',
     'tornado!=6.0.0, !=6.0.1, !=6.0.2',
     'jupyterlab_server~=1.0.0',
-    'jinja>=2.10'
+    'jinja2>=2.10'
 ]
 
 setup_args['extras_require'] = {
