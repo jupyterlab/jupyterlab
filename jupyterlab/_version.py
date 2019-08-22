eeaaf8976f4d54ad03bd2c53c424a13514d78e8d
Steven Silvester <steven.silvester@ieee.org>
2019-08-22 12:36:48 -0500

    prep for rc release

--- jupyterlab/_version.py
+++ jupyterlab/_version.py
@@ -12,7 +12,7 @@ VersionInfo = namedtuple('VersionInfo', [
 ])
 
 # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
-version_info = VersionInfo(1, 1, 0, 'alpha', 2)
+version_info = VersionInfo(1, 1, 0, 'candidate', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
