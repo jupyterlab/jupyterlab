78f46b57ce9db55b70670a119a5a8683a6fd8f14
Imam <imam.k@flipkart.com>
2019-08-27 14:35:08 +0530

    Fixing lint errors

--- packages/apputils/src/clientsession.tsx
+++ packages/apputils/src/clientsession.tsx
@@ -585,7 +585,10 @@ export class ClientSession implements IClientSession {
     }
     let session = this._session;
     if (session && session.status !== 'dead') {
-      return session.changeKernel(options).catch(err => { this._handleSessionError(err); return Promise.reject(err);});
+      return session.changeKernel(options).catch(err => {
+        void this._handleSessionError(err);
+        return Promise.reject(err);
+      });
     } else {
       return this._startSession(options);
     }
