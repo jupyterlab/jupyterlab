6dde5c07796da75d7fc3df24ef92fd93af602ab6
Jason Grout <jgrout6@bloomberg.net>
2019-09-05 22:09:27 -0700

    Client session should dispose the session it owns when it is disposed.
    Fixes #7144
    
    This causes an issue with the session manager list of sessions, which should reflect which sessions are active on the server. When a session is disposed, it emits a terminated signal, which in turn causes the session model to be deleted from the session manager’s list, indicating that the session does not exist on the server when it in fact does. We have conflated deleting the js object with the server-side session terminating. The net result of this is that the session in the list of running sessions and the green session indicator on a filename both disappear until the next server session poll, at which point they are restored to accurately reflect what sessions are active on the server.
    
    To solve this issue, we do not automatically delete the session model, but instead refresh our session list from the server, to make sure our session model list accurately reflects what is on the server.

--- packages/services/src/session/manager.ts
+++ packages/services/src/session/manager.ts
@@ -335,11 +335,11 @@ export class SessionManager implements Session.IManager {
    * Handle a session terminating.
    */
   private _onTerminated(id: string): void {
-    let index = ArrayExt.findFirstIndex(this._models, value => value.id === id);
-    if (index !== -1) {
-      this._models.splice(index, 1);
-      this._runningChanged.emit(this._models.slice());
-    }
+    // A session termination emission could mean the server session is deleted,
+    // or that the session JS object is disposed and the session still exists on
+    // the server, so we refresh from the server to make sure we reflect the
+    // server state.
+    void this.refreshRunning();
   }
 
   /**
