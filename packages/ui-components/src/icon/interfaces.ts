bdb632c63fb5acb49de8b5878b273ed6138cde57
telamonian <mklein@jhu.edu>
2019-09-11 16:13:48 -0400

    added fallback flag to `icon`, moved toolbar to new icon handling
    also added some docstrings to the icon-related interfaces

--- packages/ui-components/src/icon/interfaces.ts
+++ packages/ui-components/src/icon/interfaces.ts
@@ -47,8 +47,20 @@ export namespace Icon {
    * A representation of the resources underlying an inline svg icon
    */
   export interface IModel {
+    /**
+     * The icon name. For a 'foo-bar.svg' file, the icon name is 'foo-bar'.
+     */
     name: string;
+
+    /**
+     * Manually set the className corresponding to the icon name. By default,
+     * the className is generated from the name: 'foo-bar' -> 'jp-FooBarIcon'
+     */
     className?: string;
+
+    /**
+     * A string containing the html corresponding to an SVG element
+     */
     svg: string;
   }
 
@@ -56,8 +68,32 @@ export namespace Icon {
    * The options used when creating an icon node
    */
   export interface INodeOptions extends IIconStyle {
+    /**
+     * The icon name. For a 'foo-bar.svg' file, the icon name is 'foo-bar'.
+     * For backwards compatibility, 'jp-FooBarIcon' is also a valid icon name.
+     *
+     * TODO: until Jlab 2.0
+     * If fallback is set, the name is added to the className
+     * of the resulting icon node
+     */
     name: string;
+
+    /**
+     * Extra classNames, used in addition to the typestyle className
+     */
     className?: string;
+
+    /**
+     * Icon title
+     */
     title?: string;
+
+    /**
+     * If true, if icon name resolution fails, fallback to old
+     * icon handling behavior.
+     *
+     * TODO: remove in Jlab 2.0
+     */
+    fallback?: boolean;
   }
 }
