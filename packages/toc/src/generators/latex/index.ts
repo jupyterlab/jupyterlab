cfc25cbf3f8697cb38e8c17a9a72b6e4f37bc404
kgryte <kgryte@gmail.com>
2019-10-17 14:29:56 -0700

    Refactor and clean-up

--- packages/toc/src/generators/latex/index.ts
+++ packages/toc/src/generators/latex/index.ts
@@ -39,7 +39,7 @@ const LATEX_LEVELS: { [label: string]: number } = {
  * const arr = toEntries([4,5,6]);
  * // returns [[4,0], [5,1], [6,2]]
  */
-function toEntries(arr: Array<any>): Array<any> {
+function toEntries(arr: Array<any>): Array<[any, number]> {
   for (let i = 0; i < arr.length; i++) {
     arr[i] = [arr[i], i];
   }
@@ -50,13 +50,59 @@ function toEntries(arr: Array<any>): Array<any> {
  * Returns a boolean indicating whether this ToC generator is enabled.
  *
  * @private
- * @param editor - editor
+ * @param editor - editor widget
  * @returns boolean indicating whether this ToC generator is enabled
  */
 function isEnabled(editor: IDocumentWidget<FileEditor>) {
-  // Only enable this if the editor MIME type matches one of a few LaTeX variants.
+  // Only enable this if the editor MIME type matches one of a few LaTeX variants:
   let mime = editor.content.model.mimeType;
   return mime === 'text/x-latex' || mime === 'text/x-stex';
+}
+
+/**
+ * Generates a table of contents.
+ *
+ * @private
+ * @param editor - editor widget
+ * @returns a list of headings
+ */
+function generate(editor: IDocumentWidget<FileEditor>): Array<IHeading> {
+  // Split the text into lines:
+  let lines = editor.content.model.value.text.split('\n') as Array<any>;
+
+  // Convert the list into "entries" so we can use the line number to scroll the editor upon ToC item click:
+  lines = toEntries(lines);
+
+  // Iterate over the lines to get the heading level and text for each line:
+  let headings: IHeading[] = [];
+  for (let i = 0; i < lines.length; i++) {
+    const RE = /^\s*\\(section|subsection|subsubsection){(.+)}/;
+    const match = lines[i][0].match(RE);
+    if (match) {
+      headings.push({
+        text: match[2],
+        level: LATEX_LEVELS[match[1]],
+        onClick: onClick(lines[i][1])
+      });
+    }
+  }
+  return headings;
+
+  /**
+   * Returns a "click" handler.
+   *
+   * @private
+   * @param line - line number
+   * @returns click handler
+   */
+  function onClick(line: number) {
+    return () => {
+      editor.content.editor.setCursorPosition({
+        line: line,
+        column: 0
+      });
+    };
+  }
 }
 
 /**
@@ -73,44 +119,7 @@ function createLatexGenerator(
     tracker,
     usesLatex: true,
     isEnabled: isEnabled,
-    generate: editor => {
-      // Split the text into lines, with the line number for each.
-      // We will use the line number to scroll the editor upon
-      // ToC item click.
-      const lines = toEntries(editor.content.model.value.text.split('\n'));
-
-      // Iterate over the lines to get the header level and
-      // the text for the line.
-      let headings: IHeading[] = [];
-      for (let i = 0; i < lines.length; i++) {
-        const RE = /^\s*\\(section|subsection|subsubsection){(.+)}/;
-        const match = lines[i][0].match(RE);
-        if (match) {
-          headings.push({
-            text: match[2],
-            level: LATEX_LEVELS[match[1]],
-            onClick: onClick(lines[i][1])
-          });
-        }
-      }
-      return headings;
-
-      /**
-       * Returns a "click" handler.
-       *
-       * @private
-       * @param line - line number
-       * @returns click handler
-       */
-      function onClick(line: number) {
-        return () => {
-          editor.content.editor.setCursorPosition({
-            line: line,
-            column: 0
-          });
-        };
-      }
-    }
+    generate: generate
   };
 }
 
