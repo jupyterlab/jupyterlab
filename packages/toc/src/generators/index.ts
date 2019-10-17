c2a707b50466ae32f08798cbb8e4895e43446cee
kgryte <kgryte@gmail.com>
2019-10-17 16:30:11 -0700

    Add comment

--- packages/toc/src/generators/index.ts
+++ packages/toc/src/generators/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+// Note: keep in alphabetical order...
 export * from './latex';
 export * from './markdown';
 export * from './notebook';
