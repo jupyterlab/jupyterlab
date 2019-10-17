b7c9ab0584cbba29646bbc95d44c55be7da7a9de
kgryte <kgryte@gmail.com>
2019-10-17 16:29:33 -0700

    Rename folder

--- packages/toc/src/generators/index.ts
+++ packages/toc/src/generators/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-export * from './markdowndoc';
 export * from './latex';
+export * from './markdown';
 export * from './notebook';
