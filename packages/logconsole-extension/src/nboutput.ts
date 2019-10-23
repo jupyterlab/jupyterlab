a6dfcc83f82d6f3796e367bc5c8cfa8a4040d397
Jason Grout <jgrout6@bloomberg.net>
2019-10-23 12:52:02 -0700

    Refactor notebook output logging code

--- packages/logconsole-extension/src/nboutput.ts
+++ packages/logconsole-extension/src/nboutput.ts
@@ -30,48 +30,43 @@ function activateNBOutput(
   nbtracker: INotebookTracker
 ) {
   function registerNB(nb: NotebookPanel) {
-    nb.context.session.iopubMessage.connect(
-      (_, msg: KernelMessage.IIOPubMessage) => {
+    function logOutput(
+      msg: KernelMessage.IIOPubMessage,
+      levelNormal: LogLevel,
+      levelError: LogLevel
+    ) {
+      if (
+        KernelMessage.isDisplayDataMsg(msg) ||
+        KernelMessage.isStreamMsg(msg) ||
+        KernelMessage.isErrorMsg(msg) ||
+        KernelMessage.isExecuteResultMsg(msg)
+      ) {
+        const logger = loggerRegistry.getLogger(nb.context.path);
+        logger.rendermime = nb.content.rendermime;
+        const data: nbformat.IOutput = {
+          ...msg.content,
+          output_type: msg.header.msg_type
+        };
+        let level: LogLevel = levelNormal;
         if (
-          KernelMessage.isDisplayDataMsg(msg) ||
-          KernelMessage.isStreamMsg(msg) ||
           KernelMessage.isErrorMsg(msg) ||
-          KernelMessage.isExecuteResultMsg(msg)
+          (KernelMessage.isStreamMsg(msg) && msg.content.name === 'stderr')
         ) {
-          const logger = loggerRegistry.getLogger(nb.context.path);
-          logger.rendermime = nb.content.rendermime;
-          const data: nbformat.IOutput = {
-            ...msg.content,
-            output_type: msg.header.msg_type
-          };
-          logger.log({ type: 'output', data, level: 'info' });
+          level = levelError;
         }
+        logger.log({ type: 'output', data, level });
       }
+    }
+
+    // There is overlap here since unhandled messages are also emitted in the
+    // iopubMessage signal. However, unhandled messages warrant a higher log
+    // severity, so we'll accept that they are logged twice.
+    nb.context.session.iopubMessage.connect(
+      (_, msg: KernelMessage.IIOPubMessage) => logOutput(msg, 'info', 'info')
     );
     nb.context.session.unhandledMessage.connect(
-      (_, msg: KernelMessage.IIOPubMessage) => {
-        if (
-          KernelMessage.isDisplayDataMsg(msg) ||
-          KernelMessage.isStreamMsg(msg) ||
-          KernelMessage.isErrorMsg(msg) ||
-          KernelMessage.isExecuteResultMsg(msg)
-        ) {
-          const logger = loggerRegistry.getLogger(nb.context.path);
-          logger.rendermime = nb.content.rendermime;
-          const data: nbformat.IOutput = {
-            ...msg.content,
-            output_type: msg.header.msg_type
-          };
-          let level: LogLevel = 'warning';
-          if (
-            KernelMessage.isErrorMsg(msg) ||
-            (KernelMessage.isStreamMsg(msg) && msg.content.name === 'stderr')
-          ) {
-            level = 'error';
-          }
-          logger.log({ type: 'output', data, level });
-        }
-      }
+      (_, msg: KernelMessage.IIOPubMessage) =>
+        logOutput(msg, 'warning', 'error')
     );
   }
   nbtracker.forEach(nb => registerNB(nb));
