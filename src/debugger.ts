51120669251e241d34dae16888c5ff35ee1c70a4
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-10-23 16:20:29 +0200

    Remove cyclic reference sidebar <-> model

--- src/debugger.ts
+++ src/debugger.ts
@@ -37,7 +37,7 @@ export class Debugger extends SplitPanel {
 
     this.model = new Debugger.Model(options);
 
-    this.sidebar = new DebuggerSidebar(this.model);
+    this.sidebar = new DebuggerSidebar();
     this.model.sidebar = this.sidebar;
 
     const { editorFactory } = options;
