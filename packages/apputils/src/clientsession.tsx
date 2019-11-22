483c056a19876c3b5724b82b4476c2c7ef894598
Jason Grout <jgrout6@bloomberg.net>
2019-11-22 10:24:04 -0800

    More correctly use the status in change signals in clientsession.

--- packages/apputils/src/clientsession.tsx
+++ packages/apputils/src/clientsession.tsx
@@ -765,7 +765,7 @@ export class ClientSession implements IClientSession {
     // If we have already, and now we aren't busy, dispose
     // of the busy disposable.
     if (this._setBusy) {
-      if (this.session.kernel && this.session.kernel.status === 'busy') {
+      if (status === 'busy') {
         if (!this._busyDisposable) {
           this._busyDisposable = this._setBusy();
         }
