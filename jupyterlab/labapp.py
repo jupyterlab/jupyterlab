79f5a0d5020002d52f1ccbe6f4c5aa22f30ddbc2
Eric Charles <eric@datalayer.io>
2019-12-01 05:51:11 -0600

    list the py deps

--- jupyterlab/labapp.py
+++ jupyterlab/labapp.py
@@ -12,9 +12,9 @@ import sys
 import warnings
 
 from jupyter_core.application import JupyterApp, base_aliases
-# TODO ECH Flags are failing with nbclassic, but succeed with jupyter_sever
-# from jupyter_server.serverapp import aliases, flags
-from nbclassic.notebookapp import aliases, flags
+# TODO ECH Flags are failing with nbclassic, but succeed with jupyter_server
+from jupyter_server.serverapp import aliases, flags
+# from nbclassic.notebookapp import aliases, flags
 from jupyter_server.utils import url_path_join as ujoin
 from traitlets import Bool, Instance, Unicode
 
--- setup.py
+++ setup.py
@@ -136,12 +136,13 @@ setup_args = dict(
 )
 
 
-# TODO Pin to a released jupyterlab_server once available.
+# TODO Pin to a released jupyterlab_server and jupyter_server once available.
 setup_args['install_requires'] = [
     'notebook>=4.3.1',
     'nbclassic',
     'tornado!=6.0.0, !=6.0.1, !=6.0.2',
     'jupyterlab_server @ git+https://github.com/datalayer-contrib/jupyterlab-server@jupyter_server',
+    'jupyter_server @ git+https://github.com/Zsailer/jupyter_server/@master',
     'jinja2>=2.10'
 ]
 
