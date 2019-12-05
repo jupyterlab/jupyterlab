0e435b2473bb6adea02b0c31308b88480ff6ce3b
Jason Grout <jgrout6@bloomberg.net>
2019-12-05 13:16:20 -0600

    Export kernel spec rest api

--- packages/services/src/kernelspec/index.ts
+++ packages/services/src/kernelspec/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 import * as KernelSpec from './kernelspec';
+import * as KernelSpecAPI from './restapi';
 
 export * from './manager';
-export { KernelSpec };
+export { KernelSpec, KernelSpecAPI };
