d00749a55fec1ad19950506790721aad8a191adf
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2019-12-09 22:59:41 +0000

    Statusbar with server status (#106)
    * WIP to imporve the statusbar
    
    * Add a TODO
    
    * WIP to imporve the statusbar
    
    * Add a TODO
    
    * WIP for statusbar improvements
    
    * Initial styling of the pop-up, implement collapsible list
    
    * Further pop-up styling improvements
    
    * Finish first version of the mockup implementation
    
    * Prettier css
    
    * Move the verbose message to the hover, link to docs
    
    * Prettier
    
    * Tslint

--- adapters/jupyterlab/components/statusbar.tsx
+++ adapters/jupyterlab/components/statusbar.tsx
@@ -5,6 +5,7 @@
 import React from 'react';
 
 import { VDomRenderer, VDomModel } from '@jupyterlab/apputils';
+import '../../../../style/statusbar.css';
 
 import {
   interactiveItem,
@@ -18,23 +19,187 @@ import { DefaultIconReact } from '@jupyterlab/ui-components';
 import { JupyterLabWidgetAdapter } from '../jl_adapter';
 import { VirtualDocument } from '../../../virtual/document';
 import { LSPConnection } from '../../../connection';
+import { PageConfig } from '@jupyterlab/coreutils';
+
+interface IServerStatusProps {
+  server: IServer;
+}
+
+function ServerStatus(props: IServerStatusProps) {
+  let list = props.server.spec.languages.map(language => <li>{language}</li>);
+  return (
+    <div className={'lsp-server-status'}>
+      <h5>{props.server.spec.display_name}</h5>
+      <ul>{list}</ul>
+    </div>
+  );
+}
+
+export interface IListProps {
+  /**
+   * A title to display.
+   */
+  title: string;
+  list: any[];
+  /**
+   * By default the list will be expanded; to change the initial state to collapsed, set to true.
+   */
+  startCollapsed?: boolean;
+}
+
+export interface ICollapsibleListStates {
+  isCollapsed: boolean;
+}
+
+class CollapsibleList extends React.Component<
+  IListProps,
+  ICollapsibleListStates
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = { isCollapsed: props.startCollapsed || false };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isCollapsed: !state.isCollapsed
+    }));
+  }
+
+  render() {
+    return (
+      <div
+        className={
+          'lsp-collapsible-list ' +
+          (this.state.isCollapsed ? 'lsp-collapsed' : '')
+        }
+      >
+        <h4 onClick={this.handleClick}>
+          <span className={'lsp-caret'}></span>
+          {this.props.title} ({this.props.list.length})
+        </h4>
+        <div>{this.props.list}</div>
+      </div>
+    );
+  }
+}
 
 class LSPPopup extends VDomRenderer<LSPStatus.Model> {
   constructor(model: LSPStatus.Model) {
     super();
     this.model = model;
-    // TODO: add proper, custom class?
-    this.addClass('p-Menu');
+    this.addClass('lsp-popover');
   }
   render() {
     if (!this.model) {
       return null;
     }
+    const servers_available = this.model.servers_available_not_in_use.map(
+      (session: IServer) => <ServerStatus server={session} />
+    );
+
+    let running_servers = new Array<any>();
+    for (let [
+      session,
+      documents_by_language
+    ] of this.model.documents_by_server.entries()) {
+      let documents_html = new Array<any>();
+      for (let [language, documents] of documents_by_language) {
+        // TODO user readable document ids: filename, [cell id]
+        // TODO: stop button
+        // TODO: add a config buttons next to the language header
+        let list = documents.map(document => {
+          let connection = this.model.adapter.connection_manager.connections.get(
+            document.id_path
+          );
+
+          let status = '';
+          if (connection.isInitialized) {
+            status = 'initialized';
+          } else if (connection.isConnected) {
+            status = 'connected';
+          } else {
+            status = 'not connected';
+          }
+
+          return (
+            <li>
+              {document.id_path}
+              <span className={'lsp-document-status'}>
+                {status}
+                <span
+                  className={
+                    'lsp-document-status-icon ' +
+                    (status === 'initialized'
+                      ? 'jp-FilledCircleIcon'
+                      : 'jp-CircleIcon')
+                  }
+                ></span>
+              </span>
+            </li>
+          );
+        });
+
+        documents_html.push(
+          <div className={'lsp-documents-by-language'}>
+            <h5>
+              {language}{' '}
+              <span className={'lsp-language-server-name'}>
+                ({session.spec.display_name})
+              </span>
+            </h5>
+            <ul>{list}</ul>
+          </div>
+        );
+      }
+
+      running_servers.push(<div>{documents_html}</div>);
+    }
+
+    const missing_languages = this.model.missing_languages.map(language => (
+      <div className={'lsp-missing-server'}>{language}</div>
+    ));
     return (
-      <GroupItem spacing={4} className={'p-Menu-item'}>
-        <TextItem source={this.model.lsp_servers} />
-        <TextItem source={this.model.long_message} />
-      </GroupItem>
+      <div className={'lsp-popover-content'}>
+        <div className={'lsp-servers-menu'}>
+          <h3 className={'lsp-servers-title'}>LSP servers</h3>
+          <div className={'lsp-servers-lists'}>
+            {servers_available.length ? (
+              <CollapsibleList
+                title={'Available'}
+                list={servers_available}
+                startCollapsed={true}
+              />
+            ) : (
+              ''
+            )}
+            {running_servers.length ? (
+              <CollapsibleList title={'Running'} list={running_servers} />
+            ) : (
+              ''
+            )}
+            {missing_languages.length ? (
+              <CollapsibleList title={'Missing'} list={missing_languages} />
+            ) : (
+              ''
+            )}
+          </div>
+        </div>
+        <div className={'lsp-popover-status'}>
+          Documentation:{' '}
+          <a
+            href={
+              'https://github.com/krassowski/jupyterlab-lsp/blob/master/LANGUAGESERVERS.md'
+            }
+            target={'_blank'}
+          >
+            Language Servers
+          </a>
+        </div>
+      </div>
     );
   }
 }
@@ -64,15 +229,14 @@ export class LSPStatus extends VDomRenderer<LSPStatus.Model> {
     return (
       <GroupItem
         spacing={4}
-        title={'LSP Code Intelligence'}
+        title={this.model.long_message}
         onClick={this.handleClick}
       >
-        <DefaultIconReact name={'file'} top={'2px'} kind={'statusBar'} />
-        <TextItem source={this.model.lsp_servers_truncated} />
         <DefaultIconReact
           name={this.model.status_icon}
           top={'2px'}
           kind={'statusBar'}
+          title={'LSP Code Intelligence'}
         />
         <TextItem source={this.model.short_message} />
         <TextItem source={this.model.feature_message} />
@@ -102,14 +266,23 @@ export interface IStatus {
   status: StatusCode;
 }
 
-function collect_languages(virtual_document: VirtualDocument): Set<string> {
-  let collected = new Set<string>();
-  collected.add(virtual_document.language);
+function collect_documents(
+  virtual_document: VirtualDocument
+): Set<VirtualDocument> {
+  let collected = new Set<VirtualDocument>();
+  collected.add(virtual_document);
   for (let foreign of virtual_document.foreign_documents.values()) {
-    let foreign_languages = collect_languages(foreign);
+    let foreign_languages = collect_documents(foreign);
     foreign_languages.forEach(collected.add, collected);
   }
   return collected;
+}
+
+function collect_languages(virtual_document: VirtualDocument): Set<string> {
+  let documents = collect_documents(virtual_document);
+  return new Set(
+    [...documents].map(document => document.language.toLocaleLowerCase())
+  );
 }
 
 type StatusMap = Record<StatusCode, string>;
@@ -124,41 +297,133 @@ const iconByStatus: StatusMap = {
 const shortMessageByStatus: StatusMap = {
   waiting: 'Waiting...',
   initialized: 'Fully initialized',
-  initializing: 'Fully connected & partially initialized',
+  initializing: 'Partially initialized',
   connecting: 'Connecting...'
 };
+
+// TODO ideally, this would be generated from schema
+export interface IServerSpecification {
+  display_name: string;
+  install: object;
+  urls: object;
+  languages: string[];
+}
+
+export interface IServer {
+  spec: IServerSpecification;
+  status: string;
+  handler_count: number;
+  last_server_message_at: string;
+  last_handler_message_at: string;
+}
 
 export namespace LSPStatus {
   /**
    * A VDomModel for the LSP of current file editor/notebook.
    */
   export class Model extends VDomModel {
-    get lsp_servers(): string {
-      if (!this.adapter) {
-        return '';
-      }
-      let document = this.adapter.virtual_editor.virtual_document;
-      return `Languages detected: ${[...collect_languages(document)].join(
-        ', '
-      )}`;
+    server_extension_status: any = null;
+
+    constructor() {
+      super();
+
+      // PathExt.join skips on of the slashes in https://
+      let url = PageConfig.getBaseUrl() + 'lsp';
+      fetch(url)
+        .then(response => {
+          // TODO: retry a few times
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          response
+            .json()
+            .then(data => (this.server_extension_status = data))
+            .catch(console.warn);
+        })
+        .catch(console.error);
     }
 
-    get lsp_servers_truncated(): string {
-      if (!this.adapter) {
-        return '';
-      }
-      let document = this.adapter.virtual_editor.virtual_document;
-      let foreign_languages = collect_languages(document);
-      foreign_languages.delete(this.adapter.language);
-      if (foreign_languages.size) {
-        if (foreign_languages.size < 4) {
-          return `${this.adapter.language}, ${[...foreign_languages].join(
-            ', '
-          )}`;
+    get available_servers(): Array<IServer> {
+      return this.server_extension_status.sessions;
+    }
+
+    get supported_languages(): Set<string> {
+      const languages = new Set<string>();
+      for (let server of this.available_servers) {
+        for (let language of server.spec.languages) {
+          languages.add(language.toLocaleLowerCase());
         }
-        return `${this.adapter.language} (+${foreign_languages.size} more)`;
       }
-      return this.adapter.language;
+      return languages;
+    }
+
+    private is_server_running(server: IServer): boolean {
+      for (let language of server.spec.languages) {
+        if (this.detected_languages.has(language.toLocaleLowerCase())) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    get documents_by_server(): Map<IServer, Map<string, VirtualDocument[]>> {
+      let data = new Map();
+      if (!this.adapter) {
+        return new Map();
+      }
+
+      let main_document = this.adapter.virtual_editor.virtual_document;
+      let documents = collect_documents(main_document);
+
+      for (let document of documents.values()) {
+        let language = document.language.toLocaleLowerCase();
+        let servers = this.available_servers.filter(
+          server => server.spec.languages.indexOf(language) !== -1
+        );
+        if (servers.length > 1) {
+          console.warn('More than one server per language for' + language);
+        }
+        if (servers.length === 0) {
+          continue;
+        }
+        let server = servers[0];
+
+        if (!data.has(server)) {
+          data.set(server, new Map<string, VirtualDocument>());
+        }
+
+        let documents_map = data.get(server);
+
+        if (!documents_map.has(language)) {
+          documents_map.set(language, new Array<VirtualDocument>());
+        }
+
+        let documents = documents_map.get(language);
+        documents.push(document);
+      }
+      return data;
+    }
+
+    get servers_available_not_in_use(): Array<IServer> {
+      return this.available_servers.filter(
+        server => !this.is_server_running(server)
+      );
+    }
+
+    get detected_languages(): Set<string> {
+      if (!this.adapter) {
+        return new Set<string>();
+      }
+
+      let document = this.adapter.virtual_editor.virtual_document;
+      return collect_languages(document);
+    }
+
+    get missing_languages(): Array<string> {
+      // TODO: false negative for r vs R?
+      return [...this.detected_languages].filter(
+        language => !this.supported_languages.has(language.toLocaleLowerCase())
+      );
     }
 
     get status(): IStatus {
