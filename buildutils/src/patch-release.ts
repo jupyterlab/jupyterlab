5fab3f143091ebdb197631619cdcd5182aaca029
Steven Silvester <stslve@amazon.com>
2019-12-11 12:37:40 -0600

    updates to release process

--- buildutils/src/patch-release.ts
+++ buildutils/src/patch-release.ts
@@ -8,48 +8,48 @@ import * as utils from './utils';
 
 // Specify the program signature.
 commander
-  .description('Create a patch release with optional patch JS releases')
-  .arguments('[pkgs...]')
+  .description('Create a patch release')
   .option('--force', 'Force the upgrade')
-  .action((pkgNames: Array<string>, options: any) => {
+  .action((options: any) => {
     // Make sure we can patch release.
     const pyVersion = utils.getPythonVersion();
-    if (pyVersion.includes('a') || pyVersion.includes('rc')) {
+    if (
+      pyVersion.includes('a') ||
+      pyVersion.includes('b') ||
+      pyVersion.includes('rc')
+    ) {
       throw new Error('Can only make a patch release from a final version');
     }
 
     // Run pre-bump actions.
     utils.prebump();
 
-    // Version the desired packages
-    const pkgs = pkgNames.join(',');
-    if (pkgs) {
-      let cmd = `lerna version patch -m \"New version\" --force-publish=${pkgs} --no-push`;
-      if (options.force) {
-        cmd += ' --yes';
-      }
-      let oldVersion = utils.run(
-        'git rev-parse HEAD',
-        {
-          stdio: 'pipe',
-          encoding: 'utf8'
-        },
-        true
-      );
-      utils.run(cmd);
-      let newVersion = utils.run(
-        'git rev-parse HEAD',
-        {
-          stdio: 'pipe',
-          encoding: 'utf8'
-        },
-        true
-      );
-      if (oldVersion === newVersion) {
-        console.log('aborting');
-        // lerna didn't version anything, so we assume the user aborted
-        throw new Error('Lerna aborted');
-      }
+    // Version the changed
+    let cmd = `lerna version patch -m \"New version\" --no-push`;
+    if (options.force) {
+      cmd += ' --yes';
+    }
+    let oldVersion = utils.run(
+      'git rev-parse HEAD',
+      {
+        stdio: 'pipe',
+        encoding: 'utf8'
+      },
+      true
+    );
+    utils.run(cmd);
+    let newVersion = utils.run(
+      'git rev-parse HEAD',
+      {
+        stdio: 'pipe',
+        encoding: 'utf8'
+      },
+      true
+    );
+    if (oldVersion === newVersion) {
+      console.log('aborting');
+      // lerna didn't version anything, so we assume the user aborted
+      throw new Error('Lerna aborted');
     }
 
     // Patch the python version
