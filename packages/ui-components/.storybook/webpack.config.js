57c5f16c16a9b85538cb68f200688b2c491c75c0
telamonian <mklein@jhu.edu>
2019-12-12 18:19:43 -0500

    added story for DefaultIconReact

--- packages/ui-components/.storybook/webpack.config.js
+++ packages/ui-components/.storybook/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const SRC_PATH = path.join(__dirname, '../src');
 const STORIES_PATH = path.join(__dirname, '../stories');
+const STYLE_PATH = path.resolve(__dirname, '../style');
 
 //dont need stories path if you have your stories inside your src folder
 module.exports = ({ config }) => {
@@ -16,6 +17,19 @@ module.exports = ({ config }) => {
       },
       { loader: require.resolve('react-docgen-typescript-loader') }
     ]
+  });
+
+  const fileLoaderRule = config.module.rules.find(rule =>
+    rule.test.test('.svg')
+  );
+  fileLoaderRule.exclude = STYLE_PATH;
+  config.module.rules.push({
+    test: /\.svg$/,
+    include: STYLE_PATH,
+    // issuer: { test: /\.ts$/ },
+    use: {
+      loader: 'raw-loader'
+    }
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
