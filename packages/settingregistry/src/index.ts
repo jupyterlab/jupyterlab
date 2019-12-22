1a9c264edceb851ef727c332b3fb4845ca84223b
Vidar Tonaas Fauske <vidartf@gmail.com>
2019-12-22 14:35:23 +0000

    Rename

--- packages/settingregistry/src/index.ts
+++ packages/settingregistry/src/index.ts
@@ -2,3 +2,6 @@
 | Copyright (c) Jupyter Development Team.
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
+
+export * from './settingregistry';
+export * from './tokens';
