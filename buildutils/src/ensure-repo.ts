79642b696020959a71e1b1fd91b627983da59f09
martinRenou <martin.renou@gmail.com>
2020-01-08 17:02:26 +0100

    Remove font-awesome
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- buildutils/src/ensure-repo.ts
+++ buildutils/src/ensure-repo.ts
@@ -30,7 +30,7 @@ let MISSING: Dict<string[]> = {
 
 let UNUSED: Dict<string[]> = {
   '@jupyterlab/apputils': ['@types/react'],
-  '@jupyterlab/application': ['font-awesome'],
+  '@jupyterlab/application': [],
   '@jupyterlab/apputils-extension': ['es6-promise'],
   '@jupyterlab/services': ['node-fetch', 'ws'],
   '@jupyterlab/testutils': [
