6877db56f7146ce58233111d16c01f39c888b131
telamonian <mklein@jhu.edu>
2020-01-29 07:16:37 -0500

    added first actual unittest to test-ui-components

--- tests/test-ui-components/src/labicon.spec.ts
+++ tests/test-ui-components/src/labicon.spec.ts
@@ -3,12 +3,34 @@
 
 import { expect } from 'chai';
 
-import { LabIcon } from '../../ui-components';
+import { LabIcon } from '@jupyterlab/ui-components';
 
-describe('@jupyterlab/nbformat', () => {
-  describe('validateIcons', () => {
-    it('should return true for a valid json object', () => {
-      // expect('application/json', { foo: 1 }).to.equal(true);
+import fooSvgstr from '../style/foo.svg';
+const fooIcon = new LabIcon({
+  name: 'test-ui-components:foo',
+  svgstr: fooSvgstr
+});
+
+describe('@jupyterlab/ui-components', () => {
+  describe('svg import', () => {
+    it('should hold a string with the raw contents of an svg', () => {
+      expect(fooSvgstr).to.be.string;
+      expect(
+        fooSvgstr.startsWith(`<svg width="24" height="24" viewBox="0 0 24 24">`)
+      ).to.be.true;
+    });
+  });
+
+  describe('LabIcon', () => {
+    describe('attribute .svgstr', () => {
+      it('should hold a string with the raw contents of an svg', () => {
+        expect(fooIcon.svgstr).to.be.string;
+        expect(
+          fooIcon.svgstr.startsWith(
+            `<svg width="24" height="24" viewBox="0 0 24 24">`
+          )
+        ).to.be.true;
+      });
     });
   });
 });
