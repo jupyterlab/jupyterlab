b0ae89d04eb9bddf7a71bee0137401969561e454
Jason Grout <jasongrout@users.noreply.github.com>
2020-03-05 17:37:40 -0800

    Fix typos

--- packages/ui-components/src/icon/labicon.tsx
+++ packages/ui-components/src/icon/labicon.tsx
@@ -92,7 +92,7 @@ export class LabIcon implements LabIcon.ILabIcon, VirtualElement.IRenderer {
    * @param iconClass - optional, if the icon arg is not set, the iconClass arg
    * should be a CSS class associated with an existing CSS background-image
    *
-   * @depreacted fallback - don't use, optional, a LabIcon instance that will
+   * @deprecated fallback - don't use, optional, a LabIcon instance that will
    * be used if neither icon nor iconClass are defined
    *
    * @param props - any additional args are passed though to the element method
@@ -134,7 +134,7 @@ export class LabIcon implements LabIcon.ILabIcon, VirtualElement.IRenderer {
    * @param iconClass - optional, if the icon arg is not set, the iconClass arg
    * should be a CSS class associated with an existing CSS background-image
    *
-   * @depreacted fallback - don't use, optional, a LabIcon instance that will
+   * @deprecated fallback - don't use, optional, a LabIcon instance that will
    * be used if neither icon nor iconClass are defined
    *
    * @param props - any additional args are passed though to the React component
