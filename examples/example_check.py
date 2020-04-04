cb2b9c1e4296c93b60939196768007e8e20f3b66
Steven Silvester <stslve@amazon.com>
2020-04-04 06:20:47 -0500

    Update browser check to use asyncio and suppress Stream is Closed errors
    Preserve existing interface
    
    Allow a synchronous function to be passed in
    
    Fix readme and browser_check
    
    fix up synchronous handling logic
    
    Add debug note
    
    Add explicit checks for StreamClosedError
    
    Clean up

--- examples/example_check.py
+++ examples/example_check.py
@@ -15,12 +15,11 @@ from os import path as osp
 import os
 import shutil
 import sys
-import subprocess
 
 from tornado.ioloop import IOLoop
 from traitlets import Bool, Unicode
 from jupyterlab.labapp import get_app_dir
-from jupyterlab.browser_check import run_test
+from jupyterlab.browser_check import run_test, run_async_process
 
 here = osp.abspath(osp.dirname(__file__))
 
@@ -50,16 +49,16 @@ def main():
     App.launch_instance()
 
 
-def run_browser(url):
+async def run_browser(url):
     """Run the browser test and return an exit code.
     """
     target = osp.join(get_app_dir(), 'example_test')
     if not osp.exists(osp.join(target, 'node_modules')):
         os.makedirs(target)
-        subprocess.call(["jlpm", "init", "-y"], cwd=target)
-        subprocess.call(["jlpm", "add", "puppeteer"], cwd=target)
+        await run_async_process(["jlpm", "init", "-y"], cwd=target)
+        await run_async_process(["jlpm", "add", "puppeteer"], cwd=target)
     shutil.copy(osp.join(here, 'chrome-example-test.js'), osp.join(target, 'chrome-example-test.js'))
-    return subprocess.check_call(["node", "chrome-example-test.js", url], cwd=target)
+    await run_async_process(["node", "chrome-example-test.js", url], cwd=target)
 
 
 if __name__ == '__main__':
