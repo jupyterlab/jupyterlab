991582a73be3915126f72f4d83f5552ba85ff5fd
Steven Silvester <stslve@amazon.com>
2020-04-08 06:26:41 -0500

    Run eslint/tslint on staged files and ignore removed files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,33 @@
 const escape = require('shell-quote').quote;
+const fs = require('fs');
 const isWin = process.platform === 'win32';
 
 module.exports = {
-  '**/*{.ts,.tsx,.js,.jsx,.css,.json,.md}': filenames => {
+  '**/*{.css,.json,.md}': filenames => {
     const escapedFileNames = filenames
+      .filter(filename => fs.existsSync(filename))
       .map(filename => `"${isWin ? filename : escape([filename])}"`)
       .join(' ');
     return [
       `prettier --write ${escapedFileNames}`,
+      `git add ${escapedFileNames}`
+    ];
+  },
+  '**/*{.ts,.tsx}': filenames => {
+    const escapedFileNames = filenames
+      .filter(filename => fs.existsSync(filename))
+      .map(filename => `"${isWin ? filename : escape([filename])}"`)
+      .join(' ');
+    return [`tslint --fix ${escapedFileNames}`, `git add ${escapedFileNames}`];
+  },
+  '**/*{.ts,.tsx,.js,.jsx}': filenames => {
+    const escapedFileNames = filenames
+      .filter(filename => fs.existsSync(filename))
+      .map(filename => `"${isWin ? filename : escape([filename])}"`)
+      .join(' ');
+    return [
+      `prettier --write ${escapedFileNames}`,
+      `eslint --fix ${escapedFileNames}`,
       `git add ${escapedFileNames}`
     ];
   }
