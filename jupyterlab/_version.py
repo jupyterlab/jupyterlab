45493d7d2cd4b59c436df0e32aaf7735cf9ce214
Steven Silvester <stslve@amazon.com>
2020-04-09 02:53:28 -0500

    bump version

--- jupyterlab/_version.py
+++ jupyterlab/_version.py
@@ -12,7 +12,7 @@ VersionInfo = namedtuple('VersionInfo', [
 ])
 
 # DO NOT EDIT THIS DIRECTLY!  It is managed by bumpversion
-version_info = VersionInfo(1, 2, 8, 'final', 0)
+version_info = VersionInfo(1, 2, 9, 'final', 0)
 
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
 
