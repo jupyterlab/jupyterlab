d686bf9263f76fac5e17eec1730efb914ad4cd36
telamonian <telamonian@users.noreply.github.com>
2020-04-21 03:39:20 -0400

    unified monorepo @jupyterlab docs now working
    - looks mostly fine
    - has some problems with duplicate entries in various lists
      - something to do with inclusion of multiple index.ts files in a package

--- typedoc.js
+++ typedoc.js
@@ -2,20 +2,38 @@
 
 module.exports = {
   'external-modulemap': 'packages/([^/]+)/src/.*',
-  // 'sourcefile-url-prefix': `https://github.com/sinnerschrader/feature-hub/tree/${git.short()}/packages/`,
   exclude: [
     '**/*+(__tests__|internal|lib|node_modules|demos)/**/*',
-    '.history'
+    '.history',
+    '**/.storybook',
+    '**/babel.config.js',
+    '**/*.spec.ts*',
+    '**/*.stories.ts*',
+    '**/buildutils/**',
+    '**/docs/source/**',
+    '**/examples/**',
+    '**/test/**',
+    '**/tests/**',
+    '**/testutils/**',
+
+    '**/packages/celltags/**',
+    '**/packages/logconsole/**'
+
+    // '**/packages/celltags*/**',
+    // '**/packages/logconsole*/**',
+    // '**/packages/settingregistry*/**',
   ],
   excludeExternals: false,
   excludeNotExported: true,
   excludePrivate: true,
-  // gitRevision: 'master',
-  ignoreCompilerErrors: false,
+  ignoreCompilerErrors: true,
   mode: 'modules',
   name: '@jupyterlab',
   out: 'docs/api',
   readme: 'README.md',
   theme: 'typedoc-theme',
-  tsconfig: 'tsconfigbase.json'
+  tsconfig: 'tsconfigdoc.json'
+
+  // gitRevision: 'master',
+  // 'sourcefile-url-prefix': `https://github.com/sinnerschrader/feature-hub/tree/${git.short()}/packages/`,
 };
