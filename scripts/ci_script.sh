9ac4a9a0ddfa95fa940ec3b991c4e1c3d58f0a54
Steven Silvester <stslve@amazon.com>
2020-04-23 17:47:51 -0500

    clean up docs build and break integrity build into two

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -52,10 +52,6 @@ fi
 
 
 if [[ $GROUP == docs ]]; then
-    # Build the docs
-    jlpm build:packages
-    jlpm docs
-
     # Verify tutorial docs build
     pushd docs
     pip install sphinx sphinx-copybutton sphinx_rtd_theme recommonmark jsx-lexer
@@ -65,18 +61,21 @@ if [[ $GROUP == docs ]]; then
     rm build/html/genindex.html
     rm build/html/search.html
 
-    # Changelog as a lot of links and is covered in a separate job.
+    # Changelog has a lot of links and is covered in a separate job.
     changelog=./docs/source/getting_started/changelog.rst
-    py.test --check-links -k .html --deselect=$changelog build/html || py.test --check-links -k .html --lf build/html
+    py.test --check-links -k .html --deselect $changelog build/html || py.test --check-links -k .html --deselect $changelog --lf build/html
 
     popd
 fi
 
 
-
 if [[ $GROUP == docs2 ]]; then
-    # Run the link check on raw md files - allow for a link to fail once (--lf means only run last failed)
+    # Run the link check on md files - allow for a link to fail once (--lf means only run last failed)
     py.test --check-links -k .md . || py.test --check-links -k .md --lf .
+
+    # Build the API docs
+    jlpm build:packages
+    jlpm docs
 
     # Run the link check on the changelog - allow for a link to fail once (--lf means only run last failed)
     changelog=./docs/source/getting_started/changelog.rst
@@ -93,7 +92,10 @@ if [[ $GROUP == integrity ]]; then
 
     # Lint our files.
     jlpm run lint:check || (echo 'Please run `jlpm run lint` locally and push changes' && exit 1)
+fi
 
+
+if [[ $GROUP == integrity2 ]]; then
     # Build the packages individually.
     jlpm run build:src
 
