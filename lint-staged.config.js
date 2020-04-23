01e3e2965509977b7967d3fc1cd3ef144c40bbb2
Steven Silvester <stslve@amazon.com>
2020-04-23 05:48:59 -0500

    Add new lint staged config

---
+++ lint-staged.config.js
@@ -0,0 +1,35 @@
+const escape = require('shell-quote').quote;
+const fs = require('fs');
+const isWin = process.platform === 'win32';
+
+const escapeFileNames = filenames =>
+  filenames
+    .filter(filename => fs.existsSync(filename))
+    .map(filename => `"${isWin ? filename : escape([filename])}"`)
+    .join(' ');
+
+module.exports = {
+  '**/*{.css,.json,.md}': filenames => {
+    const escapedFileNames = escapeFileNames(filenames);
+    return [
+      `prettier --write ${escapedFileNames}`,
+      `git add ${escapedFileNames}`
+    ];
+  },
+  '**/*{.ts,.tsx}': filenames => {
+    const escapedFileNames = escapeFileNames(filenames);
+    return [
+      `prettier --write ${escapedFileNames}`,
+      `tslint --fix ${escapedFileNames}`,
+      `git add ${escapedFileNames}`
+    ];
+  },
+  '**/*{.js,.jsx}': filenames => {
+    const escapedFileNames = escapeFileNames(filenames);
+    return [
+      `prettier --write ${escapedFileNames}`,
+      `eslint --fix ${escapedFileNames}`,
+      `git add ${escapedFileNames}`
+    ];
+  }
+};
