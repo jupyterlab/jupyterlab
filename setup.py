3f7b6c732809768beeb85f8bffbaeb5bf7a986b0
Steven Silvester <stslve@amazon.com>
2020-04-29 16:54:08 -0500

    specify less than 2.0 for jupyterlab_server requirement

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ setup_args = dict(
 setup_args['install_requires'] = [
     'notebook>=4.3.1',
     'tornado!=6.0.0, !=6.0.1, !=6.0.2',
-    'jupyterlab_server>=1.1.0',
+    'jupyterlab_server>=1.1.0, <2.0',
     'jinja2>=2.10'
 ]
 
