3092f412b06a4823995ad5c504a6b3be8d874ff4
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-06-03 17:15:32 +0200

    Add test for the breakpoints UI

--- test/debugger.spec.ts
+++ test/debugger.spec.ts
@@ -1,3 +1,5 @@
+import { act } from 'react-dom/test-utils';
+
 import {
   CodeMirrorEditorFactory,
   CodeMirrorMimeTypeService
@@ -9,9 +11,25 @@ import { KernelSpecManager } from '@jupyterlab/services';
 
 import { CommandRegistry } from '@lumino/commands';
 
+import { createSession } from '@jupyterlab/testutils';
+
+import { Session } from '@jupyterlab/services';
+
+import { UUID } from '@lumino/coreutils';
+
+import { MessageLoop } from '@lumino/messaging';
+
+import { Widget } from '@lumino/widgets';
+
 import { Debugger } from '../src/debugger';
 
 import { DebuggerService } from '../src/service';
+
+import { DebuggerModel } from '../src/model';
+
+import { DebugSession } from '../src/session';
+
+import { IDebugger } from '../src/tokens';
 
 /**
  * A test sidebar.
@@ -36,9 +54,22 @@ describe('Debugger', () => {
   const factoryService = new CodeMirrorEditorFactory();
   const mimeTypeService = new CodeMirrorMimeTypeService();
 
+  let model: DebuggerModel;
+  let connection: Session.ISessionConnection;
   let sidebar: TestSidebar;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    connection = await createSession({
+      name: '',
+      type: 'test',
+      path: UUID.uuid4()
+    });
+    await connection.changeKernel({ name: 'xpython' });
+
+    model = new DebuggerModel();
+    service.model = model;
+    service.session = new DebugSession({ connection });
+
     sidebar = new TestSidebar({
       service,
       callstackCommands: {
@@ -63,6 +94,51 @@ describe('Debugger', () => {
   describe('#constructor()', () => {
     it('should create a new debugger sidebar', () => {
       expect(sidebar).toBeInstanceOf(Debugger.Sidebar);
+    });
+  });
+
+  describe('Breakpoints', () => {
+    const path = 'path/to/file.py';
+
+    beforeEach(() => {
+      const bpMap = new Map<string, IDebugger.IBreakpoint[]>();
+      bpMap.set(
+        path,
+        [3, 5].map((line: number, id: number) => {
+          return {
+            id,
+            line,
+            active: true,
+            verified: true,
+            source: {
+              path
+            }
+          };
+        })
+      );
+      act(() => {
+        Widget.attach(sidebar, document.body);
+        MessageLoop.sendMessage(sidebar, Widget.Msg.UpdateRequest);
+        model.breakpoints.restoreBreakpoints(bpMap);
+      });
+    });
+
+    it('should have the jp-DebuggerBreakpoints class', () => {
+      expect(sidebar.breakpoints.hasClass('jp-DebuggerBreakpoints')).toBe(true);
+    });
+
+    it('should contain the list of breakpoints', async () => {
+      const node = sidebar.breakpoints.node;
+      const items = node.querySelectorAll('.jp-DebuggerBreakpoint');
+      expect(items).toHaveLength(2);
+    });
+
+    it('should contain the path to the breakpoints', async () => {
+      const node = sidebar.breakpoints.node;
+      const items = node.querySelectorAll('.jp-DebuggerBreakpoint-source');
+      items.forEach(item => {
+        expect(item.innerHTML).toEqual(path);
+      });
     });
   });
 });
