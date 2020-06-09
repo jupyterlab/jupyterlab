af4d0a68544067348df30756b5f2b66b81f302c3
Martha Cryan <martha.cryan@ibm.com>
2020-06-09 10:56:24 -0500

    Update configs of separate toc packages

--- packages/toc/src/index.ts
+++ packages/toc/src/index.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+import '../style/index.css';
+
 // Note: keep in alphabetical order...
 export * from './registry';
 export * from './toc';
+export * from './generators';
