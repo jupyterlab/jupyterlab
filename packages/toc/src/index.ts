ef44c11c5b558cbf36f63a074276e4e578d27a7b
Saul Shanabrook <s.shanabrook@gmail.com>
2020-06-25 17:59:33 -0400

    Revert "Merge pull request #8538 from marthacryan/mergetoc"
    94a622d3f9ac2f8bd6b3a2bfe1933f46c77ed053

--- packages/toc/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
-
-// Note: keep in alphabetical order...
-export * from './registry';
-export * from './toc';
-export * from './generators';
