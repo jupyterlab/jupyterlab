b4fcd8e61aae3d9800a69cd6d08a4210a21f5662
Saul Shanabrook <s.shanabrook@gmail.com>
2020-07-24 14:09:25 -0400

    Fix benchmarks

--- benchmarks/src/index.ts
+++ benchmarks/src/index.ts
@@ -20,7 +20,7 @@ const SWITCHES = 5;
 /**
  * Max time to stop testing if mean of previous sample was > this.
  */
-const MAX_TIME = 2 * 1000;
+const MAX_TIME = 5 * 1000;
 
 const notebookEnv = process.env.BENCHMARK_NOTEBOOKS;
 const NOTEBOOK_PACKAGES: Array<string> = notebookEnv
@@ -32,6 +32,7 @@ type OUTPUT_TYPE = {
   time: number;
   type: string;
   n: number;
+  mode: 'switch' | 'open';
 };
 
 const stream = fs.createWriteStream(DATA_PATH);
@@ -41,15 +42,21 @@ function writeLine(line: string): Promise<void> {
     stream.write(line + '\n', error => (error ? reject(error) : resolve()));
   });
 }
-function writeOutput({ browser, n, type, time }: OUTPUT_TYPE): Promise<void> {
-  return writeLine(`${browser},${n},${type},${time}`);
+function writeOutput({
+  mode,
+  browser,
+  n,
+  type,
+  time
+}: OUTPUT_TYPE): Promise<void> {
+  return writeLine(`${mode},${browser},${n},${type},${time}`);
 }
 
 (async () => {
   const notebooks: Array<NotebookType> = (
     await Promise.all(NOTEBOOK_PACKAGES.map(path => import(path)))
   ).map(pkg => pkg.default);
-  await writeLine('browser,n,type,time');
+  await writeLine('mode,browser,n,type,time');
   await fs.promises.mkdir('data', { recursive: true });
 
   for (const browserName of BROWSERS) {
@@ -71,8 +78,29 @@ function writeOutput({ browser, n, type, time }: OUTPUT_TYPE): Promise<void> {
     /**
      * Wait for a widget to be visible.
      */
-    async function waitForWidget(id: string): Promise<void> {
+    async function waitForNotebook(id: string): Promise<void> {
       await page.waitForSelector(`#${id}`, { visibility: 'visible' });
+      await page.waitForSelector(`#${id} .jp-Notebook-cell`, {
+        visibility: 'visible'
+      });
+      await page.waitForSelector(`#${id} .jp-Spinner`, {
+        visibility: 'hidden'
+      });
+    }
+
+    function startTime(): Promise<void> {
+      return page.evaluate(`{
+        performance.clearMeasures();
+        performance.mark('start');
+      }`);
+    }
+
+    async function endTime(): Promise<number> {
+      await page.evaluate("performance.measure('duration', 'start')");
+      const time: number = await page.evaluate(
+        'performance.getEntriesByName("duration")[0].duration'
+      );
+      return time;
     }
     const waitForLaunch = () =>
       page.waitForSelector('.jp-Launcher', { visibility: 'visible' });
@@ -93,10 +121,23 @@ function writeOutput({ browser, n, type, time }: OUTPUT_TYPE): Promise<void> {
       const widgets: Array<{ type: string; id: string }> = [];
 
       console.log(`  opening`);
+
+      let totalTimes = new Map<string, number>();
+      function checkTimes() {
+        for (const [type, totalTime] of totalTimes.entries()) {
+          const meanTime = totalTime / SWITCHES;
+          if (meanTime > MAX_TIME) {
+            console.log(`  stopped test type=${type}`);
+            tooLong.add(type);
+          }
+        }
+        totalTimes = new Map<string, number>();
+      }
       for (const { notebook, waitFor, label } of notebooks) {
         if (tooLong.has(label)) {
           continue;
         }
+        // Open each notebook SWITCHES times
         console.log(`   type=${label}`);
         const path = `data/${label}-${n}.ipynb`;
 
@@ -107,42 +148,63 @@ function writeOutput({ browser, n, type, time }: OUTPUT_TYPE): Promise<void> {
         );
         await util.promisify(child_process.exec)(`jupyter trust "${path}"`);
         await page.evaluate('jupyterlab.restored');
-        const id: string = await page.evaluate(`jupyterlab.commands.execute('docmanager:open', {
-          path: ${JSON.stringify(`benchmarks/${path}`)}
-        }).then(widget => widget.node.id)`);
-        console.log(`    id=${id}`);
-        await waitForWidget(id);
-        await waitFor({ widgetID: id, page });
-        widgets.push({ type: label, id });
+
+        let id: string;
+        for (let i = 0; i < SWITCHES; i++) {
+          console.log(`    i=${i}/${SWITCHES}`);
+          await page.evaluate(
+            'window.currentWidget ? window.currentWidget.dispose() : null'
+          );
+          await startTime();
+          id = await page.evaluate(`
+            jupyterlab.commands.execute('docmanager:open', {
+              path: ${JSON.stringify(`benchmarks/${path}`)}
+            }).then(widget => {
+              window.currentWidget = widget;
+              return widget.node.id;
+            })
+          `);
+          await waitForNotebook(id);
+          await waitFor({ widgetID: id, page });
+          const time = await endTime();
+          console.log(`     time=${time}`);
+          await writeOutput({
+            mode: 'open',
+            browser: browserName,
+            type: label,
+            n,
+            time
+          });
+          totalTimes.set(label, (totalTimes.get(label) || 0) + time);
+        }
+        widgets.push({ type: label, id: id! });
+        await page.evaluate('window.currentWidget = undefined');
       }
+      checkTimes();
       console.log(`  switching`);
 
-      let totalTimes = new Map<string, number>();
       // Then switch between them repeatedly
       for (let i = 0; i < SWITCHES; i++) {
         console.log(`   i=${i}/${SWITCHES}`);
         for (const { type, id } of widgets) {
           console.log(`    type=${type}`);
+          await startTime();
           // mark before we activate, then wait for the widget to display, then get duration
           await page.evaluate(`{
-            performance.clearMeasures();
-            performance.mark('start');
             jupyterlab.shell.activateById(${JSON.stringify(id)});
           }`);
-          await waitForWidget(id);
+          await waitForNotebook(id);
           await notebooks
             .find(n => n.label === type)!
             .waitFor({
               widgetID: id,
               page
             });
-          await page.evaluate("performance.measure('duration', 'start')");
-          const time: number = await page.evaluate(
-            'performance.getEntriesByName("duration")[0].duration'
-          );
+          const time = await endTime();
           console.log(`     time=${time}`);
           totalTimes.set(type, (totalTimes.get(type) || 0) + time);
           await writeOutput({
+            mode: 'switch',
             browser: browserName,
             type,
             n,
@@ -150,13 +212,7 @@ function writeOutput({ browser, n, type, time }: OUTPUT_TYPE): Promise<void> {
           });
         }
       }
-      for (const [type, totalTime] of totalTimes.entries()) {
-        const meanTime = totalTime / SWITCHES;
-        if (meanTime > MAX_TIME) {
-          console.log(`  stopped test type=${type}`);
-          tooLong.add(type);
-        }
-      }
+      checkTimes();
       console.log(`  cleaning`);
       // dipose directly instead of using `jupyterlab.shell.closeAll()` so that
       // no dialogues will pop up if notebooks are dirty
