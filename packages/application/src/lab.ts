99f1aa19436a3690608d5c5f5a54baeb89062826
Brian E. Granger <ellisonbg@gmail.com>
2020-07-25 11:15:26 -0700

    Let state layout handle L panel logic.

--- packages/application/src/lab.ts
+++ packages/application/src/lab.ts
@@ -94,11 +94,6 @@ export class JupyterLab extends JupyterFrontEnd<ILabShell> {
           this.shell.collapseRight();
           return;
         }
-        if (this.shell.mode === 'single-document') {
-          this.shell.collapseLeft();
-        } else {
-          this.shell.expandLeft();
-        }
       }, this);
       Private.setFormat(this);
     });
