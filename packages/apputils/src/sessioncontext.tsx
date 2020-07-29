bb3f5ca94738b2542c0d2e090e617d92cdf7775c
Eric Charles <eric@datalayer.io>
2020-07-29 16:15:17 +0200

    Better typedoc for pending input

--- packages/apputils/src/sessioncontext.tsx
+++ packages/apputils/src/sessioncontext.tsx
@@ -410,7 +410,7 @@ export class SessionContext implements ISessionContext {
   }
 
   /**
-   * A flag indicating if the session phas ending input, proxied from the kernel.
+   * A flag indicating if the session has ending input, proxied from the kernel.
    */
   get pendingInput(): boolean {
     return this._pendingInput;
@@ -903,10 +903,7 @@ export class SessionContext implements ISessionContext {
         this._onConnectionStatusChanged,
         this
       );
-      session.pendingInput.connect(
-        this._onPendingInput,
-        this
-      );
+      session.pendingInput.connect(this._onPendingInput, this);
       session.iopubMessage.connect(this._onIopubMessage, this);
       session.unhandledMessage.connect(this._onUnhandledMessage, this);
 
