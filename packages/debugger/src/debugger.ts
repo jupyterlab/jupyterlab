0ea5218093dd044d448155fa8adfb45fd941a682
Afshin T. Darian <git@darian.af>
2020-07-30 15:41:35 +0100

    Add debugger-extension package

--- packages/debugger/src/debugger.ts
+++ packages/debugger/src/debugger.ts
@@ -1,7 +1,18 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+import { runIcon, stopIcon } from '@jupyterlab/ui-components';
+
 import { DebuggerConfig } from './config';
+
+import {
+  closeAllIcon as closeAll,
+  stepIntoIcon as stepInto,
+  stepOverIcon as stepOver,
+  stepOutIcon as stepOut,
+  variableIcon as variable,
+  viewBreakpointIcon as viewBreakpoint
+} from './icons';
 
 import { DebuggerModel } from './model';
 
@@ -46,4 +57,18 @@ export namespace Debugger {
    * The source and editor manager for a debugger instance.
    */
   export class Sources extends DebuggerSources {}
+
+  /**
+   * The debugger user interface icons.
+   */
+  export namespace Icons {
+    export const closeAllIcon = closeAll;
+    export const continueIcon = runIcon;
+    export const stepIntoIcon = stepInto;
+    export const stepOutIcon = stepOut;
+    export const stepOverIcon = stepOver;
+    export const terminateIcon = stopIcon;
+    export const variableIcon = variable;
+    export const viewBreakpointIcon = viewBreakpoint;
+  }
 }
