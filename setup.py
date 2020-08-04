e24c8586670606712fcfce3cda7bdc44f225b37f
Steven Silvester <stslve@amazon.com>
2020-08-04 13:16:44 -0500

    bump jupyter_server

--- setup.py
+++ setup.py
@@ -152,7 +152,7 @@ setup_args['install_requires'] = [
     'ipython',
     'tornado!=6.0.0, !=6.0.1, !=6.0.2',
     'jupyterlab_server>=2.0.0a1',
-    'jupyter_server>=1.0.0rc3',
+    'jupyter_server>=1.0.0rc4',
     'nbclassic>=0.2.0rc3',
     'jinja2>=2.10'
 ]
