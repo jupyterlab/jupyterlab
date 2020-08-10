1e1e95a4b7c00baef523480b66ee099ca18f5fe0
Steven Silvester <stslve@amazon.com>
2020-08-10 14:59:09 -0500

    Publish 3.0.0a8
    SHA256 hashes:
    
    jupyterlab-3.0.0a8-py3-none-any.whl: cf067a46afa66e504b68d1033206b5dde48dcae7e811e2f2fd408fd7da7f160c
    
    jupyterlab-3.0.0a8.tar.gz: 3c0bc6ba6c67029f87c2bbff7373d1a16e694c623348a6fd1a57a9943831754d

--- jupyterlab/staging/index.js
+++ jupyterlab/staging/index.js
@@ -4,8 +4,6 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
-require('es6-promise/auto');  // polyfill Promise on IE
-
 import {
   PageConfig
 } from '@jupyterlab/coreutils';
@@ -17,15 +15,71 @@ __webpack_public_path__ = PageConfig.getOption('fullStaticUrl') + '/';
 // This cannot be extracted because the public path is dynamic.
 require('./imports.css');
 
+
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    const newScript = document.createElement('script');
+    newScript.onerror = reject;
+    newScript.onload = resolve;
+    newScript.async = true;
+    document.head.appendChild(newScript);
+    newScript.src = url;
+  });
+}
+
+async function loadComponent(url, scope, module) {
+  await loadScript(url);
+
+  // From MIT-licensed https://github.com/module-federation/module-federation-examples/blob/af043acd6be1718ee195b2511adf6011fba4233c/advanced-api/dynamic-remotes/app1/src/App.js#L6-L12
+  await __webpack_init_sharing__('default');
+  const container = window._JUPYTERLAB[scope];
+  // Initialize the container, it may provide shared modules and may need ours
+  await container.init(__webpack_share_scopes__.default);
+
+  const factory = await window._JUPYTERLAB[scope].get(module);
+  const Module = factory();
+  return Module;
+}
+
+
+
 /**
  * The main entry point for the application.
  */
-function main() {
+async function main() {
   var JupyterLab = require('@jupyterlab/application').JupyterLab;
   var disabled = [];
   var deferred = [];
   var ignorePlugins = [];
   var register = [];
+
+  // This is all the data needed to load and activate plugins. This should be
+  // gathered by the server and put onto the initial page template.
+  const extension_data = JSON.parse(
+    PageConfig.getOption('dynamic_extensions')
+  );
+  const mime_extension_data = JSON.parse(
+    PageConfig.getOption('dynamic_mime_extensions')
+  );
+
+  // Get dynamic plugins
+  const dynamicPromises = extension_data.map(data =>
+    loadComponent(
+      data.path,
+      data.name,
+      data.module
+    )
+  );
+  const dynamicPlugins = await Promise.all(dynamicPromises);
+
+  const dynamicMimePromises = mime_extension_data.map(data =>
+    loadComponent(
+      data.path,
+      data.name,
+      data.module
+    )
+  );
+  const dynamicMimePlugins = await Promise.all(dynamicMimePromises);
 
   // Handle the registered mime extensions.
   var mimeExtensions = [];
@@ -59,6 +113,9 @@ function main() {
   }
   {{/each}}
 
+  // Add the dyanmic mime extensions.
+  dynamicMimePlugins.forEach(plugin => { mimeExtensions.push(plugin); });
+
   // Handled the registered standard extensions.
   {{#each jupyterlab_extensions}}
   try {
@@ -86,6 +143,10 @@ function main() {
     console.error(e);
   }
   {{/each}}
+
+  // Add the dynamic extensions.
+  dynamicPlugins.forEach(plugin => { register.push(plugin) });
+
   var lab = new JupyterLab({
     mimeExtensions: mimeExtensions,
     disabled: {
