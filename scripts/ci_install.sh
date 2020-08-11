ef4cd88639a9c437b7728bb8d52f04932d6fb6c2
Steven Silvester <stslve@amazon.com>
2020-08-11 09:19:04 -0500

    fix os check

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 # Building should work without yarn installed globally, so uninstall the
 # global yarn installed by default.
-if [ $OSTYPE != "Linux" ]; then
+if [ $OSTYPE == "Linux" ]; then
     sudo rm -rf $(which yarn)
     ! yarn
 fi
