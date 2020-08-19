efafe2f154bf4e7bede70769073c14089e6dd16d
Jason Grout <jgrout6@bloomberg.net>
2020-08-19 15:55:37 -0700

    Lint fixes

--- examples/federated/phosphor_package/index.js
+++ examples/federated/phosphor_package/index.js
@@ -1,8 +1,8 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import {Widget} from '@phosphor/widgets';
-import {MainAreaWidget} from '@jupyterlab/apputils';
+import { Widget } from '@phosphor/widgets';
+import { MainAreaWidget } from '@jupyterlab/apputils';
 
 const plugins = [
   {
@@ -10,11 +10,13 @@ const plugins = [
     autoStart: true,
     activate: function(app) {
       const mywidget = new Widget();
-      mywidget.node.textContent = "Phosphor extension"
-      mywidget.id = "@jupyterlab/example-federated-phosphor";
-      mywidget.title.label = "Phosphor extension";
-      const appwidget = new MainAreaWidget({content: mywidget})
-      app.restored.then(() => {app.shell.add(appwidget);});
+      mywidget.node.textContent = 'Phosphor extension';
+      mywidget.id = '@jupyterlab/example-federated-phosphor';
+      mywidget.title.label = 'Phosphor extension';
+      const appwidget = new MainAreaWidget({ content: mywidget });
+      app.restored.then(() => {
+        app.shell.add(appwidget);
+      });
     }
   }
 ];
