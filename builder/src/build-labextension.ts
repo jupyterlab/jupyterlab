90d46f64bd2f8948ccd6220b38064a1a065a86e4
Jason Grout <jasongrout@users.noreply.github.com>
2020-08-27 09:20:16 -0700

    Update builder/src/build-labextension.ts

--- builder/src/build-labextension.ts
+++ builder/src/build-labextension.ts
@@ -22,7 +22,7 @@ commander
   .description('Build an extension')
   .option('--development', 'build in development mode')
   .requiredOption('--core-path <path>', 'the core package directory')
-  .option('--static-url <url>', 'static path for build assets')
+  .option('--static-url <url>', 'url for build assets, if hosted outside the built extension')
   .option('--watch')
   .action(async cmd => {
     const mode = cmd.development ? 'development' : 'production';
