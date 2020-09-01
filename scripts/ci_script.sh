32dbd81fb7c65677ebfb8ef59820fba71c2760ad
Steven Silvester <stslve@amazon.com>
2020-09-01 15:29:19 -0500

    fix check for extension listing

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -190,9 +190,11 @@ if [[ $GROUP == usage ]]; then
     jupyter labextension disable @jupyterlab/mock-extension --debug
     jupyter labextension enable @jupyterlab/mock-extension --debug 
     jupyter labextension uninstall @jupyterlab/mock-extension --debug
-    jupyter labextension list list 1>labextensions 2>&1
-    cat labextensions | grep "No dynamic extensions found"
+    jupyter labextension list 1>labextensions 2>&1
+    # bail if mock-extension was listed
+    cat labextensions | grep -q "mock-extension" && exit 1
     popd
+
     jupyter lab workspaces export > workspace.json --debug
     jupyter lab workspaces import --name newspace workspace.json --debug
     jupyter lab workspaces export newspace > newspace.json --debug
