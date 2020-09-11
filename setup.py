10bc60bff1a654edf083a4464e9cad82f36a5b89
Steven Silvester <stslve@amazon.com>
2020-09-11 17:19:22 -0500

    work around limitation in jupyter_packaging for nested files

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ data_files_spec = [
     ('share/jupyter/lab/schemas', '%s/schemas' % NAME, '**'),
     ('share/jupyter/lab/themes', '%s/themes' % NAME, '**'),
     ('etc/jupyter/jupyter_server_config.d',
-     'jupyter-config/jupyter_server_config.d', 'jupyterlab.json'),
+     'jupyter-server-config', 'jupyterlab.json'),
     ('etc/jupyter/jupyter_notebook_config.d',
-     'jupyter-config/jupyter_notebook_config.d', 'jupyterlab.json'),
+     'jupyter-notebook-config', 'jupyterlab.json'),
 ]
 
 package_data_spec = dict()
