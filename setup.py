5b0a975c070403095f0552d09a59e8ec3ecaf2bf
Jason Grout <jgrout6@bloomberg.net>
2020-09-18 13:40:15 -0700

    update nbclassic

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ setup_args['install_requires'] = [
     'tornado!=6.0.0, !=6.0.1, !=6.0.2',
     'jupyterlab_server~=2.0.0rc0',
     'jupyter_server~=1.0.1',
-    'nbclassic~=0.2.0rc7',
+    'nbclassic~=0.2.0',
     'jinja2>=2.10'
 ]
 
