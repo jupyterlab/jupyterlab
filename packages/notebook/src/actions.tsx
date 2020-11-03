d3adb04ac5a9cea7aa4809432a91adfe4392cf6b
jd41 <65656628+jd41@users.noreply.github.com>
2020-11-03 19:00:48 +0100

    Try to set scrollifneeded to fix #9255
    Try to set scrollifneeded to fix #9255

--- packages/notebook/src/actions.tsx
+++ packages/notebook/src/actions.tsx
@@ -1265,7 +1265,7 @@ export namespace NotebookActions {
         (cell as CodeCell).outputsScrolled = true;
       }
     });
-    Private.handleState(notebook, state);
+    Private.handleState(notebook, state, scrollIfNeeded = true);
   }
 
   /**
