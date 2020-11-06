ac62b77ca84a3931c0e030025e036fde37089eea
Devansh Goenka <degoenka@microsoft.com>
2020-11-06 11:40:59 +0530

    #8600: Added UTF-8 encoding parameter to create process

--- jupyterlab/commands.py
+++ jupyterlab/commands.py
@@ -87,6 +87,7 @@ class ProgressProcess(Process):
             stderr=subprocess.STDOUT,
             stdout=subprocess.PIPE,
             universal_newlines=True,
+            encoding='utf-8',
         )
         self._kill_event = kill_event or Event()
 
