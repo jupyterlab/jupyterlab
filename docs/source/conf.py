fcde9e1c0c17aae7c3f2123432193f51877e0eee
Matthias Geier <Matthias.Geier@gmail.com>
2020-12-01 18:44:04 +0100

    DOC: Make code block background less ugly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,9 +90,6 @@ language = None
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = []
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
-
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
