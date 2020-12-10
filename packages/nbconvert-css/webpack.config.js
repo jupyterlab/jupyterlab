9c7e3689561a57bb9924c2d368c67bfbf7345420
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-12-10 17:53:33 +0100

    Remove hmr from mini-css-extract-plugin

--- packages/nbconvert-css/webpack.config.js
+++ packages/nbconvert-css/webpack.config.js
@@ -18,10 +18,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development'
-            }
+            loader: MiniCssExtractPlugin.loader
           },
           'css-loader'
         ]
