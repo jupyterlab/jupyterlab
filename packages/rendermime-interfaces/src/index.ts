64fbeaad8193049915adb28bb10f1d69db465605
Jason Grout <jgrout6@bloomberg.net>
2020-12-23 02:05:41 -0800

    Add blank line after copyright

--- packages/rendermime-interfaces/src/index.ts
+++ packages/rendermime-interfaces/src/index.ts
@@ -2,6 +2,7 @@
 | Copyright (c) Jupyter Development Team.
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
+
 import { ITranslator } from '@jupyterlab/translation';
 
 import { ReadonlyPartialJSONObject } from '@lumino/coreutils';
