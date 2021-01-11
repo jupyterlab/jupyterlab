e7157229a656ca27215f971a868e9768ab7593b4
Jason Grout <jgrout6@bloomberg.net>
2021-01-11 13:15:29 -0800

    Update federated example core webpack config to be more similar to the dev_mode webpack config.

--- examples/federated/core_package/webpack.config.js
+++ examples/federated/core_package/webpack.config.js
@@ -1,31 +1,24 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
-const data = require('./package.json');
+
+const path = require('path');
+const fs = require('fs-extra');
+const Handlebars = require('handlebars');
 const Build = require('@jupyterlab/builder').Build;
 const webpack = require('webpack');
 const merge = require('webpack-merge').default;
 const baseConfig = require('@jupyterlab/builder/lib/webpack.config.base');
 const { ModuleFederationPlugin } = webpack.container;
-const fs = require('fs-extra');
-const path = require('path');
-const Handlebars = require('handlebars');
 
-// TODO: Check this webpack config against the dev_mode webpack config and make sure all relevant changes are copied back.
+const packageData = require('./package.json');
 
-const jlab = data.jupyterlab;
 
-// Ensure a clear build directory.
-const buildDir = path.resolve(__dirname, 'build');
-if (fs.existsSync(buildDir)) {
-  fs.removeSync(buildDir);
-}
-fs.ensureDirSync(buildDir);
 
-// Configuration to handle extension assets
-const extensionAssetConfig = Build.ensureAssets({
-  packageNames: jlab.extensions,
-  output: buildDir
-});
+
+// Handle the extensions.
+const jlab = packageData.jupyterlab;
+
+
 
 const singletons = {};
 
@@ -50,50 +43,171 @@ for (const key of jlab.extensions) {
   }
 }
 
-// Create the entry point file.
+const extensionPackages = jlab.extensions;
+
+// Ensure a clear build directory.
+const buildDir = path.resolve(jlab.buildDir);
+if (fs.existsSync(buildDir)) {
+  fs.removeSync(buildDir);
+}
+fs.ensureDirSync(buildDir);
+
+const outputDir = path.resolve(jlab.outputDir);
+
+// Configuration to handle extension assets
+const extensionAssetConfig = Build.ensureAssets({
+  packageNames: extensionPackages,
+  output: outputDir
+});
+
+// Create the entry point and other assets in build directory.
 const source = fs.readFileSync('index.template.js').toString();
 const template = Handlebars.compile(source);
 const extData = {
   jupyterlab_extensions: extensions,
   jupyterlab_mime_extensions: mimeExtensions
 };
-const result = template(extData);
 
-fs.writeFileSync(path.join(buildDir, 'index.js'), result);
+fs.writeFileSync(path.join(buildDir, 'index.js'), template(extData));
 
-// Make a bootstrap entrypoint
+// Create the bootstrap file that loads federated extensions and calls the
+// initialization logic in index.js
 const entryPoint = path.join(buildDir, 'bootstrap.js');
 fs.copySync('./bootstrap.js', entryPoint);
 
-if (process.env.NODE_ENV === 'production') {
-  baseConfig.mode = 'production';
+
+
+// Set up module federation sharing config
+const shared = {};
+
+// Make sure any resolutions are shared
+for (let [pkg, requiredVersion] of Object.entries(packageData.resolutions)) {
+  shared[pkg] = { requiredVersion };
 }
+
+// Add any extension packages that are not in resolutions (i.e., installed from npm)
+for (let pkg of extensionPackages) {
+  if (!shared[pkg]) {
+    shared[pkg] = {
+      requiredVersion: require(`${pkg}/package.json`).version
+    };
+  }
+}
+
+// Add dependencies and sharedPackage config from extension packages if they
+// are not already in the shared config. This means that if there is a
+// conflict, the resolutions package version is the one that is shared.
+const extraShared = [];
+for (let pkg of extensionPackages) {
+  let pkgShared = {};
+  let {
+    dependencies = {},
+    jupyterlab: { sharedPackages = {} } = {}
+  } = require(`${pkg}/package.json`);
+  for (let [dep, requiredVersion] of Object.entries(dependencies)) {
+    if (!shared[dep]) {
+      pkgShared[dep] = { requiredVersion };
+    }
+  }
+
+  // Overwrite automatic dependency sharing with custom sharing config
+  for (let [dep, config] of Object.entries(sharedPackages)) {
+    if (config === false) {
+      delete pkgShared[dep];
+    } else {
+      if ('bundled' in config) {
+        config.import = config.bundled;
+        delete config.bundled;
+      }
+      pkgShared[dep] = config;
+    }
+  }
+  extraShared.push(pkgShared);
+}
+
+// Now merge the extra shared config
+const mergedShare = {};
+for (let sharedConfig of extraShared) {
+  for (let [pkg, config] of Object.entries(sharedConfig)) {
+    // Do not override the basic share config from resolutions
+    if (shared[pkg]) {
+      continue;
+    }
+
+    // Add if we haven't seen the config before
+    if (!mergedShare[pkg]) {
+      mergedShare[pkg] = config;
+      continue;
+    }
+
+    // Choose between the existing config and this new config. We do not try
+    // to merge configs, which may yield a config no one wants
+    let oldConfig = mergedShare[pkg];
+
+    // if the old one has import: false, use the new one
+    if (oldConfig.import === false) {
+      mergedShare[pkg] = config;
+    }
+  }
+}
+
+Object.assign(shared, mergedShare);
+
+// Transform any file:// requiredVersion to the version number from the
+// imported package. This assumes (for simplicity) that the version we get
+// importing was installed from the file.
+for (let [pkg, { requiredVersion }] of Object.entries(shared)) {
+  if (requiredVersion && requiredVersion.startsWith('file:')) {
+    shared[pkg].requiredVersion = require(`${pkg}/package.json`).version;
+  }
+}
+
+// Add singleton package information
+for (let pkg of jlab.singletonPackages) {
+  shared[pkg].singleton = true;
+}
+
+
+
+const plugins = [
+  new ModuleFederationPlugin({
+    library: {
+      type: 'var',
+      name: ['_JUPYTERLAB', 'CORE_LIBRARY_FEDERATION']
+    },
+    name: 'CORE_FEDERATION',
+    shared
+  })
+];
 
 module.exports = [
   merge(baseConfig, {
     mode: 'development',
     devtool: 'source-map',
-    entry: ['./publicpath.js', './' + path.relative(__dirname, entryPoint)],
+    entry: ['./publicpath.js', entryPoint],
     output: {
-      path: buildDir,
+      path: path.resolve(buildDir),
       library: {
         type: 'var',
         name: ['_JUPYTERLAB', 'CORE_OUTPUT']
       },
       filename: 'bundle.js'
     },
-    plugins: [
-      new ModuleFederationPlugin({
-        library: {
-          type: 'var',
-          name: ['_JUPYTERLAB', 'CORE_LIBRARY_FEDERATION']
-        },
-        name: 'CORE_FEDERATION',
-        shared: {
-          ...data.resolutions,
-          ...singletons
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          jlab_core: {
+            test: /[\\/]node_modules[\\/]@(jupyterlab|lumino)[\\/]/,
+            name: 'jlab_core'
+          }
         }
-      })
-    ]
+      }
+    },
+    plugins
   })
 ].concat(extensionAssetConfig);
+
+// For debugging, write the config out
+const logPath = path.join(buildDir, 'build_log.json');
+fs.writeFileSync(logPath, JSON.stringify(module.exports, null, '  '));
