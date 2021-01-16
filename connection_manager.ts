ad89f9d9121e1afeebb8f47c0a537939a39b0900
krassowski <krassowski.michal@gmail.com>
2021-01-16 17:46:49 +0000

    Retry connection for 5.5 minutes total rather than 40 seconds

--- connection_manager.ts
+++ connection_manager.ts
@@ -278,7 +278,11 @@ export class DocumentConnectionManager {
     }
   }
 
-  async connect(options: ISocketConnectionOptions) {
+  async connect(
+    options: ISocketConnectionOptions,
+    firstTimeoutSeconds = 30,
+    secondTimeoutMinutes = 5
+  ) {
     console.log('LSP: connection requested', options);
     let connection = await this.connect_socket(options);
 
@@ -286,10 +290,31 @@ export class DocumentConnectionManager {
 
     if (!connection.isReady) {
       try {
-        await until_ready(() => connection.isReady, 200, 200);
+        // user feedback hinted that 40 seconds was too short and some users are willing to wait more;
+        // to make the best of both worlds we first check frequently (6.6 times a second) for the first
+        // 30 seconds, and show the warning early in case if something is wrong; we then continue retrying
+        // for another 5 minutes, but only once per second.
+        await until_ready(
+          () => connection.isReady,
+          Math.round((firstTimeoutSeconds * 1000) / 150),
+          150
+        );
       } catch {
-        console.warn(`LSP: Connect timed out for ${virtual_document.uri}`);
-        return;
+        console.warn(
+          `LSP: Connection to ${virtual_document.uri} timed out after ${firstTimeoutSeconds} seconds, will continue retrying for another ${secondTimeoutMinutes} minutes`
+        );
+        try {
+          await until_ready(
+            () => connection.isReady,
+            60 * secondTimeoutMinutes,
+            1000
+          );
+        } catch {
+          console.warn(
+            `LSP: Connection to ${virtual_document.uri} timed out again after ${secondTimeoutMinutes} minutes, giving up`
+          );
+          return;
+        }
       }
     }
 
