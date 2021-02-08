1976bb4bf9a009a2eac9c47742d41a0a8896f965
krassowski <krassowski.michal@gmail.com>
2021-02-08 17:49:45 +0000

    Add one more test case for the "appends at the back" variant of the bug

--- packages/documentsearch/test/documentsearchprovider.spec.ts
+++ packages/documentsearch/test/documentsearchprovider.spec.ts
@@ -27,7 +27,7 @@ describe('documentsearch/genericsearchprovider', () => {
       return match.spanElement?.closest('pre')?.innerHTML;
     }
 
-    async function queryOne(query: RegExp) {
+    async function queryOne(query: RegExp): Promise<IGenericSearchMatch> {
       let matches = (await provider.startQuery(
         query,
         widget
@@ -40,7 +40,6 @@ describe('documentsearch/genericsearchprovider', () => {
       it('should highlight text fragment nested in a node', async () => {
         widget.node.innerHTML = '<pre>xyz</pre>';
         match = await queryOne(/x/);
-        console.log(match.spanElement);
         expect(getHTMLForMatch(match)).toBe(
           `<span class="${MATCH_CLASSES}">x</span>yz`
         );
@@ -87,6 +86,22 @@ describe('documentsearch/genericsearchprovider', () => {
         match = await queryOne(/z/);
         expect(getHTMLForMatch(match)).toBe(
           `x<span>y</span><span class="${MATCH_CLASSES}">z</span>`
+        );
+
+        widget.node.innerHTML = '<pre>xy<span>z</span></pre>';
+        match = await queryOne(/x/);
+        expect(getHTMLForMatch(match)).toBe(
+          `<span class="${MATCH_CLASSES}">x</span>y<span>z</span>`
+        );
+
+        match = await queryOne(/y/);
+        expect(getHTMLForMatch(match)).toBe(
+          `x<span class="${MATCH_CLASSES}">y</span><span>z</span>`
+        );
+
+        match = await queryOne(/z/);
+        expect(getHTMLForMatch(match)).toBe(
+          `xy<span><span class="${MATCH_CLASSES}">z</span></span>`
         );
       });
 
