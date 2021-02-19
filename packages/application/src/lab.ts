91ec5a4369f093a8a1c75eeefc8fe8ebfc4b30cf
Jason Grout <jgrout6@bloomberg.net>
2021-02-19 12:40:37 -0800

    Remove the auto-switch to “mobile” mode, and behavior switches associated with mobile mode
    Fixes #9567
    
    Reverts part of #8456
    
    See https://github.com/jupyterlab/jupyterlab/issues/9567 for an extensive discussion about this. In short, the existing auto-switching behavior was disruptive to desktop users, and conflated issues of responsive layout, mobile platform, and simple interface. The plan is to take a step back and design an approach that disentangles each of these concerns better.

--- packages/application/src/lab.ts
+++ packages/application/src/lab.ts
@@ -9,8 +9,6 @@ import { IRenderMime } from '@jupyterlab/rendermime-interfaces';
 
 import { Token } from '@lumino/coreutils';
 
-import { Throttler } from '@lumino/polling';
-
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from './frontend';
 
 import { createRendermimePlugins } from './mimerenderers';
@@ -85,18 +83,6 @@ export class JupyterLab extends JupyterFrontEnd<ILabShell> {
         this.registerPlugin(plugin);
       }
     }
-
-    void this.restored.then(() => {
-      this.formatChanged.connect((_, format) => {
-        if (format === 'mobile') {
-          this.shell.mode = 'single-document';
-          this.shell.collapseLeft();
-          this.shell.collapseRight();
-          return;
-        }
-      }, this);
-      Private.setFormat(this);
-    });
   }
 
   /**
@@ -145,18 +131,6 @@ export class JupyterLab extends JupyterFrontEnd<ILabShell> {
   }
 
   /**
-   * Handle the DOM events for the application.
-   *
-   * @param event - The DOM event sent to the application.
-   */
-  handleEvent(event: Event): void {
-    super.handleEvent(event);
-    if (event.type === 'resize') {
-      void this._formatter.invoke();
-    }
-  }
-
-  /**
    * Register plugins from a plugin module.
    *
    * @param mod - The plugin module to register.
@@ -190,9 +164,6 @@ export class JupyterLab extends JupyterFrontEnd<ILabShell> {
     });
   }
 
-  private _formatter = new Throttler(() => {
-    Private.setFormat(this);
-  }, 250);
   private _info: JupyterLab.IInfo;
   private _paths: JupyterFrontEnd.IPaths;
 }
@@ -297,24 +268,5 @@ export namespace JupyterLab {
     default:
       | JupyterFrontEndPlugin<any, any, any>
       | JupyterFrontEndPlugin<any, any, any>[];
-  }
-}
-
-/**
- * A namespace for module-private functionality.
- */
-namespace Private {
-  /**
-   * Media query for mobile devices.
-   */
-  const MOBILE_QUERY = 'only screen and (max-width: 760px)';
-
-  /**
-   * Sets the `format` of a Jupyter front-end application.
-   *
-   * @param app The front-end application whose format is set.
-   */
-  export function setFormat(app: JupyterLab): void {
-    app.format = window.matchMedia(MOBILE_QUERY).matches ? 'mobile' : 'desktop';
   }
 }
