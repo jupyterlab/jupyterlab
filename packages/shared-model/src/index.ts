9fe2f37f2c6ca44ce3dd3b09ed6af76d732e96b1
Eric Charles <eric@datalayer.io>
2021-03-07 16:08:48 +0100

    initial skeleton for a shared model package

---
+++ packages/shared-model/src/index.ts
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------------
+| Copyright (c) Jupyter Development Team.
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
+/**
+ * @packageDocumentation
+ * @module shared-model
+ */
+
+export * from './api';
+export * from './local';
