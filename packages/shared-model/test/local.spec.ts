7e322e6646954dfc5354240e1d1ff0d4a621968e
Eric Charles <eric@datalayer.io>
2021-03-08 18:06:41 +0100

    more shared types

--- packages/shared-model/test/local.spec.ts
+++ packages/shared-model/test/local.spec.ts
@@ -1,13 +1,19 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { LocalCellJupyterMetadata } from './../src';
+import { LocalNotebook, LocalCellJupyterMetadata } from './../src';
 
 describe('@jupyterlab/shared-model', () => {
   describe('local', () => {
-    it('should return create a local metadata', () => {
-      const m = new LocalCellJupyterMetadata();
-      expect(m.source_hidden).toBe(false);
+    it('should create a notebook', () => {
+      const notebook = new LocalNotebook();
+      expect(notebook.cells).toBe(undefined);
+      notebook.cells = [];
+      expect(notebook.getCell(0)).toBe(undefined);
+    });
+    it('should create a jupyter metadata', () => {
+      const jupyterMetadata = new LocalCellJupyterMetadata();
+      expect(jupyterMetadata.source_hidden).toBe(false);
     });
   });
 });
