71c6c80530557ec4b5f787bf3d0ee7aae9a3bf8a
Eric Charles <eric@datalayer.io>
2021-03-08 18:06:41 +0100

    more shared types

--- packages/shared-model/src/local.ts
+++ packages/shared-model/src/local.ts
@@ -3,56 +3,158 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
-import * as nbformat from '@jupyterlab/nbformat';
+import {
+  IRawCellMetadata,
+  IAttachments,
+  MultilineString
+} from '@jupyterlab/nbformat';
 
-import { PartialJSONValue } from '@lumino/coreutils';
+import {
+  PartialJSONValue,
+  PartialJSONObject,
+  PartialJSONArray
+} from '@lumino/coreutils';
 
-import { ISharedCellJupyterMetadata, ISharedCodeCellMetadata } from './api';
+import * as shared from './api';
 
-export class LocalCellJupyterMetadata implements ISharedCellJupyterMetadata {
-  [key: string]: PartialJSONValue | undefined;
+/**
+ * Local implementation of the shared model types.
+ */
 
-  get source_hidden() {
-    return this._source_hidden;
+// Local Notebook.
+
+export class LocalNotebook implements shared.ISharedNotebook {
+  [key: string]: any;
+
+  public metadata: shared.ISharedNotebookMetadata;
+  public nbformat_minor: number;
+  public nbformat: number;
+  public cells: shared.ISharedCell[];
+
+  getCell(index: number): shared.ISharedCell {
+    return this.cells[index];
   }
-
-  private _source_hidden: boolean = false;
+  insertCell(cell: shared.ISharedCell): void {
+    throw new Error('Method not implemented.');
+  }
+  insertCells(cells: shared.ISharedCell[]): void {
+    throw new Error('Method not implemented.');
+  }
+  moveCell(fromIndex: number, toIndex: number): void {
+    throw new Error('Method not implemented.');
+  }
+  deleteCell(index: number): void {
+    throw new Error('Method not implemented.');
+  }
+  undo(): void {
+    throw new Error('Method not implemented.');
+  }
+  redo(): void {
+    throw new Error('Method not implemented.');
+  }
 }
 
-export class LocalCodeCellMetadata implements ISharedCodeCellMetadata {
-  [key: string]: PartialJSONValue | undefined;
+// Local Notebook Metadata.
 
-  get collapsed() {
-    return this._collapsed;
+export class LocalNotebookMetadata implements shared.ISharedNotebookMetadata {
+  [key: string]:
+    | string
+    | number
+    | boolean
+    | PartialJSONObject
+    | PartialJSONArray
+    | null
+    | undefined;
+
+  public kernelspec?: LocalKernelspecMetadata | undefined;
+  public language_info?: LocalLanguageInfoMetadata | undefined;
+  public orig_nbformat: number;
+}
+
+export class LocalKernelspecMetadata
+  implements shared.ISharedKernelspecMetadata {
+  [key: string]:
+    | string
+    | number
+    | boolean
+    | PartialJSONObject
+    | PartialJSONArray
+    | null
+    | undefined;
+
+  public name: string;
+  public display_name: string;
+}
+
+export class LocalLanguageInfoMetadata
+  implements shared.ISharedLanguageInfoMetadata {
+  [key: string]:
+    | string
+    | number
+    | boolean
+    | PartialJSONObject
+    | PartialJSONArray
+    | null
+    | undefined;
+
+  public name: string;
+  public codemirror_mode?: string | PartialJSONObject | undefined;
+  public file_extension?: string | undefined;
+  public mimetype?: string | undefined;
+  public pygments_lexer?: string | undefined;
+}
+
+// Local Cell.
+
+export class LocalRawCell implements shared.ISharedRawCell {
+  [key: string]:
+    | string
+    | number
+    | boolean
+    | PartialJSONObject
+    | PartialJSONArray
+    | null
+    | undefined;
+
+  public cell_type: 'raw';
+  public metadata: Partial<IRawCellMetadata>;
+  public attachments?: IAttachments | undefined;
+  public source: MultilineString;
+}
+
+// Local Cell Metadata.
+
+export class LocalCodeCellMetadata implements shared.ISharedCodeCellMetadata {
+  [key: string]: PartialJSONValue;
+
+  constructor() {
+    this._collapsed = false;
+    this._jupyter = new LocalCodeCellJupyterMetadata();
+    this._scrolled = false;
+    this._trusted = false;
+    this._name = '';
+    this._tags = [];
   }
 
-  get jupyter() {
-    return this._jupyter;
-  }
+  public collapsed: boolean;
+  public jupyter: Partial<shared.ISharedCodeCellJupyterMetadata>;
+  public scrolled: boolean | 'auto';
+  public trusted: boolean;
+  public name: string;
+  public tags: string[];
+}
 
-  get scrolled() {
-    return this._scrolled;
-  }
+export class LocalCodeCellJupyterMetadata
+  implements shared.ISharedCodeCellJupyterMetadata {
+  [key: string]: PartialJSONValue;
 
-  get trusted() {
-    return this._trusted;
-  }
+  public outputs_hidden: boolean;
+  public source_hidden: boolean;
+}
 
-  get name() {
-    return this._name;
-  }
+export class LocalCellJupyterMetadata
+  implements shared.ISharedCellJupyterMetadata {
+  [key: string]: PartialJSONValue;
 
-  get tags() {
-    return this._tags;
-  }
-
-  private _collapsed: boolean = false;
-  private _jupyter: Partial<nbformat.ICodeCellJupyterMetadata> = {
-    outputs_hidden: false,
-    source_hidden: false
-  };
-  private _scrolled: boolean | 'auto' = false;
-  private _trusted: boolean = false;
-  private _name: string = '';
-  private _tags: string[] = [];
+  public source_hidden: boolean = false;
 }
