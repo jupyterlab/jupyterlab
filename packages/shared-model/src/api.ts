a1aed279a14ca84978d305e5ee82778f17b698c2
Eric Charles <eric@datalayer.io>
2021-03-08 18:06:41 +0100

    more shared types

--- packages/shared-model/src/api.ts
+++ packages/shared-model/src/api.ts
@@ -5,24 +5,22 @@
 
 import * as nbformat from '@jupyterlab/nbformat';
 
-export interface ISharedCellJupyterMetadata
-  extends nbformat.IBaseCellJupyterMetadata {}
+/**
+ * Definition of the shared model types.
+ *
+ * - Notebook Type.
+ * - Notebook Metadata Types.
+ * - Cell Types.
+ * - Cell Metadata Types.
+ */
 
-export type ISharedCodeCellMetadata = nbformat.ICodeCellMetadata;
-
-export interface ISharedCodeCell extends nbformat.ICodeCell {
-  metadata: Partial<ISharedCodeCellMetadata>;
-}
-export interface ISharedRawCell extends nbformat.IRawCell {}
-export interface ISharedMardownCell extends nbformat.IMarkdownCell {}
-export interface ISharedUnrecognizedCell extends nbformat.IUnrecognizedCell {}
-export declare type ISharedCell =
-  | ISharedCodeCell
-  | ISharedRawCell
-  | ISharedMardownCell
-  | ISharedUnrecognizedCell;
+// Notebook Type.
 
 export type ISharedNotebook = nbformat.INotebookContent & {
+  metadata: ISharedNotebookMetadata;
+  nbformat_minor: number;
+  nbformat: number;
+  cells: ISharedCell[];
   getCell(index: number): ISharedCell;
   insertCell(cell: ISharedCell): void;
   insertCells(cells: Array<ISharedCell>): void;
@@ -31,3 +29,42 @@ export type ISharedNotebook = nbformat.INotebookContent & {
   undo(): void;
   redo(): void;
 };
+
+// Notebook Metadata Types.
+
+export type ISharedNotebookMetadata = nbformat.INotebookMetadata & {
+  kernelspec?: ISharedKernelspecMetadata;
+  language_info?: ISharedLanguageInfoMetadata;
+  orig_nbformat: number;
+};
+
+export type ISharedKernelspecMetadata = nbformat.IKernelspecMetadata;
+
+export type ISharedLanguageInfoMetadata = nbformat.ILanguageInfoMetadata;
+
+// Cell Types.
+
+export type ISharedCell =
+  | ISharedCodeCell
+  | ISharedRawCell
+  | ISharedMardownCell
+  | ISharedUnrecognizedCell;
+
+export interface ISharedCodeCell extends nbformat.ICodeCell {
+  metadata: Partial<ISharedCodeCellMetadata>;
+}
+export type ISharedMardownCell = nbformat.IMarkdownCell;
+export type ISharedRawCell = nbformat.IRawCell;
+export type ISharedUnrecognizedCell = nbformat.IUnrecognizedCell;
+
+// Cell Metadata Types.
+
+export type ISharedCodeCellMetadata = nbformat.ICodeCellMetadata & {
+  collapsed: boolean;
+  jupyter: Partial<ISharedCodeCellJupyterMetadata>;
+  scrolled: boolean | 'auto';
+};
+
+export type ISharedCodeCellJupyterMetadata = nbformat.ICodeCellJupyterMetadata;
+
+export type ISharedCellJupyterMetadata = nbformat.IBaseCellJupyterMetadata;
