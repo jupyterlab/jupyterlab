5815c6544d733da6f316a54b387875f105d3e821
Eric Charles <eric@datalayer.io>
2021-03-10 09:46:42 +0100

    add lumino signals with delta content

--- packages/nbmodel/src/api.ts
+++ packages/nbmodel/src/api.ts
@@ -5,6 +5,12 @@
 
 import * as nbformat from '@jupyterlab/nbformat';
 
+import { IDisposable } from '@lumino/disposable';
+
+import { ISignal } from '@lumino/signaling';
+
+import Delta from 'quill-delta';
+
 /**
  * Definition of the shared model types.
  *
@@ -15,8 +21,10 @@ import * as nbformat from '@jupyterlab/nbformat';
  */
 
 // Notebook Type.
-
-export type ISharedNotebook = nbformat.INotebookContent & {
+export interface ISharedNotebook
+  extends nbformat.INotebookContent,
+    IDisposable {
+  [key: string]: any;
   metadata: ISharedNotebookMetadata;
   nbformat_minor: number;
   nbformat: number;
@@ -28,43 +36,93 @@ export type ISharedNotebook = nbformat.INotebookContent & {
   deleteCell(index: number): void;
   undo(): void;
   redo(): void;
-};
+  readonly changed: ISignal<this, Delta>;
+}
 
 // Notebook Metadata Types.
-
-export type ISharedNotebookMetadata = nbformat.INotebookMetadata & {
+export interface ISharedNotebookMetadata
+  extends nbformat.INotebookMetadata,
+    IDisposable {
+  [key: string]: any;
   kernelspec?: ISharedKernelspecMetadata;
   language_info?: ISharedLanguageInfoMetadata;
   orig_nbformat: number;
-};
-
-export type ISharedKernelspecMetadata = nbformat.IKernelspecMetadata;
-
-export type ISharedLanguageInfoMetadata = nbformat.ILanguageInfoMetadata;
+  readonly changed: ISignal<this, Delta>;
+}
+export interface ISharedKernelspecMetadata
+  extends nbformat.IKernelspecMetadata,
+    IDisposable {
+  [key: string]: any;
+}
+export interface ISharedLanguageInfoMetadata
+  extends nbformat.ILanguageInfoMetadata,
+    IDisposable {
+  [key: string]: any;
+}
 
 // Cell Types.
-
 export type ISharedCell =
   | ISharedCodeCell
   | ISharedRawCell
   | ISharedMardownCell
   | ISharedUnrecognizedCell;
-
-export interface ISharedCodeCell extends nbformat.ICodeCell {
+export interface ISharedBaseCell extends nbformat.IBaseCell, IDisposable {
+  [key: string]: any;
+  readonly changed: ISignal<this, Delta>;
+}
+export interface ISharedCodeCell
+  extends ISharedBaseCell,
+    nbformat.ICodeCell,
+    IDisposable {
+  [key: string]: any;
+  cell_type: 'code';
   metadata: Partial<ISharedCodeCellMetadata>;
 }
-export type ISharedMardownCell = nbformat.IMarkdownCell;
-export type ISharedRawCell = nbformat.IRawCell;
-export type ISharedUnrecognizedCell = nbformat.IUnrecognizedCell;
+export interface ISharedMardownCell
+  extends ISharedBaseCell,
+    nbformat.IMarkdownCell,
+    IDisposable {
+  [key: string]: any;
+  cell_type: 'markdown';
+}
+export interface ISharedRawCell
+  extends ISharedBaseCell,
+    nbformat.IRawCell,
+    IDisposable {
+  [key: string]: any;
+  cell_type: 'raw';
+  metadata: Partial<ISharedRawCellMetadata>;
+}
+export interface ISharedUnrecognizedCell
+  extends ISharedBaseCell,
+    nbformat.IUnrecognizedCell,
+    IDisposable {
+  [key: string]: any;
+  readonly changed: ISignal<this, Delta>;
+}
 
 // Cell Metadata Types.
-
-export type ISharedCodeCellMetadata = nbformat.ICodeCellMetadata & {
+export interface ISharedCodeCellMetadata
+  extends nbformat.ICodeCellMetadata,
+    IDisposable {
+  [key: string]: any;
   collapsed: boolean;
   jupyter: Partial<ISharedCodeCellJupyterMetadata>;
   scrolled: boolean | 'auto';
-};
-
-export type ISharedCodeCellJupyterMetadata = nbformat.ICodeCellJupyterMetadata;
-
-export type ISharedCellJupyterMetadata = nbformat.IBaseCellJupyterMetadata;
+  readonly changed: ISignal<this, Delta>;
+}
+export interface ISharedCodeCellJupyterMetadata
+  extends nbformat.ICodeCellJupyterMetadata,
+    IDisposable {
+  [key: string]: any;
+}
+export interface ISharedCellJupyterMetadata
+  extends nbformat.IBaseCellJupyterMetadata,
+    IDisposable {
+  [key: string]: any;
+}
+export interface ISharedRawCellMetadata
+  extends nbformat.IRawCellMetadata,
+    IDisposable {
+  [key: string]: any;
+}
