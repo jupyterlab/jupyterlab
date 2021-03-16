3c0fc21a82005541e48dc69a71c8f26ca80eb7d3
RodyLipson <rlipson95@gmail.com>
2021-03-16 23:01:14 +0000

    changing ... to ellipses character

--- packages/json-extension/src/component.tsx
+++ packages/json-extension/src/component.tsx
@@ -64,7 +64,7 @@ export class Component extends React.Component<IProps, IState> {
         <InputGroup
           className="filter"
           type="text"
-          placeholder={trans.__('Filter...')}
+          placeholder={trans.__('Filter…')}
           onChange={this.handleChange}
           value={this.state.value}
           rightIcon="ui-components:search"
