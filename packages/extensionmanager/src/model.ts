52ffd9465d9ab697ef5c645329d6371fca7f7914
Jason Grout <jgrout6@bloomberg.net>
2021-03-22 05:18:05 -0700

    Delete obsolete comment.

--- packages/extensionmanager/src/model.ts
+++ packages/extensionmanager/src/model.ts
@@ -746,7 +746,6 @@ export class ListModel extends VDomModel {
    */
   protected async update(refreshInstalled = false) {
     if (ListModel.isDisclaimed()) {
-      // Start both queries before awaiting:
       const [searchMap, installedMap] = await Promise.all([this.performSearch(), this.queryInstalled(refreshInstalled)])
 
       // Map results to attributes:
