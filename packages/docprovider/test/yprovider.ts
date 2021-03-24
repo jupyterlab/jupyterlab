672983c3f8a09ba95e0ba3f0db129ab5d08f242a
Eric Charles <eric@datalayer.io>
2021-03-24 11:08:51 +0100

    docprovider package skeleton

---
+++ packages/docprovider/test/yprovider.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+describe('@jupyterlab/docprovider', () => {
+  describe('docprovider', () => {
+    it('should provide', () => {
+      console.log('test me...');
+    });
+  });
+});
