45ce1774877099b985826d4cd32773e2cbce666e
Eric Charles <eric@datalayer.io>
2021-03-24 11:32:40 +0100

    test for the docprovider skeleton

--- packages/docprovider/test/yprovider.spec.ts
+++ packages/docprovider/test/yprovider.spec.ts
@@ -1,10 +1,12 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+import { DOC_PROVIDER_TYPE } from '../src';
+
 describe('@jupyterlab/docprovider', () => {
   describe('docprovider', () => {
-    it('should provide', () => {
-      console.log('test me...');
+    it('should have a type', () => {
+      expect(DOC_PROVIDER_TYPE).toBe('ws_yjs');
     });
   });
 });
