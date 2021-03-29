a3d3d1c7b6a8f8b712939b150b2397a32c476be1
Eric Charles <eric@datalayer.io>
2021-03-29 09:11:35 +0200

    nbmodel: remove unused utils

--- packages/nbmodel/src/utils.ts
+++ packages/nbmodel/src/utils.ts
@@ -3,49 +3,9 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
-import {
-  IObservableJSON,
-  IObservableList,
-  IObservableString
-} from '@jupyterlab/observables';
-
 import * as Y from 'yjs';
 
 import * as nbmodel from './api';
-
-/**
- * Changes on Sequence-like data are expressed as Quill-inspired deltas.
- *
- * @source https://quilljs.com/docs/delta/
- */
-export type Delta<T> = Array<{ insert?: T; delete?: number; retain?: number }>;
-
-/**
- * @deprecated
- */
-export function asJsonChange(
-  cellChange: nbmodel.MapChange
-): IObservableJSON.IChangedArgs[] {
-  throw new Error('Method not implemented.');
-}
-
-/**
- * @deprecated
- */
-export function asStringChange(
-  delta: Delta<string>
-): IObservableString.IChangedArgs[] {
-  throw new Error('Method not implemented.');
-}
-
-/**
- * @deprecated
- */
-export function asListChange(
-  delta: Delta<Array<any>>
-): IObservableList.IChangedArgs<any>[] {
-  throw new Error('Method not implemented.');
-}
 
 export function convertYMapEventToMapChange(
   event: Y.YMapEvent<any>
