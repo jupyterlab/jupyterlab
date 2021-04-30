cceadd3cfa13dfbf5eb78de05e36e7573f55e6c7
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-04-30 11:41:34 +0200

    Update test-hyphens mock package to jupyter-packaging 0.9

--- jupyterlab/tests/mock_packages/test-hyphens/setup.py
+++ jupyterlab/tests/mock_packages/test-hyphens/setup.py
@@ -1,46 +1,26 @@
 from os import path
 from setuptools import setup
-from jupyter_packaging import (
-    create_cmdclass, install_npm,
-    ensure_targets, combine_commands
-)
+from jupyter_packaging import npm_builder, wrap_installers
 
-version = '3.0.2'
-name = 'test-hyphens'
-module_name = 'test-hyphens'
-lab_ext_name = 'test-hyphens'
+version = "3.0.2"
+name = "test-hyphens"
+module_name = "test-hyphens"
+lab_ext_name = "test-hyphens"
 
 HERE = path.abspath(path.dirname(__file__))
-lab_path = path.join(HERE, module_name, 'labextension')
+lab_path = path.join(HERE, module_name, "labextension")
 
 # Representative files that should exist after a successful build
-jstargets = [
-    path.join(lab_path, 'package.json'),
+ensured_targets = [
+    path.join(lab_path, "package.json"),
 ]
 
-package_data_spec = {
-    module_name: [
-        'labextension/*'
-    ]
-}
-
-data_files_spec = [
-    ("share/jupyter/labextensions/" + lab_ext_name, lab_path, "**")
-]
-
-cmdclass = create_cmdclass('js', package_data_spec=package_data_spec, data_files_spec=data_files_spec)
-cmdclass['js'] = combine_commands(
-    install_npm(
-        path=path.join(HERE),
-        npm=["jlpm"],
-        build_cmd="build:labextension"
-    ),
-    ensure_targets(jstargets),
+post_develop = npm_builder(
+    build_cmd="install:extension", source_dir="src", build_dir=lab_path, npm="jlpm"
 )
 
-setup(
-    name=name,
-    version=version,
-    packages=[module_name],
-    cmdclass=cmdclass
-)
+data_files_spec = [("share/jupyter/labextensions/" + lab_ext_name, lab_path, "**")]
+
+cmdclass = wrap_installers(post_develop=post_develop, ensured_targets=ensured_targets)
+
+setup(name=name, version=version, packages=[module_name], cmdclass=cmdclass)
