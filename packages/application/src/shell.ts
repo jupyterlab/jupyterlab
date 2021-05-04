5cd22029d1f5a724baee48ed8daa17bc7cf00cc0
0.618 <o.6180339@gmail.com>
2021-05-04 16:33:43 -0700

    fix: remove package.json and console.log

--- packages/application/src/shell.ts
+++ packages/application/src/shell.ts
@@ -354,7 +354,6 @@ export class LabShell extends Widget implements JupyterFrontEnd.IShell {
       const input = document.querySelector(
         '#filename-searcher .bp3-input'
       ) as HTMLInputElement;
-      console.log('awesome', input);
       input.focus();
     });
     skipLinkWidget.addClass('jp-skiplink');
