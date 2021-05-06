d805013d29dc136390ca8d98c385ba26c7941a62
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-05-06 15:40:50 +0200

    Update other mock packages to jupyter-packaging 0.10

--- jupyterlab/tests/mock_packages/test-hyphens/setup.py
+++ jupyterlab/tests/mock_packages/test-hyphens/setup.py
@@ -10,12 +10,16 @@ lab_ext_name = "test-hyphens"
 HERE = path.abspath(path.dirname(__file__))
 lab_path = path.join(HERE, module_name, "labextension")
 
-post_develop = npm_builder(
-    build_cmd="install:extension", source_dir="src", build_dir=lab_path, npm="jlpm"
-)
+post_develop = npm_builder(source_dir="src", build_dir=lab_path)
 
 data_files_spec = [("share/jupyter/labextensions/" + lab_ext_name, lab_path, "**")]
 
 cmdclass = wrap_installers(post_develop=post_develop)
 
-setup(name=name, version=version, packages=[module_name], cmdclass=cmdclass)
+setup(
+    name=name,
+    version=version,
+    packages=[module_name],
+    cmdclass=cmdclass,
+    data_files_spec=data_files_spec,
+)
--- jupyterlab/tests/mock_packages/test_no_hyphens/setup.py
+++ jupyterlab/tests/mock_packages/test_no_hyphens/setup.py
@@ -1,46 +1,25 @@
 from os import path
 from setuptools import setup
-from jupyter_packaging import (
-    create_cmdclass, install_npm,
-    ensure_targets, combine_commands
-)
+from jupyter_packaging import npm_builder, wrap_installers
 
-version = '3.0.2'
-name = 'test_no_hyphens'
-module_name = 'test_no_hyphens'
-lab_ext_name = 'test_no_hyphens'
+version = "3.0.2"
+name = "test_no_hyphens"
+module_name = "test_no_hyphens"
+lab_ext_name = "test_no_hyphens"
 
 HERE = path.abspath(path.dirname(__file__))
-lab_path = path.join(HERE, module_name, 'labextension')
+lab_path = path.join(HERE, module_name, "labextension")
 
-# Representative files that should exist after a successful build
-jstargets = [
-    path.join(lab_path, 'package.json'),
-]
+post_develop = npm_builder(source_dir="src", build_dir=lab_path)
 
-package_data_spec = {
-    module_name: [
-        'labextension/*'
-    ]
-}
+data_files_spec = [("share/jupyter/labextensions/" + lab_ext_name, lab_path, "**")]
 
-data_files_spec = [
-    ("share/jupyter/labextensions/" + lab_ext_name, lab_path, "**")
-]
-
-cmdclass = create_cmdclass('js', package_data_spec=package_data_spec, data_files_spec=data_files_spec)
-cmdclass['js'] = combine_commands(
-    install_npm(
-        path=path.join(HERE),
-        npm=["jlpm"],
-        build_cmd="build:labextension"
-    ),
-    ensure_targets(jstargets),
-)
+cmdclass = wrap_installers(post_develop=post_develop)
 
 setup(
     name=name,
     version=version,
     packages=[module_name],
-    cmdclass=cmdclass
+    cmdclass=cmdclass,
+    data_files_spec=data_files_spec,
 )
