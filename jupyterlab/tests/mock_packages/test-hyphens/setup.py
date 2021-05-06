55695ab8d93721dd3c53edf42a07790ff7d0f867
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-05-06 15:14:20 +0200

    Update to jupyter-packaging 0.10

--- jupyterlab/tests/mock_packages/test-hyphens/setup.py
+++ jupyterlab/tests/mock_packages/test-hyphens/setup.py
@@ -10,17 +10,12 @@ lab_ext_name = "test-hyphens"
 HERE = path.abspath(path.dirname(__file__))
 lab_path = path.join(HERE, module_name, "labextension")
 
-# Representative files that should exist after a successful build
-ensured_targets = [
-    path.join(lab_path, "package.json"),
-]
-
 post_develop = npm_builder(
     build_cmd="install:extension", source_dir="src", build_dir=lab_path, npm="jlpm"
 )
 
 data_files_spec = [("share/jupyter/labextensions/" + lab_ext_name, lab_path, "**")]
 
-cmdclass = wrap_installers(post_develop=post_develop, ensured_targets=ensured_targets)
+cmdclass = wrap_installers(post_develop=post_develop)
 
 setup(name=name, version=version, packages=[module_name], cmdclass=cmdclass)
