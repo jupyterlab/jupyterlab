19f4b770b63ea541257343ca8f5a330cf9895988
Mehmet Bektas <mbektasgh@outlook.com>
2021-05-27 16:37:42 -0700

    launch jlab in empty directory

--- ui-tests/jupyter_server_config.py
+++ ui-tests/jupyter_server_config.py
@@ -3,5 +3,6 @@ c.ServerApp.token = ""
 c.ServerApp.password = ""
 c.ServerApp.disable_check_xsrf = True
 c.ServerApp.open_browser = False
+c.ServerApp.root_dir = 'jlab_root'
 c.LabApp.open_browser = False
 c.LabApp.expose_app_in_browser = True
