477a6b4ca94f46b010b8b2a3bbc4403e1eefce8e
Mehmet Bektas <mbektasgh@outlook.com>
2021-06-02 22:06:30 -0700

    use --dev-mode, remove ipywidgets dependency

--- ui-tests/jupyter_server_config.py
+++ ui-tests/jupyter_server_config.py
@@ -4,5 +4,6 @@ c.ServerApp.password = ""
 c.ServerApp.disable_check_xsrf = True
 c.ServerApp.open_browser = False
 c.ServerApp.root_dir = 'jlab_root'
+c.LabApp.dev_mode = True
 c.LabApp.open_browser = False
 c.LabApp.expose_app_in_browser = True
