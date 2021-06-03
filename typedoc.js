4f890770571acdb45589fb08f66cf7ec1508788b
Frédéric Collonval <fcollonval@gmail.com>
2021-06-03 09:15:16 +0200

    Lint the code

--- typedoc.js
+++ typedoc.js
@@ -103,7 +103,7 @@ const entryPoints = packages
 const exclude =
   packages.flatMap(p => [`packages/${p}/test`]) +
   [
-    'packages/application-extension/src/index.tsx',
+    'packages/application-extension/src/index.tsx'
     //'packages/*/test/*.spec.ts',
   ];
 
