6e9f4382dc8dd70b97cccd49f5679ff53183d199
rely10 <rely10@bloomberg.net>
2021-06-10 17:02:33 -0700

    Hook up copied cell example to copied CompletionConnector

---
+++ examples/completer/src/index.ts
@@ -0,0 +1,169 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { PageConfig, URLExt } from '@jupyterlab/coreutils';
+(window as any).__webpack_public_path__ = URLExt.join(
+  PageConfig.getBaseUrl(),
+  'example/'
+);
+
+import '@jupyterlab/application/style/index.css';
+import '@jupyterlab/cells/style/index.css';
+import '@jupyterlab/theme-light-extension/style/theme.css';
+import '@jupyterlab/completer/style/index.css';
+import '../index.css';
+
+import { SessionContext, Toolbar } from '@jupyterlab/apputils';
+
+import { CodeCell, CodeCellModel } from '@jupyterlab/cells';
+
+import { CodeMirrorMimeTypeService } from '@jupyterlab/codemirror';
+
+import {
+  Completer,
+  CompleterModel,
+  CompletionHandler
+} from '@jupyterlab/completer';
+
+import { CompletionConnector } from './connector';
+
+import {
+  standardRendererFactories as initialFactories,
+  RenderMimeRegistry
+} from '@jupyterlab/rendermime';
+
+import {
+  KernelManager,
+  KernelSpecManager,
+  SessionManager
+} from '@jupyterlab/services';
+
+import { CommandRegistry } from '@lumino/commands';
+
+import { BoxPanel, Widget } from '@lumino/widgets';
+
+function main(): void {
+  const kernelManager = new KernelManager();
+  const specsManager = new KernelSpecManager();
+  const sessionManager = new SessionManager({ kernelManager });
+  const sessionContext = new SessionContext({
+    sessionManager,
+    specsManager,
+    name: 'Example'
+  });
+  const mimeService = new CodeMirrorMimeTypeService();
+
+  // Initialize the command registry with the bindings.
+  const commands = new CommandRegistry();
+  const useCapture = true;
+
+  // Setup the keydown listener for the document.
+  document.addEventListener(
+    'keydown',
+    event => {
+      commands.processKeydownEvent(event);
+    },
+    useCapture
+  );
+
+  // Create the cell widget with a default rendermime instance.
+  const rendermime = new RenderMimeRegistry({ initialFactories });
+
+  const cellWidget = new CodeCell({
+    rendermime,
+    model: new CodeCellModel({})
+  }).initializeState();
+
+  // Handle the mimeType for the current kernel asynchronously.
+  sessionContext.kernelChanged.connect(() => {
+    void sessionContext.session?.kernel?.info.then(info => {
+      const lang = info.language_info;
+      const mimeType = mimeService.getMimeTypeByLanguage(lang);
+      cellWidget.model.mimeType = mimeType;
+    });
+  });
+
+  // Use the default kernel.
+  sessionContext.kernelPreference = { autoStartDefault: true };
+
+  // Set up a completer.
+  const editor = cellWidget.editor;
+  const model = new CompleterModel();
+  const completer = new Completer({ editor, model });
+  const connector = new CompletionConnector({
+    session: sessionContext.session,
+    editor
+  });
+  const handler = new CompletionHandler({ completer, connector });
+
+  //sessionContext.session?.kernel.
+  void sessionContext.ready.then(() => {
+    handler.connector = new CompletionConnector({
+      session: sessionContext.session,
+      editor
+    });
+  });
+
+  // Set the handler's editor.
+  handler.editor = editor;
+
+  // Hide the widget when it first loads.
+  completer.hide();
+  completer.addClass('jp-Completer-Cell');
+
+  // Create a toolbar for the cell.
+  const toolbar = new Toolbar();
+  toolbar.addItem('spacer', Toolbar.createSpacerItem());
+  toolbar.addItem('interrupt', Toolbar.createInterruptButton(sessionContext));
+  toolbar.addItem('restart', Toolbar.createRestartButton(sessionContext));
+  toolbar.addItem('name', Toolbar.createKernelNameItem(sessionContext));
+  toolbar.addItem('status', Toolbar.createKernelStatusItem(sessionContext));
+
+  // Lay out the widgets.
+  const panel = new BoxPanel();
+  panel.id = 'main';
+  panel.direction = 'top-to-bottom';
+  panel.spacing = 0;
+  panel.addWidget(toolbar);
+  panel.addWidget(cellWidget);
+  BoxPanel.setStretch(toolbar, 0);
+  BoxPanel.setStretch(cellWidget, 1);
+
+  // Attach the panel to the DOM.
+  Widget.attach(panel, document.body);
+  Widget.attach(completer, document.body);
+
+  // Handle widget state.
+  window.addEventListener('resize', () => {
+    panel.update();
+  });
+  cellWidget.activate();
+
+  // Add the commands.
+  commands.addCommand('invoke:completer', {
+    execute: () => {
+      handler.invoke();
+    }
+  });
+  commands.addCommand('run:cell', {
+    execute: () => CodeCell.execute(cellWidget, sessionContext)
+  });
+
+  commands.addKeyBinding({
+    selector: '.jp-InputArea-editor.jp-mod-completer-enabled',
+    keys: ['Tab'],
+    command: 'invoke:completer'
+  });
+  commands.addKeyBinding({
+    selector: '.jp-InputArea-editor',
+    keys: ['Shift Enter'],
+    command: 'run:cell'
+  });
+
+  // Start up the kernel.
+  void sessionContext.initialize().then(() => {
+    console.debug('Example started!');
+  });
+}
+
+window.addEventListener('load', main);
---
+++ examples/completer/webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['whatwg-fetch', './build/index.js'],
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  bail: true,
+  devtool: 'cheap-source-map',
+  mode: 'production',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.html$/, use: 'file-loader' },
+      { test: /\.md$/, use: 'raw-loader' },
+      { test: /\.js.map$/, use: 'file-loader' },
+      {
+        // In .css files, svg is loaded as a data URI.
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: /\.css$/,
+        use: {
+          loader: 'svg-url-loader',
+          options: { encoding: 'none', limit: 10000 }
+        }
+      },
+      {
+        // In .ts and .tsx files (both of which compile to .js), svg files
+        // must be loaded as a raw string instead of data URIs.
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        issuer: /\.js$/,
+        use: {
+          loader: 'raw-loader'
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|ttf|woff|woff2|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [{ loader: 'url-loader', options: { limit: 10000 } }]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // Needed for Blueprint. See https://github.com/palantir/blueprint/issues/4393
+      'process.env': '{}',
+      // Needed for various packages using cwd(), like the path polyfill
+      process: { cwd: () => '/' }
+    })
+  ]
+};
