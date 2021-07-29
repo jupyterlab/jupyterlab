357afe8b777071a4e2db5044bde108e441dfb3c8
Jessica Xu <jessicaxujiayi@gmail.com>
2021-07-29 02:42:48 -0700

    RTC: persist rtc user color & name in state db (#10490)
    * persist rtc user color & name in state db
    
    lint changes
    
    make statedb optional in docprovider
    
    give env param precedence over statedb
    
    fix color string format
    
    fix check
    
    change 'required' to 'optional'
    
    Add '| null' to state in activate function
    
    store USER as JSONObject
    
    * persist rtc user color & name in state db
    
    lint changes
    
    make statedb optional in docprovider
    
    give env param precedence over statedb
    
    fix color string format
    
    fix check
    
    change 'required' to 'optional'
    
    Add '| null' to state in activate function
    
    store USER as JSONObject
    
    Fix bug in currState
    
    clean up
    
    * Package integrity updates

--- packages/docprovider-extension/src/index.ts
+++ packages/docprovider-extension/src/index.ts
@@ -17,14 +17,19 @@ import {
   WebSocketProviderWithLocks
 } from '@jupyterlab/docprovider';
 import { ServerConnection } from '@jupyterlab/services';
+import { IStateDB } from '@jupyterlab/statedb';
 
 /**
  * The default document provider plugin
  */
 const docProviderPlugin: JupyterFrontEndPlugin<IDocumentProviderFactory> = {
   id: '@jupyterlab/docprovider-extension:plugin',
+  optional: [IStateDB],
   provides: IDocumentProviderFactory,
-  activate: (app: JupyterFrontEnd): IDocumentProviderFactory => {
+  activate: (
+    app: JupyterFrontEnd,
+    state: IStateDB | null
+  ): IDocumentProviderFactory => {
     const server = ServerConnection.makeSettings();
     const url = URLExt.join(server.wsUrl, 'api/yjs');
     const collaborative =
@@ -35,7 +40,8 @@ const docProviderPlugin: JupyterFrontEndPlugin<IDocumentProviderFactory> = {
       return collaborative
         ? new WebSocketProviderWithLocks({
             ...options,
-            url
+            url,
+            state
           })
         : new ProviderMock();
     };
