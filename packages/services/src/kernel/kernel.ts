372e1eff77b3f73f8ca597521380d9e47cbbe0df
Eric Charles <eric@datalayer.io>
2021-08-10 11:33:48 +0200

    Add guard to avoid kernel deadlock on multiple inputs

--- packages/services/src/kernel/kernel.ts
+++ packages/services/src/kernel/kernel.ts
@@ -111,6 +111,15 @@ export interface IKernelConnection extends IObservableDisposable {
   handleComms: boolean;
 
   /**
+   * Whether the kernel connection has pending input.
+   *
+   * #### Notes
+   * This is a guard to avoid deadlock is the user asks input
+   * as second time before submitting his first input
+   */
+  hasPendingInput: boolean;
+
+  /**
    * Send a shell message to the kernel.
    *
    * @param msg - The fully-formed shell message to send.
@@ -485,6 +494,11 @@ export interface IKernelConnection extends IObservableDisposable {
    * message should be treated as read-only.
    */
   anyMessage: ISignal<this, IAnyMessageArgs>;
+
+  /**
+   * A signal emitted when a kernel has pending inputs from the user.
+   */
+  pendingInput: ISignal<this, boolean>;
 
   /**
    * The server settings for the kernel.
