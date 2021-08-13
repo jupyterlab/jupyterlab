77075df385a6e08b8211e12851cfc149b072aaa3
krassowski <krassowski.michal@gmail.com>
2021-08-13 09:41:46 +0100

    Correct expected prompt, deal with non-matching prompt
    and add other suggestions from offline chat

--- buildutils/src/local-repository.ts
+++ buildutils/src/local-repository.ts
@@ -134,13 +134,13 @@ packages:
   console.log('Logging in');
   const loginPs = child_process.spawn(
     'npm',
-    `login -e ${email} -r ${local_registry}`.split(' ')
+    `login -r ${local_registry}`.split(' ')
   );
 
   const loggedIn = new Promise<void>((accept, reject) => {
     loginPs.stdout.on('data', (chunk: string) => {
       const data = Buffer.from(chunk, 'utf-8').toString().trim();
-      console.log('debug:', data);
+      console.log('stdout:', data);
       switch (data) {
         case 'Username:':
           console.log('Passing username...');
@@ -150,16 +150,21 @@ packages:
           console.log('Passing password...');
           loginPs.stdin.write(pass + '\n');
           break;
-        case 'Email':
+        case 'Email: (this IS public)':
           console.log('Passing email...');
           loginPs.stdin.write(email + '\n');
           break;
+        default:
+          reject(`Unexpected prompt: "${data}"`);
       }
       if (data.indexOf('Logged in as') !== -1) {
-        console.log('debug: matched');
         loginPs.stdin.end();
-        accept();
+        // do not accept here yet, the token may not have been written
       }
+      loginPs.stderr.on('data', (chunk: string) => {
+        const data = Buffer.from(chunk, 'utf-8').toString().trim();
+        console.log('stderr:', data);
+      });
     });
     loginPs.on('error', error => reject(error));
     loginPs.on('close', () => accept());
