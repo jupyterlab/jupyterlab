2e8b75d74c39e6ecff2040a56012391810d1c358
Trung Le <leductrungxf@gmail.com>
2021-08-23 09:06:12 +0200

    Fix  `translateKernelStatuses`

--- packages/apputils/src/kernelstatuses.ts
+++ packages/apputils/src/kernelstatuses.ts
@@ -1,22 +1,6 @@
 import { ITranslator, nullTranslator } from '@jupyterlab/translation';
 import { ISessionContext } from './sessioncontext';
 
-const KERNEL_STATUSES: Record<ISessionContext.KernelDisplayStatus, string> = {
-  unknown: 'Unknown',
-  starting: 'Starting',
-  idle: 'Idle',
-  busy: 'Busy',
-  terminating: 'Terminating',
-  restarting: 'Restarting',
-  autorestarting: 'Autorestarting',
-  dead: 'Dead',
-  connected: 'Connected',
-  connecting: 'Connecting',
-  disconnected: 'Disconnected',
-  initializing: 'Initializing',
-  '': ''
-};
-
 /**
  * Helper function to translate kernel statuses mapping by using
  * input translator.
@@ -29,13 +13,20 @@ export function translateKernelStatuses(
 ): Record<ISessionContext.KernelDisplayStatus, string> {
   translator = translator || nullTranslator;
   const trans = translator.load('jupyterlab');
-  const translated: Record<
-    ISessionContext.KernelDisplayStatus,
-    string
-  > = KERNEL_STATUSES;
-  for (const key in KERNEL_STATUSES) {
-    const recordKey = key as ISessionContext.KernelDisplayStatus;
-    translated[recordKey] = trans.__(KERNEL_STATUSES[recordKey]);
-  }
+  const translated: Record<ISessionContext.KernelDisplayStatus, string> = {
+    unknown: trans.__('Unknown'),
+    starting: trans.__('Starting'),
+    idle: trans.__('Idle'),
+    busy: trans.__('Busy'),
+    terminating: trans.__('Terminating'),
+    restarting: trans.__('Restarting'),
+    autorestarting: trans.__('Autorestarting'),
+    dead: trans.__('Dead'),
+    connected: trans.__('Connected'),
+    connecting: trans.__('Connecting'),
+    disconnected: trans.__('Disconnected'),
+    initializing: trans.__('Initializing'),
+    '': ''
+  };
   return translated;
 }
