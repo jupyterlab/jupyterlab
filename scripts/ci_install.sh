e24dce1e3eb1d186c68bebee65eaecb623404c82
telamonian <telamonian@users.noreply.github.com>
2021-08-28 08:51:50 -0400

    fix outdated jlpm cli calls in `ci_install.sh`

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -24,8 +24,9 @@ pip install -q --upgrade pip --user
 pip --version
 # Show a verbose install if the install fails, for debugging
 pip install -e ".[dev,test]" || pip install -v -e ".[dev,test]"
-jlpm versions
-jlpm config current
+yarn --version
+node -p process.versions
+jlpm config
 
 if [[ $GROUP == nonode ]]; then
     # Build the wheel
