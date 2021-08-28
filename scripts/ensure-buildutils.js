04cfb3e39c5607542b60656be631db3c68964b7a
telamonian <telamonian@users.noreply.github.com>
2021-08-28 18:14:06 -0400

    refactor `npm run build` -> `jlpm run build` in `ensure-buildutils.js`

--- scripts/ensure-buildutils.js
+++ scripts/ensure-buildutils.js
@@ -52,13 +52,11 @@ function ensurePackage(p) {
   }
 
   if (!current) {
-    // This must be "npm" because it is run during `pip install -e .` before
-    // jlpm is installed.
     // childProcess.execSync('npm run clean', {
     //   stdio: [0, 1, 2],
     //   cwd: path.resolve('./' + p)
     // });
-    childProcess.execSync('npm run build', {
+    childProcess.execSync('jlpm run build', {
       stdio: [0, 1, 2],
       cwd: path.resolve('./' + p)
     });
