618a89547e293cf761c39151800dc284c5bf1c7a
telamonian <telamonian@users.noreply.github.com>
2021-08-28 21:58:18 -0400

    apply yarn cli fixes from `ci_install.sh` to `ci_install.ps1`

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -20,8 +20,12 @@ pip install -q --upgrade pip --user
 pip --version
 # Show a verbose install if the install fails, for debugging
 pip install -e ".[test]" || pip install -v -e ".[test]"
-yarn --version
-node -p process.versions
+
+# next two lines equivalent to deprecated `yarn versions` cmd from yarn@1.x
+jlpm --version
+jlpm node -p process.versions
+
+# print current yarn config info
 jlpm config
 
 # TODO: remove when we no longer support classic notebook
