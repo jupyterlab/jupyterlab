e722670add62cb97bba92c5f7e05edbc6fd44373
Frédéric Collonval <fcollonval@gmail.com>
2021-09-03 12:01:39 +0200

    Clean up handling of npm dist tag
    ## References
    More progress toward https://github.com/jupyterlab/jupyterlab/issues/10340
    
    ## Code changes
    Improve handling of `npm` `dist-tag` in `buildutils` and when using Jupyter Releaser.
    
    ## User-facing changes
    None
    
    ## Backwards-incompatible changes
    None

--- buildutils/src/ensure-repo.ts
+++ buildutils/src/ensure-repo.ts
@@ -736,6 +736,27 @@ export async function ensureIntegrity(): Promise<boolean> {
   // Handle buildutils
   ensureBuildUtils();
 
+  // Handle the pyproject.toml package
+  const pyprojectPath = path.resolve('.', 'pyproject.toml');
+  const curr = utils.getPythonVersion();
+  let tag = 'latest';
+  if (!/\d+\.\d+\.\d+$/.test(curr)) {
+    tag = 'next';
+  }
+  const publishCommand = `npm publish --tag ${tag}`;
+  let pyprojectText = fs.readFileSync(pyprojectPath, { encoding: 'utf8' });
+  if (pyprojectText.indexOf(publishCommand) === -1) {
+    pyprojectText = pyprojectText.replace(
+      /npm publish --tag [a-z]+/,
+      publishCommand
+    );
+    fs.writeFileSync(pyprojectPath, pyprojectText, { encoding: 'utf8' });
+    if (!messages['top']) {
+      messages['top'] = [];
+    }
+    messages['top'].push('Update npm publish command in pyproject.toml');
+  }
+
   // Handle the JupyterLab application top package.
   pkgMessages = ensureJupyterlab();
   if (pkgMessages.length > 0) {
