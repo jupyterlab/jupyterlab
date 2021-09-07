172f1cfff7fbb2e017b09711620b787dbd5f2d3f
telamonian <telamonian@users.noreply.github.com>
2021-09-07 08:17:32 -0400

    fix promise handling bug in `local-repository.ts`

--- buildutils/src/local-repository.ts
+++ buildutils/src/local-repository.ts
@@ -185,7 +185,12 @@ packages:
             loginPs.stdin.write(email + '\n');
             break;
           default:
+            if (data.indexOf('Logged in as') !== -1) {
+            loginPs.stdin.end();
+            // do not accept here yet, the token may not have been written
+          } else {
             reject(`Unexpected prompt: "${data}"`);
+          }
         }
       }
       loginPs.stderr.on('data', (chunk: string) => {
