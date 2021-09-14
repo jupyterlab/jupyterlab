3d9c749edc6354b32b881f867d4281fdba587136
Martha Cryan <martha.cryan@ibm.com>
2021-09-14 14:11:46 -0500

    Package integrity updates

--- packages/settingeditor/style/index.js
+++ packages/settingeditor/style/index.js
@@ -8,9 +8,8 @@ import '@lumino/widgets/style/index.js';
 import '@jupyterlab/ui-components/style/index.js';
 import '@jupyterlab/apputils/style/index.js';
 import '@jupyterlab/codeeditor/style/index.js';
-import '@jupyterlab/rendermime/style/index.js';
-import '@jupyterlab/application/style/index.js';
 import '@jupyterlab/formeditor/style/index.js';
+import '@jupyterlab/rendermime/style/index.js';
 import '@jupyterlab/inspector/style/index.js';
 
 import './base.css';
