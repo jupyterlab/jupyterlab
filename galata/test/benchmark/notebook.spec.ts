b75cec444f9980c7ec67ea9d7f7e19ac5234f5af
Frédéric Collonval <fcollonval@gmail.com>
2021-09-27 10:20:53 +0200

    Improve `galata.newPage`

--- galata/test/benchmark/notebook.spec.ts
+++ galata/test/benchmark/notebook.spec.ts
@@ -42,70 +42,98 @@ test.describe('Benchmark', () => {
   let page: IJupyterLabPageFixture;
 
   // Generate the files for the benchmark
-  test.beforeAll(async ({ baseURL, browser, tmpPath, waitForApplication }) => {
-    const contents = galata.newContentsHelper(baseURL);
+  test.beforeAll(
+    async ({
+      appPath,
+      autoGoto,
+      baseURL,
+      browser,
+      mockSettings,
+      mockState,
+      sessions,
+      terminals,
+      tmpPath,
+      waitForApplication
+    }) => {
+      const contents = galata.newContentsHelper(baseURL);
 
-    const codeContent = galata.Notebook.generateNotebook(300, 'code', [
-      'for x in range(OUTPUT_LENGTH):\n',
-      '    print(f"{PREFIX} {x}")'
-    ]);
+      const codeContent = galata.Notebook.generateNotebook(300, 'code', [
+        'for x in range(OUTPUT_LENGTH):\n',
+        '    print(f"{PREFIX} {x}")'
+      ]);
 
-    await contents.uploadContent(
-      JSON.stringify(codeContent),
-      'text',
-      `${tmpPath}/${codeNotebook}`
-    );
+      await contents.uploadContent(
+        JSON.stringify(codeContent),
+        'text',
+        `${tmpPath}/${codeNotebook}`
+      );
 
-    const mdContent = galata.Notebook.generateNotebook(300, 'markdown', [
-      '# Demonstration of proper behaviour with non-LaTeX uses of `$`\n',
-      '\n',
-      '## This should be highlighted as a heading\n',
-      '\n',
-      'Sample code:\n',
-      '\n',
-      '    ```\n',
-      '    echo $HOME\n',
-      '    ```\n',
-      '\n',
-      '```shell\n',
-      'echo $HOME\n',
-      '```\n',
-      '\n',
-      'The code block below should be properly highlighted:\n',
-      '\n',
-      '```bash\n',
-      'echo $HOME\n',
-      '```\n',
-      '\n',
-      '\n',
-      '### Heading\n',
-      '\n',
-      '`$test`\n',
-      '\n',
-      '### This heading should be highlighted too'
-    ]);
+      const mdContent = galata.Notebook.generateNotebook(300, 'markdown', [
+        '# Demonstration of proper behaviour with non-LaTeX uses of `$`\n',
+        '\n',
+        '## This should be highlighted as a heading\n',
+        '\n',
+        'Sample code:\n',
+        '\n',
+        '    ```\n',
+        '    echo $HOME\n',
+        '    ```\n',
+        '\n',
+        '```shell\n',
+        'echo $HOME\n',
+        '```\n',
+        '\n',
+        'The code block below should be properly highlighted:\n',
+        '\n',
+        '```bash\n',
+        'echo $HOME\n',
+        '```\n',
+        '\n',
+        '\n',
+        '### Heading\n',
+        '\n',
+        '`$test`\n',
+        '\n',
+        '### This heading should be highlighted too'
+      ]);
 
-    await contents.uploadContent(
-      JSON.stringify(mdContent),
-      'text',
-      `${tmpPath}/${mdNotebook}`
-    );
+      await contents.uploadContent(
+        JSON.stringify(mdContent),
+        'text',
+        `${tmpPath}/${mdNotebook}`
+      );
 
-    const loremIpsum =
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie suscipit libero non volutpat. Suspendisse et tincidunt metus. Proin laoreet magna rutrum egestas tristique. Proin vel neque sit amet lectus egestas pellentesque nec quis nisl. Quisque faucibus condimentum leo, quis euismod eros ultrices in. Vivamus maximus malesuada tempor. Aliquam maximus maximus elit, ac imperdiet tellus posuere nec. Sed at rutrum velit. Etiam et lectus convallis, sagittis nibh sit amet, gravida turpis. Nulla nec velit id est tristique iaculis.\n\nDonec vel finibus mauris, eu tristique justo. Pellentesque turpis lorem, lobortis eu tincidunt non, cursus sit amet ex. Vivamus eget ligula a leo vulputate egestas a eu felis. Donec sollicitudin maximus neque quis condimentum. Cras vestibulum nulla libero, sed semper velit faucibus ac. Phasellus et consequat risus. Sed suscipit ligula est. Etiam ultricies ac lacus sit amet cursus. Nam non leo vehicula, iaculis eros eu, consequat sapien. Ut quis odio quis augue pharetra porttitor sit amet eget nisl. Vestibulum magna eros, rutrum ac nisi non, lobortis varius ipsum. Proin luctus euismod arcu eget sollicitudin. Praesent nec erat gravida, tincidunt diam eget, tempor tortor.';
-    await contents.uploadContent(loremIpsum, 'text', `${tmpPath}/${textFile}`);
+      const loremIpsum =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie suscipit libero non volutpat. Suspendisse et tincidunt metus. Proin laoreet magna rutrum egestas tristique. Proin vel neque sit amet lectus egestas pellentesque nec quis nisl. Quisque faucibus condimentum leo, quis euismod eros ultrices in. Vivamus maximus malesuada tempor. Aliquam maximus maximus elit, ac imperdiet tellus posuere nec. Sed at rutrum velit. Etiam et lectus convallis, sagittis nibh sit amet, gravida turpis. Nulla nec velit id est tristique iaculis.\n\nDonec vel finibus mauris, eu tristique justo. Pellentesque turpis lorem, lobortis eu tincidunt non, cursus sit amet ex. Vivamus eget ligula a leo vulputate egestas a eu felis. Donec sollicitudin maximus neque quis condimentum. Cras vestibulum nulla libero, sed semper velit faucibus ac. Phasellus et consequat risus. Sed suscipit ligula est. Etiam ultricies ac lacus sit amet cursus. Nam non leo vehicula, iaculis eros eu, consequat sapien. Ut quis odio quis augue pharetra porttitor sit amet eget nisl. Vestibulum magna eros, rutrum ac nisi non, lobortis varius ipsum. Proin luctus euismod arcu eget sollicitudin. Praesent nec erat gravida, tincidunt diam eget, tempor tortor.';
+      await contents.uploadContent(
+        loremIpsum,
+        'text',
+        `${tmpPath}/${textFile}`
+      );
 
-    page = await galata.newPage(browser, baseURL, waitForApplication);
-    // If some unsaved documents are opened, a browser dialog will pop-up
-    // requesting confirmation.
-    page.on('dialog', async dialog => {
-      await dialog.accept();
-    });
-  });
+      page = await galata.newPage(
+        appPath,
+        autoGoto,
+        baseURL!,
+        browser,
+        mockSettings,
+        mockState,
+        sessions,
+        terminals,
+        tmpPath,
+        waitForApplication
+      );
+      // If some unsaved documents are opened, a browser dialog will pop-up
+      // requesting confirmation.
+      page.on('dialog', async dialog => {
+        await dialog.accept();
+      });
+    }
+  );
 
   test.beforeEach(async () => {
     // Force workspace reset
-    page.goto('?reset');
+    await page.goto('?reset');
   });
 
   // Remove benchmark files
