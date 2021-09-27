071487e61fc73d7596b63665ab4a522bb0b40e15
Frédéric Collonval <fcollonval@gmail.com>
2021-09-27 08:31:05 +0200

    [skip ci] Run all tests in the same page

--- galata/test/benchmark/notebook.spec.ts
+++ galata/test/benchmark/notebook.spec.ts
@@ -2,7 +2,12 @@
 // Distributed under the terms of the Modified BSD License.
 
 import { expect } from '@playwright/test';
-import { benchmark, galata, test } from '@jupyterlab/galata';
+import {
+  benchmark,
+  galata,
+  IJupyterLabPageFixture,
+  test
+} from '@jupyterlab/galata';
 import path from 'path';
 
 test.use({
@@ -34,8 +39,10 @@ const parameters = [].concat(
 );
 
 test.describe('Benchmark', () => {
+  let page: IJupyterLabPageFixture;
+
   // Generate the files for the benchmark
-  test.beforeAll(async ({ baseURL, tmpPath }) => {
+  test.beforeAll(async ({ baseURL, browser, tmpPath, waitForApplication }) => {
     const contents = galata.newContentsHelper(baseURL);
 
     const codeContent = galata.Notebook.generateNotebook(300, 'code', [
@@ -87,10 +94,24 @@ test.describe('Benchmark', () => {
     const loremIpsum =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie suscipit libero non volutpat. Suspendisse et tincidunt metus. Proin laoreet magna rutrum egestas tristique. Proin vel neque sit amet lectus egestas pellentesque nec quis nisl. Quisque faucibus condimentum leo, quis euismod eros ultrices in. Vivamus maximus malesuada tempor. Aliquam maximus maximus elit, ac imperdiet tellus posuere nec. Sed at rutrum velit. Etiam et lectus convallis, sagittis nibh sit amet, gravida turpis. Nulla nec velit id est tristique iaculis.\n\nDonec vel finibus mauris, eu tristique justo. Pellentesque turpis lorem, lobortis eu tincidunt non, cursus sit amet ex. Vivamus eget ligula a leo vulputate egestas a eu felis. Donec sollicitudin maximus neque quis condimentum. Cras vestibulum nulla libero, sed semper velit faucibus ac. Phasellus et consequat risus. Sed suscipit ligula est. Etiam ultricies ac lacus sit amet cursus. Nam non leo vehicula, iaculis eros eu, consequat sapien. Ut quis odio quis augue pharetra porttitor sit amet eget nisl. Vestibulum magna eros, rutrum ac nisi non, lobortis varius ipsum. Proin luctus euismod arcu eget sollicitudin. Praesent nec erat gravida, tincidunt diam eget, tempor tortor.';
     await contents.uploadContent(loremIpsum, 'text', `${tmpPath}/${textFile}`);
+
+    page = await galata.newPage(browser, baseURL, waitForApplication);
+    // If some unsaved documents are opened, a browser dialog will pop-up
+    // requesting confirmation.
+    page.on('dialog', async dialog => {
+      await dialog.accept();
+    });
+  });
+
+  test.beforeEach(async () => {
+    // Force workspace reset
+    page.goto('?reset');
   });
 
   // Remove benchmark files
   test.afterAll(async ({ baseURL, tmpPath }) => {
+    await page.close();
+
     const contents = galata.newContentsHelper(baseURL);
     await contents.deleteDirectory(tmpPath);
   });
@@ -104,7 +125,6 @@ test.describe('Benchmark', () => {
   for (const [file, sample] of parameters) {
     test(`measure ${file} - ${sample + 1}`, async ({
       browserName,
-      page,
       tmpPath
     }, testInfo) => {
       const attachmentCommon = {
