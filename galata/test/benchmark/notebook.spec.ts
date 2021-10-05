d24aa2d0ba7cfaf1aaaf3b4b0f8cabec9d3f7197
Frédéric Collonval <fcollonval@gmail.com>
2021-10-05 09:33:41 +0200

    [skip ci] delay 500ms

--- galata/test/benchmark/notebook.spec.ts
+++ galata/test/benchmark/notebook.spec.ts
@@ -126,7 +126,7 @@ test.describe('Benchmark', () => {
           state: 'hidden'
         });
 
-        await page.waitForTimeout(1000);
+        await page.waitForTimeout(500);
 
         // if (file === mdNotebook) {
         //   // Wait for Latex rendering => consider as acceptable to require additional time
