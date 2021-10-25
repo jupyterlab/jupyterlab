8eda3f5aca8db84f0fe5ebc94de41c77f290e379
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-10-25 16:36:25 +0200

    Fix config

--- galata/playwright.config.js
+++ galata/playwright.config.js
@@ -18,6 +18,5 @@ module.exports = {
   // Switch to 'always' to keep raw assets for all tests
   preserveOutput: 'failures-only', // Breaks HTML report if use.video == 'on'
   // Try one retry as some tests are flaky
-  retries: 1,
-  reported: 'html'
+  retries: 1
 };
