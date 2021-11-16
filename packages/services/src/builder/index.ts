0db38ae5917592730ad5082281916e3ee8d28235
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2021-11-16 19:30:06 +0000

    Apply suggestion: website -> repository
    Co-authored-by: Jeremy Tuloup <jeremy.tuloup@gmail.com>

--- packages/services/src/builder/index.ts
+++ packages/services/src/builder/index.ts
@@ -84,7 +84,7 @@ export class BuildManager {
       if (response.status !== 200) {
         const message = `Build failed with ${response.status}.
 
-        If you are experiencing the build failure after installing an extension (or trying to include previously installed extension after updating JupyterLab) please check the extension website for new installation instructions as many extensions migrated to the prebuilt extensions system which no longer requires rebuilding JupyterLab (but uses a different installation procedure, typically involving a package manager such as 'pip' or 'conda').
+        If you are experiencing the build failure after installing an extension (or trying to include previously installed extension after updating JupyterLab) please check the extension repository for new installation instructions as many extensions migrated to the prebuilt extensions system which no longer requires rebuilding JupyterLab (but uses a different installation procedure, typically involving a package manager such as 'pip' or 'conda').
 
         If you specifically intended to install a source extension, please run 'jupyter lab build' on the server for full output.`;
         throw new ServerConnection.ResponseError(response, message);
