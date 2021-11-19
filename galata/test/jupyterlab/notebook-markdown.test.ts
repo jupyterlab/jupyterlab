ee1436736daf6f856d99ab125cc2b83592f6d196
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2021-11-19 07:42:09 +0000

    Fix Tex highlights affecting Markdown with standalone `$` (#11488)

--- galata/test/jupyterlab/notebook-markdown.test.ts
+++ galata/test/jupyterlab/notebook-markdown.test.ts
@@ -1,11 +1,35 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { test } from '@jupyterlab/galata';
+import { IJupyterLabPageFixture, test } from '@jupyterlab/galata';
 import { expect } from '@playwright/test';
 import * as path from 'path';
 
 const fileName = 'markdown_notebook.ipynb';
+
+async function enterEditingModeForScreenshot(
+  page: IJupyterLabPageFixture,
+  cellIndex: number
+) {
+  await page.notebook.enterCellEditingMode(cellIndex);
+  const cell = await page.notebook.getCell(cellIndex);
+  // Make sure cursor is consistently in the same position to avoid screenshot flake
+  await page.keyboard.press('Home');
+  await page.keyboard.press('PageUp');
+  // Add some timeout to stabilize codemirror bounding box
+  const cellBox = await cell.boundingBox();
+  const cellNew = await page.notebook.getCell(cellIndex);
+  const cellNewBox = await cellNew.boundingBox();
+  if (
+    cellBox.x != cellNewBox.x ||
+    cellBox.y != cellNewBox.y ||
+    cellBox.width != cellNewBox.width ||
+    cellBox.height != cellNewBox.height
+  ) {
+    // Wait a bit if the bounding box have changed
+    await page.waitForTimeout(100);
+  }
+}
 
 test.describe('Notebook Markdown', () => {
   test.beforeEach(async ({ page, tmpPath }) => {
@@ -24,29 +48,25 @@ test.describe('Notebook Markdown', () => {
     expect(await cell.screenshot()).toMatchSnapshot(imageName);
   });
 
-  test('Do not highlight non-LaTeX syntax', async ({ page, tmpPath }) => {
+  test('Do not highlight TeX in code blocks', async ({ page, tmpPath }) => {
     await page.notebook.openByPath(`${tmpPath}/${fileName}`);
 
     const imageName = 'do-not-highlight-not-latex.png';
-    await page.notebook.enterCellEditingMode(1);
-    const cell = await page.notebook.getCell(1);
-    // Make sure cursor is consistently in the same position to avoid screenshot flake
-    await page.keyboard.press('Home');
-    await page.keyboard.press('PageUp');
-    // Add some timeout to stabilize codemirror bounding box
-    const cellBox = await cell.boundingBox();
-    const cellNew = await page.notebook.getCell(1);
-    const cellNewBox = await cellNew.boundingBox();
-    if (
-      cellBox.x != cellNewBox.x ||
-      cellBox.y != cellNewBox.y ||
-      cellBox.width != cellNewBox.width ||
-      cellBox.height != cellNewBox.height
-    ) {
-      // Wait a bit if the bounding box have changed
-      await page.waitForTimeout(100);
-    }
+    await enterEditingModeForScreenshot(page, 1);
     expect(await (await page.notebook.getCell(1)).screenshot()).toMatchSnapshot(
+      imageName
+    );
+  });
+
+  test('Do not enter math mode for standalone dollar', async ({
+    page,
+    tmpPath
+  }) => {
+    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+
+    const imageName = 'do-not-highlight-standalone-dollar.png';
+    await enterEditingModeForScreenshot(page, 2);
+    expect(await (await page.notebook.getCell(2)).screenshot()).toMatchSnapshot(
       imageName
     );
   });
