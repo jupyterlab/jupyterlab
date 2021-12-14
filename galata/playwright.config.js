61a00800f51e3aa117f0b2da79f9497a4c1b534e
Martha Cryan <martha.cryan@ibm.com>
2021-12-14 16:04:40 -0600

    Update snapshot for settings menu

--- galata/playwright.config.js
+++ galata/playwright.config.js
@@ -15,6 +15,7 @@ module.exports = {
       testMatch: 'test/jupyterlab/**'
     }
   ],
+  workers: 1,
   // Switch to 'always' to keep raw assets for all tests
   preserveOutput: 'failures-only', // Breaks HTML report if use.video == 'on'
   // Try one retry as some tests are flaky
