2eca43baf1a817224e0e358b76590eac322b7f08
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-12-30 18:35:54 +0100

    Revert "Update OSTYPE check"
    This reverts commit 04b031dd91e3800b6e6342949cd52a3bf08f82b1.

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -7,7 +7,8 @@ set -o pipefail
 
 # Building should work without yarn installed globally, so uninstall the
 # global yarn installed by default.
-if [ $OSTYPE == "linux-gnu" ]; then
+echo $OSTYPE
+if [ $OSTYPE == "Linux" ]; then
     sudo rm -rf $(which yarn)
     ! yarn
 fi
