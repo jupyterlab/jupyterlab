cd914fd1bd8cec12c39e0d5faa700566569541ae
Frédéric Collonval <fcollonval@gmail.com>
2022-01-05 11:21:32 +0100

    Add replace ui tests

---
+++ galata/test/jupyterlab/notebook-replace.test.ts
@@ -0,0 +1,106 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { galata, test } from '@jupyterlab/galata';
+import { expect } from '@playwright/test';
+import * as path from 'path';
+
+const fileName = 'search.ipynb';
+
+test.describe('Notebook Search and Replace', () => {
+  test.beforeEach(async ({ baseURL, tmpPath }) => {
+    const contents = galata.newContentsHelper(baseURL);
+    await contents.uploadFile(
+      path.resolve(__dirname, `./notebooks/${fileName}`),
+      `${tmpPath}/${fileName}`
+    );
+  });
+
+  test.afterEach(async ({ baseURL, tmpPath }) => {
+    const contents = galata.newContentsHelper(baseURL);
+    await contents.deleteDirectory(tmpPath);
+  });
+
+  test('Replace in cell', async ({ page, tmpPath }) => {
+    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    await page.notebook.activate(fileName);
+
+    // Open search box
+    await page.keyboard.press('Control+f');
+
+    await page.fill('[placeholder="Find"]', 'with');
+
+    await page.waitForSelector('text=1/28');
+
+    await page.click('button[title="Toggle Replace"]');
+
+    await page.fill('[placeholder="Replace"]', 'egg');
+
+    await page.click('button:has-text("Replace")');
+
+    await page.waitForSelector('text=1/19');
+
+    const nbPanel = await page.notebook.getNotebookInPanel();
+
+    expect(await nbPanel.screenshot()).toMatchSnapshot('replace-in-cell.png');
+  });
+
+  test('Replace on markdown rendered cell', async ({ page, tmpPath }) => {
+    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    await page.notebook.activate(fileName);
+
+    // Open search box
+    await page.keyboard.press('Control+f');
+
+    await page.fill('[placeholder="Find"]', 'with');
+
+    await page.waitForSelector('text=1/28');
+
+    // Click next button
+    await page.click('button[title="Next Match"]', {
+      clickCount: 4
+    });
+
+    await page.click('button[title="Toggle Replace"]');
+
+    await page.fill('[placeholder="Replace"]', 'egg');
+
+    // First click will unrendered the cell an highlight the first match
+    await page.click('button:has-text("Replace")');
+
+    // The second click will replace the hit
+    await page.click('button:has-text("Replace")');
+
+    await page.waitForSelector('text=5/20');
+
+    const cell = await page.notebook.getCell(1);
+
+    expect(await (await cell.$('.jp-Editor')).screenshot()).toMatchSnapshot(
+      'replace-in-markdown-rendered-cell.png'
+    );
+  });
+
+  test('Replace all', async ({ page, tmpPath }) => {
+    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    await page.notebook.activate(fileName);
+
+    // Open search box
+    await page.keyboard.press('Control+f');
+
+    await page.fill('[placeholder="Find"]', 'with');
+
+    await page.waitForSelector('text=1/28');
+
+    await page.click('button[title="Toggle Replace"]');
+
+    await page.fill('[placeholder="Replace"]', 'egg');
+
+    await page.click('button:has-text("Replace All")');
+
+    await page.waitForSelector('text=-/-');
+
+    const nbPanel = await page.notebook.getNotebookInPanel();
+
+    expect(await nbPanel.screenshot()).toMatchSnapshot('replace-all.png');
+  });
+});
