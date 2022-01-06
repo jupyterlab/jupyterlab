490f221c3def5cf257c9588ff81885c88e48bd14
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-01-06 16:28:12 +0100

    Update mobile test

--- galata/test/jupyterlab/notebook-mobile.test.ts
+++ galata/test/jupyterlab/notebook-mobile.test.ts
@@ -5,20 +5,14 @@ import { test } from '@jupyterlab/galata';
 
 import { expect } from '@playwright/test';
 
-const fileName = 'notebook.ipynb';
-
-test.use({ viewport: { width: 512, height: 768 } });
-
 test.describe('Notebook Layout on Mobile', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.notebook.createNew(fileName);
-  });
-
   test('Execute code cell', async ({ page }) => {
+    await page.notebook.createNew();
     await page.notebook.setCell(0, 'code', 'print("hello")');
     await page.notebook.addCell('code', '2 * 3');
     await page.notebook.runCellByCell();
     await page.sidebar.close('left');
+    await page.setViewportSize({ width: 512, height: 768 });
     const nbPanel = await page.notebook.getNotebookInPanel();
     const imageName = 'mobile-layout.png';
     expect(await nbPanel.screenshot()).toMatchSnapshot(imageName);
