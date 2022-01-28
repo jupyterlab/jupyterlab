f2fc57147240d55eae8ef3f2f176162b2aa2fab3
Frédéric Collonval <fcollonval@gmail.com>
2022-01-28 10:19:36 +0100

    Add slow motion to reduce flakyness

--- galata/playwright-documentation.config.js
+++ galata/playwright-documentation.config.js
@@ -11,6 +11,13 @@ module.exports = {
       testMatch: 'test/documentation/**'
     }
   ],
+  use: {
+    ...baseConfig.use,
+    launchOptions: {
+      // Force slow motion
+      slowMo: 30
+    }
+  },
   // Switch to 'always' to keep raw assets for all tests
   preserveOutput: 'failures-only', // Breaks HTML report if use.video == 'on'
   // Try one retry as some tests are flaky
