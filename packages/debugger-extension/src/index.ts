a425823f044ff9955aea1620ac437e72740bc025
Frédéric Collonval <fcollonval@gmail.com>
2022-02-01 14:46:17 +0100

    Prettify

--- packages/debugger-extension/src/index.ts
+++ packages/debugger-extension/src/index.ts
@@ -721,12 +721,14 @@ const main: JupyterFrontEndPlugin<void> = {
     });
 
     commands.addCommand(CommandIDs.pause, {
-      label: () => service.isPausingOnExceptions
-        ? trans.__('Disable pausing on exceptions')
-        : trans.__('Enable pausing on exceptions'),
-      caption: () => service.isPausingOnExceptions
-        ? trans.__('Disable pausing on exceptions')
-        : trans.__('Enable pausing on exceptions'),
+      label: () =>
+        service.isPausingOnExceptions
+          ? trans.__('Disable pausing on exceptions')
+          : trans.__('Enable pausing on exceptions'),
+      caption: () =>
+        service.isPausingOnExceptions
+          ? trans.__('Disable pausing on exceptions')
+          : trans.__('Enable pausing on exceptions'),
       className: 'jp-PauseOnExceptions',
       icon: Debugger.Icons.pauseOnExceptionsIcon,
       isToggled: () => {
