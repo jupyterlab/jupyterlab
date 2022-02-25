c72cf8a9882fda4d08dbd1bce057a90575f571cc
Frédéric Collonval <fcollonval@gmail.com>
2022-02-25 10:46:10 +0100

    Add test for shortcut uniqueness

--- galata/test/jupyterlab/shortcuts.test.ts
+++ galata/test/jupyterlab/shortcuts.test.ts
@@ -4,7 +4,14 @@
 import { test } from '@jupyterlab/galata';
 import { expect } from '@playwright/test';
 
-test('Shortcut commands must exists', async ({ page }) => {
+const DUPLICATE_SHORTCUT_WARNING =
+  'Skipping this default shortcut because it collides with another default shortcut.';
+
+test.use({ autoGoto: false });
+
+test('Shortcut commands must exist', async ({ page }) => {
+  await page.goto();
+
   const [shortcuts, commands] = await page.evaluate(async () => {
     const shortcuts = window.jupyterapp.commands.keyBindings;
     const commandIds = window.jupyterapp.commands.listCommands();
@@ -17,4 +24,28 @@ test('Shortcut commands must exists', async ({ page }) => {
   );
 
   expect(missingCommands).toEqual([]);
+});
+
+test('Shortcuts must be unique', async ({ page }) => {
+  const warnings: string[] = [];
+
+  page.on('console', message => {
+    if (message.type() === 'warning') {
+      warnings.push(message.text());
+    }
+  });
+
+  await page.goto();
+
+  expect(
+    warnings
+      .filter(s => s.startsWith(DUPLICATE_SHORTCUT_WARNING))
+      // List warning messages only once
+      .reduce((agg, message) => {
+        if (!agg.includes(message)) {
+          agg.push(message);
+        }
+        return agg;
+      }, [])
+  ).toEqual([]);
 });
