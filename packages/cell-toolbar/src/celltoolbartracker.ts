4170c1b68f3bd65cb340b02c53913b17146bf3b4
Jason Weill <jweill@amazon.com>
2022-02-28 16:24:55 -0800

    Moves functions back into namespace

--- packages/cell-toolbar/src/celltoolbartracker.ts
+++ packages/cell-toolbar/src/celltoolbartracker.ts
@@ -60,76 +60,6 @@ export const moveUpIcon = new LabIcon({
   svgstr: moveUpSvg
 });
 
-// This is declared outside the scope of the Notebook class so that the MutationObserver function can call
-// it without the class in scope
-function updateCellForToolbarOverlap(activeCellElement: HTMLElement) {
-  // Remove the "toolbar overlap" class from the cell, rendering the cell's toolbar
-  activeCellElement.classList.remove('jp-toolbar-overlap');
-
-  let cellContentOverlapsToolbar: boolean = false;
-
-  if (activeCellElement.classList.contains("jp-mod-rendered")) {
-    // Check for overlap in rendered markdown content
-    cellContentOverlapsToolbar = markdownOverlapsToolbar(activeCellElement);
-  }
-  else {
-  // Check for overlap in code content
-    cellContentOverlapsToolbar = codeOverlapsToolbar(activeCellElement);
-  }
-
-  if (cellContentOverlapsToolbar) {
-    // Add the "toolbar overlap" class to the cell, completely concealing the toolbar,
-    // if the first line of the content overlaps with it at all
-    activeCellElement.classList.add('jp-toolbar-overlap');
-  }
-}
-
-function markdownOverlapsToolbar(activeCellElement: HTMLElement): boolean {
-  const markdownOutputElements = activeCellElement.getElementsByClassName("jp-MarkdownOutput");
-  if (markdownOutputElements.length < 1) {
-    return false;
-  }
-
-  const firstOutputElementChild = markdownOutputElements[0].firstElementChild as HTMLElement;
-  if (firstOutputElementChild === null) {
-    return false;
-  }
-
-  // Temporarily set the element's max width so that the bounding client rectangle only encompasses the content.
-  const oldMaxWidth = firstOutputElementChild.style.maxWidth;
-  firstOutputElementChild.style.maxWidth = 'max-content';
-
-  const lineRight = firstOutputElementChild.getBoundingClientRect().right;
-
-  // Reinstate the old max width.
-  firstOutputElementChild.style.maxWidth = oldMaxWidth;
-
-  const toolbarLeft = cellToolbarLeft(activeCellElement);
-
-  return toolbarLeft === null ? false : lineRight > toolbarLeft;
-}
-
-function codeOverlapsToolbar(activeCellElement: HTMLElement): boolean {
-  const lineRight = activeCellElement
-    .getElementsByClassName('jp-InputArea-editor')[0]
-    .getElementsByClassName('CodeMirror-line')[0].children[0] // First span under first pre
-    .getBoundingClientRect().right;
-
-  const toolbarLeft = cellToolbarLeft(activeCellElement);
-
-  return toolbarLeft === null ? false : lineRight > toolbarLeft;
-}
-
-function cellToolbarLeft(activeCellElement: HTMLElement): number | null {
-  const activeCellToolbar = activeCellElement.querySelector('.jp-cell-bar');
-  
-  if (activeCellToolbar === null || activeCellToolbar === undefined) { 
-    return null;
-  }
-
-  return activeCellToolbar.getBoundingClientRect().left;
-}
-
 /**
  * Watch a notebook so that a cell toolbar appears on the active cell
  */
@@ -169,7 +99,7 @@ export class CellToolbarTracker implements IDisposable {
     this._addToolbar(activeCell.model);
     this._previousActiveCell = activeCell;
 
-    updateCellForToolbarOverlap(activeCell.node);
+    this._updateCellForToolbarOverlap(activeCell.node);
   }
 
   get isDisposed(): boolean {
@@ -306,7 +236,7 @@ export class CellToolbarTracker implements IDisposable {
       return;
     }
 
-    updateCellForToolbarOverlap(activeCell.node);
+    this._updateCellForToolbarOverlap(activeCell.node);
   }
 
   private _resizeEventCallback(event: UIEvent): void {
@@ -315,8 +245,76 @@ export class CellToolbarTracker implements IDisposable {
       return;
     }
 
-    updateCellForToolbarOverlap(activeCell.node);
+    this._updateCellForToolbarOverlap(activeCell.node);
   }
+
+  private _updateCellForToolbarOverlap(activeCellElement: HTMLElement) {
+    // Remove the "toolbar overlap" class from the cell, rendering the cell's toolbar
+    activeCellElement.classList.remove('jp-toolbar-overlap');
+  
+    let cellContentOverlapsToolbar: boolean = false;
+  
+    if (activeCellElement.classList.contains("jp-mod-rendered")) {
+      // Check for overlap in rendered markdown content
+      cellContentOverlapsToolbar = this._markdownOverlapsToolbar(activeCellElement);
+    }
+    else {
+    // Check for overlap in code content
+      cellContentOverlapsToolbar = this._codeOverlapsToolbar(activeCellElement);
+    }
+  
+    if (cellContentOverlapsToolbar) {
+      // Add the "toolbar overlap" class to the cell, completely concealing the toolbar,
+      // if the first line of the content overlaps with it at all
+      activeCellElement.classList.add('jp-toolbar-overlap');
+    }
+  }
+  
+  private _markdownOverlapsToolbar(activeCellElement: HTMLElement): boolean {
+    const markdownOutputElements = activeCellElement.getElementsByClassName("jp-MarkdownOutput");
+    if (markdownOutputElements.length < 1) {
+      return false;
+    }
+  
+    const firstOutputElementChild = markdownOutputElements[0].firstElementChild as HTMLElement;
+    if (firstOutputElementChild === null) {
+      return false;
+    }
+  
+    // Temporarily set the element's max width so that the bounding client rectangle only encompasses the content.
+    const oldMaxWidth = firstOutputElementChild.style.maxWidth;
+    firstOutputElementChild.style.maxWidth = 'max-content';
+  
+    const lineRight = firstOutputElementChild.getBoundingClientRect().right;
+  
+    // Reinstate the old max width.
+    firstOutputElementChild.style.maxWidth = oldMaxWidth;
+  
+    const toolbarLeft = this._cellToolbarLeft(activeCellElement);
+  
+    return toolbarLeft === null ? false : lineRight > toolbarLeft;
+  }
+  
+  private _codeOverlapsToolbar(activeCellElement: HTMLElement): boolean {
+    const lineRight = activeCellElement
+      .getElementsByClassName('jp-InputArea-editor')[0]
+      .getElementsByClassName('CodeMirror-line')[0].children[0] // First span under first pre
+      .getBoundingClientRect().right;
+  
+    const toolbarLeft = this._cellToolbarLeft(activeCellElement);
+  
+    return toolbarLeft === null ? false : lineRight > toolbarLeft;
+  }
+  
+  private _cellToolbarLeft(activeCellElement: HTMLElement): number | null {
+    const activeCellToolbar = activeCellElement.querySelector('.jp-cell-bar');
+    
+    if (activeCellToolbar === null || activeCellToolbar === undefined) { 
+      return null;
+    }
+  
+    return activeCellToolbar.getBoundingClientRect().left;
+  }  
 
   private _commands: CommandRegistry;
   private _isDisposed = false;
