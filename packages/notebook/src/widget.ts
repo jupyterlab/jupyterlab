c6014d647026a0dcfa319303d51b695bf42f3562
Ravi Teja Gurrampati <gurrampa@deshaw.com>
2022-03-03 22:04:53 -0500

    renderCellOnIdle improvements
    Increased timeout to avoid lot of cells getting idle callback at closer intervals.
    Avoiding place holder cells' widget rendering in case of requestIdleCallback timeout and re-scheduling.
    Rendering place holder cells' widget only during full idle frames (deadline.timeRemaining() will be 50) i.e., when there are no UI updates/user inputs queued. https://w3c.github.io/requestidlecallback/#idle-periods
    
    Fixes: Performance issues with larger notebooks.

--- packages/notebook/src/widget.ts
+++ packages/notebook/src/widget.ts
@@ -603,15 +603,28 @@ export class StaticNotebook extends Widget {
       this.onCellInserted(index, widget);
     }
 
+    this._scheduleCellRenderOnIdle();
+  }
+
+  private _scheduleCellRenderOnIdle() {
     if (this._observer && this.notebookConfig.renderCellOnIdle) {
       const renderPlaceholderCells = this._renderPlaceholderCells.bind(this);
       (window as any).requestIdleCallback(renderPlaceholderCells, {
-        timeout: 1000
+        timeout: 60000
       });
     }
   }
 
   private _renderPlaceholderCells(deadline: any) {
+    const timeRemaining = deadline.timeRemaining();
+
+    // In case this got triggered because of timeout or when there are screen updates (https://w3c.github.io/requestidlecallback/#idle-periods),
+    // avoiding the render and rescheduling the place holder cell rendering.
+    if (deadline.didTimeout || timeRemaining < 50) {
+      this._scheduleCellRenderOnIdle();
+      return;
+    }
+
     if (
       this._renderedCellsCount < this._cellsArray.length &&
       this._renderedCellsCount >=
@@ -623,6 +636,11 @@ export class StaticNotebook extends Widget {
   }
 
   private _renderPlaceholderCell(cell: Cell, index: number) {
+    // We don't have cancel mechanism for scheduled requestIdleCallback(renderPlaceholderCells),
+    // adding defensive check for layout in case tab is closed.
+    if (!this.layout) {
+      return;
+    }
     const pl = this.layout as PanelLayout;
     pl.removeWidgetAt(index);
     pl.insertWidget(index, cell);
