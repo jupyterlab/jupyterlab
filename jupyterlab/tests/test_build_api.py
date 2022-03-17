0f78a8eca7eaee821c9fec60015f2f163d7e35ab
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-03-17 10:48:00 +0100

    Inline expected_http_error function for the tests

--- jupyterlab/tests/test_build_api.py
+++ jupyterlab/tests/test_build_api.py
@@ -6,7 +6,28 @@ from tempfile import TemporaryDirectory
 
 import pytest
 import tornado
-from jupyterlab_server.tests.utils import expected_http_error
+
+
+def expected_http_error(error, expected_code, expected_message=None):
+    """Check that the error matches the expected output error."""
+    e = error.value
+    if isinstance(e, tornado.web.HTTPError):
+        if expected_code != e.status_code:
+            return False
+        if expected_message is not None and expected_message != str(e):
+            return False
+        return True
+    elif any([
+        isinstance(e, tornado.httpclient.HTTPClientError),
+        isinstance(e, tornado.httpclient.HTTPError)
+    ]):
+        if expected_code != e.code:
+            return False
+        if expected_message:
+            message = json.loads(e.response.body.decode())['message']
+            if expected_message != message:
+                return False
+        return True
 
 
 @pytest.fixture
