fa7578e39b24741e2a5eb2ebd7cc50fbb3a02424
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2022-03-29 14:28:48 +0200

    Backport PR #12260 on branch master (Adds preferKernel option to JupyterLab code) (#12287)
    * Backport PR #12260: Adds preferKernel option to JupyterLab code
    
    * Lint code
    
    Co-authored-by: Jason Weill <93281816+jweill-aws@users.noreply.github.com>
    Co-authored-by: Frédéric Collonval <fcollonval@users.noreply.github.com>

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -914,6 +914,12 @@ function activateWidgetFactory(
   translator: ITranslator,
   settingRegistry: ISettingRegistry | null
 ): NotebookWidgetFactory.IFactory {
+  const preferKernelOption = PageConfig.getOption('notebookStartsKernel');
+
+  // If the option is not set, assume `true`
+  const preferKernelValue =
+    preferKernelOption === '' || preferKernelOption.toLowerCase() === 'true';
+
   const { commands } = app;
   let toolbarFactory:
     | ((
@@ -969,7 +975,7 @@ function activateWidgetFactory(
     fileTypes: ['notebook'],
     modelName: 'notebook',
     defaultFor: ['notebook'],
-    preferKernel: true,
+    preferKernel: preferKernelValue,
     canStartKernel: true,
     rendermime,
     contentFactory,
