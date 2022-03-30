8b6660c22f627976583794b0c1c918e2117b10d0
Josh Geden <31145777+Josh0823@users.noreply.github.com>
2022-03-30 11:05:27 -0400

    add "Toggle Contextual Help" command (#12091)
    * add "Toggle Contextual Help" command
    
    * fix prettier formatting
    
    * fix review suggestions
    
    * add inspector:close command
    
    * Allow to close the inspector even if not focused
    
    Co-authored-by: krassowski <5832902+krassowski@users.noreply.github.com>

--- packages/inspector-extension/src/index.ts
+++ packages/inspector-extension/src/index.ts
@@ -33,6 +33,8 @@ import { inspectorIcon } from '@jupyterlab/ui-components';
  */
 namespace CommandIDs {
   export const open = 'inspector:open';
+  export const close = 'inspector:close';
+  export const toggle = 'inspector:toggle';
 }
 
 /**
@@ -53,10 +55,12 @@ const inspector: JupyterFrontEndPlugin<IInspector> = {
   ): IInspector => {
     const trans = translator.load('jupyterlab');
     const { commands, shell } = app;
-    const command = CommandIDs.open;
-    const label = trans.__('Show Contextual Help');
+    const caption = trans.__(
+      'Live updating code documentation from the active kernel'
+    );
     const openedLabel = trans.__('Contextual Help');
     const namespace = 'inspector';
+    const datasetKey = 'jpInspector';
     const tracker = new WidgetTracker<MainAreaWidget<InspectorPanel>>({
       namespace
     });
@@ -84,20 +88,24 @@ const inspector: JupyterFrontEndPlugin<IInspector> = {
         shell.add(inspector, 'main', { activate: false, mode: 'split-right' });
       }
       shell.activateById(inspector.id);
+      document.body.dataset[datasetKey] = 'open';
       return inspector;
     }
+    function closeInspector(): void {
+      inspector.dispose();
+      delete document.body.dataset[datasetKey];
+    }
 
-    // Add command to registry.
-    commands.addCommand(command, {
-      caption: trans.__(
-        'Live updating code documentation from the active kernel'
-      ),
+    // Add inspector:open command to registry.
+    const showLabel = trans.__('Show Contextual Help');
+    commands.addCommand(CommandIDs.open, {
+      caption,
       isEnabled: () =>
         !inspector ||
         inspector.isDisposed ||
         !inspector.isAttached ||
         !inspector.isVisible,
-      label,
+      label: showLabel,
       icon: args => (args.isLauncher ? inspectorIcon : undefined),
       execute: args => {
         const text = args && (args.text as string);
@@ -109,17 +117,48 @@ const inspector: JupyterFrontEndPlugin<IInspector> = {
       }
     });
 
-    // Add command to UI where possible.
-    if (palette) {
-      palette.addItem({ command, category: label });
-    }
+    // Add inspector:close command to registry.
+    const closeLabel = trans.__('Hide Contextual Help');
+    commands.addCommand(CommandIDs.close, {
+      caption,
+      isEnabled: () => isInspectorOpen(),
+      label: closeLabel,
+      icon: args => (args.isLauncher ? inspectorIcon : undefined),
+      execute: () => closeInspector()
+    });
+
+    // Add inspector:toggle command to registry.
+    const toggleLabel = trans.__('Show Contextual Help');
+    commands.addCommand(CommandIDs.toggle, {
+      caption,
+      label: toggleLabel,
+      isToggled: () => isInspectorOpen(),
+      execute: args => {
+        if (isInspectorOpen()) {
+          closeInspector();
+        } else {
+          const text = args && (args.text as string);
+          openInspector(text);
+        }
+      }
+    });
+
+    // Add open command to launcher if possible.
     if (launcher) {
-      launcher.add({ command, args: { isLauncher: true } });
+      launcher.add({ command: CommandIDs.open, args: { isLauncher: true } });
+    }
+
+    // Add toggle command to command palette if possible.
+    if (palette) {
+      palette.addItem({ command: CommandIDs.toggle, category: toggleLabel });
     }
 
     // Handle state restoration.
     if (restorer) {
-      void restorer.restore(tracker, { command, name: () => 'inspector' });
+      void restorer.restore(tracker, {
+        command: CommandIDs.toggle,
+        name: () => 'inspector'
+      });
     }
 
     // Create a proxy to pass the `source` to the current inspector.
