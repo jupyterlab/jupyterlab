4f5bbe586091a6e611fce6e0f65ab0573fcc915d
Jason Weill <jweill@amazon.com>
2022-04-14 16:15:04 -0700

    Set theme on initial render

--- packages/vega5-extension/src/index.ts
+++ packages/vega5-extension/src/index.ts
@@ -85,6 +85,13 @@ export class RenderedVega extends Widget implements IRenderMime.IRenderer {
       | undefined;
     const embedOptions =
       metadata && metadata.embed_options ? metadata.embed_options : {};
+
+    // If the JupyterLab theme is dark, render this using a dark Vega theme.
+    let bodyThemeLight = document.body.dataset.jpThemeLight === 'true';
+    if (!bodyThemeLight) {
+      embedOptions.theme = 'dark';
+    }
+
     const mode: VegaModuleType.Mode =
       this._mimeType === VEGA_MIME_TYPE ? 'vega' : 'vega-lite';
 
