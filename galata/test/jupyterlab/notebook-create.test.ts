85a9bb3092a82f72029cd34162c104a100b46b08
Jason Weill <jweill@amazon.com>
2022-04-15 10:26:00 -0700

    Adds cell with Vega visualization

--- galata/test/jupyterlab/notebook-create.test.ts
+++ galata/test/jupyterlab/notebook-create.test.ts
@@ -14,6 +14,34 @@ async function populateNotebook(page: IJupyterLabPageFixture) {
     '## This is **bold** and *italic* [link to jupyter.org!](http://jupyter.org)'
   );
   await page.notebook.addCell('code', '2 ** 3');
+  await page.notebook.addCell(
+    'code',
+    `
+from IPython.display import display
+
+display({
+    "application/vnd.vegalite.v3+json": {
+        "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+        "description": "A simple bar chart with embedded data.",
+        "data": {
+            "values": [
+                {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
+                {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
+                {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
+            ]
+        },
+        "mark": "bar",
+        "encoding": {
+            "x": {"field": "a", "type": "ordinal"},
+            "y": {"field": "b", "type": "quantitative"}
+        },
+        "metadata": {
+            "theme": "dark"
+        },
+    }
+}, raw=True)
+`
+  );
 }
 
 test.describe('Notebook Create', () => {
