b2049dfa78f8d93e16dcb3f8fd6418ec1f532729
Jason Weill <jweill@amazon.com>
2022-04-15 11:13:43 -0700

    Simplifies and modernizes Vega example, extends timeout for notebook-create tests

--- galata/test/jupyterlab/notebook-create.test.ts
+++ galata/test/jupyterlab/notebook-create.test.ts
@@ -16,29 +16,20 @@ async function populateNotebook(page: IJupyterLabPageFixture) {
   await page.notebook.addCell('code', '2 ** 3');
   await page.notebook.addCell(
     'code',
-    `
-from IPython.display import display
+    `from IPython.display import display
 
 display({
-    "application/vnd.vegalite.v3+json": {
-        "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
-        "description": "A simple bar chart with embedded data.",
-        "data": {
-            "values": [
-                {"a": "A", "b": 28}, {"a": "B", "b": 55}, {"a": "C", "b": 43},
-                {"a": "D", "b": 91}, {"a": "E", "b": 81}, {"a": "F", "b": 53},
-                {"a": "G", "b": 19}, {"a": "H", "b": 87}, {"a": "I", "b": 52}
-            ]
-        },
-        "mark": "bar",
-        "encoding": {
-            "x": {"field": "a", "type": "ordinal"},
-            "y": {"field": "b", "type": "quantitative"}
-        },
-        "metadata": {
-            "theme": "dark"
-        },
-    }
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "data": {
+    "values": [
+      {"a": "A", "b": 28}, {"a": "B", "b": 55}
+    ]
+  },
+  "mark": "bar",
+  "encoding": {
+    "x": {"field": "a", "type": "nominal", "axis": {"labelAngle": 0}},
+    "y": {"field": "b", "type": "quantitative"}
+  }
 }, raw=True)
 `
   );
@@ -72,6 +63,7 @@ test.describe('Notebook Create', () => {
   });
 
   test('Save Notebook', async ({ page }) => {
+    test.setTimeout(120000);
     await populateNotebook(page);
 
     await expect(page.notebook.save()).resolves.not.toThrow();
@@ -94,6 +86,7 @@ test.describe('Notebook Create', () => {
   });
 
   test('Run cells', async ({ page }) => {
+    test.setTimeout(120000);
     await populateNotebook(page);
     await page.notebook.run();
     await page.notebook.save();
@@ -107,6 +100,7 @@ test.describe('Notebook Create', () => {
   });
 
   test('Toggle Dark theme', async ({ page }) => {
+    test.setTimeout(120000);
     await populateNotebook(page);
     await page.notebook.run();
     await page.theme.setDarkTheme();
