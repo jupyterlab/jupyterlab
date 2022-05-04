d602cf2da8ab2d110579dc9e7072c4ee35b0801d
Max Klein <telamonian@users.noreply.github.com>
2022-05-04 05:21:20 -0400

    Add line history (via arrow up/down) to all `Stdin` widgets in cell outputs (#12502)
    * add basic line history to `Stdin` widgets
    
    - fixes #7006
    - the `Stdin` widgets in question are those that get displayed in cell outputs, and are part of the implementation of the `OutputArea` widget
    - current bug: "ArrowLeft" and "ArrowRight" keydown events are completely ignored and do not fire their default behavior
    
    * fixed too-broad default selector for `notebook:move-cursor-heading...` cmds
    
    - fixes #12500, since these cmds were eating the left/right arrow keydown evts for `<input>` elements shown in cell outputs
    
    * inform users of line history feature via `input.placeholder` of `Stdin` widget
    
    * use separate up arrow/down arrow glyphs in `placeholder` of `Stdin`

--- packages/outputarea/src/widget.ts
+++ packages/outputarea/src/widget.ts
@@ -872,6 +872,27 @@ export interface IStdin extends Widget {
  * The default stdin widget.
  */
 export class Stdin extends Widget implements IStdin {
+  private static _history: string[] = [];
+
+  private static _historyAt(ix: number): string | undefined {
+    const len = Stdin._history.length;
+    // interpret negative ix exactly like Array.at
+    ix = ix < 0 ? len + ix : ix;
+
+    if (ix < len) {
+      return Stdin._history[ix];
+    }
+    // return undefined if ix is out of bounds
+  }
+
+  private static _historyPush(line: string): void {
+    Stdin._history.push(line);
+    if (Stdin._history.length > 1000) {
+      // truncate line history if it's too long
+      Stdin._history.shift();
+    }
+  }
+
   /**
    * Construct a new input widget.
    */
@@ -880,8 +901,11 @@ export class Stdin extends Widget implements IStdin {
       node: Private.createInputWidgetNode(options.prompt, options.password)
     });
     this.addClass(STDIN_CLASS);
+    this._history_ix = 0;
     this._input = this.node.getElementsByTagName('input')[0];
     this._input.focus();
+    // make users aware of the line history feature
+    this._input.placeholder = '↑↓ for history';
     this._future = options.future;
     this._parent_header = options.parent_header;
     this._value = options.prompt + ' ';
@@ -904,11 +928,32 @@ export class Stdin extends Widget implements IStdin {
    * called in response to events on the dock panel's node. It should
    * not be called directly by user code.
    */
-  handleEvent(event: Event): void {
+  handleEvent(event: KeyboardEvent): void {
     const input = this._input;
     if (event.type === 'keydown') {
-      if ((event as KeyboardEvent).keyCode === 13) {
-        // Enter
+      if (event.key === 'ArrowUp') {
+        const historyLine = Stdin._historyAt(this._history_ix - 1);
+        if (historyLine) {
+          if (this._history_ix === 0) {
+            this._value_cache = input.value;
+          }
+          input.value = historyLine;
+          --this._history_ix;
+        }
+      } else if (event.key === 'ArrowDown') {
+        if (this._history_ix === 0) {
+          // do nothing
+        } else if (this._history_ix === -1) {
+          input.value = this._value_cache;
+          ++this._history_ix;
+        } else {
+          const historyLine = Stdin._historyAt(this._history_ix + 1);
+          if (historyLine) {
+            input.value = historyLine;
+            ++this._history_ix;
+          }
+        }
+      } else if (event.key === 'Enter') {
         this._future.sendInputReply(
           {
             status: 'ok',
@@ -920,6 +965,7 @@ export class Stdin extends Widget implements IStdin {
           this._value += Array(input.value.length + 1).join('·');
         } else {
           this._value += input.value;
+          Stdin._historyPush(input.value);
         }
         this._promise.resolve(void 0);
       }
@@ -948,10 +994,12 @@ export class Stdin extends Widget implements IStdin {
     this._input.removeEventListener('keydown', this);
   }
 
+  private _history_ix: number;
   private _parent_header: KernelMessage.IInputReplyMsg['parent_header'];
   private _future: Kernel.IShellFuture;
   private _input: HTMLInputElement;
   private _value: string;
+  private _value_cache: string;
   private _promise = new PromiseDelegate<void>();
 }
 
