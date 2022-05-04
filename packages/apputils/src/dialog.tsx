ba668943a1a9ddbcd352efd307c1d969398efefb
John Nnamchi <john.nnamchi@mail.mcgill.ca>
2022-05-04 00:41:11 -0700

    Handled new dialog creation with no buttons (#12496)
    * Handled new dialog creation with no buttons
    
    * Replaced undefined checks with nullish operator
    
    Co-authored-by: jnnamchi <jnnamchi@bloomberg.net>

--- packages/apputils/src/dialog.tsx
+++ packages/apputils/src/dialog.tsx
@@ -249,7 +249,7 @@ export class Dialog<T> extends Widget {
         this._primary = el as HTMLElement;
       }
     }
-    this._primary.focus();
+    this._primary?.focus();
   }
 
   /**
@@ -392,7 +392,7 @@ export class Dialog<T> extends Widget {
     const target = event.target as HTMLElement;
     if (!this.node.contains(target as HTMLElement)) {
       event.stopPropagation();
-      this._buttonNodes[this._defaultButton].focus();
+      this._buttonNodes[this._defaultButton]?.focus();
     }
   }
 
