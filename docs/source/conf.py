56edf157995e8c16e38306a5f7af5ac263436363
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-06-17 16:04:49 +0200

    Backport PR #12707: Explicitly set language to `en` in `conf.py` (#12712)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,7 +80,7 @@ release = version_ns["__version__"]
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
