a7670e734d58304cd5e93c3fbd3dfc2fef27edbc
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-07-19 11:55:01 +0200

    jlpm config

--- jupyterlab/browser_check.py
+++ jupyterlab/browser_check.py
@@ -143,9 +143,8 @@ async def run_browser(url):
         if not osp.exists(target):
             os.makedirs(osp.join(target))
         await run_async_process(["jlpm", "init", "-y"], cwd=target)
-        await run_async_process(
-            ["jlpm", "add", "playwright", "--network-timeout", "1000000000"], cwd=target
-        )
+        await run_async_process(["jlpm", "config", "set", "network-timeout", "1000000000"], cwd=target)
+        await run_async_process(["jlpm", "add", "playwright"], cwd=target)
     shutil.copy(osp.join(here, "browser-test.js"), osp.join(target, "browser-test.js"))
     await run_async_process(["node", "browser-test.js", url], cwd=target)
 
@@ -156,9 +155,8 @@ def run_browser_sync(url):
     if not osp.exists(osp.join(target, "node_modules")):
         os.makedirs(target)
         subprocess.call(["jlpm", "init", "-y"], cwd=target)
-        subprocess.call(
-            ["jlpm", "add", "playwright", "--network-timeout", "1000000000"], cwd=target
-        )
+        subprocess.call(["jlpm", "config", "set", "network-timeout", "1000000000"], cwd=target)
+        subprocess.call(["jlpm", "add", "playwright"], cwd=target)
     shutil.copy(osp.join(here, "browser-test.js"), osp.join(target, "browser-test.js"))
     return subprocess.check_call(["node", "browser-test.js", url], cwd=target)
 
