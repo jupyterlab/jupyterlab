a3b328aa93b1ff83a472f088c0ac0cc3369d09ef
Frédéric Collonval <fcollonval@gmail.com>
2022-08-04 10:47:02 +0200

    Translate internal CM6 strings

--- packages/codemirror/src/editor.ts
+++ packages/codemirror/src/editor.ts
@@ -201,12 +201,54 @@ export class CodeMirrorEditor implements CodeEditor.IEditor {
       }
     );
 
+    // The list of internal strings is available at https://codemirror.net/examples/translate/
+    const translation = EditorState.phrases.of({
+      // @codemirror/view
+      'Control character': this._trans.__('Control character'),
+      // @codemirror/commands
+      'Selection deleted': this._trans.__('Selection deleted'),
+      // @codemirror/language
+      'Folded lines': this._trans.__('Folded lines'),
+      'Unfolded lines': this._trans.__('Unfolded lines'),
+      to: this._trans.__('to'),
+      'folded code': this._trans.__('folded code'),
+      unfold: this._trans.__('unfold'),
+      'Fold line': this._trans.__('Fold line'),
+      'Unfold line': this._trans.__('Unfold line'),
+      // @codemirror/search
+      'Go to line': this._trans.__('Go to line'),
+      go: this._trans.__('go'),
+      Find: this._trans.__('Find'),
+      Replace: this._trans.__('Replace'),
+      next: this._trans.__('next'),
+      previous: this._trans.__('previous'),
+      all: this._trans.__('all'),
+      'match case': this._trans.__('match case'),
+      replace: this._trans.__('replace'),
+      'replace all': this._trans.__('replace all'),
+      close: this._trans.__('close'),
+      'current match': this._trans.__('current match'),
+      'replaced $ matches': this._trans.__('replaced $ matches'),
+      'replaced match on line $': this._trans.__('replaced match on line $'),
+      'on line': this._trans.__('on line'),
+      // @codemirror/autocomplete
+      Completions: this._trans.__('Completions'),
+      // @codemirror/lint
+      Diagnostics: this._trans.__('Diagnostics'),
+      'No diagnostics': this._trans.__('No diagnostics')
+    });
+
     this._editor = Private.createEditor(
       host,
       fullConfig,
       this._yeditorBinding,
       this._editorConfig,
-      [this._markField, Prec.high(domEventHandlers), updateListener]
+      [
+        this._markField,
+        Prec.high(domEventHandlers),
+        updateListener,
+        translation
+      ]
     );
 
     // every time the model is switched, we need to re-initialize the editor binding
