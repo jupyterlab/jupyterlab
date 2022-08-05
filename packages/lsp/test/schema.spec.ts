1a3df80f36b56b6932964c1836cc9640ba886f1a
Frédéric Collonval <fcollonval@users.noreply.github.com>
2022-08-05 16:17:07 +0200

    [skip ci] Prettify schema.spec.ts

--- packages/lsp/test/schema.spec.ts
+++ packages/lsp/test/schema.spec.ts
@@ -4,7 +4,7 @@ describe('@jupyterlab/lsp', () => {
   describe('schema.ts', () => {
     it('verify the interfaces defined in `schema.ts` with the schema in jupyter_lsp', () => {
       const value = childProcess.execSync('yarn build:schema check', {});
-      try{
+      try {
         expect(value.toString().includes('true')).toBe(true);
       } catch (reason) {
         throw new Error(
