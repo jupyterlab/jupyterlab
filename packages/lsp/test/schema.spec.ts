09a6edf20d85343e307626aacef830a5220128df
Frédéric Collonval <fcollonval@users.noreply.github.com>
2022-08-05 15:23:08 +0200

    [skip ci] Improve error message for lsp schema validation

--- packages/lsp/test/schema.spec.ts
+++ packages/lsp/test/schema.spec.ts
@@ -4,7 +4,14 @@ describe('@jupyterlab/lsp', () => {
   describe('schema.ts', () => {
     it('verify the interfaces defined in `schema.ts` with the schema in jupyter_lsp', () => {
       const value = childProcess.execSync('yarn build:schema check', {});
-      expect(value.toString().includes('true')).toBe(true);
+      try{
+        expect(value.toString().includes('true')).toBe(true);
+      } catch (reason) {
+        throw new Error(
+          `'schema.ts' in '@jupyterlab/lsp' is not matching the 'jupyter_lsp' backend schema.
+  Please update 'schema.ts' by running \`npm run build:schema\` in '@jupyterlab/lsp' and commit the changes.`
+        );
+      }
     });
   });
 });
