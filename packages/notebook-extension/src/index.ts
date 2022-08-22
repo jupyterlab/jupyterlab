ff37c4f4324fbc6640f5c35777a79d64552696e2
Krishna Kumar <krshnhari@gmail.com>
2022-08-22 21:29:17 +0800

    Add default shortcuts for moving cells (#9031)
    * Add default shortcuts for moving cells
    
    Changes also includes a mechanism to raise silent
    notifications that can be recognised by screen readers
    
    * Translate message in silent notification
    
    Suggested by: krassowski
    
    * Update snapshots
    
    Co-authored-by: Frédéric Collonval <fcollonval@gmail.com>

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -2448,7 +2448,11 @@ function addCommands(
       const current = getCurrent(tracker, shell, args);
 
       if (current) {
-        return NotebookActions.moveUp(current.content);
+        NotebookActions.moveUp(current.content);
+        Private.raiseSilentNotification(
+          trans.__('Notebook cell shifted up successfully'),
+          current.node
+        );
       }
     },
     isEnabled,
@@ -2460,7 +2464,11 @@ function addCommands(
       const current = getCurrent(tracker, shell, args);
 
       if (current) {
-        return NotebookActions.moveDown(current.content);
+        NotebookActions.moveDown(current.content);
+        Private.raiseSilentNotification(
+          trans.__('Notebook cell shifted down successfully'),
+          current.node
+        );
       }
     },
     isEnabled,
@@ -3186,6 +3194,42 @@ namespace Private {
       script: trans.__('Executable Script'),
       slides: trans.__('Reveal.js Slides')
     };
+  }
+
+  /**
+   * Raises a silent notification that is read by screen readers
+   *
+   * FIXME: Once a notificatiom API is introduced (https://github.com/jupyterlab/jupyterlab/issues/689),
+   * this can be refactored to use the same.
+   *
+   * More discussion at https://github.com/jupyterlab/jupyterlab/pull/9031#issuecomment-773541469
+   *
+   *
+   * @param message Message to be relayed to screen readers
+   * @param notebookNode DOM node to which the notification container is attached
+   */
+  export function raiseSilentNotification(
+    message: string,
+    notebookNode: HTMLElement
+  ) {
+    const hiddenAlertContainerId = `sr-message-container-${notebookNode.id}`;
+
+    const hiddenAlertContainer =
+      document.getElementById(hiddenAlertContainerId) ||
+      document.createElement('div');
+
+    // If the container is not available, append the newly created container
+    // to the current notebook panel and set related properties
+    if (hiddenAlertContainer.getAttribute('id') !== hiddenAlertContainerId) {
+      hiddenAlertContainer.classList.add('sr-only');
+      hiddenAlertContainer.setAttribute('id', hiddenAlertContainerId);
+      hiddenAlertContainer.setAttribute('role', 'alert');
+      hiddenAlertContainer.hidden = true;
+      notebookNode.appendChild(hiddenAlertContainer);
+    }
+
+    // Insert/Update alert container with the notification message
+    hiddenAlertContainer.innerText = message;
   }
 
   /**
