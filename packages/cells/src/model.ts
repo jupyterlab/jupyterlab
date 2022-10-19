40137650f77c32f1653c3ff5c458ef9c89b403b8
David Brochart <david.brochart@gmail.com>
2022-10-19 15:16:26 +0200

    Fix notebook trust in RTC

--- packages/cells/src/model.ts
+++ packages/cells/src/model.ts
@@ -431,6 +431,7 @@ export class CellModel extends CodeEditor.Model implements ICellModel {
           break;
         case 'trusted':
           this.metadata.set('trusted', metadata.trusted);
+          this.trusted = metadata.trusted as boolean;
           break;
         default:
           // The default is applied for custom metadata that are not
@@ -796,6 +797,12 @@ export class CodeCellModel extends CellModel implements ICodeCellModel {
   ): void {
     if (this._outputs) {
       this._outputs.trusted = args.newValue as boolean;
+    }
+    if (args.newValue as boolean) {
+      const codeCell = this.sharedModel as models.YCodeCell;
+      const metadata = codeCell.getMetadata();
+      metadata.trusted = true;
+      codeCell.setMetadata(metadata);
     }
     this.stateChanged.emit({
       name: 'trusted',
