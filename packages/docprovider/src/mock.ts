b149643d8ee73c154921ce7291cc4375b439d3ef
David Brochart <david.brochart@gmail.com>
2022-10-28 17:03:24 +0200

    Use file ID (#13246)
    * Use file ID
    
    * Add file_id2path in settings for use in jupyter-server-ydoc
    
    * Pin jupyter_server_fileid >=0.3.1
    
    * Clean tsconfigdoc.json
    
    * Pin jupyter_server==2.0.0rc3
    
    * Remove dependency on jupyter_server_fileid
    
    * Remove file_id2path in settings
    
    * Change YMap 'context' to 'state'
    
    * Pin jupyter_ydoc>=0.2.2
    
    * File path to room ID is a PUT with a body, returns a room ID
    
    * Use room ID only in collaborative mode
    
    * Update tsconfigdoc.json
    
    Co-authored-by: Frédéric Collonval <fcollonval@users.noreply.github.com>
    
    * Review
    
    * Review
    
    * If the ready promise is already in flight, do not re-trigger the request.
    
    * integrity clean up
    
    * Move some logic to the constructor
    
    * Clear unwanted changes
    
    * Pin jupyter_server_ydoc>=0.1.14
    
    * Undo yarn.lock changes
    
    * Use `IDisposable` instead of `destroy`
    
    * Do change ycontext in this PR
    
    * Make ready promise resolve to void like other ready promises in JupyterLab
    
    * Make ready promise resolve to void like other ready promises in JupyterLab
    
    * Update packages/docprovider/src/yprovider.ts
    
    * Fix follow-up rebase
    
    Co-authored-by: Frédéric Collonval <fcollonval@users.noreply.github.com>
    Co-authored-by: Afshin T. Darian <git@darian.email>
    Co-authored-by: Frédéric Collonval <fcollonval@gmail.com>

--- packages/docprovider/src/mock.ts
+++ packages/docprovider/src/mock.ts
@@ -5,14 +5,33 @@
 
 import { IDocumentProvider } from './index';
 
+/**
+ * Fallback document provider
+ */
 export class ProviderMock implements IDocumentProvider {
-  destroy(): void {
-    /* nop */
+  /**
+   * Test whether the object has been disposed.
+   */
+  get isDisposed(): boolean {
+    return this._isDisposed;
   }
-  setPath(path: string): void {
-    /* nop */
+
+  /**
+   * Returns a Promise that resolves when the document provider is ready.
+   */
+  get ready(): Promise<void> {
+    return Promise.resolve();
   }
-  get renameAck(): Promise<boolean> {
-    return Promise.resolve(false);
+
+  /**
+   * Dispose of the resources held by the object.
+   */
+  dispose(): void {
+    if (this.isDisposed) {
+      return;
+    }
+    this._isDisposed = true;
   }
+
+  private _isDisposed = false;
 }
