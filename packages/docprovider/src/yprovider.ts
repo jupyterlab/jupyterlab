f64b4ee6187a18c53203345796eba15e50824d43
Frédéric Collonval <fcollonval@users.noreply.github.com>
2022-10-31 16:33:23 +0100

    Backport PR #13246: Use file ID (#13352)
    Co-authored-by: David Brochart <david.brochart@gmail.com>

--- packages/docprovider/src/yprovider.ts
+++ packages/docprovider/src/yprovider.ts
@@ -3,13 +3,21 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
+import { URLExt } from '@jupyterlab/coreutils';
 import { DocumentChange, YDocument } from '@jupyterlab/shared-models';
-import { UserManager } from '@jupyterlab/services';
+import { ServerConnection, UserManager } from '@jupyterlab/services';
 import { PromiseDelegate } from '@lumino/coreutils';
-import * as decoding from 'lib0/decoding';
-import * as encoding from 'lib0/encoding';
+import { Signal } from '@lumino/signaling';
+import { Awareness } from 'y-protocols/awareness';
 import { WebsocketProvider as YWebsocketProvider } from 'y-websocket';
+import type { Doc } from 'yjs';
 import { IDocumentProvider, IDocumentProviderFactory } from './tokens';
+
+/**
+ * Room Id endpoint provided by `jupyter-server-ydoc`
+ * See https://github.com/jupyter-server/jupyter_server_ydoc
+ */
+const FILE_PATH_TO_ROOM_ID_URL = 'api/yjs/roomid';
 
 /**
  * A class to provide Yjs synchronization over WebSocket.
@@ -17,116 +25,100 @@ import { IDocumentProvider, IDocumentProviderFactory } from './tokens';
  * We specify custom messages that the server can interpret. For reference please look in yjs_ws_server.
  *
  */
-export class WebSocketProvider
-  extends YWebsocketProvider
-  implements IDocumentProvider {
+export class WebSocketProvider implements IDocumentProvider {
   /**
    * Construct a new WebSocketProvider
    *
    * @param options The instantiation options for a WebSocketProvider
    */
   constructor(options: WebSocketProvider.IOptions) {
-    super(
-      options.url,
-      options.format + ':' + options.contentType + ':' + options.path,
-      options.model.ydoc,
-      {
-        awareness: options.model.awareness
-      }
-    );
+    this._isDisposed = false;
     this._path = options.path;
     this._contentType = options.contentType;
     this._format = options.format;
     this._serverUrl = options.url;
+    this._ydoc = options.model.ydoc;
+    this._awareness = options.model.awareness;
 
-    // Message handler that receives the rename acknowledge
-    this.messageHandlers[127] = (
-      encoder,
-      decoder,
-      provider,
-      emitSynced,
-      messageType
-    ) => {
-      this._renameAck.resolve(
-        decoding.readTailAsUint8Array(decoder)[0] ? true : false
-      );
-    };
-
-    const awareness = options.model.awareness;
     const user = options.user;
-    const userChanged = () => {
-      awareness.setLocalStateField('user', user.identity);
+
+    user.ready
+      .then(() => {
+        this._onUserChanged(user);
+      })
+      .catch(e => console.error(e));
+    user.userChanged.connect(this._onUserChanged, this);
+
+    const serverSettings = ServerConnection.makeSettings();
+    const url = URLExt.join(
+      serverSettings.baseUrl,
+      FILE_PATH_TO_ROOM_ID_URL,
+      encodeURIComponent(this._path)
+    );
+    const data = {
+      method: 'PUT',
+      body: JSON.stringify({ format: this._format, type: this._contentType })
     };
-    if (user.isReady) {
-      userChanged();
-    }
-    user.ready.then(userChanged).catch(e => console.error(e));
-    user.userChanged.connect(userChanged);
-  }
-
-  get renameAck(): Promise<boolean> {
-    return this._renameAck.promise;
-  }
-
-  setPath(newPath: string): void {
-    if (newPath !== this._path) {
-      this._path = newPath;
-      const encoder = encoding.createEncoder();
-      this._renameAck = new PromiseDelegate<boolean>();
-      encoding.write(encoder, 127);
-      // writing a utf8 string to the encoder
-      const escapedPath = unescape(
-        encodeURIComponent(
-          this._format + ':' + this._contentType + ':' + newPath
-        )
-      );
-      for (let i = 0; i < escapedPath.length; i++) {
-        encoding.write(
-          encoder,
-          /** @type {number} */ escapedPath.codePointAt(i)!
+    ServerConnection.makeRequest(url, data, serverSettings)
+      .then(response => {
+        if (response.status !== 200 && response.status !== 201) {
+          throw new ServerConnection.ResponseError(response);
+        }
+        return response.text();
+      })
+      .then(roomid => {
+        this._yWebsocketProvider = new YWebsocketProvider(
+          this._serverUrl,
+          roomid,
+          this._ydoc,
+          {
+            awareness: this._awareness
+          }
         );
-      }
-      this._sendMessage(encoding.toUint8Array(encoder));
-      // prevent publishing messages to the old channel id.
-      this.disconnectBc();
-      // The next time the provider connects, we should connect through a different server url
-      this.bcChannel =
-        this._serverUrl +
-        '/' +
-        this._format +
-        ':' +
-        this._contentType +
-        ':' +
-        this._path;
-      this.url = this.bcChannel;
-      this.connectBc();
-    }
+      })
+      .then(() => this._ready.resolve())
+      .catch(reason => console.warn(reason));
   }
 
   /**
-   * Send a new message to WebSocket server.
-   *
-   * @param message The message to send
+   * Test whether the object has been disposed.
    */
-  private _sendMessage(message: Uint8Array): void {
-    // send once connected
-    const send = () => {
-      setTimeout(() => {
-        if (this.wsconnected) {
-          this.ws!.send(message);
-        } else {
-          this.once('status', send);
-        }
-      }, 0);
-    };
-    send();
+  get isDisposed(): boolean {
+    return this._isDisposed;
   }
 
-  private _path: string;
+  /**
+   * A promise that resolves when the document provider is ready.
+   */
+  get ready(): Promise<void> {
+    return this._ready.promise;
+  }
+
+  /**
+   * Dispose of the resources held by the object.
+   */
+  dispose(): void {
+    if (this.isDisposed) {
+      return;
+    }
+    this._isDisposed = true;
+    this._yWebsocketProvider.destroy();
+    Signal.clearData(this);
+  }
+
+  private _onUserChanged(user: UserManager.IManager): void {
+    this._awareness.setLocalStateField('user', user.identity);
+  }
+
+  private _awareness: Awareness;
   private _contentType: string;
   private _format: string;
+  private _isDisposed: boolean;
+  private _path: string;
+  private _ready = new PromiseDelegate<void>();
   private _serverUrl: string;
-  private _renameAck: PromiseDelegate<boolean>;
+  private _ydoc: Doc;
+  private _yWebsocketProvider: YWebsocketProvider;
 }
 
 /**
