01ca9496073520a28ad799cda99a13c9d55258ad
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-12-02 16:10:16 +0000

    Remove terser options

--- dev_mode/webpack.prod.minimize.config.js
+++ dev_mode/webpack.prod.minimize.config.js
@@ -21,7 +21,6 @@ config[0] = merge(config[0], {
     minimizer: [
       new TerserPlugin({
         parallel: true,
-        sourceMap: true,
         terserOptions: {
           compress: false,
           ecma: 6,
@@ -31,8 +30,7 @@ config[0] = merge(config[0], {
             comments: false
           },
           safari10: true
-        },
-        cache: process.platform !== 'win32'
+        }
       })
     ]
   },
