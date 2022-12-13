3db71f785ad5e7149d8c11d205787603bd02b39c
Frédéric Collonval <fcollonval@gmail.com>
2022-12-13 15:50:12 +0100

    Fix latest linter errors

--- binder/jupyter_notebook_config.py
+++ binder/jupyter_notebook_config.py
@@ -1,9 +1,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 import logging
-from typing import Any
-
-c: Any
 
 common = [
     "--no-browser",
