3da21f3ea641e0caaf9b641023cb5a7053e5e24c
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-01-14 18:17:51 +0100

    Backport PR #13741 on branch 3.5.x (Drop the dependency on @jupyterlab/buildutils in @jupyterlab/builder) (#13753)
    * Backport PR #13741: Drop the dependency on `@jupyterlab/buildutils` in `@jupyterlab/builder`
    
    * Fix python dist checks
    
    Co-authored-by: Jeremy Tuloup <jeremy.tuloup@gmail.com>

--- builder/src/extensionConfig.ts
+++ builder/src/extensionConfig.ts
@@ -9,7 +9,6 @@ import { merge } from 'webpack-merge';
 import * as fs from 'fs-extra';
 import * as glob from 'glob';
 import Ajv from 'ajv';
-import { readJSONFile, writeJSONFile } from '@jupyterlab/buildutils';
 
 const baseConfig = require('./webpack.config.base');
 const { ModuleFederationPlugin } = webpack.container;
@@ -189,7 +188,7 @@ function generateConfig({
         }
 
         // Find the remoteEntry file and add it to the package.json metadata
-        const data = readJSONFile(path.join(outputPath, 'package.json'));
+        const data = fs.readJSONSync(path.join(outputPath, 'package.json'));
         const _build: any = {
           load: path.join('static', newEntry)
         };
@@ -203,7 +202,9 @@ function generateConfig({
           _build.style = './style';
         }
         data.jupyterlab._build = _build;
-        writeJSONFile(path.join(outputPath, 'package.json'), data);
+        fs.writeJSONSync(path.join(outputPath, 'package.json'), data, {
+          spaces: 2
+        });
       });
     }
   }
