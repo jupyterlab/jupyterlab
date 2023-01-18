1a19a92c0a8513716953ab64bcfa4c6e32083664
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-01-18 13:38:07 +0000

    Remove one more deprecated

--- galata/src/global.ts
+++ galata/src/global.ts
@@ -10,13 +10,6 @@ import { IGalataInpage } from './inpage/tokens';
 declare global {
   interface Window {
     /**
-     * Access JupyterLab Application object
-     *
-     * @deprecated since v4
-     * Please use window.jupyterapp to access the Jupyter Application
-     */
-    jupyterlab?: JupyterFrontEnd;
-    /**
      * Access Jupyter Application object
      */
     jupyterapp: JupyterFrontEnd;
