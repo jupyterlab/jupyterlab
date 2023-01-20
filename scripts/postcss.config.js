96b54e22841ea60e4628898e5ff1ea6a901d0c0b
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-01-20 17:42:02 +0100

    Automatic application of license header

--- scripts/postcss.config.js
+++ scripts/postcss.config.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Jupyter Development Team.
+ * Distributed under the terms of the Modified BSD License.
+ */
+
 // for the integrity2 CI in scripts/ci_script.sh
 module.exports = {
   // use a postcss plugin that already exists in the top-level project
