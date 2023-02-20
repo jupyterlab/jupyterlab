fc38aaefe9bb2f474670609dd1ed01db33abc7ec
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-02-20 11:27:02 +0000

    Display `B` instead of `Bytes` for consistency

--- packages/filebrowser/src/listing.ts
+++ packages/filebrowser/src/listing.ts
@@ -2533,10 +2533,10 @@ namespace Private {
   ): string {
     // https://www.codexworld.com/how-to/convert-file-size-bytes-kb-mb-gb-javascript/
     if (bytes === 0) {
-      return '0 Bytes';
+      return '0 B';
     }
     const dm = decimalPoint || 2;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     if (i >= 0 && i < sizes.length) {
       return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
