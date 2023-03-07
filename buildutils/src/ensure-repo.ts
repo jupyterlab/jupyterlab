328fd960bf59c49d0039b7375bb6f7ae01466374
Frédéric Collonval <fcollonval@gmail.com>
2023-03-07 10:12:11 +0100

    Add dependency to @jupyterlab/nbformat

--- buildutils/src/ensure-repo.ts
+++ buildutils/src/ensure-repo.ts
@@ -89,7 +89,7 @@ const UNUSED: Dict<string[]> = {
   '@jupyterlab/coreutils': ['path-browserify'],
   '@jupyterlab/galata': ['node-fetch', 'http-server'],
   '@jupyterlab/services': ['node-fetch', 'ws'],
-  '@jupyterlab/shared-models': ['@jupyter/ydoc'],
+  '@jupyterlab/shared-models': ['@jupyter/ydoc', '@jupyterlab/nbformat'],
   '@jupyterlab/rendermime': ['@jupyterlab/mathjax2'],
   '@jupyterlab/testutils': [
     'node-fetch',
