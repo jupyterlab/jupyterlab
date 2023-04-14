ff98c74a577ad238a143264771a06326d6dd93a5
j264415 <j264415@protonmail.com>
2023-04-14 16:17:29 +0000

    added aria labels for side bar tabs

--- packages/launcher/src/widget.tsx
+++ packages/launcher/src/widget.tsx
@@ -175,6 +175,7 @@ export class Launcher extends VDomRenderer<ILauncher.IModel> {
                 iconClass={classes(iconClass, 'jp-Icon-cover')}
                 stylesheet="launcherSection"
                 title={cat + ' Icon'}
+                aria-hidden={true}
               />
               <h2 className="jp-Launcher-sectionTitle" title={cat + ' Title'}>
                 {cat}
