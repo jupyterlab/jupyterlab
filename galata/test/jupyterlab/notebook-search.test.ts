2b5db6004fd661e07216692c949b53bbe007b804
krassowski <5832902+krassowski@users.noreply.github.com>
2023-04-23 19:18:32 +0100

    Update expectations, split `Search from cursor` out, test cycling

--- galata/test/jupyterlab/notebook-search.test.ts
+++ galata/test/jupyterlab/notebook-search.test.ts
@@ -262,8 +262,11 @@ test.describe('Notebook Search', () => {
     // Expect updated text
     await page.waitForSelector('text=Search in 2 Selected Cells');
 
-    // Expect 15 matches
-    await page.waitForSelector('text=1/15');
+    // Expect 15 matches; this is 6/15, not 1/15 because current match is set
+    // in second cell and when selection is extended, it does not move; keeping
+    // the current match whhn extending the selection is desired as user may use
+    // it as a reference, especially when it was set as closest to the cursor.
+    await page.waitForSelector('text=6/15');
 
     const nbPanel = await page.notebook.getNotebookInPanel();
     expect(await nbPanel.screenshot()).toMatchSnapshot(
@@ -390,19 +393,37 @@ test.describe('Notebook Search', () => {
 
     await page.waitForSelector('text=1/21');
 
-    const cell = await page.notebook.getCell(5);
-    await cell.click();
-    await page.keyboard.press('Escape');
-    await cell.scrollIntoViewIfNeeded();
-
     // Click previous button
     await page.click('button[title="Previous Match"]');
+    // Should cycle back
+    await page.waitForSelector('text=21/21');
+
+    // Click previous button twice
+    await page.click('button[title="Previous Match"]');
+    await page.click('button[title="Previous Match"]');
+    // Should move up by two
     await page.waitForSelector('text=19/21');
 
     const hit = await page.notebook.getCell(2);
     expect(await hit.screenshot()).toMatchSnapshot(
       'highlight-previous-element.png'
     );
+  });
+
+  test('Search from cursor', async ({ page }) => {
+    const cell = await page.notebook.getCell(5);
+    await cell.click();
+    await page.keyboard.press('Escape');
+    await cell.scrollIntoViewIfNeeded();
+
+    // Open search box
+    await page.keyboard.press('Control+f');
+    await page.fill('[placeholder="Find"]', 'with');
+    await page.waitForSelector('text=20/21');
+
+    // Click previous button
+    await page.click('button[title="Previous Match"]');
+    await page.waitForSelector('text=19/21');
   });
 
   test('Highlight on markdown rendered state change', async ({ page }) => {
