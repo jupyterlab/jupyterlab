f3e34ca7a9838c27810183d0f64cd2102adade95
Frédéric Collonval <fcollonval@gmail.com>
2023-04-25 15:53:24 +0200

    Add test for filtered values

--- packages/settingeditor/test/settingsformeditor.spec.tsx
+++ packages/settingeditor/test/settingsformeditor.spec.tsx
@@ -10,6 +10,7 @@ import {
   SettingRegistry,
   Settings
 } from '@jupyterlab/settingregistry';
+import { FormComponent } from '@jupyterlab/ui-components';
 
 class TestConnector extends StateDB {
   schemas: { [key: string]: ISettingRegistry.ISchema } = {};
@@ -79,6 +80,44 @@ describe('@jupyterlab/settingeditor', () => {
       );
 
       expect(component).not.toBeNull();
+    });
+
+    it('should render a subset of the schema', async () => {
+      // If
+      const id = 'alpha';
+      const schema: ISettingRegistry.ISchema = {
+        type: 'object',
+        properties: {
+          keyA: { type: 'string', default: 'A' },
+          keyB: { type: 'string', default: 'B' },
+          keyC: { title: 'Third key', type: 'string', default: 'C' },
+          keyD: { type: 'string', default: 'D' }
+        }
+      };
+      connector.schemas[id] = schema;
+      settings = (await registry.load(id)) as Settings;
+
+      // When
+      const component = renderer.create(
+        <SettingsFormEditor
+          // Filter by field and by title
+          filteredValues={['keyA', 'Third key']}
+          hasError={() => void 0}
+          onSelect={() => void 0}
+          renderers={{}}
+          settings={settings}
+          translator={nullTranslator}
+          updateDirtyState={() => void 0}
+        ></SettingsFormEditor>
+      );
+
+      // Then
+      const instance = component.root;
+      const form = instance.findByType(FormComponent);
+      expect(form.props.formData).toEqual({ keyA: 'A', keyC: 'C' });
+      expect(Object.keys(form.props.schema.properties)).toEqual(
+        Object.keys(form.props.formData)
+      );
     });
   });
 });
