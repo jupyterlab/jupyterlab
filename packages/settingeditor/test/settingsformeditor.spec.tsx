3d1828e10cacb7fa39269d1f7b57f37ee04da89a
Frédéric Collonval <fcollonval@gmail.com>
2023-04-25 15:11:48 +0200

    Set up test for SettingsFormEditor

---
+++ packages/settingeditor/test/settingsformeditor.spec.tsx
@@ -0,0 +1,84 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+import { nullTranslator } from '@jupyterlab/translation';
+import { SettingsFormEditor } from '../src/SettingsFormEditor';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { StateDB } from '@jupyterlab/statedb';
+import {
+  ISettingRegistry,
+  SettingRegistry,
+  Settings
+} from '@jupyterlab/settingregistry';
+
+class TestConnector extends StateDB {
+  schemas: { [key: string]: ISettingRegistry.ISchema } = {};
+
+  async fetch(id: string): Promise<ISettingRegistry.IPlugin | undefined> {
+    const fetched = await super.fetch(id);
+    if (!fetched && !this.schemas[id]) {
+      return undefined;
+    }
+
+    const schema: ISettingRegistry.ISchema = this.schemas[id] || {
+      type: 'object'
+    };
+    const composite = {};
+    const user = {};
+    const raw = (fetched as string) || '{ }';
+    const version = 'test';
+    return { id, data: { composite, user }, raw, schema, version };
+  }
+
+  async list(): Promise<any> {
+    return Promise.reject('list method not implemented');
+  }
+}
+
+describe('@jupyterlab/settingeditor', () => {
+  describe('SettingFormEditor', () => {
+    let connector: TestConnector;
+    let registry: SettingRegistry;
+    let settings: Settings | null;
+
+    beforeAll(() => {
+      connector = new TestConnector();
+    });
+
+    beforeEach(() => {
+      registry = new SettingRegistry({ connector });
+    });
+
+    afterEach(async () => {
+      if (settings) {
+        settings.dispose();
+        settings = null;
+      }
+      connector.schemas = {};
+      await connector.clear();
+    });
+
+    it('should render a dummy schema', async () => {
+      const id = 'alpha';
+      const schema: ISettingRegistry.ISchema = { type: 'object' };
+
+      connector.schemas[id] = schema;
+
+      settings = (await registry.load(id)) as Settings;
+
+      const component = renderer.create(
+        <SettingsFormEditor
+          filteredValues={null}
+          hasError={() => void 0}
+          onSelect={() => void 0}
+          renderers={{}}
+          settings={settings}
+          translator={nullTranslator}
+          updateDirtyState={() => void 0}
+        ></SettingsFormEditor>
+      );
+
+      expect(component).not.toBeNull();
+    });
+  });
+});
