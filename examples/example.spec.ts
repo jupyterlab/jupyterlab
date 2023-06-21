a71236f834d794ac5d012e94a05db252d4f536ae
Frédéric Collonval <fcollonval@gmail.com>
2023-06-21 17:19:18 +0200

    Add snapshot comparison on examples

--- examples/example.spec.ts
+++ examples/example.spec.ts
@@ -22,7 +22,15 @@ test('should load the example', async ({ page }) => {
     const text = msg.text();
     console.log(msg.type(), '>>', text);
 
-    if (msg.type() === 'error') {
+    if (
+      msg.type() === 'error' &&
+      // Ignore 404 for MathJax fonts
+      msg
+        .location()
+        .url.match(
+          /js\/output\/chtml\/fonts\/tex-woff-v2\/MathJax_(Zero|Math-Italic|Main-Regular)\.woff$/
+        ) === null
+    ) {
       errorLogs += 1;
     }
 
@@ -39,11 +47,23 @@ test('should load the example', async ({ page }) => {
   // Wait for the local file to redirect on notebook >= 6.0. Refs:
   // https://jupyter-notebook.readthedocs.io/en/stable/changelog.html?highlight=redirect
   // https://stackoverflow.com/q/46948489/425458
-  await page.waitForNavigation();
+  await page.waitForURL('http://**');
 
-  await expect(page.locator('#jupyter-config-data')).toHaveCount(1);
+  await expect.soft(page.locator('#jupyter-config-data')).toHaveCount(1);
 
   await waitForTestEnd;
 
-  expect(errorLogs).toEqual(0); // Missing lsp handlers
+  if (process.env['TEST_SNAPSHOT'] === '1') {
+    await page.waitForLoadState('networkidle');
+    await page.waitForTimeout(1000);
+    expect
+      .soft(
+        await page.screenshot({
+          mask: [page.locator('.jp-DirListing-itemModified')]
+        })
+      )
+      .toMatchSnapshot('example.png');
+  }
+
+  expect(errorLogs).toEqual(0);
 });
