1c2ea516c5ac60439352429a9f662c55fc1b524c
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-07-02 12:11:17 -0500

    verify vega notebook is updated with png

--- galata/test/jupyterlab/notebook-run-vega.test.ts
+++ galata/test/jupyterlab/notebook-run-vega.test.ts
@@ -1,12 +1,28 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { expect, galata, test } from '@jupyterlab/galata';
+import {
+  expect,
+  galata,
+  IJupyterLabPageFixture,
+  test
+} from '@jupyterlab/galata';
+import * as fs from 'fs';
 import * as path from 'path';
 
 const fileName = 'vega_notebook.ipynb';
 
+const PNG_MIME_TYPE = 'image/png';
+
 test.use({ tmpPath: 'notebook-run-vega-test' });
+
+async function nbDiskContent(
+  page: IJupyterLabPageFixture,
+  nbPath: string
+): Promise<string> {
+  await page.notebook.save();
+  return fs.readFileSync(nbPath).toString('utf8');
+}
 
 test.describe.serial('Notebook Run Vega', () => {
   test.beforeAll(async ({ request, tmpPath }) => {
@@ -30,8 +46,11 @@ test.describe.serial('Notebook Run Vega', () => {
     page,
     tmpPath
   }) => {
-    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    const nbPath = `${tmpPath}/${fileName}`;
+    await page.notebook.openByPath(nbPath);
     await page.notebook.activate(fileName);
+
+    expect(await nbDiskContent(page, nbPath)).not.toContain(PNG_MIME_TYPE);
 
     const imageName = 'run-cells-vega.png';
 
@@ -41,15 +60,19 @@ test.describe.serial('Notebook Run Vega', () => {
     const nbPanel = await page.notebook.getNotebookInPanel();
 
     expect(await nbPanel.screenshot()).toMatchSnapshot(imageName);
+    expect(await nbDiskContent(page, nbPath)).toContain(PNG_MIME_TYPE);
   });
 
   test('Run notebook with Vega cell in dark theme', async ({
     page,
     tmpPath
   }) => {
-    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    const nbPath = `${tmpPath}/${fileName}`;
+    await page.notebook.openByPath(nbPath);
     await page.notebook.activate(fileName);
     await page.theme.setDarkTheme();
+
+    expect(await nbDiskContent(page, nbPath)).not.toContain(PNG_MIME_TYPE);
 
     const imageName = 'run-cells-dark-vega.png';
 
@@ -59,5 +82,6 @@ test.describe.serial('Notebook Run Vega', () => {
     const nbPanel = await page.notebook.getNotebookInPanel();
 
     expect(await nbPanel.screenshot()).toMatchSnapshot(imageName);
+    expect(await nbDiskContent(page, nbPath)).toContain(PNG_MIME_TYPE);
   });
 });
