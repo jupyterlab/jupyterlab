b79dab2de5e91f05d069e7abd1c9ed97434da61c
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2023-07-18 04:58:02 -0700

    Backport PR #14785: Emit changed signal from output area when an output is updated (#14835)
    Co-authored-by: Nicholas Bollweg <nick.bollweg@gmail.com>

--- galata/test/jupyterlab/notebook-run-vega.test.ts
+++ galata/test/jupyterlab/notebook-run-vega.test.ts
@@ -1,37 +1,52 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { expect, galata, test } from '@jupyterlab/galata';
+import {
+  expect,
+  galata,
+  IJupyterLabPageFixture,
+  test
+} from '@jupyterlab/galata';
 import * as path from 'path';
 
 const fileName = 'vega_notebook.ipynb';
 
-test.use({ tmpPath: 'notebook-run-vega-test' });
+const PNG_MIME_TYPE = 'image/png';
 
-test.describe.serial('Notebook Run Vega', () => {
-  test.beforeAll(async ({ request, tmpPath }) => {
+async function nbDiskContent(
+  page: IJupyterLabPageFixture,
+  nbPath: string
+): Promise<string> {
+  await page.notebook.save();
+  // Use the `files` API as figure out the local path is though.
+  const response = await page.request.fetch(`/files/${nbPath}`);
+  if (!response.ok()) {
+    return '';
+  }
+  const buffer = await response.body();
+  return buffer.toString();
+}
+
+test.describe('Notebook Run Vega', () => {
+  test.beforeEach(async ({ page, request, tmpPath }) => {
     const contents = galata.newContentsHelper(request);
     await contents.uploadFile(
       path.resolve(__dirname, `./notebooks/${fileName}`),
       `${tmpPath}/${fileName}`
     );
-  });
 
-  test.beforeEach(async ({ page, tmpPath }) => {
     await page.filebrowser.openDirectory(tmpPath);
-  });
-
-  test.afterAll(async ({ request, tmpPath }) => {
-    const contents = galata.newContentsHelper(request);
-    await contents.deleteDirectory(tmpPath);
   });
 
   test('Run notebook with Vega cell in default theme', async ({
     page,
     tmpPath
   }) => {
-    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    const nbPath = `${tmpPath}/${fileName}`;
+    await page.notebook.openByPath(nbPath);
     await page.notebook.activate(fileName);
+
+    expect(await nbDiskContent(page, nbPath)).not.toContain(PNG_MIME_TYPE);
 
     const imageName = 'run-cells-vega.png';
 
@@ -41,15 +56,19 @@ test.describe.serial('Notebook Run Vega', () => {
     const nbPanel = await page.notebook.getNotebookInPanel();
 
     expect(await nbPanel.screenshot()).toMatchSnapshot(imageName);
+    expect(await nbDiskContent(page, nbPath)).toContain(PNG_MIME_TYPE);
   });
 
   test('Run notebook with Vega cell in dark theme', async ({
     page,
     tmpPath
   }) => {
-    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    const nbPath = `${tmpPath}/${fileName}`;
+    await page.notebook.openByPath(nbPath);
     await page.notebook.activate(fileName);
     await page.theme.setDarkTheme();
+
+    expect(await nbDiskContent(page, nbPath)).not.toContain(PNG_MIME_TYPE);
 
     const imageName = 'run-cells-dark-vega.png';
 
@@ -59,5 +78,6 @@ test.describe.serial('Notebook Run Vega', () => {
     const nbPanel = await page.notebook.getNotebookInPanel();
 
     expect(await nbPanel.screenshot()).toMatchSnapshot(imageName);
+    expect(await nbDiskContent(page, nbPath)).toContain(PNG_MIME_TYPE);
   });
 });
