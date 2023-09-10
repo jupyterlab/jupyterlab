65da8c460253281bef6c05e0b15a690ff6359a69
krassowski <5832902+krassowski@users.noreply.github.com>
2023-09-10 17:25:39 +0100

    Remove `unusedDocuments`, fix culling of foreign documents

--- packages/lsp/src/virtual/document.ts
+++ packages/lsp/src/virtual/document.ts
@@ -241,7 +241,6 @@ export class VirtualDocument implements IDisposable {
     );
     this._remainingLifetime = 6;
 
-    this.unusedDocuments = new Set();
     this.documentInfo = new VirtualDocumentInfo(this);
     this.updateManager = new UpdateManager(this);
     this.updateManager.updateBegan.connect(this._updateBeganSlot, this);
@@ -446,7 +445,6 @@ export class VirtualDocument implements IDisposable {
 
     this.foreignDocuments.clear();
     this.sourceLines.clear();
-    this.unusedDocuments.clear();
     this.unusedStandaloneDocuments.clear();
     this.virtualLines.clear();
 
@@ -470,7 +468,6 @@ export class VirtualDocument implements IDisposable {
     // TODO - deep clear (assure that there is no memory leak)
     this.unusedStandaloneDocuments.clear();
 
-    this.unusedDocuments = new Set();
     this.virtualLines.clear();
     this.sourceLines.clear();
     this.lastVirtualLine = 0;
@@ -806,10 +803,36 @@ export class VirtualDocument implements IDisposable {
    * Close all expired documents.
    */
   closeExpiredDocuments(): void {
-    for (let document of this.unusedDocuments.values()) {
+    const usedDocuments = new Set<VirtualDocument>();
+    for (const line of this.sourceLines.values()) {
+      for (const block of line.foreignDocumentsMap.values()) {
+        usedDocuments.add(block.virtualDocument as VirtualDocument);
+      }
+    }
+
+    const documentIDs = new Map<VirtualDocument, string[]>();
+    for (const [id, document] of (
+      this.foreignDocuments as Map<string, VirtualDocument>
+    ).entries()) {
+      const ids = documentIDs.get(document);
+      if (typeof ids !== 'undefined') {
+        documentIDs.set(document, [...ids, id]);
+      }
+      documentIDs.set(document, [id]);
+    }
+    const allDocuments = new Set<VirtualDocument>(documentIDs.keys());
+    const unusedVirtualDocuments = new Set(
+      [...allDocuments].filter(x => !usedDocuments.has(x))
+    );
+
+    for (let document of unusedVirtualDocuments.values()) {
       document.remainingLifetime -= 1;
       if (document.remainingLifetime <= 0) {
         document.dispose();
+        const ids = documentIDs.get(document)!;
+        for (const id of ids) {
+          this.foreignDocuments.delete(id);
+        }
       }
     }
   }
@@ -927,7 +950,6 @@ export class VirtualDocument implements IDisposable {
   protected sourceLines: Map<number, ISourceLine>;
   protected lineBlocks: Array<string>;
 
-  protected unusedDocuments: Set<VirtualDocument>;
   protected unusedStandaloneDocuments: DefaultMap<
     language,
     Array<VirtualDocument>
