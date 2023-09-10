ea0ad99a8022b897d3b3258dd2e983dde40e3821
krassowski <5832902+krassowski@users.noreply.github.com>
2023-09-10 18:47:53 +0100

    Remove not needed test config

--- packages/pluginmanager-extension/jest.config.js
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) Jupyter Development Team.
- * Distributed under the terms of the Modified BSD License.
- */
-
-const func = require('@jupyterlab/testing/lib/jest-config');
-module.exports = func(__dirname);
