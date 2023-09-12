854dfb992c23f548d5650bb6680f0cdbcd233ac1
Afshin T. Darian <git@darian.email>
2023-09-12 15:49:11 +0100

    Get renderer superclass for free

--- packages/notebook/src/widget.ts
+++ packages/notebook/src/widget.ts
@@ -1082,7 +1082,7 @@ export namespace StaticNotebook {
   /**
    * The renderer class for static notebooks.
    */
-  class Renderer implements WindowedList.IRenderer {
+  class Renderer extends WindowedList.Renderer {
     createOuter() {
       return document.createElement('div');
     }
