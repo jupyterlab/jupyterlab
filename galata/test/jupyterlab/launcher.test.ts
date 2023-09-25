4617138bfa6537794697ae42b59a2ced3b1a62ed
j264415 <j264415@protonmail.com>
2023-09-25 13:06:56 +0000

    added snapshot capability

--- galata/test/jupyterlab/launcher.test.ts
+++ galata/test/jupyterlab/launcher.test.ts
@@ -5,32 +5,16 @@ import { expect, test } from '@jupyterlab/galata';
 
 test.describe('Dynamic Text Spacing', () => {
   test('should Use Dynamic Text Spacing', async ({ page }) => {
-    await page.waitForSelector('.jp-LauncherCard');
+    await page.goto();
+    await page.waitForSelector('.jp-LauncherCard-label');
 
-    let element = page.locator('div.jp-LauncherCard');
+    let element = page.locator('div >> .jp-LauncherCard-label');
     let height = await element.evaluate(el => {
       return window.getComputedStyle(el).getPropertyValue('min-height');
     });
-    await expect(height).toBe('var(--jp-private-launcher-card-size)');
-  });
+    await expect(height).toBe('var(--jp-private-launcher-card-label-height)');
 
-  test('should Use Dynamic Text Spacing', async ({ page }) => {
-    await page.waitForSelector('.jp-LauncherCard-label ');
-
-    let element = page.locator('div.jp-LauncherCard-label');
-    let height = await element.evaluate(el => {
-      return window.getComputedStyle(el).getPropertyValue('min-height');
-    });
-    await expect(height).toBe('var(--jp-private-launcher-card-size)');
-  });
-
-  test('should Use Dynamic Text Spacing', async ({ page }) => {
-    await page.waitForSelector('.jp-LauncherCard-label p');
-
-    let element = page.locator('div.jp-LauncherCard-label p');
-    let height = await element.evaluate(el => {
-      return window.getComputedStyle(el).getPropertyValue('min-height');
-    });
-    await expect(height).toBe('var(--jp-private-launcher-card-size)');
+    const imageName = 'launcher-card-label-height.png';
+    expect(await page.screenshot()).toMatchSnapshot(imageName.toLowerCase());
   });
 });
