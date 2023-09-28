43dec56f60f23426d2c37c68f2fb8631da4ca455
j264415 <j264415@protonmail.com>
2023-09-28 14:39:11 +0000

    used parseFloat

--- galata/test/jupyterlab/launcher.test.ts
+++ galata/test/jupyterlab/launcher.test.ts
@@ -1,16 +1,28 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { expect, test } from '@jupyterlab/galata';
+import { expect, galata, test } from '@jupyterlab/galata';
+
+test.use({
+  autoGoto: false,
+  mockState: galata.DEFAULT_DOCUMENTATION_STATE,
+  viewport: { height: 720, width: 1280 }
+});
 
 test.describe('Dynamic Text Spacing', () => {
   test('should Use Dynamic Text Spacing', async ({ page }) => {
     await page.goto();
     await page.waitForSelector('.jp-LauncherCard-label');
 
-    const element = page.locator('div.jp-LauncherCard-label');
-    await expect(element).toHaveCSS('min-height', '2.462em');
-
+    let element = page.locator('div.jp-LauncherCard-label');
+    for (let i = 0; i < (await element.count()); i++) {
+      let height = await element
+        .nth(i)
+        .evaluate(el =>
+          parseFloat(window.getComputedStyle(el).getPropertyValue('min-height'))
+        );
+      await expect(height).toContain('2.462em');
+    }
     const imageName = 'launcher-card-label-height.png';
     expect(await page.screenshot()).toMatchSnapshot(imageName.toLowerCase());
   });
