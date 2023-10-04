bf57e7a721350b400621c64766b99de077051e21
Frédéric Collonval <fcollonval@users.noreply.github.com>
2023-10-04 14:43:05 +0200

    Revert one change

--- packages/codemirror/src/language.ts
+++ packages/codemirror/src/language.ts
@@ -27,7 +27,7 @@ export class EditorLanguageRegistry implements IEditorLanguageRegistry {
     // Add default language text/plain -> No expressions to parse
     this.addLanguage({
       name: 'none',
-      mime: IEditorMimeTypeService.defaultMimeType,
+      mime: 'text/plain',
       support: new LanguageSupport(
         // Create a dummy parser that as no expression to parse
         LRLanguage.define({ parser: buildParser('@top Program { }') })
