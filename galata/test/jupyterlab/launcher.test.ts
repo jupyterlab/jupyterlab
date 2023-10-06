c83bd4c8e0d13ac0aa6d8df935455beab7e1c3eb
j264415 <j264415@protonmail.com>
2023-10-06 07:55:01 +0000

    changes made to add px

--- galata/test/jupyterlab/launcher.test.ts
+++ galata/test/jupyterlab/launcher.test.ts
@@ -21,21 +21,21 @@ test.describe('Dynamic Text Spacing', () => {
         .evaluate(el =>
           window.getComputedStyle(el).getPropertyValue('min-height')
         );
-      expect(height).toEqual(
+
+      let expectedValue =
         2.462 *
-          parseFloat(
-            await element
-              .nth(i)
-              .evaluate(
-                el =>
-                  window
-                    .getComputedStyle(el)
-                    .getPropertyValue('font-size')
-                    .toString(),
-                +'px'
-              )
-          )
-      );
+        parseFloat(
+          await element
+            .nth(i)
+            .evaluate(el =>
+              window
+                .getComputedStyle(el)
+                .getPropertyValue('font-size')
+                .toString()
+            )
+        );
+
+      expect(height).toEqual(expectedValue + 'px');
     }
     // I'd convert the value to a string first then use toEqual
     const imageName = 'launcher-card-label-height.png';
