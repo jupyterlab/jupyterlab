79b406753204e54d660a22eef7725e8d2c3b1571
Nishikant Parmar <nishikantparmar123@gmail.com>
2023-10-07 16:14:36 +0530

    Backport PR #14534: Hide completer when changing notebook tabs

--- packages/completer/src/widget.ts
+++ packages/completer/src/widget.ts
@@ -159,8 +159,8 @@ export class Completer extends Widget {
       case 'keydown':
         this._evtKeydown(event as KeyboardEvent);
         break;
-      case 'mousedown':
-        this._evtMousedown(event as MouseEvent);
+      case 'pointerdown':
+        this._evtPointerdown(event as PointerEvent);
         break;
       case 'scroll':
         this._evtScroll(event as MouseEvent);
@@ -199,7 +199,7 @@ export class Completer extends Widget {
    */
   protected onAfterAttach(msg: Message): void {
     document.addEventListener('keydown', this, USE_CAPTURE);
-    document.addEventListener('mousedown', this, USE_CAPTURE);
+    document.addEventListener('pointerdown', this, USE_CAPTURE);
     document.addEventListener('scroll', this, USE_CAPTURE);
   }
 
@@ -208,7 +208,7 @@ export class Completer extends Widget {
    */
   protected onBeforeDetach(msg: Message): void {
     document.removeEventListener('keydown', this, USE_CAPTURE);
-    document.removeEventListener('mousedown', this, USE_CAPTURE);
+    document.removeEventListener('pointerdown', this, USE_CAPTURE);
     document.removeEventListener('scroll', this, USE_CAPTURE);
   }
 
@@ -488,7 +488,7 @@ export class Completer extends Widget {
   /**
    * Handle mousedown events for the widget.
    */
-  private _evtMousedown(event: MouseEvent) {
+  private _evtPointerdown(event: PointerEvent) {
     if (this.isHidden || !this._editor) {
       return;
     }
