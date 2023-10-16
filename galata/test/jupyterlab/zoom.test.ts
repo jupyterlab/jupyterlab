07053f16ab4e0df25d6f91cad6ed8cdd82b03eec
j264415 <j264415@protonmail.com>
2023-10-16 13:06:57 +0000

    changed file name, added commenta to css style, added launch screen full page at 400%

--- galata/test/jupyterlab/zoom.test.ts
+++ galata/test/jupyterlab/zoom.test.ts
@@ -4,10 +4,17 @@
 import { expect, test } from '@jupyterlab/galata';
 
 test.describe('Low Vision / Zoom Support', () => {
+  // test('Launch Screen at 400% zoom', async ({ page }) => {
+  //   await page.evaluate('document.body.style.zoom = 4.0');
+  //   expect(await page.screenshot()).toMatchSnapshot(
+  //     'launch-screen-at-400-zoom.png'
+  //   );
+  // });
+
   test('Should show visibility of menu bar at 400% zoom', async ({ page }) => {
     await page.evaluate('document.body.style.zoom = 4.0');
     expect(
-      await page.screenshot({ clip: { x: 0, y: 0, width: 1280, height: 100 } })
+      await page.screenshot({ clip: { x: 0, y: 0, width: 1280, height: 110 } })
     ).toMatchSnapshot('visibility-of-menu-bar-at-400-zoom.png');
   });
 });
