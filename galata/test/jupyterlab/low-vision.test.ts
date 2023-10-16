f97fc01ffb12cf76a486dfde2d4b9ed7c4b347f3
j264415 <128609898+j264415@users.noreply.github.com>
2023-10-16 10:53:36 +0000

    Update galata/test/jupyterlab/low-vision.test.ts
    Co-authored-by: gabalafou <gabriel@fouasnon.com>

--- galata/test/jupyterlab/low-vision.test.ts
+++ galata/test/jupyterlab/low-vision.test.ts
@@ -3,14 +3,11 @@
 
 import { expect, galata, test } from '@jupyterlab/galata';
 
-test.describe('Low Vision Support Test', () => {
-  test('Should show visability of menu-bar at 400-zoom', async ({ page }) => {
-    await page.goto();
-    await page.evaluate('document.body.style.zoom=4.0');
-
-    const imageName = 'visability-of-menu-bar-at-400-zoom.png';
+test.describe('Low Vision / Zoom Support', () => {
+  test('Should show visibility of menu bar at 400% zoom', async ({ page }) => {
+    await page.evaluate('document.body.style.zoom = 4.0');
     expect(
       await page.screenshot({ clip: { x: 0, y: 0, width: 1280, height: 100 } })
-    ).toMatchSnapshot(imageName.toLowerCase());
+    ).toMatchSnapshot('visibility-of-menu-bar-at-400-zoom.png');
   });
 });
