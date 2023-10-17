45c545f89aecff47bebc532fd8985761aeabfcb7
j264415 <j264415@protonmail.com>
2023-10-17 10:22:29 +0000

    changed screenshot width

--- galata/test/jupyterlab/zoom.test.ts
+++ galata/test/jupyterlab/zoom.test.ts
@@ -10,15 +10,13 @@ test.describe('Low Vision / Zoom Support', () => {
   //     'launch-screen-at-400-zoom.png'
   //   );
   // });
+
   const { firefox } = require('playwright');
 
   test('Should show visibility of menu bar at 400% zoom', async ({ page }) => {
-    let browser = await firefox.launch();
-    page = await browser.newPage();
-    await page.setViewportSize({ width: 1280, height: 1024 });
     await page.evaluate('document.body.style.zoom = 4.0');
     expect(
-      await page.screenshot({ clip: { x: 0, y: 0, width: 1280, height: 150 } })
+      await page.screenshot({ clip: { x: 0, y: 0, width: 320, height: 150 } })
     ).toMatchSnapshot('visibility-of-menu-bar-at-400-zoom.png');
   });
 });
