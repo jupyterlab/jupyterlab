dca42a04c44fd11d34b70c2f32ca16a4e216171a
j264415 <j264415@protonmail.com>
2023-10-17 13:56:01 +0000

    different height and width

--- galata/test/jupyterlab/zoom.test.ts
+++ galata/test/jupyterlab/zoom.test.ts
@@ -12,7 +12,7 @@ test.describe('Low Vision / Zoom Support', () => {
   // });
 
   test('Should show visibility of menu bar at 400% zoom', async ({ page }) => {
-    await page.setViewportSize({ width: 320, height: 256 });
+    await page.setViewportSize({ width: 1280, height: 1024 });
     await page.evaluate('document.body.style.zoom = 4.0');
     expect(
       await page.screenshot({ clip: { x: 0, y: 0, width: 320, height: 150 } })
