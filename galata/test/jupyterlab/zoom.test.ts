8d89626f12c576f64e363ae91d3baa9b84472bb3
j264415 <j264415@protonmail.com>
2023-10-19 13:40:53 +0000

    getting full page screenshot

--- galata/test/jupyterlab/zoom.test.ts
+++ galata/test/jupyterlab/zoom.test.ts
@@ -6,17 +6,17 @@ import { expect, test } from '@jupyterlab/galata';
 test.use({ viewport: { width: 1280, height: 1024 } });
 
 test.describe('Low Vision / Zoom Support', () => {
-  // test('Launch Screen at 400% zoom', async ({ page }) => {
-  //   await page.evaluate('document.body.style.zoom = 4.0');
-  //   expect(await page.screenshot()).toMatchSnapshot(
-  //     'launch-screen-at-400-zoom.png'
-  //   );
-  // });
+  test('Launch Screen at 400% zoom', async ({ page }) => {
+    await page.evaluate('document.body.style.zoom = 4.0');
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
+      'launch-screen-at-400-zoom.png'
+    );
+  });
 
   test('Should show visibility of menu bar at 400% zoom', async ({ page }) => {
     await page.evaluate('document.body.style.zoom = 4.0');
     expect(
-      await page.screenshot({ clip: { x: 0, y: 0, width: 1700, height: 150 } })
+      await page.screenshot({ clip: { x: 0, y: 0, width: 1280, height: 150 } })
     ).toMatchSnapshot('visibility-of-menu-bar-at-400-zoom.png');
   });
 });
