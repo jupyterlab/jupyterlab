b8495a73c49c93913f7a22014ff1c52170b8a316
j264415 <j264415@protonmail.com>
2023-10-19 09:46:53 +0000

    changed with value

--- galata/test/jupyterlab/zoom.test.ts
+++ galata/test/jupyterlab/zoom.test.ts
@@ -16,7 +16,7 @@ test.describe('Low Vision / Zoom Support', () => {
   test('Should show visibility of menu bar at 400% zoom', async ({ page }) => {
     await page.evaluate('document.body.style.zoom = 4.0');
     expect(
-      await page.screenshot({ clip: { x: 0, y: 0, width: 1300, height: 150 } })
+      await page.screenshot({ clip: { x: 0, y: 0, width: 1700, height: 150 } })
     ).toMatchSnapshot('visibility-of-menu-bar-at-400-zoom.png');
   });
 });
