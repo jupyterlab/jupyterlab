18cb371039b9a0d8d43cc61ce7daee9f5a7317d7
krassowski <5832902+krassowski@users.noreply.github.com>
2023-10-22 19:42:27 +0100

    Add a test case for #14878

--- galata/test/jupyterlab/notebook-scroll.test.ts
+++ galata/test/jupyterlab/notebook-scroll.test.ts
@@ -5,6 +5,7 @@ import { expect, galata, test } from '@jupyterlab/galata';
 import * as path from 'path';
 
 const fileName = 'scroll.ipynb';
+const longOutputsNb = 'long_outputs.ipynb';
 
 test.use({
   mockSettings: {
@@ -16,7 +17,7 @@ test.use({
   }
 });
 
-test.describe('Notebook Scroll', () => {
+test.describe('Notebook scroll on navigation', () => {
   test.beforeEach(async ({ page, tmpPath }) => {
     await page.contents.uploadFile(
       path.resolve(__dirname, `./notebooks/${fileName}`),
@@ -54,4 +55,84 @@ test.describe('Notebook Scroll', () => {
       expect(await lastCell.boundingBox()).toBeTruthy();
     });
   }
+});
+
+test.describe('Notebook scroll on execution', () => {
+  test.beforeEach(async ({ page, tmpPath }) => {
+    await page.contents.uploadFile(
+      path.resolve(__dirname, `./notebooks/${longOutputsNb}`),
+      `${tmpPath}/${longOutputsNb}`
+    );
+
+    await page.notebook.openByPath(`${tmpPath}/${longOutputsNb}`);
+    await page.notebook.activate(longOutputsNb);
+  });
+
+  test('should scroll when advancing if top is only marginally visible', async ({
+    page,
+    tmpPath
+  }) => {
+    await page.notebook.openByPath(`${tmpPath}/${longOutputsNb}`);
+
+    const notebook = await page.notebook.getNotebookInPanel();
+    const thirdCell = await page.notebook.getCell(2);
+
+    const notebookBbox = await notebook.boundingBox();
+    const thirdCellBBox = await thirdCell.boundingBox();
+    await page.mouse.move(notebookBbox.x, notebookBbox.y);
+    const scrollOffset =
+      thirdCellBBox.y -
+      notebookBbox.y -
+      notebookBbox.height +
+      thirdCellBBox.height * 0.025;
+    await Promise.all([page.mouse.wheel(0, scrollOffset)]);
+    // Select second cell
+    await page.notebook.selectCells(1);
+
+    const thirdCellLocator = page.locator(
+      '.jp-Cell[data-windowed-list-index="2"]'
+    );
+    // The third cell should be positioned at the bottom, revealing between 1 to 5% of its content.
+    await expect(thirdCellLocator).toBeInViewport({ ratio: 0.01 });
+    await expect(thirdCellLocator).not.toBeInViewport({ ratio: 0.05 });
+
+    // Run second cell
+    await page.notebook.runCell(1);
+    // After running the second cell, the third cell should be revealed, in at least 25%
+    await expect(thirdCellLocator).toBeInViewport({ ratio: 0.25 });
+  });
+
+  test('should not scroll when advancing if top is non-marginally visible', async ({
+    page,
+    tmpPath
+  }) => {
+    await page.notebook.openByPath(`${tmpPath}/${longOutputsNb}`);
+
+    const notebook = await page.notebook.getNotebookInPanel();
+    const thirdCell = await page.notebook.getCell(2);
+
+    const notebookBbox = await notebook.boundingBox();
+    const thirdCellBBox = await thirdCell.boundingBox();
+    await page.mouse.move(notebookBbox.x, notebookBbox.y);
+    const scrollOffset =
+      thirdCellBBox.y -
+      notebookBbox.y -
+      notebookBbox.height +
+      thirdCellBBox.height * 0.15;
+    await Promise.all([page.mouse.wheel(0, scrollOffset)]);
+    // Select second cell
+    await page.notebook.selectCells(1);
+
+    const thirdCellLocator = page.locator(
+      '.jp-Cell[data-windowed-list-index="2"]'
+    );
+    // The third cell should be positioned at the bottom, revealing between 10 to 20% of its content.
+    await expect(thirdCellLocator).toBeInViewport({ ratio: 0.1 });
+    await expect(thirdCellLocator).not.toBeInViewport({ ratio: 0.2 });
+
+    // Run second cell
+    await page.notebook.runCell(1);
+    // After running the second cell, the third cell should not be scrolled
+    await expect(thirdCellLocator).not.toBeInViewport({ ratio: 0.2 });
+  });
 });
