3ef7748feff16a1c23ea6aa9364badb413b21040
j264415 <j264415@protonmail.com>
2023-10-23 14:09:47 +0000

    testing the tabbar screenshots

--- galata/test/jupyterlab/zoom.test.ts
+++ galata/test/jupyterlab/zoom.test.ts
@@ -1,22 +1,29 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { expect, test } from '@jupyterlab/galata';
+import { expect, galata, test } from '@jupyterlab/galata';
 
-// const sidebarIds: galata.SidebarTabId[] = [
-//   'filebrowser',
-//   'jp-property-inspector',
-//   'jp-running-sessions',
-//   'table-of-contents',
-//   'extensionmanager.main-view'
-// ];
+const sidebarIds: galata.SidebarTabId[] = [
+  'filebrowser',
+  'jp-property-inspector',
+  'jp-running-sessions',
+  'table-of-contents',
+  'extensionmanager.main-view'
+];
 
 test.use({ viewport: { width: 320, height: 256 } });
 
 test.describe('Low Vision / Zoom Support', () => {
+  sidebarIds.forEach(sidebarId => {
+    test(`Open Sidebar tab ${sidebarId}`, async ({ page }) => {
+      await page.sidebar.openTab(sidebarId);
+      expect(await page.sidebar.isTabOpen(sidebarId)).toEqual(true);
+    });
+  });
+
   test('Launch Screen at 400% zoom', async ({ page }) => {
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(
-      'launch-screen-at-400-zoom.png'
+      'launch-screen-at-low-vision.png'
     );
   });
 
@@ -26,6 +33,16 @@ test.describe('Low Vision / Zoom Support', () => {
         fullPage: true,
         clip: { x: 0, y: 0, width: 320, height: 150 }
       })
-    ).toMatchSnapshot('visibility-of-menu-bar-at-400-zoom.png');
+    ).toMatchSnapshot('visibility-of-menu-bar-at-low-vision.png');
+  });
+
+  test('Should show visibility of left light tabBar at low vision', async ({
+    page
+  }) => {
+    await page.theme.setLightTheme();
+    const tabbar = await page.sidebar.getTabBar();
+    expect(await tabbar.screenshot()).toMatchSnapshot(
+      'left-light-tabbar-of-status-bar-at-low-vision.png'
+    );
   });
 });
