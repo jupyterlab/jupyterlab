3ce6ba341a84a544541395423f96e8ce12cb3109
j264415 <j264415@protonmail.com>
2023-11-02 17:04:56 +0000

    changed title to dialog

--- packages/apputils/src/dialog.tsx
+++ packages/apputils/src/dialog.tsx
@@ -875,7 +875,7 @@ export namespace Dialog {
                   iconClass="jp-Icon"
                   className="jp-ToolbarButtonComponent-icon"
                   tag="span"
-                  title={trans.__('Close popup')}
+                  title={trans.__('Close dialog')}
                 />
               </Button>
             )}
