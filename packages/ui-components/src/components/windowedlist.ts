1052b5d6a8c1ea3b7aabc8a534e8019709f8d07e
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2023-11-08 23:58:32 +0100

    Backport PR #15357: Update notebook window on resize if height changes (#15366)
    Co-authored-by: Michał Krassowski <5832902+krassowski@users.noreply.github.com>

--- packages/ui-components/src/components/windowedlist.ts
+++ packages/ui-components/src/components/windowedlist.ts
@@ -16,6 +16,7 @@ import { ArrayExt } from '@lumino/algorithm';
 import { PromiseDelegate } from '@lumino/coreutils';
 import { IDisposable } from '@lumino/disposable';
 import { Message, MessageLoop } from '@lumino/messaging';
+import { Throttler } from '@lumino/polling';
 import { ISignal, Signal } from '@lumino/signaling';
 import { PanelLayout, Widget } from '@lumino/widgets';
 
@@ -688,6 +689,7 @@ export class WindowedList<
     this._scrollToItem = null;
     this._scrollRepaint = null;
     this._scrollUpdateWasRequested = false;
+    this._updater = new Throttler(() => this.update(), 50);
     this._resizeObserver = null;
 
     this._viewModel.stateChanged.connect(this.onStateChanged, this);
@@ -725,6 +727,14 @@ export class WindowedList<
    */
   protected get viewModel(): T {
     return this._viewModel;
+  }
+
+  /**
+   * Dispose the windowed list.
+   */
+  dispose(): void {
+    this._updater.dispose();
+    super.dispose();
   }
 
   /**
@@ -880,8 +890,12 @@ export class WindowedList<
    * A message handler invoked on an `'resize-request'` message.
    */
   protected onResize(msg: Widget.ResizeMessage): void {
+    const previousHeight = this.viewModel.height;
     this.viewModel.height =
       msg.height >= 0 ? msg.height : this.node.getBoundingClientRect().height;
+    if (this.viewModel.height !== previousHeight) {
+      void this._updater.invoke();
+    }
     super.onResize(msg);
   }
 
@@ -1114,6 +1128,7 @@ export class WindowedList<
   private _scrollRepaint: number | null;
   private _scrollToItem: [number, WindowedList.ScrollToAlign] | null;
   private _scrollUpdateWasRequested: boolean;
+  private _updater: Throttler;
 }
 
 /**
