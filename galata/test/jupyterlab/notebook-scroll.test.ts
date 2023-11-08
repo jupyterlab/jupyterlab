5fa331677000a997a48c5068ab28194660a8df3c
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2023-11-08 16:03:18 +0000

    Fix overreactive scrolling to next cell after `Shift + Enter` (#15288)
    * Add a test case for #14878
    
    * Fix the smart/auto scrolling logic for long items
    
    Items larger than the size of the viewport were not
    considered by the scroll logic leading to bad UX when
    smart/auto scroll was requested on certain operations.
    
    * Use pixel-based, cell-height-derived threshold for scrolling
    
    * Implement dual behaviour, simplify code, account for top padding
    
    * Use greater or equal to avoid scrolling if item fully visible
    
    * Update tests to reflect new expectations
    
    * Clean up the temp directory after tests

--- galata/test/jupyterlab/notebook-scroll.test.ts
+++ galata/test/jupyterlab/notebook-scroll.test.ts
@@ -5,6 +5,7 @@ import { expect, galata, test } from '@jupyterlab/galata';
 import * as path from 'path';
 
 const fileName = 'scroll.ipynb';
+const longOutputsNb = 'long_outputs.ipynb';
 
 test.use({
   mockSettings: {
@@ -16,7 +17,7 @@ test.use({
   }
 });
 
-test.describe('Notebook Scroll', () => {
+test.describe('Notebook scroll on navigation', () => {
   test.beforeEach(async ({ page, tmpPath }) => {
     await page.contents.uploadFile(
       path.resolve(__dirname, `./notebooks/${fileName}`),
@@ -51,6 +52,84 @@ test.describe('Notebook Scroll', () => {
       await lastCell.waitForElementState('visible');
     });
   }
+});
+
+test.describe('Notebook scroll on execution', () => {
+  test.beforeEach(async ({ page, tmpPath }) => {
+    await page.contents.uploadFile(
+      path.resolve(__dirname, `./notebooks/${longOutputsNb}`),
+      `${tmpPath}/${longOutputsNb}`
+    );
+
+    await page.notebook.openByPath(`${tmpPath}/${longOutputsNb}`);
+    await page.notebook.activate(longOutputsNb);
+  });
+
+  test.afterEach(async ({ page, tmpPath }) => {
+    await page.contents.deleteDirectory(tmpPath);
+  });
+
+  test('should scroll when advancing if top is only marginally visible', async ({
+    page
+  }) => {
+    const notebook = await page.notebook.getNotebookInPanel();
+    const thirdCell = await page.notebook.getCell(2);
+
+    const notebookBbox = await notebook.boundingBox();
+    const thirdCellBBox = await thirdCell.boundingBox();
+    await page.mouse.move(notebookBbox.x, notebookBbox.y);
+    const scrollOffset =
+      thirdCellBBox.y -
+      notebookBbox.y -
+      notebookBbox.height +
+      thirdCellBBox.height * 0.01;
+    await Promise.all([page.mouse.wheel(0, scrollOffset)]);
+    // Select second cell
+    await page.notebook.selectCells(1);
+
+    const thirdCellLocator = page.locator(
+      '.jp-Cell[data-windowed-list-index="2"]'
+    );
+    // The third cell should be positioned at the bottom, revealing between 0 to 2% of its content.
+    await expect(thirdCellLocator).toBeInViewport({ ratio: 0.0 });
+    await expect(thirdCellLocator).not.toBeInViewport({ ratio: 0.02 });
+
+    // Run second cell
+    await page.notebook.runCell(1);
+    // After running the second cell, the third cell should be revealed, in at least 25%
+    await expect(thirdCellLocator).toBeInViewport({ ratio: 0.25 });
+  });
+
+  test('should not scroll when advancing if top is non-marginally visible', async ({
+    page
+  }) => {
+    const notebook = await page.notebook.getNotebookInPanel();
+    const thirdCell = await page.notebook.getCell(2);
+
+    const notebookBbox = await notebook.boundingBox();
+    const thirdCellBBox = await thirdCell.boundingBox();
+    await page.mouse.move(notebookBbox.x, notebookBbox.y);
+    const scrollOffset =
+      thirdCellBBox.y -
+      notebookBbox.y -
+      notebookBbox.height +
+      thirdCellBBox.height * 0.15;
+    await Promise.all([page.mouse.wheel(0, scrollOffset)]);
+    // Select second cell
+    await page.notebook.selectCells(1);
+
+    const thirdCellLocator = page.locator(
+      '.jp-Cell[data-windowed-list-index="2"]'
+    );
+    // The third cell should be positioned at the bottom, revealing between 10 to 20% of its content.
+    await expect(thirdCellLocator).toBeInViewport({ ratio: 0.1 });
+    await expect(thirdCellLocator).not.toBeInViewport({ ratio: 0.2 });
+
+    // Run second cell
+    await page.notebook.runCell(1);
+    // After running the second cell, the third cell should not be scrolled
+    await expect(thirdCellLocator).not.toBeInViewport({ ratio: 0.2 });
+  });
 });
 
 test.describe('Notebook scroll over long outputs', () => {
