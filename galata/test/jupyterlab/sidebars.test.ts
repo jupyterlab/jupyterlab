e80b1688e998e3fae0b51f645d87ecd666d86650
e218736 <147728997+e218736@users.noreply.github.com>
2023-11-21 13:35:42 +0000

    test refactor

--- galata/test/jupyterlab/sidebars.test.ts
+++ galata/test/jupyterlab/sidebars.test.ts
@@ -268,13 +268,17 @@ test.describe('Sidebar keyboard navigation @a11y', () => {
   test('Open Extension Manager tab', async ({ page }) => {
     await page.goto();
 
+    const fileBrowserTab = page.getByRole('tab', {
+      name: 'File Browser (Ctrl+Shift+F)'
+    });
+
     // eslint-disable-next-line no-constant-condition
     while (true) {
       await page.keyboard.press('Tab');
       let fileBrowserIsFocused = await page.evaluate(
-        () => document.activeElement
+        () => document.activeElement?.getAttribute('data-id')
       );
-      if (fileBrowserIsFocused?.getAttribute('data-id') === 'filebrowser') {
+      if (fileBrowserIsFocused === 'filebrowser') {
         break;
       }
     }
@@ -283,12 +287,9 @@ test.describe('Sidebar keyboard navigation @a11y', () => {
     while (true) {
       await page.keyboard.press('ArrowDown');
       let tableOFContentsIsFocused = await page.evaluate(
-        () => document.activeElement
+        () => document.activeElement?.getAttribute('data-id')
       );
-      if (
-        tableOFContentsIsFocused?.getAttribute('data-id') ===
-        'extensionmanager.main-view'
-      ) {
+      if (tableOFContentsIsFocused === 'extensionmanager.main-view') {
         break;
       }
     }
