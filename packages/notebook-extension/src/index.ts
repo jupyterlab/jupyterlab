2e19555fa129c06b4434440da323cf9e70e33839
Afshin T. Darian <git@darian.email>
2023-11-30 00:37:38 +0000

    lint

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -3452,7 +3452,9 @@ function addCommands(
   });
   commands.addCommand(CommandIDs.virtualScrollbar, {
     label: trans.__('Virtual Scrollbar'),
-    caption: trans.__('Toggle virtual scrollbar (enabled with windowing mode: full)'),
+    caption: trans.__(
+      'Toggle virtual scrollbar (enabled with windowing mode: full)'
+    ),
     execute: args => {
       const current = getCurrent(tracker, shell, args);
 
