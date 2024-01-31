35e3e9d68d084dbf593b33b7d6206196b359a650
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-01-31 15:36:02 -0600

    more isolated, verbose diagram screenshot names

--- galata/test/jupyterlab/notebook-mermaid-diagrams.test.ts
+++ galata/test/jupyterlab/notebook-mermaid-diagrams.test.ts
@@ -6,25 +6,34 @@ import * as path from 'path';
 
 const fileName = 'mermaid_diagrams.ipynb';
 
-const EXPECTED_MERMAID_COUNT = 14;
+const EXPECTED_MERMAID_ORDER = [
+  'flowchart',
+  'sequence',
+  'class',
+  'state',
+  'er',
+  'journey',
+  'gantt',
+  'pie',
+  'quadrant',
+  'requirement',
+  'c4',
+  'mindmap',
+  'timeline',
+  'sankey',
+  'xy'
+];
 
-test.describe('Notebook Mermaid Diagrams', () => {
-  test.beforeEach(async ({ page, request, tmpPath }) => {
-    const contents = galata.newContentsHelper(request);
-    await contents.uploadFile(
-      path.resolve(__dirname, `./notebooks/${fileName}`),
-      `${tmpPath}/${fileName}`
-    );
-    await page.filebrowser.openDirectory(tmpPath);
-  });
-
-  for (const theme of ['default', 'dark']) {
-    const dark = theme === 'dark';
-
-    test(`Mermaid Markdown diagrams in ${theme} theme`, async ({
-      page,
-      tmpPath
-    }) => {
+for (const theme of ['default', 'dark']) {
+  const dark = theme === 'dark';
+  test.describe(`Notebook Mermaid Diagrams ${theme}`, () => {
+    test.beforeEach(async ({ page, request, tmpPath }) => {
+      const contents = galata.newContentsHelper(request);
+      await contents.uploadFile(
+        path.resolve(__dirname, `./notebooks/${fileName}`),
+        `${tmpPath}/${fileName}`
+      );
+      await page.filebrowser.openDirectory(tmpPath);
       const nbPath = `${tmpPath}/${fileName}`;
 
       if (dark) {
@@ -33,16 +42,23 @@ test.describe('Notebook Mermaid Diagrams', () => {
 
       await page.notebook.openByPath(nbPath);
       await page.notebook.activate(fileName);
+    });
 
-      for (let i = 0; i < EXPECTED_MERMAID_COUNT; i++) {
+    for (let i = 0; i < EXPECTED_MERMAID_ORDER.length; i++) {
+      let diagram = EXPECTED_MERMAID_ORDER[i];
+      const iZero = `${i}`.padStart(2, '0');
+
+      test(`Mermaid Diagram ${i} ${diagram} in ${theme} theme`, async ({
+        page
+      }) => {
         const output = await page.waitForSelector(
           `.jp-Cell:nth-child(${i + 1}) .jp-RenderedMermaid`
         );
-        const iZero = `${i}`.padStart(2, '0');
+
         expect(await output.screenshot()).toMatchSnapshot(
-          `mermaid-diagram-${theme}-${iZero}.png`
+          `mermaid-diagram-${theme}-${iZero}-${diagram}.png`
         );
-      }
-    });
-  }
-});
+      });
+    }
+  });
+}
