3992502ec296910150d341d0ff803d996cff2bea
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2024-03-11 13:46:40 +0000

    Error out if jupyter-config-data is not found

--- jupyterlab/browser-test.js
+++ jupyterlab/browser-test.js
@@ -28,6 +28,7 @@ if (OUTPUT) {
 async function main() {
   /* eslint-disable no-console */
   console.info(`Starting headless ${BROWSER}...`);
+  let testError = null;
 
   const pwBrowser = playwright[BROWSER];
   const browser = await pwBrowser.launch({
@@ -70,6 +71,7 @@ async function main() {
     console.error('Error loading JupyterLab page:', reason);
     // Limit to 1000 characters
     console.error((await page.content()).substring(0, 1000));
+    testError = reason;
   }
 
   console.log('Waiting for #main selector...');
@@ -81,7 +83,6 @@ async function main() {
     state: 'attached'
   });
   console.log('Waiting for application to start...');
-  let testError = null;
 
   try {
     await page.waitForSelector('.completed', { state: 'attached' });
