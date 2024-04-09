fce834a4033aa36eacac63fc3ea67afc93154a02
Jason Weill <jweill@amazon.com>
2024-04-09 14:03:14 -0700

    Updates comment about ES version

--- packages/coreutils/src/time.ts
+++ packages/coreutils/src/time.ts
@@ -17,7 +17,8 @@ const UNITS: { name: Intl.RelativeTimeFormatUnit; milliseconds: number }[] = [
  * The namespace for date functions.
  */
 export namespace Time {
-  // Intl.RelativeTimeFormatStyle contains these, but we can't compile with it yet
+  // Intl.RelativeTimeFormatStyle contains these, but it requires `ES2020.Intl`.
+  // We currently compile to an `ES2018` target.
   export type HumanStyle = 'long' | 'short' | 'narrow';
 
   /**
