93b7705be3a9436deb57d08c7e9254fc60bff26a
krassowski <5832902+krassowski@users.noreply.github.com>
2024-04-18 11:33:10 +0100

    Remove unused private `shutdownAllLabel` prop from `List` and `ListWidget`

--- packages/running/src/index.tsx
+++ packages/running/src/index.tsx
@@ -305,7 +305,6 @@ function List(props: {
   child?: boolean;
   runningItems: IRunningSessions.IRunningItem[];
   shutdownLabel?: string | ((item: IRunningSessions.IRunningItem) => string);
-  shutdownAllLabel?: string;
   shutdownItemIcon?: LabIcon;
   filter?: (item: IRunningSessions.IRunningItem) => Partial<IScore> | null;
   translator?: ITranslator;
@@ -420,7 +419,6 @@ class ListWidget extends ReactWidget {
     private _options: {
       manager: IRunningSessions.IManager;
       runningItems: IRunningSessions.IRunningItem[];
-      shutdownAllLabel: string;
       filterProvider?: IFilterProvider;
       translator?: ITranslator;
       collapseToggled: ISignal<Section, boolean>;
@@ -461,7 +459,6 @@ class ListWidget extends ReactWidget {
               <List
                 runningItems={options.runningItems}
                 shutdownLabel={options.manager.shutdownLabel}
-                shutdownAllLabel={options.shutdownAllLabel}
                 shutdownItemIcon={options.manager.shutdownItemIcon}
                 filter={options.filterProvider?.filter}
                 translator={options.translator}
@@ -539,7 +536,6 @@ class Section extends PanelWithToolbar {
     this.addWidget(
       new ListWidget({
         runningItems,
-        shutdownAllLabel: this._shutdownAllLabel,
         collapseToggled: this._collapseToggled,
         ...options
       })
