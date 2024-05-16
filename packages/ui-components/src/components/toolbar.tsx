dea199aa34c4887911e2f82f5cebc9e36615a8bf
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-05-16 14:09:17 +0200

    Avoid conflicting call of _onResize()

--- packages/ui-components/src/components/toolbar.tsx
+++ packages/ui-components/src/components/toolbar.tsx
@@ -559,8 +559,8 @@ export class ReactiveToolbar extends Toolbar<Widget> {
     const toolbarPadding = 2 + 5;
     let width = opener.isHidden ? toolbarPadding : toolbarPadding + openerWidth;
 
-    this._getWidgetsToRemove(width, toolbarWidth, openerWidth)
-      .then(values => {
+    return this._getWidgetsToRemove(width, toolbarWidth, openerWidth)
+      .then(async values => {
         let { width, widgetsToRemove } = values;
         while (widgetsToRemove.length > 0) {
           // Insert the widget at the right position in the opener popup, relatively
@@ -626,7 +626,7 @@ export class ReactiveToolbar extends Toolbar<Widget> {
           opener.hide();
         }
         if (callTwice) {
-          void this._onResize();
+          await this._onResize();
         }
       })
       .catch(msg => {
