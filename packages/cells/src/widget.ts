933ce0e7cee2b2873d1a364c8f28cf16949646c1
David Brochart <david.brochart@gmail.com>
2024-05-30 11:56:43 +0200

    Support collaborative stdin output

--- packages/cells/src/widget.ts
+++ packages/cells/src/widget.ts
@@ -3,9 +3,9 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
-import { Extension } from '@codemirror/state';
+import { Extension, StateCommand } from '@codemirror/state';
 
-import { EditorView } from '@codemirror/view';
+import { EditorView, KeyBinding, keymap } from '@codemirror/view';
 
 import { ElementExt } from '@lumino/domutils';
 
@@ -20,6 +20,10 @@ import { CodeEditor, CodeEditorWrapper } from '@jupyterlab/codeeditor';
 import { DirListing } from '@jupyterlab/filebrowser';
 
 import * as nbformat from '@jupyterlab/nbformat';
+
+import * as Y from 'yjs';
+
+import { ybinding } from '@jupyterlab/codemirror';
 
 import {
   IOutputPrompt,
@@ -39,7 +43,7 @@ import {
 
 import { Kernel, KernelMessage } from '@jupyterlab/services';
 
-import { IMapChange } from '@jupyter/ydoc';
+import { IMapChange, YCodeCell } from '@jupyter/ydoc';
 
 import { TableOfContentsUtils } from '@jupyterlab/toc';
 
@@ -182,6 +186,14 @@ const RENDER_TIMEOUT = 1000;
  * The mime type for a rich contents drag object.
  */
 const CONTENTS_MIME_RICH = 'application/x-jupyter-icontentsrich';
+
+const OUTPUT_AREA_ITEM_CLASS = 'jp-OutputArea-child';
+const OUTPUT_AREA_STDIN_ITEM_CLASS = 'jp-OutputArea-stdin-item';
+const OUTPUT_AREA_PROMPT_CLASS = 'jp-OutputArea-prompt';
+const OUTPUT_AREA_OUTPUT_CLASS = 'jp-OutputArea-output';
+const STDIN_CLASS = 'jp-Stdin';
+const STDIN_PROMPT_CLASS = 'jp-Stdin-prompt';
+const STDIN_INPUT_CLASS = 'jp-Stdin-input';
 
 /** ****************************************************************************
  * Cell
@@ -1100,7 +1112,53 @@ export class CodeCell extends Cell<ICodeCellModel> {
     model.outputs.changed.connect(this.onOutputChanged, this);
     model.outputs.stateChanged.connect(this.onOutputChanged, this);
     model.stateChanged.connect(this.onStateChanged, this);
+    model.sharedModel.changed.connect(this.handleStdin, this);
+    const youtputs = (model.sharedModel as YCodeCell).ymodel.get('outputs');
+    for (const youtput of youtputs) {
+      if (youtput instanceof Y.Map && youtput.get('output_type') === 'stdin') {
+        const prompt = youtput.get('prompt');
+        const password = youtput.get('password');
+        this.createInputWidget(prompt, password, youtput);
+      }
+    }
   }
+
+  handleStdin(sender: any, args: any): void {
+    if (
+      args.outputsChange !== undefined &&
+      args.outputsChange[0].insert !== undefined
+    ) {
+      const newOutput = args.outputsChange[0].insert[0];
+      const output_type = newOutput.get('output_type');
+      if (output_type === 'stdin') {
+        const prompt = newOutput.get('prompt');
+        const password = newOutput.get('password');
+        this.createInputWidget(prompt, password, newOutput);
+      }
+    }
+  }
+
+  createInputWidget = (
+    prompt: string,
+    password: boolean,
+    stdinOutput: Y.Map<any>
+  ) => {
+    const inputWidget = new InputWidget(
+      prompt,
+      password,
+      stdinOutput,
+      (this.model.sharedModel as YCodeCell).awareness
+    );
+    const panel = new Panel();
+    panel.addClass(OUTPUT_AREA_ITEM_CLASS);
+    panel.addClass(OUTPUT_AREA_STDIN_ITEM_CLASS);
+    const outputPrompt = new OutputPrompt();
+    outputPrompt.addClass(OUTPUT_AREA_PROMPT_CLASS);
+    panel.addWidget(outputPrompt);
+    inputWidget.addClass(OUTPUT_AREA_OUTPUT_CLASS);
+    panel.addWidget(inputWidget);
+    this.outputArea.layout.addWidget(panel);
+  };
 
   /**
    * Detect the movement of the caret in the output area.
@@ -2564,4 +2622,45 @@ export namespace RawCell {
    * An options object for initializing a base cell widget.
    */
   export interface IOptions extends Cell.IOptions<IRawCellModel> {}
+}
+
+class InputWidget extends Widget {
+  constructor(
+    prompt: string,
+    password: boolean,
+    stdinOutput: any,
+    awareness: any
+  ) {
+    const node = document.createElement('div');
+    const promptNode = document.createElement('pre');
+    promptNode.className = STDIN_PROMPT_CLASS;
+    promptNode.textContent = prompt;
+    const input1 = document.createElement('div');
+    input1.className = STDIN_INPUT_CLASS;
+    input1.style.border = 'thin solid';
+    const input2 = document.createElement('div');
+    if (password === true) {
+      (input2.style as any).webkitTextSecurity = 'disc';
+    }
+    input1.appendChild(input2);
+    node.appendChild(promptNode);
+    promptNode.appendChild(input1);
+    const stdin = stdinOutput.get('value');
+    const ybind = ybinding({ ytext: stdin });
+    const submit: StateCommand = ({ state, dispatch }) => {
+      stdinOutput.set('submitted', true);
+      return true;
+    };
+    const submitWithEnter: KeyBinding = {
+      key: 'Enter',
+      run: submit
+    };
+    new EditorView({
+      doc: stdin.toString(),
+      extensions: [keymap.of([submitWithEnter]), ybind],
+      parent: input2
+    });
+    super({ node });
+    this.addClass(STDIN_CLASS);
+  }
 }
