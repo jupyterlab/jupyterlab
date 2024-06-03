7eacbc51069a4dc330d94ef34e74cafe01ee3156
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-06-03 20:48:30 +0000

    Add setuptools to the dependencies

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -225,9 +225,6 @@ if [[ $GROUP == usage ]]; then
     jupyter labextension enable @jupyterlab/notebook-extension --level user
     cat $USER_PAGE_CONFIG | grep "\"@jupyterlab/notebook-extension\": false"
 
-    # TMP: install setuptools
-    python -m pip install setuptools
-
     # Test with a prebuilt install
     jupyter labextension develop extension --debug
     jupyter labextension build extension
