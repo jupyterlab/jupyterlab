3b36778d115edd1f7862e1e0840f2f5b34dbdd98
Jason Weill <jweill@amazon.com>
2024-06-27 17:19:16 -0700

    Sets forceRefresh: false for file browser

--- packages/filebrowser/src/browser.ts
+++ packages/filebrowser/src/browser.ts
@@ -103,7 +103,7 @@ export class FileBrowser extends SidePanel {
       },
       useFuzzyFilter: true,
       placeholder: this._trans.__('Filter files by name'),
-      forceRefresh: true,
+      forceRefresh: false,
       showIcon: false
     });
     searcher.addClass(FILTERBOX_CLASS);
