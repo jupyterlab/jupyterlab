40f361b49f132645ab5764aff04a62951b982d7a
Jason Weill <jweill@amazon.com>
2024-06-27 17:03:54 -0700

    Updates help text in workspace dialog, more specific props passing

--- packages/ui-components/src/components/search.tsx
+++ packages/ui-components/src/components/search.tsx
@@ -212,5 +212,14 @@ export const FilterBox = (props: IFilterBoxProps): JSX.Element => {
  * A widget which hosts a input textbox to filter on file names.
  */
 export const FilenameSearcher = (props: IFilterBoxProps): ReactWidget => {
-  return ReactWidget.create(<FilterBox {...props} />);
+  return ReactWidget.create(
+    <FilterBox
+      updateFilter={props.updateFilter}
+      useFuzzyFilter={props.useFuzzyFilter}
+      placeholder={props.placeholder}
+      forceRefresh={props.forceRefresh}
+      caseSensitive={props.caseSensitive}
+      showIcon={props.showIcon}
+    />
+  );
 };
