8c01cab4f97c5ff70febb9cc28fb84e899a7f3b8
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-07-10 18:40:17 +0530

    add custom select widget

--- packages/ui-components/src/components/form.tsx
+++ packages/ui-components/src/components/form.tsx
@@ -9,13 +9,18 @@ import { JSONExt, ReadonlyJSONObject } from '@lumino/coreutils';
 import Form, { FormProps, IChangeEvent } from '@rjsf/core';
 import {
   ariaDescribedByIds,
+  enumOptionsIndexForValue,
+  enumOptionsValueForIndex,
+  schemaRequiresTrueValue,
+  labelValue, 
   FormContextType,
   RJSFSchema,
-  schemaRequiresTrueValue,
   StrictRJSFSchema,
   WidgetProps
 } from '@rjsf/utils';
+import { Field, Option, OptionOnSelectData } from '@jupyter/react-components';
 import { Checkbox } from '@jupyter/react-components';
+import { Select } from '@jupyter/react-components';
 import {
   ADDITIONAL_PROPERTY_FLAG,
   ArrayFieldTemplateProps,
@@ -744,5 +749,86 @@ function CustomCheckbox<
 }
 
 export const customWidgets = {
-  CheckboxWidget: CustomCheckbox
+  CheckboxWidget: CustomCheckbox,
+  SelectWidget: CustomSelect,
 };
+
+function CustomSelect<
+  T = any, 
+  S extends StrictRJSFSchema = RJSFSchema, 
+  F extends FormContextType = any
+  >({
+  id,
+  options,
+  label,
+  hideLabel,
+  value,
+  required,
+  disabled,
+  readonly,
+  multiple = false,
+  autofocus = false,
+  rawErrors = [],
+  onChange,
+  onBlur,
+  onFocus,
+  schema,
+  placeholder,
+}: WidgetProps<T, S, F>) {
+  const { enumOptions, enumDisabled, emptyValue: optEmptyVal } = options;
+
+  const selectedIndexes = enumOptionsIndexForValue<S>(value, enumOptions, multiple);
+  let selectedIndexesAsArray: string[] = [];
+
+  if (typeof selectedIndexes === 'string') {
+    selectedIndexesAsArray = [selectedIndexes];
+  } else if (Array.isArray(selectedIndexes)) {
+    selectedIndexesAsArray = selectedIndexes.map((index) => String(index));
+  }
+
+  const dropdownValue = selectedIndexesAsArray
+    .map((index) => (enumOptions ? enumOptions[Number(index)].label : undefined))
+    .join(', ');
+
+  const _onBlur = () => onBlur(id, selectedIndexes);
+  const _onFocus = () => onFocus(id, selectedIndexes);
+  const _onChange = (_: any, data: OptionOnSelectData) => {
+    const newValue = getValue(data, multiple);
+    return onChange(enumOptionsValueForIndex<S>(newValue, enumOptions, optEmptyVal));
+  };
+  const showPlaceholderOption = !multiple && schema.default === undefined;
+
+  return (
+    <Field
+      label={labelValue(label, hideLabel)}
+      validationState={rawErrors.length ? 'error' : undefined}
+      required={required}
+    >
+      <Select
+        id={id}
+        name={id}
+        multiselect={multiple}
+        className='form-control'
+        value={dropdownValue}
+        disabled={disabled || readonly}
+        autoFocus={autofocus}
+        onBlur={_onBlur}
+        onFocus={_onFocus}
+        onOptionSelect={_onChange}
+        selectedOptions={selectedIndexesAsArray}
+        aria-describedby={ariaDescribedByIds<T>(id)}
+      >
+        {showPlaceholderOption && <Option value=''>{placeholder || ''}</Option>}
+        {Array.isArray(enumOptions) &&
+          enumOptions.map(({ value, label }, i) => {
+            const disabled = enumDisabled && enumDisabled.indexOf(value) !== -1;
+            return (
+              <Option key={i} value={String(i)} disabled={disabled}>
+                {label}
+              </Option>
+            );
+          })}
+      </Select>
+    </Field>
+  );
+}
