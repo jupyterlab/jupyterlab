51f0ab2f0533ef9622853cda1f91ae97c8642377
Darshan808 <pranishpoudel10@gmail.com>
2024-10-04 21:44:34 +0545

    fix-code-formatting

--- packages/shortcuts-extension/src/components/ShortcutItem.tsx
+++ packages/shortcuts-extension/src/components/ShortcutItem.tsx
@@ -250,9 +250,11 @@ export class ShortcutItem extends React.Component<
         {this.isLocationBeingEdited(index)
           ? this.getShortCutAsInput(binding, index)
           : this.getShortCutForDisplayOnly(binding)}
-        {!(index===this._nonEmptyBindings.length-1 && 
-        Object.values(this.state.displayReplaceInput).some(Boolean)) && 
-        this.getOrDiplayIfNeeded(index < nonEmptyBindings.length - 1)}
+        {!(
+          index === this._nonEmptyBindings.length - 1 &&
+          Object.values(this.state.displayReplaceInput).some(Boolean)
+        ) &&
+          this.getOrDiplayIfNeeded(index < this._nonEmptyBindings.length - 1)}
       </div>
     );
   }
