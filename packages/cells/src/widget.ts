a32147962d2381ed72261873f85449c8a91ba225
Marten Richter <marten.richter@freenet.de>
2024-12-20 15:37:20 +0100

    Fix html sanitizer, if html data is array of strings

--- packages/cells/src/widget.ts
+++ packages/cells/src/widget.ts
@@ -1312,9 +1312,13 @@ export class CodeCell extends Cell<ICodeCellModel> {
 
         // Parse HTML output
         if (htmlType) {
+          let htmlData = m.data[htmlType] as string | string[];
+          if (typeof htmlData !== 'string') {
+            htmlData = htmlData.join('');
+          }
           headings.push(
             ...TableOfContentsUtils.getHTMLHeadings(
-              this._rendermime.sanitizer.sanitize(m.data[htmlType] as string)
+              this._rendermime.sanitizer.sanitize(htmlData)
             ).map(heading => {
               return {
                 ...heading,
