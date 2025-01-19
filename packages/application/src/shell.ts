d04979f2205de4c83d12bdbf0a937e13d3aa462c
rishab <kumarjharishab@gmail.com>
2025-01-19 20:26:38 +0530

    aria-labels added

--- packages/application/src/shell.ts
+++ packages/application/src/shell.ts
@@ -756,6 +756,15 @@ export class LabShell extends Widget implements JupyterFrontEnd.IShell {
         'aria-label',
         trans.__('alternate sidebar')
       );
+      this._topHandler.panel.node.setAttribute(
+        'aria-label',
+        trans.__('top bar')
+      );
+      this._bottomPanel.node.setAttribute(
+        'aria-label',
+        trans.__('bottom pannel')
+      );
+      this._dockPanel.node.setAttribute('aria-label', trans.__('main content'));
     }
   }
 
