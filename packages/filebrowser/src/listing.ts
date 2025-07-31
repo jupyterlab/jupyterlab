20702d2214c9bd90d7b188f820a95906b80cd5f6
martinRenou <martin.renou@gmail.com>
2025-07-31 11:22:45 +0200

    Fix initial size for the first render

--- packages/filebrowser/src/listing.ts
+++ packages/filebrowser/src/listing.ts
@@ -1068,6 +1068,8 @@ export class DirListing extends Widget {
     // Fetch common variables.
     const items = this._sortedItems;
     const nodes = this._items;
+    // If we didn't get any item yet, we'll need to resize once items are added
+    const needsResize = nodes.length === 0;
     const content = DOMUtils.findElement(this.node, CONTENT_CLASS);
     const renderer = this._renderer;
 
@@ -1142,6 +1144,11 @@ export class DirListing extends Widget {
     }
 
     this.updateNodes(items, nodes);
+
+    if (needsResize) {
+      this._width = this._computeContentWidth();
+      this._updateColumnSizes();
+    }
 
     this._prevPath = this._model.path;
   }
