11060e3b17e561204ebe5801a256196ed0a0116b
Darshan808 <pranishpoudel10@gmail.com>
2025-08-04 12:04:33 +0545

    fix-integrity

--- packages/codemirror-extension/style/index.js
+++ packages/codemirror-extension/style/index.js
@@ -7,6 +7,7 @@
 import '@lumino/widgets/style/index.js';
 import '@jupyterlab/ui-components/style/index.js';
 import '@jupyterlab/statusbar/style/index.js';
+import '@jupyterlab/apputils/style/index.js';
 import '@jupyterlab/codeeditor/style/index.js';
 import '@jupyterlab/application/style/index.js';
 import '@jupyterlab/codemirror/style/index.js';
