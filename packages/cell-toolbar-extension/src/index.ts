3dd549e0d62174d37f7e62e01e8ab00f4c07b59b
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-08-12 16:34:45 +0200

    Add a plugin that adds a run button to the cell prompt

--- packages/cell-toolbar-extension/src/index.ts
+++ packages/cell-toolbar-extension/src/index.ts
@@ -14,15 +14,24 @@ import {
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 import { CellBarExtension } from '@jupyterlab/cell-toolbar';
 import {
+  INotebookTracker,
+  NotebookActions,
+  NotebookPanel
+} from '@jupyterlab/notebook';
+import {
   createToolbarFactory,
   IToolbarWidgetRegistry
 } from '@jupyterlab/apputils';
 import { ITranslator, nullTranslator } from '@jupyterlab/translation';
+import { runIcon, ToolbarButton } from '@jupyterlab/ui-components';
 
-const PLUGIN_ID = '@jupyterlab/cell-toolbar-extension:plugin';
+const PLUGIN_ID = {
+  cellToolbar: '@jupyterlab/cell-toolbar-extension:plugin',
+  runButton: '@jupyterlab/cell-toolbar-extension:run-button'
+};
 
 const cellToolbar: JupyterFrontEndPlugin<void> = {
-  id: PLUGIN_ID,
+  id: PLUGIN_ID.cellToolbar,
   description: 'Add the cells toolbar.',
   autoStart: true,
   activate: async (
@@ -62,15 +71,16 @@ const cellToolbar: JupyterFrontEndPlugin<void> = {
     // Wait for the application to be restored and
     // for the settings for this plugin to be loaded
     if (settingRegistry !== null) {
-      void Promise.all([app.restored, settingRegistry.load(PLUGIN_ID)]).then(
-        ([, setting]) => {
-          // Read the settings
-          loadSetting(setting);
+      void Promise.all([
+        app.restored,
+        settingRegistry.load(PLUGIN_ID.cellToolbar)
+      ]).then(([, setting]) => {
+        // Read the settings
+        loadSetting(setting);
 
-          // Listen for your plugin setting changes using Signal
-          setting.changed.connect(loadSetting);
-        }
-      );
+        // Listen for your plugin setting changes using Signal
+        setting.changed.connect(loadSetting);
+      });
     }
 
     app.docRegistry.addWidgetExtension('Notebook', extension);
@@ -78,4 +88,48 @@ const cellToolbar: JupyterFrontEndPlugin<void> = {
   optional: [ISettingRegistry, IToolbarWidgetRegistry, ITranslator]
 };
 
-export default cellToolbar;
+const runCellButton: JupyterFrontEndPlugin<void> = {
+  id: PLUGIN_ID.runButton,
+  description: 'Add the cells toolbar.',
+  autoStart: true,
+  requires: [INotebookTracker],
+  optional: [ITranslator],
+  activate: (
+    app: JupyterFrontEnd,
+    tracker: INotebookTracker,
+    translator: ITranslator
+  ) => {
+    const trans = (translator ?? nullTranslator).load('jupyterlab');
+
+    const runButtonFactory = (panel: NotebookPanel) =>
+      new ToolbarButton({
+        icon: runIcon,
+        onClick: () => {
+          void NotebookActions.runAndAdvance(
+            panel.content,
+            panel.sessionContext
+          );
+        },
+        tooltip: trans.__('Run the selected cells and advance')
+      });
+
+    tracker.widgetAdded.connect((_, panel) => {
+      const cellListChanged = () => {
+        panel.content.widgets.forEach(cell => {
+          cell.ready.then(() => {
+            if (cell.inputArea) {
+              cell.inputArea.prompt.runButton = runButtonFactory(panel);
+            }
+          });
+        });
+      };
+      panel.content.model?.cells.changed.connect(cellListChanged);
+
+      panel.disposed.connect(() => {
+        panel.content.model?.cells.changed.disconnect(cellListChanged);
+      });
+    });
+  }
+};
+
+export default [cellToolbar, runCellButton];
