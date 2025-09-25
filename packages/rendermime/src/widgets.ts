88ef373039a8cc09f27d3814382a512d9033675c
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2025-09-25 16:41:42 +0100

    Merge commit from fork
    * Add tests for hardening of URLs
    
    * Harden anchor links for latex rendering, add more tests
    
    * Run `mathjax-extension` tests on CI

--- packages/rendermime/src/widgets.ts
+++ packages/rendermime/src/widgets.ts
@@ -204,7 +204,7 @@ export class RenderedHTML extends RenderedHTMLCommon {
     this._rendered
       .then(() => {
         if (this.latexTypesetter) {
-          this.latexTypesetter.typeset(this.node);
+          Private.typeset(this.node, this.latexTypesetter, this.resolver);
         }
       })
       .catch(console.warn);
@@ -251,7 +251,7 @@ export class RenderedLatex extends RenderedCommon {
     this._rendered
       .then(() => {
         if (this.latexTypesetter) {
-          this.latexTypesetter.typeset(this.node);
+          Private.typeset(this.node, this.latexTypesetter, this.resolver);
         }
       })
       .catch(console.warn);
@@ -352,7 +352,7 @@ export class RenderedMarkdown extends RenderedHTMLCommon {
     this._rendered
       .then(() => {
         if (this.latexTypesetter) {
-          this.latexTypesetter.typeset(this.node);
+          Private.typeset(this.node, this.latexTypesetter, this.resolver);
         }
       })
       .catch(console.warn);
@@ -403,7 +403,7 @@ export class RenderedSVG extends RenderedCommon {
     this._rendered
       .then(() => {
         if (this.latexTypesetter) {
-          this.latexTypesetter.typeset(this.node);
+          Private.typeset(this.node, this.latexTypesetter, this.resolver);
         }
       })
       .catch(console.warn);
@@ -492,5 +492,28 @@ export class RenderedJavaScript extends RenderedCommon {
       source: trans.__('JavaScript output is disabled in JupyterLab'),
       translator: this.translator
     });
+  }
+}
+
+/**
+ * The namespace for module implementation details.
+ */
+namespace Private {
+  /**
+   * Run typesetter on the given node and harden links.
+   */
+  export function typeset(
+    host: HTMLElement,
+    latexTypesetter: IRenderMime.ILatexTypesetter,
+    resolver?: IRenderMime.IResolver | null
+  ) {
+    const result = latexTypesetter.typeset(host);
+    // Harden anchors to contain secure target/rel attributes.
+    if (result instanceof Promise) {
+      // If promised was returned, await for rendering to complete.
+      result.then(() => renderers.hardenAnchorLinks(host, resolver));
+    } else {
+      renderers.hardenAnchorLinks(host, resolver);
+    }
   }
 }
