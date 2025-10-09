983bd66d8ba2aa604dc30da519cbbfcd54c12a90
Darshan808 <pranishpoudel10@gmail.com>
2025-10-09 11:42:18 +0545

    fix-integrity

--- packages/docmanager/style/index.js
+++ packages/docmanager/style/index.js
@@ -9,3 +9,4 @@ import '@jupyterlab/ui-components/style/index.js';
 import '@jupyterlab/apputils/style/index.js';
 import '@jupyterlab/rendermime/style/index.js';
 import '@jupyterlab/docregistry/style/index.js';
+import '@jupyterlab/application/style/index.js';
