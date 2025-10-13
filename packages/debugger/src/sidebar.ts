f7f31dd6b1d8ed387e9e0313fe47c56887312fbf
Florence Haudin <haudin.florence@gmail.com>
2025-10-13 11:07:12 +0200

    Remove unused files from packages/debugger/src/panels/sources.

--- packages/debugger/src/sidebar.ts
+++ packages/debugger/src/sidebar.ts
@@ -17,8 +17,6 @@ import { Breakpoints as BreakpointsPanel } from './panels/breakpoints';
 
 import { Callstack as CallstackPanel } from './panels/callstack';
 
-import { Sources as SourcesPanel } from './panels/sources';
-
 import { KernelSources as KernelSourcesPanel } from './panels/kernelSources';
 
 import { Variables as VariablesPanel } from './panels/variables';
@@ -103,9 +101,8 @@ export class DebuggerSidebar extends SidePanel {
   readonly breakpoints: BreakpointsPanel;
 
   /**
-   * The sources widget.
+   * The kernel sources widget.
    */
-  readonly sources: SourcesPanel;
 
   readonly kernelSources: KernelSourcesPanel;
 }
