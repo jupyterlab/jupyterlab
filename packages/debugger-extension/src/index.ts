76fa50dd4c54af9cd7f829bb79f9c9e8859a0918
Florence Haudin <haudin.florence@gmail.com>
2025-10-14 13:04:32 +0200

    Try to fix failing lint test.

--- packages/debugger-extension/src/index.ts
+++ packages/debugger-extension/src/index.ts
@@ -890,7 +890,7 @@ const sourceViewer: JupyterFrontEndPlugin<IDebugger.ISourceViewer> = {
           });
         });
 
-      openOrRevealSourceInMainArea(frame);
+      void openOrRevealSourceInMainArea(frame);
     };
     model.callstack.currentFrameChanged.connect(onCurrentFrameChanged);
 
