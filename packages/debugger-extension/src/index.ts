c1af0e7a61948c3ee9452ee8d5fb6f9d87a794a1
Florence Haudin <99649086+HaudinFlorence@users.noreply.github.com>
2025-10-15 14:53:34 +0200

    Apply suggestions from code review
    Co-authored-by: martinRenou <martin.renou@gmail.com>

--- packages/debugger-extension/src/index.ts
+++ packages/debugger-extension/src/index.ts
@@ -775,7 +775,7 @@ const sourceViewer: JupyterFrontEndPlugin<IDebugger.ISourceViewer> = {
         return;
       }
       try {
-        const source = await service.getSource({ path: frame.source?.path });
+        const source = await service.getSource({ path: frame.source.path });
         if (source) {
           openSource(source, frame);
         }
@@ -793,7 +793,7 @@ const sourceViewer: JupyterFrontEndPlugin<IDebugger.ISourceViewer> = {
         return;
       }
       const { content, mimeType, path } = source;
-      if (breakpointOrFrame) {
+      if (breakpointOrFrame && typeof breakpointOrFrame.line !== 'undefined') {
         const results = debuggerSources.find({
           focus: true,
           kernel: service.session?.connection?.kernel?.name ?? '',
@@ -802,7 +802,6 @@ const sourceViewer: JupyterFrontEndPlugin<IDebugger.ISourceViewer> = {
         });
 
         if (results.length > 0) {
-          if (typeof breakpointOrFrame.line !== 'undefined') {
             results.forEach(editor => {
               void editor.reveal().then(() => {
                 const edit = editor.get();
