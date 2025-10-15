73f5307c4607209d08f92bd1846e6071e934da64
Greg <gregory.mooney@quantstack.net>
2025-10-15 11:38:13 +0200

    Tidy up

--- packages/debugger/src/tokens.ts
+++ packages/debugger/src/tokens.ts
@@ -21,7 +21,7 @@ import { DebuggerHandler } from './handler';
 
 import { IDebuggerSourceDisplayProvider } from './displayregistry';
 
-import { VariableViewOptionKey } from './model';
+import { VariablesFilterOptionKey } from './model';
 
 /**
  * An interface describing an application's visual debugger.
@@ -995,11 +995,11 @@ export namespace IDebugger {
        */
       clear(): void;
 
-      variableViewOptions: Map<VariableViewOptionKey, boolean>;
+      variablesFilterOptions: Map<VariablesFilterOptionKey, boolean>;
 
-      readonly variableViewOptionsChanged: ISignal<
+      readonly variablesFilterOptionsChanged: ISignal<
         this,
-        Map<VariableViewOptionKey, boolean>
+        Map<VariablesFilterOptionKey, boolean>
       >;
     }
 
