232a12ddfffc896cd08f46418eafd0afa4bd8d87
Greg <gregory.mooney@quantstack.net>
2025-10-15 16:30:20 +0200

    Restore ellipsis

--- packages/debugger-extension/src/index.ts
+++ packages/debugger-extension/src/index.ts
@@ -338,8 +338,8 @@ const notebooks: JupyterFrontEndPlugin<IDebugger.IHandler> = {
 
     const trans = translator.load('jupyterlab');
     app.commands.addCommand(Debugger.CommandIDs.restartAndDebug, {
-      label: trans.__('Restart Kernel and Debug'),
-      caption: trans.__('Restart Kernel and Debug'),
+      label: trans.__('Restart Kernel and Debug…'),
+      caption: trans.__('Restart Kernel and Debug…'),
       isEnabled: () => service.isStarted,
       describedBy: {
         args: {
