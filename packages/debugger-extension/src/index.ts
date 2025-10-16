0f89af4b4fe1139c29341e7003338b050f2943bb
Florence Haudin <haudin.florence@gmail.com>
2025-10-16 15:52:17 +0200

    Fix an error in the index.ts of the debugger-extension.

--- packages/debugger-extension/src/index.ts
+++ packages/debugger-extension/src/index.ts
@@ -817,8 +817,8 @@ const sourceViewer: JupyterFrontEndPlugin<IDebugger.ISourceViewer> = {
               }
             });
           });
+          return;
         }
-        return;
       }
 
       const editorWrapper = readOnlyEditorFactory.createNewEditor({
