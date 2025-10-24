1e70f6a00ff9dc60bc36a362c3c87eb5e19d7d25
Meriem-BenIsmail <meriembenismail867@gmail.com>
2025-10-24 14:16:29 +0200

    update comments

--- packages/fileeditor-extension/src/commands.ts
+++ packages/fileeditor-extension/src/commands.ts
@@ -1237,7 +1237,7 @@ export namespace Commands {
             editor.replaceSelection && editor.replaceSelection(clipboardData);
           }
         } catch (err) {
-          // Firefox fallback
+          // browser limitation fallback (e.g Firefox)
           void showDialog({
             title: trans.__('Paste Unavailable'),
             body: trans.__(
