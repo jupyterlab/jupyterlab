6e1fde7642a7c3d2014eaa27673eae80ea261e4a
Florence Haudin <haudin.florence@gmail.com>
2025-12-02 15:02:25 +0100

    Revert "Remove unused files from packages/debugger/src/panels/sources."
    This reverts commit 4a36647fc2e5e451210c8d244714759cf9282ed3.

--- packages/debugger/src/sidebar.ts
+++ packages/debugger/src/sidebar.ts
@@ -15,6 +15,8 @@ import { Breakpoints as BreakpointsPanel } from './panels/breakpoints';
 
 import { Callstack as CallstackPanel } from './panels/callstack';
 
+import { Sources as SourcesPanel } from './panels/sources';
+
 import { KernelSources as KernelSourcesPanel } from './panels/kernelSources';
 
 import { Variables as VariablesPanel } from './panels/variables';
@@ -112,8 +114,9 @@ export class DebuggerSidebar extends SidePanel {
   readonly breakpoints: BreakpointsPanel;
 
   /**
-   * The kernel sources widget.
+   * The sources widget.
    */
+  readonly sources: SourcesPanel;
 
   readonly kernelSources: KernelSourcesPanel;
 }
