9752839f919c04942be8cbc6ea392b6fc461be3a
Jason Grout <jason@jasongrout.org>
2025-12-02 16:42:12 -0700

    Fix jlpm integrity
    List the style-loader in the builder package unused dependencies again

--- buildutils/src/ensure-repo.ts
+++ buildutils/src/ensure-repo.ts
@@ -80,6 +80,7 @@ const UNUSED: Dict<string[]> = {
     'css-loader',
     'path-browserify',
     'process',
+    'style-loader',
     'terser-webpack-plugin',
     'worker-loader',
     'source-map-loader'
