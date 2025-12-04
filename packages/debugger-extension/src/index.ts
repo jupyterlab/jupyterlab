f7e04d71d3ca612f2cd8f939d738b97d1df6091a
Florence Haudin <haudin.florence@gmail.com>
2025-12-04 23:37:14 +0100

    Take review suggestions into account.

--- packages/debugger-extension/src/index.ts
+++ packages/debugger-extension/src/index.ts
@@ -812,7 +812,7 @@ const sidebar: JupyterFrontEndPlugin<IDebugger.ISidebar> = {
       breakpointsCommands,
       editorServices,
       themeManager,
-      translator,
+      translator
     });
 
     if (settingRegistry) {
@@ -820,8 +820,8 @@ const sidebar: JupyterFrontEndPlugin<IDebugger.ISidebar> = {
 
       settings.changed.connect(() => {
         const showSourcesInMainArea = settings.get('showSourcesInMainArea')
-      .composite as boolean;
-        sidebar.showSources = !!showSourcesInMainArea;
+          .composite as boolean;
+        sidebar.showSourcesPanel = !showSourcesInMainArea;
       });
 
       const updateSettings = (): void => {
