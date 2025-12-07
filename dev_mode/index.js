ac9113c350ba4cf70b47f0cfd17a2a55df0d2a68
Darshan Poudel <pranishpoudel10@gmail.com>
2025-12-07 15:48:02 +0545

    detect-slow-plugins

--- dev_mode/index.js
+++ dev_mode/index.js
@@ -56,7 +56,39 @@ export async function main() {
      };
   }
 
-  var pluginRegistry = new PluginRegistry();
+  class JPPluginRegistry extends PluginRegistry {
+    constructor() {
+      super();
+    }
+
+    async activatePlugin(id) {
+      const startTime = performance.now();
+
+      // Set a timeout to detect if the plugin stalls
+      let timeoutId = setTimeout(() => {
+        console.warn(`Plugin ${id} is taking too long to activate.`);
+      }, 3000);
+
+      try {
+        const result = await super.activatePlugin(id);
+        clearTimeout(timeoutId);
+        const endTime = performance.now();
+        const activationTime = endTime - startTime;
+
+        if (activationTime > 30000) {
+          console.warn(`Plugin ${id} took ${activationTime}ms to activate.`);
+        }
+
+        return result;
+      } catch (error) {
+        clearTimeout(timeoutId);
+        console.error(`Error activating plugin: ${id}`, error);
+        throw error;
+      }
+    }
+  }
+
+  var pluginRegistry = new JPPluginRegistry();
   var JupyterLab = require('@jupyterlab/application').JupyterLab;
   var disabled = [];
   var deferred = [];
