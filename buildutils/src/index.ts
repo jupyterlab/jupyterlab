8a72eb82b16cc7d1d303beda8fc171366a9c353a
Sanchit Rishi <sharmasanchitrishi@gmail.com>
2025-12-09 00:07:59 +0530

    Fix: Ghost kernels issue by updating kernel/widget logic

--- buildutils/src/index.ts
+++ buildutils/src/index.ts
@@ -2,7 +2,3 @@
 | Copyright (c) Jupyter Development Team.
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
-
-export * from './ensure-package';
-export * from './get-dependency';
-export * from './utils';
