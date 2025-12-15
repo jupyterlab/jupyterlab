6aba8c5200573f47d986a8a6c05e54d6365c1ca4
Florence Haudin <99649086+HaudinFlorence@users.noreply.github.com>
2025-12-15 16:41:22 +0100

    Update packages/debugger-extension/src/index.ts
    Co-authored-by: Michał Krassowski <5832902+krassowski@users.noreply.github.com>

--- packages/debugger-extension/src/index.ts
+++ packages/debugger-extension/src/index.ts
@@ -466,7 +466,7 @@ const service: JupyterFrontEndPlugin<IDebugger> = {
     displayRegistry: IDebuggerDisplayRegistry | null,
     debuggerSources: IDebugger.ISources | null,
     translator: ITranslator | null,
-    editorServices: IEditorServices
+    editorServices: IEditorServices | null
   ) =>
     new Debugger.Service({
       config,
