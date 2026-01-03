ba8fcf3bc6f9482b0672a318d610883f6310f394
Florence Haudin <haudin.florence@gmail.com>
2026-01-03 15:16:19 +0100

    Update galata/test/jupyterlab/sidebar.test.ts.

--- galata/test/jupyterlab/sidebars.test.ts
+++ galata/test/jupyterlab/sidebars.test.ts
@@ -226,6 +226,7 @@ const elementAriaLabels = {
     'Variables Section',
     'Callstack Section',
     'Breakpoints Section',
+    'Source Section',
     'Kernel Sources Section'
   ],
   'extensionmanager.main-view': [
