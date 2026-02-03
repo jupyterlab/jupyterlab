5cc06bad51d1cefeb320b578352cd2bfb7cf637b
Darshan Poudel <pranishpoudel10@gmail.com>
2026-02-03 23:28:34 +0545

    migrate-galata/eslint-config

---
+++ galata/eslint.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'eslint/config';
+import baseConfig from '../eslint.config.mjs';
+
+export default defineConfig([
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    
+    languageOptions: {
+      parserOptions: {
+        project: 'tsconfig.eslint.json'
+      }
+    },
+
+    rules: {
+      'jest/no-done-callback': 'off'
+    }
+  }
+]);
