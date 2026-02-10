7c6184b4721e1fbb815e3471c75eee71e2d465e6
Nicolas Brichet <nicolas.brichet@quantstack.net>
2026-02-10 10:20:24 +0100

    Avoid using system clipboard in Notebook widget

--- packages/notebook/src/actions.tsx
+++ packages/notebook/src/actions.tsx
@@ -1513,7 +1513,7 @@ export namespace NotebookActions {
     notebook.activeCellIndex = prevActiveCellIndex + values.length;
     notebook.deselectAll();
     if (cellsFromClipboard) {
-      notebook.lastClipboardInteraction = 'paste';
+      notebook.cellClipboardInteraction('paste', values);
     }
     void Private.handleState(notebook, state, true);
   }
@@ -2752,9 +2752,9 @@ namespace Private {
       notebook.deselectAll();
     }
     if (cut) {
-      notebook.lastClipboardInteraction = 'cut';
+      notebook.cellClipboardInteraction('cut', data);
     } else {
-      notebook.lastClipboardInteraction = 'copy';
+      notebook.cellClipboardInteraction('copy', data);
     }
     void handleState(notebook, state);
   }
@@ -2789,9 +2789,9 @@ namespace Private {
       notebook.deselectAll();
     }
     if (cut) {
-      notebook.lastClipboardInteraction = 'cut';
+      notebook.cellClipboardInteraction('cut', data);
     } else {
-      notebook.lastClipboardInteraction = 'copy';
+      notebook.cellClipboardInteraction('copy', data);
     }
     void handleState(notebook, state);
   }
