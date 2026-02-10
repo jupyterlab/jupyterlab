081578c2db93a0a12b1b34fb3909ef3d11c62b24
krassowski <5832902+krassowski@users.noreply.github.com>
2026-02-10 09:00:55 +0000

    Try 4

--- scripts/ci_script.ps1
+++ scripts/ci_script.ps1
@@ -17,7 +17,7 @@ if ($Env:GROUP -eq "python") {
     Remove-Item Env:\YARN_ENABLE_IMMUTABLE_INSTALLS
 
     # Run the python tests
-    python -m pytest -n 2
+    python -m pytest -n 4
     if ($LASTEXITCODE -ne 0) { throw "Command failed. See above errors for details" }
 }
 
