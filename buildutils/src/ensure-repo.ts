416639bdfcafc3b187411c4bf64452e34ec6c633
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2026-02-11 09:01:59 +0100

    beta 2

--- buildutils/src/ensure-repo.ts
+++ buildutils/src/ensure-repo.ts
@@ -76,11 +76,7 @@ const UNUSED: Dict<string[]> = {
     'source-map-loader',
 
     // Peer dependency of @rspack/core, required at runtime by ModuleFederationPlugin.
-    '@module-federation/runtime-tools',
-
-    // TODO: remove
-    // Transitive dependency of @rspack/core, needed at runtime.
-    '@rspack/lite-tapable'
+    '@module-federation/runtime-tools'
   ],
   '@jupyterlab/buildutils': ['inquirer', 'verdaccio'],
   '@jupyterlab/codemirror': [
