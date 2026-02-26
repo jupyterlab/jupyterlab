94f915a35cb11eb0b0453cd3dd3515407b471002
krassowski <5832902+krassowski@users.noreply.github.com>
2026-02-26 16:13:50 +0000

    Wait for fonts to be fully loaded

--- galata/test/documentation/internationalization.test.ts
+++ galata/test/documentation/internationalization.test.ts
@@ -25,6 +25,9 @@ test.describe('Internationalization', () => {
     await page.click('text=Settings');
     await page.click('.lm-Menu ul[role="menu"] >> text=Language');
 
+    // Wait for fonts to be loaded
+    await page.evaluate(() => document.fonts.ready);
+
     expect
       .soft(
         await page.screenshot({
