920e71cf5d6e2b2e389b371aba2044da6a79323d
Darshan Poudel <pranishpoudel10@gmail.com>
2026-05-18 17:43:04 +0545

    Update packages/notebook/src/actions.tsx

--- packages/notebook/src/actions.tsx
+++ packages/notebook/src/actions.tsx
@@ -2563,7 +2563,7 @@ namespace Private {
       return null;
     }
     let done = false;
-    future.done.finally(() => {
+   void future.done.finally(() => {
       done = true;
     });
     const buffered: KernelMessage.IIOPubMessage[] = [];
