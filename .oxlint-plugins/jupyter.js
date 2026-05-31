1080a33c3b9d6242ecde3e93acfee01d90e16bb8
darshan <pranishpoudel10@gmail.com>
2026-05-31 14:08:11 +0545

    update-patch-file

--- .oxlint-plugins/jupyter.js
+++ .oxlint-plugins/jupyter.js
@@ -1,8 +1,5 @@
 'use strict';
 // TODO: Remove this wrapper once @jupyter/eslint-plugin exposes
-// `meta: { name: 'jupyter' }` directly. When that lands, replace every
-// occurrence of "./.oxlint-plugins/jupyter.js" in .oxlintrc.json with
-// "@jupyter/eslint-plugin" and delete this file.
-// Tracked in: https://github.com/jupyterlab/jupyterlab/issues (file wrapper ticket)
+// `meta: { name: 'jupyter' }` directly.
 const plugin = require('@jupyter/eslint-plugin');
 module.exports = { ...plugin, meta: { name: 'jupyter' } };
