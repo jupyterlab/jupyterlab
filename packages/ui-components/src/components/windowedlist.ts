d1ca0bd04f38aa466a486e2c4d19c19ef0f22adb
darshan <pranishpoudel10@gmail.com>
2026-06-04 11:16:51 +0545

    try-increasing-timeout

--- packages/ui-components/src/components/windowedlist.ts
+++ packages/ui-components/src/components/windowedlist.ts
@@ -1199,7 +1199,7 @@ export class WindowedList<
         observer.disconnect();
         const item = this._innerElement.querySelector(selector);
         resolve(item instanceof HTMLElement ? item : null);
-      }, MAXIMUM_TIME_REMAINING);
+      }, 5000);
     });
   }
 
