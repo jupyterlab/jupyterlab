db558a43fba2213ef9ddf75bb8f6fe0a99931db5
Manas8114 <Manas8114@users.noreply.github.com>
2026-06-27 16:13:13 +0530

    Pick-issue files for #7214

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -347,6 +347,8 @@ namespace CommandIDs {
 
   export const virtualScrollbar = 'notebook:toggle-virtual-scrollbar';
 
+  export const toggleToolbar = 'notebook:toggle-toolbar';
+
   export const copySelectedtext = 'notebook:copy-selected-text';
 
   export const pasteText = 'notebook:paste-text';
@@ -4977,6 +4979,32 @@ function addCommands(
     }
   });
 
+  commands.addCommand(CommandIDs.toggleToolbar, {
+    label: trans.__('Show Toolbar'),
+    caption: trans.__('Show or hide the notebook toolbar'),
+    execute: args => {
+      const current = getCurrent(tracker, shell, args);
+      if (current) {
+        if (current.toolbar.isHidden) {
+          current.toolbar.show();
+        } else {
+          current.toolbar.hide();
+        }
+      }
+    },
+    isEnabled,
+    isToggled: args => {
+      const current = getCurrent(tracker, shell, { ...args, activate: false });
+      return current ? !current.toolbar.isHidden : false;
+    },
+    describedBy: {
+      args: {
+        type: 'object',
+        properties: {}
+      }
+    }
+  });
+
   // All commands with isEnabled defined directly or in a semantic commands
   // To simplify here we added all commands as most of them have isEnabled
   const skip = [CommandIDs.createNew, CommandIDs.createOutputView];
@@ -5026,7 +5054,8 @@ function populatePalette(
     CommandIDs.expandAllCmd,
     CommandIDs.accessPreviousHistory,
     CommandIDs.accessNextHistory,
-    CommandIDs.virtualScrollbar
+    CommandIDs.virtualScrollbar,
+    CommandIDs.toggleToolbar
   ].forEach(command => {
     palette.addItem({ command, category });
   });
@@ -5173,6 +5202,11 @@ function populateMenus(mainMenu: IMainMenu, isEnabled: () => boolean): void {
   mainMenu.viewMenu.editorViewers.toggleMinimap.add({
     id: CommandIDs.virtualScrollbar,
     isEnabled: () => true
+  });
+
+  mainMenu.viewMenu.editorViewers.toggleToolbar.add({
+    id: CommandIDs.toggleToolbar,
+    isEnabled
   });
 
   // Add an ICodeRunner to the application run menu
