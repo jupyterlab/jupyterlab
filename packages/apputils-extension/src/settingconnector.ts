044e9494c65d56281bf5d8e36a4679605d0df4e3
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2026-07-08 10:42:37 +0200

    Re-export SettingConnector from apputils-extension

---
+++ packages/apputils-extension/src/settingconnector.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Jupyter Development Team.
+ * Distributed under the terms of the Modified BSD License.
+ */
+
+export {
+  /**
+   * @deprecated The setting connector is now in the settingregistry package. Import from '@jupyterlab/settingregistry' instead.
+   * Use: import { SettingConnector } from '@jupyterlab/settingregistry';
+   */
+  SettingConnector
+} from '@jupyterlab/settingregistry';
