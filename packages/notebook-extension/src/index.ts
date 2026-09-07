34a6ef09a8befcc3f39f7e04753f0b7e0ad83ea1
Meriem-BenIsmail <meriembenismail867@gmail.com>
2026-09-07 10:13:01 +0200

    implement cell.viewonly to prevent dirty state

--- packages/notebook-extension/src/index.ts
+++ packages/notebook-extension/src/index.ts
@@ -2430,7 +2430,7 @@ function activateNotebookHandler(
     }
 
     for (const cell of panel.content.widgets) {
-      cell.readOnly = viewOnly;
+      cell.viewOnly = viewOnly;
     }
 
     panel.content.node.classList.toggle('jp-mod-view-only', viewOnly);
