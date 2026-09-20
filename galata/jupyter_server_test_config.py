a60b67c977790a14872f30ba5db2719ce814dcf5
Muthuraj <muthuraj68m@gmail.com>
2026-09-20 15:19:50 +0530

    Use static IPython banner for documentation screenshots (addresses #18552)

--- galata/jupyter_server_test_config.py
+++ galata/jupyter_server_test_config.py
@@ -1,7 +1,12 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+import os
+
 from jupyterlab.galata import configure_jupyter_server
+
+# Static IPython banner for reproducible documentation screenshots
+os.environ.setdefault("SOURCE_DATE_EPOCH", "1704067200")
 
 configure_jupyter_server(c)
 c.LabApp.dev_mode = True
